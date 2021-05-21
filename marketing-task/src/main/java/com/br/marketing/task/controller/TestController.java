1c45f0b794e318a10603011471ac931668cbd4dc
juman.wang <juman.wang@brgroup.com>
2021-05-21 10:31:51 +0800

    修改多套配置文件的维护

--- marketing-task/src/main/java/com/br/marketing/task/controller/TestController.java
+++ marketing-task/src/main/java/com/br/marketing/task/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.br.marketing.task.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RestController
 public class TestController {
+
+    @Value("${myenv}")
+    private String myenv;
+
     @GetMapping("/index")
     public String index(){
-        return "hello word";
+        return "hello word".concat(myenv);
     }
 }
