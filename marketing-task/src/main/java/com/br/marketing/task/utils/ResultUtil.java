485431c8946b8fc35bbca5ba3b7434755f10bf7e
juman.wang <juman.wang@brgroup.com>
2021-07-05 12:42:58 +0800

    添加日志

--- marketing-task/src/main/java/com/br/marketing/task/utils/ResultUtil.java
+++ marketing-task/src/main/java/com/br/marketing/task/utils/ResultUtil.java
@@ -270,7 +270,11 @@ public class ResultUtil {
         }
         log.info("batch_number:{} products:{}",user.getBatchNumber(),products);
         ProductResultUtil.dealProResult(hxJson,products,sb,proFieldMap,sep,user.getApiCode());
+        if(log.isWarnEnabled()){
+            log.warn("sb信息--"+sb.toString());
+        }
         if(sb.toString().split(",").length>5){
+            log.warn("sb写入fw--"+sb.toString());
             fw.append(sb + "\r\n");
             mh.setIdCard(user.getIdCard());
             mh.setName(user.getName());
