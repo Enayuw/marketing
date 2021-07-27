5b0a87366c77973c233124532efb35fe8c9c904c
juman.wang <juman.wang@brgroup.com>
2021-07-27 15:50:52 +0800

    修改straFileId

--- marketing-cs/src/main/java/com/br/marketing/entity/LoanFile.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/LoanFile.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 @Data
 public class LoanFile extends BaseRowModel {
-    private Integer id;
+    private Long id;
     private String apiCode;
     private String filePath;
     private String createTime;
