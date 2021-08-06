aef01b3c030d54d9fa2e424fd24f8a079ba8624c
xiaoxin.pang <xiaoxin.pang@brgroup.com>
2021-08-06 12:30:45 +0800

    增加测试接口

---
+++ marketing-cs/src/main/java/com/br/marketing/mapper/DataTestMapper.java
@@ -0,0 +1,37 @@
+package com.br.marketing.mapper;
+
+import com.br.marketing.entity.DataTest;
+import com.br.marketing.entity.DataTestExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DataTestMapper {
+    int countByExample(DataTestExample example);
+
+    int deleteByExample(DataTestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(DataTest record);
+
+    int insertSelective(DataTest record);
+
+    List<DataTest> selectByExampleWithBLOBs(DataTestExample example);
+
+    List<DataTest> selectByExample(DataTestExample example);
+
+    DataTest selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") DataTest record, @Param("example") DataTestExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") DataTest record, @Param("example") DataTestExample example);
+
+    int updateByExample(@Param("record") DataTest record, @Param("example") DataTestExample example);
+
+    int updateByPrimaryKeySelective(DataTest record);
+
+    int updateByPrimaryKeyWithBLOBs(DataTest record);
+
+    int updateByPrimaryKey(DataTest record);
+}
