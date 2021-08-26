794f762452e6f40189e3e4ae4f0aa6bb0e71eb82
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-08-26 14:41:28 +0800

    郭泽强：修改调用方式改为内部调用
    *

--- marketing-cs/src/main/java/com/br/marketing/client/DtbStrategyClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/DtbStrategyClient.java
@@ -4,15 +4,12 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.br.marketing.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /** 规则引擎访问客户端，通过ribbon调用规则引擎
  * @author Wang Weiwei
@@ -66,15 +63,18 @@ public class DtbStrategyClient {
         Map<String,Object> urlVariables = new HashMap<>();
         urlVariables.put("apiCode",apiCode);
         urlVariables.put("code",strategyId);
-        String  result="";
-        try{
-            result = restTemplate.getForObject("http://k8s.brapp.com/compass-api/api/strategycenter-service/" +
-                    "dataStrategy/getDtbByCode?apiCode={apiCode}&code={code}", String.class, urlVariables);
+        String result = "";
+        String url = "http://strategycenter-service/dataStrategy/getDtbByCode?apiCode={apiCode}&code={code}";
+        try {
+//            result = restTemplate.getForObject("http://k8s.brapp.com/compass-api/api/strategycenter-service/" +
+//                    "dataStrategy/getDtbByCode?apiCode={apiCode}&code={code}", String.class, urlVariables);
+            result = restTemplate.getForObject(url, String.class, urlVariables);
         }catch (Exception e){
             log.warn("调用数据策略错误",e);
-            try{
-                result = restTemplate.getForObject("http://k8s.brapp.com/compass-api/api/strategycenter-service" +
-                        "/dataStrategy/getDtbByCode?apiCode={apiCode}&code={code}", String.class, urlVariables);
+            try {
+//                result = restTemplate.getForObject("http://k8s.brapp.com/compass-api/api/strategycenter-service" +
+//                        "/dataStrategy/getDtbByCode?apiCode={apiCode}&code={code}", String.class, urlVariables);
+                result = restTemplate.getForObject(url, String.class, urlVariables);
             }catch (Exception e1){
                 log.error("调用数据策略重试错误",e1);
             }
