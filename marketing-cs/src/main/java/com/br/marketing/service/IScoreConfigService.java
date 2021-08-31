781cbb2a6ce59c8262e0d64b24432b3a3b2b4306
juman.wang <juman.wang@brgroup.com>
2021-08-31 14:56:13 +0800

    修改表结构

--- marketing-cs/src/main/java/com/br/marketing/service/IScoreConfigService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/IScoreConfigService.java
@@ -1,2 +1,8 @@
-package com.br.marketing.service;public interface IScoreConfigService {
+package com.br.marketing.service;
+
+import com.br.marketing.common.commondto.Result;
+import com.br.marketing.entity.SoleRuleConfig;
+
+public interface IScoreConfigService {
+    Result<SoleRuleConfig> getSoleConfig(String apiCode);
 }
