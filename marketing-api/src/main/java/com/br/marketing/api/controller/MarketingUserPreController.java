5c0c22c5301281ed9b648d381fcac083e651ab42
juman.wang <juman.wang@brgroup.com>
2021-09-09 18:02:55 +0800

    去掉注解

--- marketing-api/src/main/java/com/br/marketing/api/controller/MarketingUserPreController.java
+++ marketing-api/src/main/java/com/br/marketing/api/controller/MarketingUserPreController.java
@@ -18,8 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
 
 /**
  * 营销数据接入接口
@@ -44,7 +42,6 @@ public class MarketingUserPreController {
      */
     @ApiOperation(value = "批量接入营销人员数据")
     @PostMapping("/receiveMarketingPreUser")
-    @Produces(value = "application/x-www-form-urlencoded")
     public ApiNoDataResult receiveMarketingPreUserSync(@RequestParam("apiCode") String apiCode, @RequestParam("jsonData") String jsonData) {
         try {
             long l = System.currentTimeMillis();
