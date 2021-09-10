15f4f06c16f8b382f0fa02a2d8bcfdf649623064
juman.wang <juman.wang@brgroup.com>
2021-09-10 16:10:41 +0800

    暂存

---
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/RuleRedisServiceImpl.java
@@ -0,0 +1,9 @@
+package com.br.marketing.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RuleRedisServiceImpl {
+
+    final static String soleConfigByApiCodeKey = "sole:config:";
+}
