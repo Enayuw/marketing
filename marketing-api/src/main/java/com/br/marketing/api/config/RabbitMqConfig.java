6853dafac75cdde200302e8095e1ef662c9e634d
juman.wang <juman.wang@brgroup.com>
2021-10-13 16:58:25 +0800

    添加一个新的rabbitmqfactory

--- marketing-api/src/main/java/com/br/marketing/api/config/RabbitMqConfig.java
+++ marketing-api/src/main/java/com/br/marketing/api/config/RabbitMqConfig.java
@@ -279,7 +279,14 @@ public class RabbitMqConfig {
     public SimpleRabbitListenerContainerFactory primaryContainerFactory(
             SimpleRabbitListenerContainerFactoryConfigurer configurer,
             @Qualifier("primaryConnectionFactory") ConnectionFactory connectionFactory) {
-        return containerFactory(configurer, connectionFactory);
+        return containerFactory(configurer, connectionFactory,null);
+    }
+
+    @Bean(name = "fiveDataContainerFactory")
+    public SimpleRabbitListenerContainerFactory fiveDataContainerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            @Qualifier("primaryConnectionFactory") ConnectionFactory connectionFactory) {
+        return containerFactory(configurer, connectionFactory,5);
     }
 
     /**
@@ -291,9 +298,12 @@ public class RabbitMqConfig {
      */
     private SimpleRabbitListenerContainerFactory containerFactory(
             SimpleRabbitListenerContainerFactoryConfigurer configurer,
-            ConnectionFactory connectionFactory) {
+            ConnectionFactory connectionFactory,Integer prefetchCount) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        if(prefetchCount!=null&&prefetchCount>0){
+            factory.setPrefetchCount(prefetchCount);
+        }
         configurer.configure(factory, connectionFactory);
         return factory;
     }
