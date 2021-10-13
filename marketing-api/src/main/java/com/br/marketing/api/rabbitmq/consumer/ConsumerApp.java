94cbeeb487d442be1349e9d05ab598b32d9ae18e
juman.wang <juman.wang@brgroup.com>
2021-10-13 16:59:43 +0800

    修改消费端取数逻辑

--- marketing-api/src/main/java/com/br/marketing/api/rabbitmq/consumer/ConsumerApp.java
+++ marketing-api/src/main/java/com/br/marketing/api/rabbitmq/consumer/ConsumerApp.java
@@ -65,7 +65,7 @@ public class ConsumerApp {
      * @param channel 通道
      * @param message 消息体
      */
-    @RabbitListener(queues = "Marketing_PreUser_Receive", containerFactory = "primaryContainerFactory")
+    @RabbitListener(queues = "Marketing_PreUser_Receive", containerFactory = "fiveDataContainerFactory")
     public void consumerPreUser(Channel channel, Message message) {
         Long o = JSON.parseObject(new String(message.getBody(), StandardCharsets.UTF_8), new TypeReference<Long>() {
         }.getType());
@@ -78,7 +78,7 @@ public class ConsumerApp {
      * @param channel 通道
      * @param message 消息体
      */
-    @RabbitListener(queues = MQConstants.MARKETING_TRANSFER_RECEIVE, containerFactory = "primaryContainerFactory")
+    @RabbitListener(queues = MQConstants.MARKETING_TRANSFER_RECEIVE, containerFactory = "fiveDataContainerFactory")
     public void consumerTransferUser(Channel channel, Message message) {
         Long o = JSON.parseObject(new String(message.getBody(), StandardCharsets.UTF_8), new TypeReference<Long>() {
         }.getType());
