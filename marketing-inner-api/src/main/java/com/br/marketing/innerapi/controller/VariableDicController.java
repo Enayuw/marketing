75f1cfe8a208d631804b6fd3b1cff6ff4e9b630a
juanjuan.song <juanjuan.song@brgroup.com>
2021-10-26 15:53:24 +0800

    EntityOptLog实体类

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/VariableDicController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/VariableDicController.java
@@ -4,7 +4,6 @@ import com.br.marketing.common.commondto.ApiResult;
 import com.br.marketing.common.enums.ServiceResultEnum;
 import com.br.marketing.commonentity.PageResultReturn;
 import com.br.marketing.dto.userinfo.UserDetail;
-import com.br.marketing.entity.MarketingCustomer;
 import com.br.marketing.innerapi.config.ThreadContextInfo;
 import com.br.marketing.service.VariableDicService;
 import com.br.marketing.vo.VariableDicListVO;
