afed04268466d4decf19d3002f57147ad5667cad
juanjuan.song <juanjuan.song@brgroup.com>
2021-10-26 15:56:15 +0800

    EntityOptLog实体类

--- marketing-cs/src/main/java/com/br/marketing/mapper/EntityOptLogMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/EntityOptLogMapper.java
@@ -1,31 +1,6 @@
 package com.br.marketing.mapper;
 
-import com.br.marketing.entity.EntityOptLog;
-import com.br.marketing.entity.EntityOptLogExample;
-import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
+public interface EntityOptLogMapper extends EntityOptLogMapperBase{
 
-public interface EntityOptLogMapper {
-    int countByExample(EntityOptLogExample example);
-
-    int deleteByExample(EntityOptLogExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(EntityOptLog record);
-
-    int insertSelective(EntityOptLog record);
-
-    List<EntityOptLog> selectByExample(EntityOptLogExample example);
-
-    EntityOptLog selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") EntityOptLog record, @Param("example") EntityOptLogExample example);
-
-    int updateByExample(@Param("record") EntityOptLog record, @Param("example") EntityOptLogExample example);
-
-    int updateByPrimaryKeySelective(EntityOptLog record);
-
-    int updateByPrimaryKey(EntityOptLog record);
 }
