1eb0b6d21fbad05556489c20e5b942133244bb6a
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-10-27 16:42:35 +0800

    华强：修改发送消息方式 *

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -130,9 +130,9 @@ public class PushRuleServiceImpl implements PushRuleService {
 
     @Resource
     private AlarmApiClient alarmClient;
-    @Value("${otherConfig.alarm.outsideSecretKey:00}")
+    @Value("${otherConfig.alarm.secretKey:00}")
     private String secretKey;
-    @Value("${otherConfig.alarm.outsideAppName:00}")
+    @Value("${otherConfig.alarm.appName:00}")
     private String appName;
 
     @Override
