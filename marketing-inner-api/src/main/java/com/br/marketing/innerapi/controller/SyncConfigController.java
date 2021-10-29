de38250a620f8e29ca1a43494cea5da851e3d09c
juanjuan.song <juanjuan.song@brgroup.com>
2021-10-29 17:16:18 +0800

    修改入参补充

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/SyncConfigController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/SyncConfigController.java
@@ -31,8 +31,8 @@ public class SyncConfigController {
 
     @GetMapping("/getSftpList")
     @ApiOperation(value = "客户sftp账号列表", notes = "客户sftp账号列表", httpMethod = "GET")
-    @ApiImplicitParams({@ApiImplicitParam(name = "page", value = "页号", paramType = "query", dataType = "integer", defaultValue = "1")
-            , @ApiImplicitParam(name = "pageSize", value = "页大小", paramType = "query", dataType = "integer", defaultValue = "10")
+    @ApiImplicitParams({@ApiImplicitParam(name = "current", value = "页号", paramType = "query", dataType = "integer", defaultValue = "1")
+            , @ApiImplicitParam(name = "size", value = "页大小", paramType = "query", dataType = "integer", defaultValue = "10")
             , @ApiImplicitParam(name = "apiCode", paramType = "query", dataType = "string")
     })
     @ApiResponses(value = {@ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR", response = MarketingCustomer.class)})
