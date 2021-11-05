f791676260027b683fa5636e19a57c84777226a3
linquan.guo <linquan.guo@100credit.com>
2021-11-05 14:24:26 +0800

    上传日志发mom

--- marketing-api/src/main/java/com/br/marketing/api/controller/MarketingTransferDataController.java
+++ marketing-api/src/main/java/com/br/marketing/api/controller/MarketingTransferDataController.java
@@ -3,10 +3,15 @@ package com.br.marketing.api.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
+import com.br.marketing.aspect.LogAnnotation;
 import com.br.marketing.common.commondto.ApiNoDataResult;
 import com.br.marketing.common.commondto.ApiResult;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.constants.MarketingErrorInfo;
+import com.br.marketing.common.utils.BrCipherJsonUtils;
+import com.br.marketing.common.utils.Constants;
+import com.br.marketing.context.RuntimeDataContext;
+import com.br.marketing.entity.MonitorTypeEnum;
 import com.br.marketing.service.PushRuleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -32,7 +37,15 @@ public class MarketingTransferDataController {
 
     @ApiOperation(value = "接收转化数据")
     @PostMapping("/receiveTransferDataSync")
-    public ApiNoDataResult receiveTransferDataSync(@RequestParam("apiCode") String apiCode, @RequestParam("jsonData") String jsonData){
+    @LogAnnotation
+    public ApiNoDataResult receiveTransferDataSync(@RequestParam("apiCode") String apiCode, @RequestParam("jsonData") String jsonData) {
+        RuntimeDataContext.getData().setUploadType(MonitorTypeEnum.UPLOAD_TYPE_2.getType());
+        RuntimeDataContext.getData().setApiCode(apiCode);
+        long l = System.currentTimeMillis();
+        RuntimeDataContext.getData().setJsonData(BrCipherJsonUtils.cipherEncodeJsonDataArr(jsonData, Constants.TAG_KEY,Constants.JSON_DATA_KEYARR));
+        if (log.isInfoEnabled()) {
+            log.info("apiCode:{},接收转化数据加密耗时：{}", apiCode, (System.currentTimeMillis() - l));
+        }
         Result result = pushRuleService.insertTransferData(apiCode, jsonData);
         return new ApiNoDataResult().fromResult(result);
     }
