100b827e6e6b7c52cd813bbb702b39db315271f2
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-11-09 18:13:54 +0800

    华强：优化信息格式

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/task/TaskPushTransferToRobotaiJob.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/task/TaskPushTransferToRobotaiJob.java
@@ -190,7 +190,7 @@ public class TaskPushTransferToRobotaiJob extends AbstractSimpleElasticJob {
     }
 
     private void sendAlarm(String smg) {
-        String title = "接口转化(通用标准)数据同步到智能客服补偿任务警告";
+        String title = "\n接口转化(通用标准)数据同步到智能客服补偿任务警告";
         log.warn(smg);
         alarmClient.sendAlarm(smg, title, appName, secretKey,
                 Constants.sendCodeMap.get("pushToCustomer"));
