0ef284e62cc0d9199b7c4ecfa0b7558ef2db6580
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-11-10 16:17:51 +0800

    华强：优化输出信息，捕获错误信息 *

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/task/TaskPushTransferToRobotaiJob.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/task/TaskPushTransferToRobotaiJob.java
@@ -88,90 +88,97 @@ public class TaskPushTransferToRobotaiJob extends AbstractSimpleElasticJob {
         List<PushTransferRobotaiLog> rows = pushTransferRobotaiLogMapper.findListByStatusIs0(shardingTotalCount, shardingItems);
         String apiCode;
         for (PushTransferRobotaiLog robotaiLog : rows) {
-            PushTransferRobotaiLog updateLog = new PushTransferRobotaiLog();
-            updateLog.setId(robotaiLog.getId());
-            updateLog.setCompensateTimes(robotaiLog.getCompensateTimes() + 1);
-            // 检查补偿次数
-            if (updateLog.getCompensateTimes() >= compensateTimes) {
-                updateLog.setPushStatus(2);
-            }
-            apiCode = robotaiLog.getApiCode();
-            TransferRobotOutboundVO<UnsuccessfulData> outboundVO = new TransferRobotOutboundVO<>();
-            MarketingTransferInfo info = new MarketingTransferInfo();
-            info.setApiCode(apiCode);
-            info.setRequestId(robotaiLog.getRequestId());
-            if (robotaiLog.getServiceCode().equals("00")) {
-                JSONObject object = JSON.parseObject(robotaiLog.getResponseBody());
-                if (StringUtils.isEmpty(object)) {
-                    log.error("应答消息为空：{}", robotaiLog);
-                    continue;
+            try {
+                PushTransferRobotaiLog updateLog = new PushTransferRobotaiLog();
+                updateLog.setId(robotaiLog.getId());
+                updateLog.setCompensateTimes(robotaiLog.getCompensateTimes() + 1);
+                // 检查补偿次数
+                if (updateLog.getCompensateTimes() >= compensateTimes) {
+                    updateLog.setPushStatus(2);
                 }
-                Object unsuccessfulData = object.get("unsuccessfulData");
-                JSONArray array = JSONArray.parseArray(unsuccessfulData.toString());
-                if (array.size() > 0) {
-                    TransferRobotOutboundDTO outboundDTO = getTransferRobotOutbound(robotaiLog, info, array);
-                    outboundVO = ruleService.pushTransferData(outboundDTO, info);
-                    if (robotaiLog.getRowSize() > array.size()) {
-                        if (!outboundVO.getAccessNumber().equals("-1")) {
-                            info.setId(robotaiLog.getTransferInfoId());
-                            pushTransferRobotaiLogService.saveLog(info, outboundDTO, outboundVO);
-                            String smg = String.format("$$apiCode:[%s];requestId:[%s]补偿部分失败的数据依然失败或部分失败!已补偿[%d];" +
-                                            "\n更新原记录为已补偿,并生成新记录！" +
-                                            "\n业务返回状态码[%s];" +
-                                            "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
-                                    , outboundVO.getCode(), outboundVO.getMessage());
-                            sendAlarm(smg);
-                        } else {
-                            String smg = String.format("@@apiCode:[%s];requestId:[%s]补偿成功,更新原记录为已补偿！已补偿[%d]" +
-                                            "\n业务返回状态码[%s];" +
-                                            "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
-                                    , outboundVO.getCode(), outboundVO.getMessage());
-                            sendAlarm(smg);
-                        }
-                        updateLog.setPushStatus(1);
-                        pushTransferRobotaiLogMapper.updateByPrimaryKeySelective(updateLog);
+                apiCode = robotaiLog.getApiCode();
+                TransferRobotOutboundVO<UnsuccessfulData> outboundVO = new TransferRobotOutboundVO<>();
+                MarketingTransferInfo info = new MarketingTransferInfo();
+                info.setApiCode(apiCode);
+                info.setRequestId(robotaiLog.getRequestId());
+                if (robotaiLog.getServiceCode().equals("00")) {
+                    JSONObject object = JSON.parseObject(robotaiLog.getResponseBody());
+                    if (StringUtils.isEmpty(object)) {
+                        log.error("应答消息为空：{}", robotaiLog);
                         continue;
-                    } else {
+                    }
+                    Object unsuccessfulData = object.get("unsuccessfulData");
+                    JSONArray array = JSONArray.parseArray(unsuccessfulData.toString());
+                    if (array.size() > 0) {
+                        TransferRobotOutboundDTO outboundDTO = getTransferRobotOutbound(robotaiLog, info, array);
+                        outboundVO = ruleService.pushTransferData(outboundDTO, info);
+                        if (robotaiLog.getRowSize() > array.size()) {
+                            if (!outboundVO.getAccessNumber().equals("-1")) {
+                                info.setId(robotaiLog.getTransferInfoId());
+                                pushTransferRobotaiLogService.saveLog(info, outboundDTO, outboundVO);
+                                String smg = String.format("$$apiCode:[%s];requestId:[%s]补偿部分失败的数据依然失败或部分失败!已补偿[%d];" +
+                                                "\n更新原记录为已补偿,并生成新记录！" +
+                                                "\n业务返回状态码[%s];" +
+                                                "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
+                                        , outboundVO.getCode(), outboundVO.getMessage());
+                                sendAlarm(smg);
+                            } else {
+                                String smg = String.format("@@apiCode:[%s];requestId:[%s]补偿成功,更新原记录为已补偿！已补偿[%d]" +
+                                                "\n业务返回状态码[%s];" +
+                                                "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
+                                        , outboundVO.getCode(), outboundVO.getMessage());
+                                sendAlarm(smg);
+                            }
+                            updateLog.setPushStatus(1);
+                            pushTransferRobotaiLogMapper.updateByPrimaryKeySelective(updateLog);
+                            continue;
+                        } else {
+                            updateLog.setRequestBody(JSON.toJSONString(outboundDTO));
+                        }
+                    }
+                } else if (StringUtils.isEmpty(robotaiLog.getServiceCode())) {
+                    outboundVO = ruleService.pushTransferData(JSON.parseObject(robotaiLog.getRequestBody(), new TypeReference<TransferRobotOutboundDTO>() {
+                    }), info);
+                } else {
+                    JSONObject object = JSON.parseObject(robotaiLog.getResponseBody());
+                    Object unsuccessfulData = object.get("unsuccessfulData");
+                    JSONArray array = JSONArray.parseArray(unsuccessfulData.toString());
+                    if (array.size() > 0) {
+                        //                    TransferRobotOutboundDTO outboundDTO1 = JSON.parseObject(robotaiLog.getRequestBody(), new TypeReference<TransferRobotOutboundDTO>() {
+                        //                    });
+                        TransferRobotOutboundDTO outboundDTO = getTransferRobotOutbound(robotaiLog, info, array);
+                        //                    outboundDTO1.getJsonData().setConversionData(outboundDTO.getJsonData().getConversionData());
+                        outboundVO = ruleService.pushTransferData(outboundDTO, info);
                         updateLog.setRequestBody(JSON.toJSONString(outboundDTO));
                     }
                 }
-            } else if (StringUtils.isEmpty(robotaiLog.getServiceCode())) {
-                outboundVO = ruleService.pushTransferData(JSON.parseObject(robotaiLog.getRequestBody(), new TypeReference<TransferRobotOutboundDTO>() {
-                }), info);
-            } else {
-                JSONObject object = JSON.parseObject(robotaiLog.getResponseBody());
-                Object unsuccessfulData = object.get("unsuccessfulData");
-                JSONArray array = JSONArray.parseArray(unsuccessfulData.toString());
-                if (array.size() > 0) {
-//                    TransferRobotOutboundDTO outboundDTO1 = JSON.parseObject(robotaiLog.getRequestBody(), new TypeReference<TransferRobotOutboundDTO>() {
-//                    });
-                    TransferRobotOutboundDTO outboundDTO = getTransferRobotOutbound(robotaiLog, info, array);
-//                    outboundDTO1.getJsonData().setConversionData(outboundDTO.getJsonData().getConversionData());
-                    outboundVO = ruleService.pushTransferData(outboundDTO, info);
-                    updateLog.setRequestBody(JSON.toJSONString(outboundDTO));
+                updateLog.setResponseBody(JSON.toJSONString(outboundVO.getData()));
+                if (outboundVO.getAccessNumber().equals("-1")) {
+                    updateLog.setPushStatus(1);
+                    String smg = String.format("@@apiCode:[%s];requestId:[%s]补偿成功！已补偿[%d]" +
+                                    "\n业务返回状态码[%s];" +
+                                    "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
+                            , outboundVO.getCode(), outboundVO.getMessage());
+                    sendAlarm(smg);
+                } else {
+                    String responseBody = updateLog.getResponseBody();
+                    String smg = String.format("$$apiCode:[%s];requestId:[%s]补偿依然失败或部分失败！已补偿[%d]" +
+                                    "\n业务返回状态码[%s];" +
+                                    "\n业务应答消息[%s]" +
+                                    "\n未成功数据情况:[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
+                            , outboundVO.getCode(), outboundVO.getMessage(),
+                            responseBody.length() > 300 ? responseBody.substring(0, 300).concat("...") : responseBody);
+                    sendAlarm(smg);
                 }
-            }
-            updateLog.setResponseBody(JSON.toJSONString(outboundVO.getData()));
-            if (outboundVO.getAccessNumber().equals("-1")) {
-                updateLog.setPushStatus(1);
-                String smg = String.format("@@apiCode:[%s];requestId:[%s]补偿成功！已补偿[%d]" +
-                                "\n业务返回状态码[%s];" +
-                                "\n业务应答消息[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
-                        , outboundVO.getCode(), outboundVO.getMessage());
-                sendAlarm(smg);
-            } else {
-                String responseBody = updateLog.getResponseBody();
-                String smg = String.format("$$apiCode:[%s];requestId:[%s]补偿依然失败或部分失败！已补偿[%d]" +
-                                "\n业务返回状态码[%s];" +
-                                "\n业务应答消息[%s]" +
-                                "\n未成功数据情况:[%s]", robotaiLog.getApiCode(), robotaiLog.getRequestId(), updateLog.getCompensateTimes()
-                        , outboundVO.getCode(), outboundVO.getMessage(),
-                        responseBody.length() > 300 ? responseBody.substring(0, 300).concat("...") : responseBody);
+                updateLog.setMessage(outboundVO.getMessage());
+                updateLog.setServiceCode(outboundVO.getCode());
+                pushTransferRobotaiLogMapper.updateByPrimaryKeySelective(updateLog);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                String smg = String.format("**apiCode:[%s];requestId:[%s]补偿任务异常！记录主键[%d]" +
+                        "\n异常信息[%s];", robotaiLog.getApiCode(), robotaiLog.getRequestId(), robotaiLog.getId(), e.getMessage());
                 sendAlarm(smg);
             }
-            updateLog.setMessage(outboundVO.getMessage());
-            updateLog.setServiceCode(outboundVO.getCode());
-            pushTransferRobotaiLogMapper.updateByPrimaryKeySelective(updateLog);
         }
         Long end = System.currentTimeMillis();
         log.warn("通用标准【转化数据同步客服补偿任务】调度结束，耗时：{},分片：{}", end - start, context.getShardingItemParameters());
