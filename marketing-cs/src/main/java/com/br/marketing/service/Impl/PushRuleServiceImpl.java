ce159550a930f9cd71e28cea3e6d31035c7fe4c9
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-11-16 13:40:01 +0800

    华强：宜信修改逻辑，未找到转化信息直接放弃消息 *

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -1395,9 +1395,10 @@ public class PushRuleServiceImpl implements PushRuleService {
                     info.setId(infoId);
                     pushTransferData(info);
                     result.setDate(false);
-                } catch (Exception exception) {
-                    log.error(exception.getMessage(), exception);
-                    alarmClient.sendAlarm(exception.getMessage(), "接口转化(通用标准)数据同步到智能客服警告", appName, secretKey,
+                } catch (Exception e) {
+                    String smg = String.format("主键[%d];apiCode[%s];requestId[%s]推送错误！\n%s", infoId, apiCode, info.getRequestId(), e.getMessage());
+                    log.error(smg, e);
+                    alarmClient.sendAlarm(smg, "接口转化(通用标准)数据同步到智能客服警告", appName, secretKey,
                             Constants.sendCodeMap.get("pushToCustomer"));
                 }
                 return result;
@@ -1423,7 +1424,7 @@ public class PushRuleServiceImpl implements PushRuleService {
             final int pageSize = 2000;
             final int retrySum = 2;
             List<PushTransferCustomerLog> logListAll = new ArrayList<>();
-            lable:
+            label:
             for (; ; ) {
                 PageHelper.startPage(page, pageSize);
                 List<MarketingTransferSyncUser> transferList = marketingTransferSyncUserMapper.selectByExample(example);
@@ -1436,16 +1437,17 @@ public class PushRuleServiceImpl implements PushRuleService {
                 switch (transferStatus) {
                     case 0:
                         if (size < 1) {
-                            PushTransferCustomerLog pushTransferCustomerLog = new PushTransferCustomerLog();
-                            pushTransferCustomerLog.setTransferInfoId(infoId);
-                            pushTransferCustomerLog.setApiCode(apiCode);
-                            pushTransferCustomerLog.setRequestId(requestId);
-                            pushTransferCustomerLog.setPushStatus(4);
-                            pushTransferCustomerLog.setRowSize(0);
-                            pushTransferCustomerLog.setTransferStatus(transferStatus);
-                            pushTransferCustomerLog.setMessage("未找到转化数据，只作为标记使用");
-                            logListAll.add(pushTransferCustomerLog);
-                            break lable;
+                            if (info.getActualNum() < 1) {
+                                PushTransferCustomerLog pushTransferCustomerLog = sendTransferDataToCustomer(
+                                        new PushCustomerRequestDTO(apiCode, transferStatus, null), 3, size);
+                                logListAll.add(pushTransferCustomerLog);
+                                break label;
+                            } else {
+                                String smg = String.format("last[0];infoId[%d];apiCode[%s];requestId[%s];tcId[%s]在[%s]转化未完成，未获取到转化数据"
+                                        , infoId, apiCode, requestId, tcId, yyyyMMdd);
+                                sendAlarm(smg);
+                                return result;
+                            }
                         }
                         b = asyncPush(transferList, logListAll);
                         break;
@@ -1456,7 +1458,7 @@ public class PushRuleServiceImpl implements PushRuleService {
                                 int len = (size - 200);
                                 b = asyncPush(transferList.subList(0, len), logListAll);
                                 listEnd = transferList.subList(len, size);
-                            } else {
+                            } else if (size > 0) {
                                 // 检查是否有开始标记
                                 int countStatus = pushTransferCustomerLogMapper.countByApiCodeAndTransferInfoTimeAndPushStatus(apiCode, createTime, "0,2");
                                 if (countStatus > 0) {
@@ -1484,6 +1486,32 @@ public class PushRuleServiceImpl implements PushRuleService {
                                         }
                                     }
                                 }
+                            } else {
+                                listEnd = null;
+                                if (info.getActualNum() < 1) {
+                                    int countStatus = pushTransferCustomerLogMapper.countByApiCodeAndTransferInfoTimeAndPushStatus(apiCode, createTime, "0,2");
+                                    if (countStatus > 0) {
+                                        countStatus = pushTransferCustomerLogMapper.countByApiCodeAndTransferInfoTimeAndPushStatus(apiCode, createTime, "1,3");
+                                        if (countStatus < 1) {
+                                            PushTransferCustomerLog pushTransferCustomerLog = sendTransferDataToCustomer(
+                                                    new PushCustomerRequestDTO(apiCode, transferStatus, null), 3, size);
+                                            logListAll.add(pushTransferCustomerLog);
+                                            break label;
+                                        }
+                                    }
+                                    PushCustomerRequestDTO pushCustomerRequestDTO = new PushCustomerRequestDTO(apiCode, transferStatus, null);
+                                    logListAll.add(new PushTransferCustomerLog(apiCode
+                                            , pushCustomerRequestDTO.getJsonData()
+                                            , size
+                                            , 1
+                                            , transferStatus
+                                    ));
+                                } else {
+                                    String smg = String.format("last[1]infoId[%d];apiCode[%s];requestId[%s];tcId[%s]在[%s]转化未完成，未获取到转化数据"
+                                            , infoId, apiCode, requestId, tcId, yyyyMMdd);
+                                    sendAlarm(smg);
+                                    return result;
+                                }
                             }
                             if (listEnd != null) {
                                 // 检查是否全部推送完成
@@ -1510,7 +1538,8 @@ public class PushRuleServiceImpl implements PushRuleService {
                         log.error("未知的标记:{}", transferStatus);
                 }
                 if (!b) {
-                    String smg = String.format("apiCode[%s]在[%s]中推送中线程任务失败", apiCode, yyyyMMdd);
+                    String smg = String.format("infoId[%d];apiCode[%s];requestId[%s];tcId[%s]在[%s]中推送中线程任务失败"
+                            , infoId, apiCode, requestId, tcId, yyyyMMdd);
                     sendAlarm(smg);
                     return result;
                 }
@@ -1534,8 +1563,9 @@ public class PushRuleServiceImpl implements PushRuleService {
                     log.info("推送客服数据已保存记录，本次保存[{}]", collect.size());
                 } else {
                     result.setMessage("保存记录失败！");
-                    log.error("保存推送记录失败：apiCode:{};requestId:{};tcId:{};infoId:{};失败数据量:{}"
+                    String smg = String.format("保存推送记录失败：apiCode:{%s};requestId:{%s};tcId:{%s};infoId:{%d};失败数据量:{%d}"
                             , apiCode, requestId, tcId, infoId, collect.size());
+                    sendAlarm(smg);
                     return result;
                 }
             } else {
@@ -1770,9 +1800,9 @@ public class PushRuleServiceImpl implements PushRuleService {
         for (; ; ) {
             PageHelper.startPage(page, pageSize);
             List<MarketingTransferSyncUser> transferList = marketingTransferSyncUserMapper.selectByExample(example);
-            if (CollectionUtils.isEmpty(transferList) && transferInfo.getActualNum() == 0) {
-                String smg = String.format("转化信息为【apiCode:[%s],RequestId:[%s],id:[%s]】没有找到对应的转化数据，此消息不再放回队列！日期:%s", apiCode
-                        , transferInfo.getRequestId(), transferInfo.getId(), DateUtils.getNowyyyy_MM_dd());
+            if (CollectionUtils.isEmpty(transferList) && transferInfo.getActualNum() < 1) {
+                String smg = String.format("转化信息为【apiCode:[%s],RequestId:[%s],infoId:[%s],tcId:[%s]】没有找到对应的转化数据，此消息不再放回队列！日期:%s", apiCode
+                        , transferInfo.getRequestId(), transferInfo.getId(), tcId, DateUtils.getNowyyyy_MM_dd());
                 alarmClient.sendAlarm(smg, title, appName, secretKey,
                         Constants.sendCodeMap.get("pushToCustomer"));
                 break;
