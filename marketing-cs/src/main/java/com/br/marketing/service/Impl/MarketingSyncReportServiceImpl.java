84d328625da823f4a3646e874abfdd5b639cce85
linquan.guo <linquan.guo@100credit.com>
2021-11-18 14:49:05 +0800

    生成report-service

---
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingSyncReportServiceImpl.java
@@ -0,0 +1,16 @@
+package com.br.marketing.service.Impl;
+
+import com.br.marketing.service.MarketingSyncReportService;
+
+/**
+ * 客户上传数据统计报表
+ *
+ * @Author linquan.guo
+ * @CreateDate 2021/11/18 14:47
+ * @UpdateUser linquan.guo
+ * @UpdateDate 2021/11/18 14:47
+ * @UpdateRemark 修改内容
+ * @Version 1.0
+ */
+public class MarketingSyncReportServiceImpl implements MarketingSyncReportService {
+}
