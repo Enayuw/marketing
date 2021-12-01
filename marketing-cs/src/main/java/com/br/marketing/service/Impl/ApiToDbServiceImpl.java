15c59da895e73f5e435f61d0b4a44d1ccb5bde38
xiaoxin.pang <xiaoxin.pang@brgroup.com>
2021-12-01 18:05:59 +0800

    不跑分需求

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/ApiToDbServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/ApiToDbServiceImpl.java
@@ -85,6 +85,10 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
 
     @Autowired
     TaskStatusMapper taskStatusMapper;
+
+    @Autowired
+    TaskStatusDistributeMapper taskStatusDistributeMapper;
+
     private final static String redisElasticJobKey = "elasticjob:contextid";
 
     private final static String redisBatchNumberKey = "batchnumber:pre";
@@ -246,7 +250,7 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
                 int currentPage = 1;
                 Integer taskNum = 0;
                 String filePath=path.concat("/").concat(Constants.monitorTypeMap.get(String.valueOf(customerScoreRuleVO.getExecType()))).concat("/").concat(apiCode).concat("/")
-                        .concat(number).concat("/").concat(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                        .concat(number).concat("/").concat(new SimpleDateFormat("yyyy-MM-dd").format(new Date())).concat("/").concat("0");
                 while (execMark&& TaskExecCommonField.isBuildTaskJob.equals(1)) {
                     String batchNumber = number;
                     Long nowMaxId = minId+5000;
@@ -350,7 +354,7 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
 
                     if(isToFile){
                         LoanFile loanFile=saveStraHisFile(task,customerScoreRuleVO,eTimeStr,filePath);
-                        saveTaskStatus(task,loanFile);
+                        saveTaskStatusDistribute(task,loanFile);
                     }
                     try{
                         StringBuilder content = new StringBuilder();
@@ -374,6 +378,7 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
         }
         return new Result().setCode(ResultCode.SUCCESS.getValue());
     }
+
     private void dataToDB(List<MarketingSyncUser> syncUserByRuleScore,String apiCode,String batchNumber,BaseHeadConfigVO baseHeadConfigVO,ExecutorService threadPool){
             threadPool.submit(()->{
                 try {
@@ -624,11 +629,23 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
         }else if(4==task.getMonitorType()){
             blf.setType(1);
         }
+        blf.setIndexNum(1);
         blf.setBatchNumber(task.getBatchNumber());
         blf.setExpectedNum(task.getActualNumber());
         blf.setShowTitle(createShowTitle(task,customerScoreRuleVO,uploadTime));
         loanFileMapper.insertFile(blf);
         return blf;
+    }
+    private void saveTaskStatusDistribute(MarketingTask task,LoanFile loanFile){
+        TaskStatusDistribute statusDistribute = new TaskStatusDistribute();
+        statusDistribute.setFileId(Long.valueOf(loanFile.getId()));
+        statusDistribute.setApiCode(task.getApiCode());
+        statusDistribute.setBatchNumber(task.getBatchNumber());
+        statusDistribute.setDistributeIndex(task.getIndex());
+        Date date = new Date();
+        statusDistribute.setCreateTime(date);
+        statusDistribute.setUpdateTime(date);
+        taskStatusDistributeMapper.insertSelective(statusDistribute);
     }
 
     private void saveTaskStatus(MarketingTask task,LoanFile loanFile){
