7b784cd1e5ea466c641e6c96ef4d7e444dabcd74
juman.wang <juman.wang@brgroup.com>
2021-12-01 20:24:12 +0800

    暂存

---
+++ marketing-cs/src/main/java/com/br/marketing/mapper/FileDbConfigMapper.java
@@ -0,0 +1,10 @@
+package com.br.marketing.mapper;
+
+import com.br.marketing.entity.FileDbConfig;
+import com.br.marketing.entity.FileDbConfigExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface FileDbConfigMapper {
+}
