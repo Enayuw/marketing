7eca87ba7eca2814080fd7067498932a988a1656
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-12-03 13:47:36 +0800

    华强: 合并优化 *

--- marketing-check/src/main/java/com/br/marketing/check/job/ApiToDbJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/ApiToDbJob.java
@@ -5,7 +5,6 @@ import com.br.marketing.service.IApiToDbService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +19,7 @@ public class ApiToDbJob extends AbstractSimpleElasticJob {
     @Override
     public void process(JobExecutionMultipleShardingContext context) {
         try {
-            iApiToDbService.pushToDb(null);
+            iApiToDbService.pushToDb(context.getJobParameter(), context.getShardingTotalCount(), context.getShardingItems());
         } catch (Exception ex) {
             log.error(ex.getMessage(), ex);
         }
