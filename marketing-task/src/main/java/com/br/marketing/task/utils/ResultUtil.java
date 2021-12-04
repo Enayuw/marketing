51cd72d0e8013250fb6a798a47a0c2f1bf4f74dc
xiaoxin.pang <xiaoxin.pang@brgroup.com>
2021-12-04 14:01:40 +0800

    跑分规则支持数据产品选择

--- marketing-task/src/main/java/com/br/marketing/task/utils/ResultUtil.java
+++ marketing-task/src/main/java/com/br/marketing/task/utils/ResultUtil.java
@@ -1,28 +1,16 @@
 package com.br.marketing.task.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.utils.StringUtils;
 import com.br.marketing.entity.MarketingUser;
-import com.br.marketing.entity.RuleField;
 import com.br.marketing.es.bean.MarketingHistory;
 import com.br.marketing.es.bean.Product;
 import com.br.marketing.es.service.impl.MarketingHistoryEsServiceImpl;
-import com.br.marketing.es.util.BrCipherMaker;
 import com.br.marketing.es.util.UuidUtils;
-import com.br.marketing.service.IProductResultSimpleService;
-import com.br.marketing.service.Impl.ProductResultByConfigSimpleServiceImpl;
-import com.br.marketing.task.Scheduler;
 import com.br.marketing.vo.StrategyProductDetailVO;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.ReadContext;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.DigestUtils;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -41,52 +29,24 @@ public class ResultUtil {
 
     public static void generateFile(JSONObject resultJson, String strategyId, Writer fw, String  sep , Map<String,String> proFieldMap,
                                     MarketingUser user, JSONObject meal, String cusBatchNumber, String fileId,String pushCustomer,
-                                    String baseHeadInfo,List<StrategyProductDetailVO> fieldInfos) throws IOException {
+                                    String baseHeadInfo,StrategyProductDetailVO fieldInfo) throws IOException {
         log.info("cus_num：{} 画像流水:{}",user.getCusNum(),resultJson);
         JSONObject esResult=new JSONObject();
         StringBuilder sb=new StringBuilder();
-        JSONObject jsonData;
-        JSONObject strategyJson=new JSONObject();
-        JSONObject flagJson =new JSONObject();
-
-        JSONObject strategyInfo;
         MarketingHistory mh = new MarketingHistory();
-        /**
-         * 调用画像打平接口，将数据产品详情打平
-         */
-        JSONObject hxJson=new JSONObject();
-
-
         /**
          * 添加基本信息
          */ Date requestTime=new Date();
               sb.append(new SimpleDateFormat("yyyy-MM-dd").format(requestTime)).append(sep)
                 .append(user.getBatchNumber()).append(sep)
                 .append(user.getCusNum()).append(sep)
-                .append(strategyId).append(sep);
+                .append(strategyId).append(sep).append(sep);
               mh.setRequestTime(requestTime);
               mh.setBatchNumber(user.getBatchNumber());
               mh.setCusNum(user.getCusNum());
               mh.setApiCode(user.getApiCode());
               mh.setStrategyId(strategyId);
-              if(strategyId.startsWith("STRB")){
-                  String hxResult1 = resultJson.getString("hxResult");
-                  if(StringUtils.isNotEmpty(hxResult1)){
-                    String  hxResult = HxUtil.hauXiangFlat(hxResult1);
-                      hxJson= JSONObject.parseObject(hxResult);
-                  }
-                  jsonData=resultJson.getJSONObject("jsonData");
-                  strategyJson=resultJson.getJSONObject("loanStrategy");
-                  flagJson = resultJson.getJSONObject("flag");
-                  strategyInfo=jsonData.getJSONObject("strategyJson");
-                  sb.append(strategyInfo.getString("useVersion")).append(sep);
-                  mh.setVersion(strategyInfo.getString("useVersion"));
-              }else if(strategyId.startsWith("DTM")){
-                  mh.setVersion("");
-                  sb.append(sep);
-                  flagJson = resultJson.getJSONObject("Flag");
-                  hxJson=resultJson;
-              }
+              mh.setVersion("");
             if(StringUtils.isNotBlank(baseHeadInfo)){
                 JSONObject jsonObject = null;
                 if(StringUtils.isNotBlank(user.getExtendJson())){
@@ -106,72 +66,41 @@ public class ResultUtil {
                     sb.append(sep);
                 }
             }
-
-
-        /**
-         * 添加风险策略信息
-         */
-        if(strategyId.startsWith("STRB")){
-            if(!strategyJson.isEmpty()){
-                sb.append(strategyJson.getString("strategyDecision")).append(sep);
-                RuleField rf=new RuleField();
-                String ruleType="Rule_W_SpecialList_c_mix_c";
-                ruleInfo(flagJson,strategyJson,ruleType,rf.getRuleSpecialListField(),sb,sep);
-                ruleType="Rule_W_InfoRelation_mix_c";
-                ruleInfo(flagJson,strategyJson,ruleType,rf.getRuleInfoRelationField(),sb,sep);
-                ruleType="Rule_W_ApplyLoanStr_mix_c";
-                ruleInfo(flagJson,strategyJson,ruleType,rf.getRuleApplyloanstrField(),sb,sep);
-                ruleType="Rule_W_ApplyLoanUsury_mix";
-                ruleInfo(flagJson,strategyJson,ruleType,rf.getRuleApplyloanusuryField(),sb,sep);
-                ruleType="Rule_W_ExecutionLimited_mix";
-                ruleInfo(flagJson,strategyJson,ruleType,rf.getRuleExecutionlimitedField(),sb,sep);
-
-            }
-
-        }
-
-
         Set<String> products=new HashSet<String>();
         for(String pro:proFieldMap.keySet()){
             products.add(pro.toLowerCase());
         }
         log.info("batch_number:{} products:{}",user.getBatchNumber(),products);
-        Result result = buildResult(hxJson, sb, sep,esResult,fieldInfos);
-        if (!ResultCode.SUCCESS.getValue().equals(result.getCode())) {
-            ProductResultUtil.dealProResult(hxJson, products, sb,sep);
-        }
+
+         buildResult(resultJson, sb, sep,esResult,fieldInfo);
+
         if(log.isInfoEnabled()){
             log.info("sb信息--"+sb.toString());
         }
-        if(countStr(sb.toString(),sep)>5){
-            fw.append(sb + "\r\n");
-            if("1".equals(pushCustomer)){
-                mh.setIdCard(user.getIdCard());
-                mh.setName(user.getName());
-                mh.setCell(user.getCell());
-                mh.setCusBatchNumber(cusBatchNumber);
-                mh.setBatchNumber(user.getBatchNumber());
-                mh.setFileId(fileId);
-                mh.setReserveField(esResult.toJSONString());
-                mh.setTaskId(user.getTaskId());
-                mh.setUserType(user.getUserType());
-                writeEs(mh,meal,hxJson);
-            }
+        //if(countStr(sb.toString(),sep)>5){
+        fw.append(sb + "\r\n");
+        if("1".equals(pushCustomer)){
+            mh.setIdCard(user.getIdCard());
+            mh.setName(user.getName());
+            mh.setCell(user.getCell());
+            mh.setCusBatchNumber(cusBatchNumber);
+            mh.setBatchNumber(user.getBatchNumber());
+            mh.setFileId(fileId);
+            mh.setReserveField(esResult.toJSONString());
+            mh.setTaskId(user.getTaskId());
+            mh.setUserType(user.getUserType());
+            writeEs(mh,meal,resultJson);
         }
+        //}
     }
 
-      static Result buildResult(JSONObject hxJson, StringBuilder sb,String sep,JSONObject esResult,List<StrategyProductDetailVO> strategyProductDetailVOs) {
+      static Result buildResult(JSONObject hxJson, StringBuilder sb,String sep,JSONObject esResult,StrategyProductDetailVO fieldInfo) {
         StringBuilder result=new StringBuilder();
-        StrategyProductDetailVO strategyProductDetailVO = null;
-        if(strategyProductDetailVOs.size()>0){
-            strategyProductDetailVO=strategyProductDetailVOs.get(0);
-        }
-
-        if(strategyProductDetailVO == null){
+        if(fieldInfo == null){
             return new Result().setCode(ResultCode.FAIL.getValue());
         }
-        for (int i = 0; i < strategyProductDetailVO.getFields().size(); i++) {
-            String field = strategyProductDetailVO.getFields().get(i);
+        for (int i = 0; i < fieldInfo.getFields().size(); i++) {
+            String field = fieldInfo.getFields().get(i);
             String fieldRes = hxJson.getString(field);
             result.append(StringUtils.isNotBlank(fieldRes)?fieldRes:"").append(sep);
             esResult.put(field,StringUtils.isNotBlank(fieldRes)?fieldRes:"");
@@ -197,60 +126,7 @@ public class ResultUtil {
         MarketingHistoryEsServiceImpl service = new MarketingHistoryEsServiceImpl();
         service.insert(mh, id);
     }
-    /**
-     * 追加规则信息
-     * @param flagJson
-     * @param strategyJson
-     * @param ruleType
-     * @param ruleField
-     * @param sb
-     */
-    private static void ruleInfo(JSONObject flagJson,JSONObject strategyJson,String ruleType,List<String> ruleField,StringBuilder sb,String sep){
-        if(flagJson.containsKey(ruleType)){
-            sb.append(flagJson.getString(ruleType)).append(sep);
-            ReadContext context = JsonPath.parse(strategyJson);
-            Object object = context.read("$..ruleArray[?(@.ruleType=='"+ruleType+"')]");
-            if(object!=null){
-                JSONArray array=JSONArray.parseArray(object.toString());
-                if(!array.isEmpty()&&array.size()>0){
-                    for(int i=0;i<array.size();i++){
-                        JSONObject jsonObject = array.getJSONObject(i);
-                        sb.append(jsonObject.getString("rulerisk")).append(sep);
-                        sb.append(jsonObject.getString("ruleWeight")).append(sep);
-                        ReadContext ruleContext = JsonPath.parse(jsonObject);
-                        for(String ruleCode:ruleField){
-                            Object rule = ruleContext.read("$..loanRule[?(@.ruleCode=='" + ruleCode + "')]");
-                            if(rule!=null){
-                                JSONArray ruleArray=JSONArray.parseArray(rule.toString());
-                                if(ruleArray!=null&&ruleArray.size()>0){
-                                    for(int j=0;j<ruleArray.size();j++){
-                                        JSONObject jsonObject1 = ruleArray.getJSONObject(j);
-                                        sb.append(jsonObject1.getString("weight")).append(sep);
-                                    }
-                                }else {
-                                    sb.append(sep);
-                                }
-                            }else{
-                                sb.append(sep);
-                            }
-                        }
-                    }
-                }else{
-                    sb.append(sep);
-                    sb.append(sep);
-                    for(String ruleCode:ruleField){
-                        sb.append(sep);
-                    }
-                }
-            }else {
-                sb.append(sep);
-                sb.append(sep);
-                for (String ruleCode : ruleField) {
-                    sb.append(sep);
-                }
-            }
-        }
-    }
+
 
     public static void generateErrorFile(JSONObject resultJson,  Writer fw, String batchNumber,String sep,String cusNum) throws IOException {
         log.info("生成错误文件：{}",resultJson);
