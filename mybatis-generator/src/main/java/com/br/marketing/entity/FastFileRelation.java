dd017b3f8712af52c69d516465cbb0ccecb02593
juman.wang <juman.wang@brgroup.com>
2021-12-09 14:54:12 +0800

    添加规则跑分关系表

---
+++ mybatis-generator/src/main/java/com/br/marketing/entity/FastFileRelation.java
@@ -0,0 +1,83 @@
+package com.br.marketing.entity;
+
+import java.util.Date;
+
+public class FastFileRelation {
+    /**
+     * 
+     */
+    private Long id;
+
+    /**
+     * 规则id
+     */
+    private Long fastTaskId;
+
+    /**
+     * 规则编号
+     */
+    private Long fileId;
+
+    /**
+     * 1-有效；9-无效
+     */
+    private Integer isDel;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFastTaskId() {
+        return fastTaskId;
+    }
+
+    public void setFastTaskId(Long fastTaskId) {
+        this.fastTaskId = fastTaskId;
+    }
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ mybatis-generator/src/main/java/com/br/marketing/mapper/FastFileRelationMapper.java
@@ -0,0 +1,30 @@
+package com.br.marketing.mapper;
+
+import com.br.marketing.entity.FastFileRelation;
+import com.br.marketing.entity.FastFileRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface FastFileRelationMapper {
+    int countByExample(FastFileRelationExample example);
+
+    int deleteByExample(FastFileRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(FastFileRelation record);
+
+    int insertSelective(FastFileRelation record);
+
+    List<FastFileRelation> selectByExample(FastFileRelationExample example);
+
+    FastFileRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") FastFileRelation record, @Param("example") FastFileRelationExample example);
+
+    int updateByExample(@Param("record") FastFileRelation record, @Param("example") FastFileRelationExample example);
+
+    int updateByPrimaryKeySelective(FastFileRelation record);
+
+    int updateByPrimaryKey(FastFileRelation record);
+}
