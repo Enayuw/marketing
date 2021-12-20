2cd818a0ecce2c845d5eefb94d8a9d4e50a19008
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2021-12-20 09:37:10 +0800

    华强：D20211215桔子分期转电销api-3710037 * 优化

--- marketing-cs/src/main/java/com/br/marketing/service/ITxtToDbService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/ITxtToDbService.java
@@ -9,4 +9,26 @@ public interface ITxtToDbService {
     Result phoneTodb(TxtToDbDTO content);
 
     Result phoneTodbByXW(TxtToDbDTO content);
+
+    /**
+     * D20211215桔子分期转电销api-3710037
+     * <p>
+     * 样例一：
+     * <p>
+     * 桔子分期-存量复购场景.txt
+     * <p>
+     * 测试编号,md5手机号,下单时间,客群类型
+     * 0,572527c05beec0898790e239bacd9a5b,2021,存量复购
+     * <p>
+     * 样例二：
+     * <p>
+     * 桔子分期-注册未认证场景.txt
+     * <p>
+     * 测试编号,md5手机号,注册时间,客群类型
+     * 100000,19d0aaa0103400db77dab60be8546ff4,2021,注册未认证
+     *
+     * @author Guo Zeqiang
+     * @dateTime 2021/12/20 9:30
+     */
+    Result phoneTodbByJuZi(TxtToDbDTO content);
 }
