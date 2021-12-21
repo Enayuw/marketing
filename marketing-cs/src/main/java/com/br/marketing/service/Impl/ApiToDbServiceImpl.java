796e8ae6a458ac36725017fcbd846c99fd83e0f5
juman.wang <juman.wang@brgroup.com>
2021-12-21 10:32:03 +0800

    修改bug

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/ApiToDbServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/ApiToDbServiceImpl.java
@@ -912,6 +912,8 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
                     task.setCusBatch(customerScoreRuleVO.getId().toString());
                     task.setActualNumber(actNum);
                     task.setTaskNumber(taskNum);
+                    task.setTaskType(customerScoreRuleVO.getTaskType());
+                    task.setProductInfo(customerScoreRuleVO.getProductInfo());
                     String s = DateUtils.format(new Date(), "yyyy-MM-dd");
                     task.setMonitorType(customerScoreRuleVO.getExecType());
                     if(Integer.valueOf(4).equals(customerScoreRuleVO.getExecType())) {
@@ -1212,6 +1214,7 @@ public class ApiToDbServiceImpl  implements IApiToDbService {
                         task.setTaskType(rule.getTaskType());
                         task.setStatus(1);
                         task.setStrategyId(rule.getStrategyId());
+                        task.setProductInfo(rule.getProductInfo());
                         task.setFileName(String.format("%s_%s", rule.getId().toString(), rule.getRuleNumber()));
                         task.setCusBatch(rule.getId().toString());
                         task.setActualNumber(actNum);
