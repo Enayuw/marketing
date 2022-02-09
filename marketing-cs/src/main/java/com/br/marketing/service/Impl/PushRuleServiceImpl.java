fc592ecd276b93e3ccc404516d357eecdda6c4b8
张广超 <50570550@qq.com>
2022-02-09 16:47:39 +0800

    Subject：<修复查询的时间条件>

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -180,14 +180,14 @@ public class PushRuleServiceImpl implements PushRuleService {
     }
 
     public CustomerBatchNumDTO getCustomerBatchNumDTO(CustomerBatchNumDTO dto){
-        if(StringUtils.isNotBlank(dto.getUploadBeginTime())){
-            Date dateUpdate = addDay(dto.getUploadEndTime(), 1, "yyyy-MM-dd");
-            dto.setUploadEndTime(DateUtils.format(dateUpdate, "yyyy-MM-dd"));
-        }
-        if(StringUtils.isNotBlank(dto.getScoreBeginTime())){
-            Date date = addDay(dto.getScoreEndTime(), 1, "yyyy-MM-dd");
-            dto.setScoreEndTime(DateUtils.format(date, "yyyy-MM-dd"));
-        }
+        //if(StringUtils.isNotBlank(dto.getUploadBeginTime())){
+        //    Date dateUpdate = addDay(dto.getUploadEndTime(), 1, "yyyy-MM-dd");
+        //    dto.setUploadEndTime(DateUtils.format(dateUpdate, "yyyy-MM-dd"));
+        //}
+        //if(StringUtils.isNotBlank(dto.getScoreBeginTime())){
+        //    Date date = addDay(dto.getScoreEndTime(), 1, "yyyy-MM-dd");
+        //    dto.setScoreEndTime(DateUtils.format(date, "yyyy-MM-dd"));
+        //}
         if(StringUtils.isNotBlank(dto.getProductName())){
             String productName = dto.getProductName();
             String[] module = productName.split(",");
