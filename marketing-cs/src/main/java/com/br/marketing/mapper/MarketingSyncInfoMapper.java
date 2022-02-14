29d4d39256715d01e0798b51ad2f519023df7f09
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-02-14 12:53:25 +0800

    华强：有效期查询 +  新增接口isPeriodOfValidity

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncInfoMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncInfoMapper.java
@@ -1,6 +1,7 @@
 package com.br.marketing.mapper;
 
 
+import com.br.marketing.dos.PeriodOfValidityDO;
 import com.br.marketing.entity.MarketingSyncInfo;
 import com.br.marketing.entity.MarketingSyncUser;
 import com.br.marketing.entity.MarketingTransfer;
@@ -48,19 +49,29 @@ public interface MarketingSyncInfoMapper extends MarketingSyncInfoMapperBase {
             , @Param("sTimeStr") String sTimeStr, @Param("eTimeStr") String eTimeStr
             , @Param("whereStr") String whereStr);
 
-    List<MarketingSyncUser> getSyncUserByRuleScore(@Param("apiCode")String apiCode
-            ,@Param("sTimeStr")String sTimeStr
-            ,@Param("eTimeStr")String eTimeStr,@Param("minId")Long minId,@Param("maxId")Long maxId,@Param("whereStr")String whereStr);
+    List<MarketingSyncUser> getSyncUserByRuleScore(@Param("apiCode") String apiCode
+            , @Param("sTimeStr") String sTimeStr
+            , @Param("eTimeStr") String eTimeStr, @Param("minId") Long minId, @Param("maxId") Long maxId, @Param("whereStr") String whereStr);
 
     List<MarketingSyncUser> getPreUserByInCust(@Param("apiCode") String apiCode, @Param("custs") Set<String> custs);
 
-    Integer countTransferFile(@Param("apiCode") String apiCode,@Param("beginTime") String beginTime
-            ,@Param("endTime") String endTime,@Param("groupType") String groupType,@Param("fileTypes") List<String> fileTypes);
+    Integer countTransferFile(@Param("apiCode") String apiCode, @Param("beginTime") String beginTime
+            , @Param("endTime") String endTime, @Param("groupType") String groupType, @Param("fileTypes") List<String> fileTypes);
 
-    List<TransferUserVO> getTransferFileUser(@Param("apiCode") String apiCode,@Param("beginTime") String beginTime
-            ,@Param("endTime") String endTime,@Param("minId") Long minId,@Param("groupType") String groupType,@Param("fileTypes") List<String> fileTypes);
+    List<TransferUserVO> getTransferFileUser(@Param("apiCode") String apiCode, @Param("beginTime") String beginTime
+            , @Param("endTime") String endTime, @Param("minId") Long minId, @Param("groupType") String groupType, @Param("fileTypes") List<String> fileTypes);
 
-    List<MarketingSyncUser> getSyncUserByTaskAndCust(@Param("apiCode") String apiCode,@Param("taskIds") List<String> taskIds,@Param("custNums") List<String> custNums);
+    List<MarketingSyncUser> getSyncUserByTaskAndCust(@Param("apiCode") String apiCode, @Param("taskIds") List<String> taskIds, @Param("custNums") List<String> custNums);
 
-
+    /**
+     * 获取有效期时间内的数量
+     *
+     * @param apiCode            apiCode
+     * @param custNum            案件编号
+     * @param periodOfValidityDO 有效期pojo
+     * @return 数量
+     * @author Guo Zeqiang
+     * @dateTime 2022/2/14 9:55
+     */
+    long getPeriodOfValiditySum(@Param("apiCode") String apiCode, @Param("custNum") String custNum, @Param("periodOfValidityDO") PeriodOfValidityDO periodOfValidityDO);
 }
