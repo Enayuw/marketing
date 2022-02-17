1d118321041b5e5d32f7309825f718ee89e56afc
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-02-17 18:10:13 +0800

    华强：修改推送电销逻辑 *

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushShuheTransferDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushShuheTransferDataServiceImpl.java
@@ -155,9 +155,10 @@ public class PushShuheTransferDataServiceImpl implements IPushShuheTransferDataS
                 // 转化
                 transferSyncUser.setIfTransform("1");
                 caseShuheUser.setIsTransfer(1);
-            } else {
-                transferSyncUser.setIfTransform("0");
             }
+
+            // 转化信息入库
+            int i = goTransferSync(apiCode, caseShuheUser, transferSyncUser);
 
             // D20220209数禾申完转电销
             if (iUserType instanceof CuShenWan) {
@@ -167,9 +168,6 @@ public class PushShuheTransferDataServiceImpl implements IPushShuheTransferDataS
                     caseShuheUser.setIsTransfer(3);
                 }
             }
-
-            // 转化信息入库
-            int i = goTransferSync(apiCode, caseShuheUser, transferSyncUser);
 
             // D20220209数禾转化数据定制化清洗入库
             int row = caseShuheUserMapper.insertSelective(caseShuheUser);
@@ -267,7 +265,7 @@ public class PushShuheTransferDataServiceImpl implements IPushShuheTransferDataS
                 transferInfo.setJsonData(caseShuheUser.getJsonData());
                 transferInfo.setActualNum(1);
                 int row_info = marketingTransferInfoMapper.insertSelective(transferInfo);
-                if (row_info > 0 && caseShuheUser.getIsTransfer() < 2) {
+                if (row_info > 0 && caseShuheUser.getIsTransfer() != null && caseShuheUser.getIsTransfer() != 2) {
                     producter.send(MQConstants.ROUTING_KEY_MARKETING_TRANSFER_PUSH_CUSTOMER, transferInfo.getId().toString());
                 }
                 return row_sync + row_info;
