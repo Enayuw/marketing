bce64b68e15ca525d041d698cae62e87425c563e
张广超 <50570550@qq.com>
2022-02-28 20:51:42 +0800

    Subject：<优化>

--- marketing-check/src/main/java/com/br/marketing/check/job/CallingToSendJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/CallingToSendJob.java
@@ -67,38 +67,38 @@ public class CallingToSendJob extends AbstractSimpleElasticJob {
             String tableColumns = getTableColumns(customerCalling);
 
             if(tableColumns!=null){
-                getPartitions(customerCalling,tableColumns);
+                doThreadSubmit(customerCalling,tableColumns);
                 //doThreadSubmit(customerCalling, getPartitions(customerCalling,tableColumns));
             }
         }
     }
 
-    private List<List<CustomerCallingDialog>> getPartitions(CustomerCalling customerCalling,String tableColumns) {
+    private void doThreadSubmit(CustomerCalling customerCalling,String tableColumns) {
+        ThreadPoolExecutor pushExecutor;
+        if (customerCalling.getPushThreadNum() != null) {
+            pushExecutor = BrExecutors.getThreadPool(customerCalling.getPushThreadNum(), customerCalling.getPushThreadNum());
+        } else {
+            pushExecutor = BrExecutors.getThreadPool(2, 2);
+        }
+
         Map<String, Object> cusMap = new HashMap<>(16);
         cusMap.put("columns", tableColumns);
         cusMap.put("apiCode", customerCalling.getApiCode());
         cusMap.put("sendStatus", 0);
         cusMap.put("conditions", customerCalling.getConditions());
         boolean index = true;
-        List<CustomerCallingDialog> list = new ArrayList<>();
+        Integer pageNo = 0;
         while (index){
-            List<CustomerCallingDialog>    customerCallingDialogsByEvery = customerCallingDialogMapper.getInfoByColumns(cusMap);
-            index =  customerCallingDialogsByEvery.size()==0?false:true;
+            cusMap.put("pageNo", pageNo*15000);
+            cusMap.put("pageSize",15000);
+            List<CustomerCallingDialog> customerCallingDialogsByEvery = customerCallingDialogMapper.getInfoByColumns(cusMap);
+            index = customerCallingDialogsByEvery.size()==0?false:true;
             if(index){
-                //List<Long> ids = customerCallingDialogsByEvery
-                //        .stream()
-                //        .map(CustomerCallingDialog::getId)
-                //        .collect(Collectors.toList());
-                //CustomerCallingDialogExample customerCallingDialogExample = new CustomerCallingDialogExample();
-                //customerCallingDialogExample.createCriteria().andIdIn(ids);
-                //CustomerCallingDialog customerCallingDialog = new CustomerCallingDialog();
-                //customerCallingDialog.setSendStatus(1);
-                //customerCallingDialogMapper.updateByExampleSelective(customerCallingDialog, customerCallingDialogExample);
-                sendPostRequest(customerCalling,customerCallingDialogsByEvery);
+                List<List<CustomerCallingDialog>> partitions = Lists.partition(customerCallingDialogsByEvery, 1500);
+                partitions.forEach((customerCallingDialogLists) -> pushExecutor.submit(new CallingDataThread(customerCallingDialogLists, customerCallingDialogMapper, customerCalling,httpProxyClient,customerCallingPushLogMapper)));
             }
+            pageNo++;
         }
-        return Lists.partition(list, 1500);
-
     }
 
     private String getTableColumns(CustomerCalling customerCalling) {
@@ -120,19 +120,6 @@ public class CallingToSendJob extends AbstractSimpleElasticJob {
         return null;
     }
 
-    private void doThreadSubmit(CustomerCalling customerCalling, List<List<CustomerCallingDialog>> partitions) {
-        if(!partitions.isEmpty()){
-            ThreadPoolExecutor pushExecutor;
-            if (customerCalling.getPushThreadNum() != null) {
-                pushExecutor = BrExecutors.getThreadPool(customerCalling.getPushThreadNum(), customerCalling.getPushThreadNum());
-            } else {
-                pushExecutor = BrExecutors.getThreadPool(2, 2);
-            }
-            log.warn("2用户处理信息：{}",partitions);
-            partitions.forEach((customerCallingDialogLists) -> pushExecutor.submit(new CallingDataThread(customerCallingDialogLists, customerCallingDialogMapper, customerCalling,httpProxyClient,customerCallingPushLogMapper)));
-        }
-    }
-
 
     private List<CustomerCalling> getCustomerCallings() {
         CustomerCallingExample customerCallingExample = new CustomerCallingExample();
@@ -140,57 +127,5 @@ public class CallingToSendJob extends AbstractSimpleElasticJob {
         return customerCallingMapper.selectByExample(customerCallingExample);
     }
 
-    private void sendPostRequest(CustomerCalling customerCalling, List<CustomerCallingDialog>  customerCallingDialogLists) {
-        String requestId = customerCalling.getApiCode() + "_" + UUID.randomUUID();
-        JSONObject param = new JSONObject();
-        param.put("requestId", requestId);
-        JSONArray dataItems = new JSONArray();
-        customerCallingDialogLists.forEach(customerCallingDialog -> dataItems.add(JSONObject.parse(toJson(customerCallingDialog))));
-        param.put("dataItems", dataItems);
-        log.warn("3用户发送数据：{}", param.toJSONString());
-        String extendConfigInfo = customerCalling.getExtendConfigInfo();
-        String pushUrl = customerCalling.getPushUrl().trim();
-        JSONObject extendConfigInfoJson = getJsonObject(extendConfigInfo);
-        JSONObject pushUrlJson = getJsonObject(pushUrl);
-        String sendUrl = pushUrlJson.getString("sendUrl");
-        Boolean isProxy = extendConfigInfoJson.getBoolean("isProxy") == null ? Boolean.TRUE : extendConfigInfoJson.getBoolean("isProxy");
-        Map<String, Object> result = httpProxyClient.request(sendUrl, param.toJSONString(), isProxy);
-        boolean sendStatus = (boolean) result.get("result");
-        updateRequestId(requestId, sendStatus == true ? 1 : 0,customerCallingDialogLists);
-        savePushLog(requestId, param, result);
-
-    }
-    private void savePushLog(String requestId, JSONObject param, Map<String, Object> result) {
-        JSONArray dataItems = param.getJSONArray("dataItems");
-        Map<String, Object> map = new HashMap<>();
-        map.put("requestId", requestId);
-        map.put("params", param.toJSONString());
-        map.put("result", result.toString());
-        map.put("createTime", new Date());
-        map.put("sum",dataItems.size());
-        customerCallingPushLogMapper.insert(map);
-        log.warn("拨打记录发送返回值：{}", result);
-    }
-
-
-    public static String toJson(Object object) {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setPrettyPrinting(); //生成格式化后的json
-        Gson gson = gsonBuilder.create();
-        return gson.toJson(object);
-    }
-
-    private void updateRequestId(String requestId,Integer sendStatus,List<CustomerCallingDialog>  customerCallingDialogLists) {
-        List<Long> ids = customerCallingDialogLists
-                .stream()
-                .map(CustomerCallingDialog::getId)
-                .collect(Collectors.toList());
-        CustomerCallingDialogExample customerCallingDialogExample = new CustomerCallingDialogExample();
-        customerCallingDialogExample.createCriteria().andIdIn(ids);
-        CustomerCallingDialog customerCallingDialog = new CustomerCallingDialog();
-        customerCallingDialog.setRequestId(requestId);
-        customerCallingDialog.setSendStatus(sendStatus);
-        customerCallingDialogMapper.updateByExampleSelective(customerCallingDialog, customerCallingDialogExample);
-    }
 
 }
