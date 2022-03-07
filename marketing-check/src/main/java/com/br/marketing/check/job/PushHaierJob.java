bade8d735a998f35504aec0f8d192a45db742902
juman.wang <juman.wang@brgroup.com>
2022-03-07 14:15:41 +0800

    格式化代码

--- marketing-check/src/main/java/com/br/marketing/check/job/PushHaierJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/PushHaierJob.java
@@ -1,30 +1,13 @@
 package com.br.marketing.check.job;
 
-import com.br.marketing.check.dto.FileContext;
-import com.br.marketing.check.service.Impl.SftpToDbByCommonService;
-import com.br.marketing.check.utils.SftpToDbUtils;
-import com.br.marketing.client.SftpClient;
-import com.br.marketing.common.commondto.Result;
-import com.br.marketing.common.commondto.ResultCode;
-import com.br.marketing.common.enums.DataTypeEnum;
-import com.br.marketing.entity.*;
-import com.br.marketing.mapper.*;
-import com.br.marketing.service.IApiToDbService;
-import com.br.marketing.service.ITxtToDbService;
+
 import com.br.marketing.service.PushDataService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
-import com.jcraft.jsch.JSchException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
 
 
 @Component
@@ -33,6 +16,7 @@ public class PushHaierJob extends AbstractSimpleElasticJob {
 
     @Autowired
     PushDataService pushDataService;
+
     @Override
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
         pushDataService.pushHaierData();
--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
@@ -54,14 +54,14 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-public class PushDataServiceImpl implements PushDataService{
+public class PushDataServiceImpl implements PushDataService {
 
     @Autowired
     PhoneSaleMapper phoneSaleMapper;
 
     @Autowired
     TwosevenFileMapper twosevenFileMapper;
-    
+
     @Autowired
     DassServiceClient dassServiceClient;
 
@@ -124,29 +124,29 @@ public class PushDataServiceImpl implements PushDataService{
         Long minId = null;
         String key = "dass:push:threadnum";
         Integer threadNum = 5;
-        if(redisChgService.exists(key)&& StringUtils.isNotBlank(redisChgService.get(key))){
+        if (redisChgService.exists(key) && StringUtils.isNotBlank(redisChgService.get(key))) {
             threadNum = Integer.valueOf(redisChgService.get(key));
         }
 
         LocalFile localFile = localFileMapper.selectByPrimaryKey(id);
-        if(localFile == null){
+        if (localFile == null) {
             return new Result().setCode(ResultCode.SUCCESS.getValue()).setMessage("文件不存在").setDate(isContiue);
         }
 
         ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(threadNum, threadNum);
         Integer number = 0;
-        while(actionMark) {
+        while (actionMark) {
             List<DassImportDataDTO> phoneSales = phoneSaleMapper.getPushDassData(id, minId);
-            number+=phoneSales.size();
+            number += phoneSales.size();
             if (phoneSales.size() > 0) {
                 DassImportDataDTO phoneSale = phoneSales.get(phoneSales.size() - 1);
                 DassImportAdapDTO dto = new DassImportAdapDTO();
                 dto.setLocalId(id);
                 dto.setList(phoneSales);
                 minId = phoneSale.getId();
-                threadPool.submit(()->{
+                threadPool.submit(() -> {
                     Result result = dassServiceClient.postHermesUserData(dto);
-                    if(!ResultCode.SUCCESS.getValue().equals(result.getCode())){
+                    if (!ResultCode.SUCCESS.getValue().equals(result.getCode())) {
                         RetryMainLog mainLog = new RetryMainLog();
                         mainLog.setRetryType(1);
                         mainLog.setRetryParam(JSON.toJSONString(dto));
@@ -161,18 +161,18 @@ public class PushDataServiceImpl implements PushDataService{
                         retryMainLogMapper.insertSelective(mainLog);
                     }
                 });
-            }else{
-                actionMark=false;
+            } else {
+                actionMark = false;
             }
         }
         threadPool.shutdown();
-        while (true){
-            if(threadPool.isTerminated()){
+        while (true) {
+            if (threadPool.isTerminated()) {
                 break;
             }
             try {
                 Thread.sleep(3000);
-            }catch (Exception e){
+            } catch (Exception e) {
             }
         }
         StringBuilder content = new StringBuilder();
@@ -180,7 +180,7 @@ public class PushDataServiceImpl implements PushDataService{
                 .append("fileName：".concat(localFile.getFileName()).concat("\r\n"))
                 .append("数量：".concat(number.toString()).concat("\r\n"))
                 .append("文件推送dass结束".concat("\r\n"));
-        alarmClient.sendAlarm(content.toString(),"Dass结果文件推送",appName,secretKey,
+        alarmClient.sendAlarm(content.toString(), "Dass结果文件推送", appName, secretKey,
                 Constants.sendCodeMap.get("uploadSuccess"));
 
         return new Result().setCode(ResultCode.SUCCESS.getValue()).setDate(isContiue);
@@ -205,52 +205,52 @@ public class PushDataServiceImpl implements PushDataService{
                 retryMainLog.setIncrId(redisChgService.incr(RedisKeyConstant.retryid));
                 retryMainLogMapper.insertSelective(retryMainLog);
             }
-        }catch (Exception ex){
-            log.error(ex.getMessage(),ex);
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
         }
 
         return new Result().setCode(ResultCode.SUCCESS.getValue()).setDate(isContiue);
     }
 
-    public Result pushAction(Long id){
+    public Result pushAction(Long id) {
         Boolean actionMark = true;
         Long minId = null;
         String key = "seven:push:transfer:threadnum";
         Integer threadNum = 5;
-        if(redisChgService.exists(key)&& StringUtils.isNotBlank(redisChgService.get(key))){
+        if (redisChgService.exists(key) && StringUtils.isNotBlank(redisChgService.get(key))) {
             threadNum = Integer.valueOf(redisChgService.get(key));
         }
 
         LocalFile localFile = localFileMapper.selectByPrimaryKey(id);
-        if(localFile == null){
+        if (localFile == null) {
             return new Result().setCode(ResultCode.SUCCESS.getValue()).setMessage("文件不存在");
         }
         AtomicInteger errorMark = new AtomicInteger();
         Integer number = 0;
         String yyyyMMddHHmmss = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        while(actionMark) {
+        while (actionMark) {
             ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(threadNum, threadNum);
             List<TransferDataItemDTO> dataItems = Collections.synchronizedList(new ArrayList<>());
             List<Long> twoFileIds = Collections.synchronizedList(new ArrayList<>());
             List<TwosevenFile> data = twosevenFileMapper.getPushData(id, minId);
-            if(data.size()<=0){
-                actionMark= false;
+            if (data.size() <= 0) {
+                actionMark = false;
                 continue;
             }
-            minId = data.get(data.size()-1).getId();
+            minId = data.get(data.size() - 1).getId();
             //region 调用撞库接口
             for (TwosevenFile datum : data) {
-                threadPool.submit(()->{
+                threadPool.submit(() -> {
                     TwosevenFile updateData = new TwosevenFile();
                     updateData.setId(datum.getId());
                     RequestSevenDTO dto = new RequestSevenDTO();
                     dto.setMobile(datum.getMobile());
                     String extendInfo = datum.getLocalId().toString().concat("-").concat(datum.getId().toString());
-                    Result<ResponseSevenZDTO> responseSevenZDTOResult = twoSevenService.requestTransferStatus(dto,extendInfo);
-                    if(!ResultCode.SUCCESS.getValue().equals(responseSevenZDTOResult.getCode())){
-                        responseSevenZDTOResult = twoSevenService.requestTransferStatus(dto,extendInfo);
+                    Result<ResponseSevenZDTO> responseSevenZDTOResult = twoSevenService.requestTransferStatus(dto, extendInfo);
+                    if (!ResultCode.SUCCESS.getValue().equals(responseSevenZDTOResult.getCode())) {
+                        responseSevenZDTOResult = twoSevenService.requestTransferStatus(dto, extendInfo);
                     }
-                    if(ResultCode.SUCCESS.getValue().equals(responseSevenZDTOResult.getCode())) {
+                    if (ResultCode.SUCCESS.getValue().equals(responseSevenZDTOResult.getCode())) {
                         ResponseSevenZDTO responSeven = responseSevenZDTOResult.getData();
                         if ("200".equals(responSeven.getRet())) {
                             SevenDetailVO sevenDetailVO = responSeven.getVolist().get(0);
@@ -263,34 +263,34 @@ public class PushDataServiceImpl implements PushDataService{
                                 updateData.setTransferOk("1");
                                 twoFileIds.add(datum.getId());
                                 dataItems.add(dataItemDTO);
-                            }else{
+                            } else {
                                 updateData.setTransferOk("0");
                             }
                             twosevenFileMapper.updateByPrimaryKeySelective(updateData);
-                        }else{
+                        } else {
                             updateData.setTransferOk(responSeven.getRet());
                             updateData.setDataMessage(responSeven.getMsg());
                             twosevenFileMapper.updateByPrimaryKeySelective(updateData);
                         }
-                    }else{
+                    } else {
                         errorMark.getAndIncrement();
                     }
                 });
             }
             threadPool.shutdown();
-            while (true){
-                if(threadPool.isTerminated()){
+            while (true) {
+                if (threadPool.isTerminated()) {
                     break;
                 }
                 try {
                     Thread.sleep(1000);
-                }catch (Exception e){
+                } catch (Exception e) {
                 }
             }
             //endregion
 
             //region 推送转化接口
-            if(dataItems.size()==0){
+            if (dataItems.size() == 0) {
                 continue;
             }
             TransferDataDTO transferDataDTO = new TransferDataDTO();
@@ -303,7 +303,7 @@ public class PushDataServiceImpl implements PushDataService{
             PushTransferDataDetailDTO detailDTO = new PushTransferDataDetailDTO();
             pushTransferDataDTO.setDto(detailDTO);
             Long miId = twoFileIds.get(0);
-            Long maId = twoFileIds.get(twoFileIds.size()-1);
+            Long maId = twoFileIds.get(twoFileIds.size() - 1);
             pushTransferDataDTO.setExtendInfo(localFile.getId().toString()
                     .concat("-").concat(miId.toString())
                     .concat("-").concat(maId.toString()));
@@ -311,7 +311,7 @@ public class PushDataServiceImpl implements PushDataService{
             detailDTO.setJsonData(JSON.toJSONString(transferDataDTO));
             Result<Boolean> booleanResult = marketingApiService.pushTransfer(pushTransferDataDTO);
             /** 调用转化接口失败需要重试 */
-            if(ResultCode.FAIL.getValue().equals(booleanResult.getCode())&&booleanResult.getData()){
+            if (ResultCode.FAIL.getValue().equals(booleanResult.getCode()) && booleanResult.getData()) {
                 RetryMainLog retryMainLog = new RetryMainLog();
                 retryMainLog.setRetryType(1);
                 retryMainLog.setRetryParam(JSON.toJSONString(pushTransferDataDTO));
@@ -329,7 +329,7 @@ public class PushDataServiceImpl implements PushDataService{
             number++;
         }
         /** 调用撞库接口有网络失败的 需要重试 */
-        if(errorMark.get()>0){
+        if (errorMark.get() > 0) {
             return new Result().setCode(ResultCode.FAIL.getValue());
         }
         return new Result().setCode(ResultCode.SUCCESS.getValue());
@@ -341,21 +341,21 @@ public class PushDataServiceImpl implements PushDataService{
         Integer day = Integer.valueOf(LocalDate.now().format(yyyyMMddDF));
         Boolean mark = Boolean.TRUE;
         Long minId = null;
-        while(mark){
+        while (mark) {
             List<HaierData> haierData = haierDataMapper.selectDataLimitId(day, minId);
-            if(haierData.size()<=0){
-                mark=Boolean.FALSE;
+            if (haierData.size() <= 0) {
+                mark = Boolean.FALSE;
                 continue;
             }
             minId = haierData.get(haierData.size() - 1).getId() + 1;
-            HashMap<String,List<HaierData>> types = new HashMap<>();
+            HashMap<String, List<HaierData>> types = new HashMap<>();
             for (HaierData haierDatum : haierData) {
                 String key = haierDatum.getType();
-                if(types.get(key) ==null){
+                if (types.get(key) == null) {
                     ArrayList<HaierData> haierData1 = new ArrayList<>();
                     haierData1.add(haierDatum);
-                    types.put(key,haierData1);
-                }else {
+                    types.put(key, haierData1);
+                } else {
                     types.get(key)
                             .add(haierDatum);
                 }
@@ -368,7 +368,7 @@ public class PushDataServiceImpl implements PushDataService{
                     Set<PushDTO.DataItems> datas = new HashSet<>();
                     ArrayList<HaierData> nolist = new ArrayList<>();
                     ArrayList<HaierData> yeslist = new ArrayList<>();
-                    getDistinctData(items,yeslist,nolist, type, day.toString());
+                    getDistinctData(items, yeslist, nolist, type, day.toString());
                     updateHaierFalse(nolist);
                     List<Long> ids = new ArrayList<>();
                     for (HaierData item : yeslist) {
@@ -395,7 +395,6 @@ public class PushDataServiceImpl implements PushDataService{
             }
 
 
-
         }
         return null;
     }
@@ -405,54 +404,54 @@ public class PushDataServiceImpl implements PushDataService{
         ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(20, 20);
         Long minId = null;
         Boolean isAction = Boolean.TRUE;
-        while (isAction){
+        while (isAction) {
             List<HaierReq> dataWithStatus = haierReqMapper.getDataWithStatus(minId);
-            if(dataWithStatus.size()<=0){
+            if (dataWithStatus.size() <= 0) {
                 isAction = Boolean.FALSE;
                 continue;
             }
-            minId = dataWithStatus.get(dataWithStatus.size()-1).getId()+1;
+            minId = dataWithStatus.get(dataWithStatus.size() - 1).getId() + 1;
 
             for (HaierReq reqData : dataWithStatus) {
-                threadPool.submit(()->{
+                threadPool.submit(() -> {
                     try {
                         Result<ResponseInfoEntity> responseInfoEntityResult = haierServiceClient.resultQueryPushToTeleSales(reqData.getReqId());
-                        if(ResultCode.SUCCESS.getValue().equals(responseInfoEntityResult.getCode())){
+                        if (ResultCode.SUCCESS.getValue().equals(responseInfoEntityResult.getCode())) {
                             ResponseInfoEntity data = responseInfoEntityResult.getData();
-                            if(data!=null&&data.getHead()!=null&&"00000".equals(data.getHead().getRetFlag())
-                                    &&data.getBody()!=null&&StringUtils.isNotBlank(data.getBody().getSts())){
+                            if (data != null && data.getHead() != null && "00000".equals(data.getHead().getRetFlag())
+                                    && data.getBody() != null && StringUtils.isNotBlank(data.getBody().getSts())) {
                                 HaierReq record = new HaierReq();
                                 record.setId(reqData.getId());
                                 record.setStatus(data.getBody().getSts());
                                 haierReqMapper.updateByPrimaryKeySelective(record);
-                                if("fail".equals(data.getBody().getSts())){
+                                if ("fail".equals(data.getBody().getSts())) {
                                     alarmClient.sendAlarm(String.format("海尔查询结果 reqId:%s 推送失败", reqData.getReqId())
                                             , "海尔推送结果查询"
                                             , app2Name, secret2Key, Constants.sendCodeMap.get("pushToHaier"));
                                 }
                             }
                         }
-                    }catch (Exception ex){
-                        log.error(ex.getMessage(),ex);
+                    } catch (Exception ex) {
+                        log.error(ex.getMessage(), ex);
                     }
                 });
             }
         }
 
         threadPool.shutdown();
-        while (true){
-            if(threadPool.isTerminated()){
+        while (true) {
+            if (threadPool.isTerminated()) {
                 break;
             }
             try {
                 Thread.sleep(1000);
-            }catch (Exception e){
+            } catch (Exception e) {
             }
         }
         return new Result().setCode(ResultCode.SUCCESS.getValue());
     }
 
-    void getDistinctData(List<HaierData> list, List<HaierData> yeslist, List<HaierData> nolist, String type, String day){
+    void getDistinctData(List<HaierData> list, List<HaierData> yeslist, List<HaierData> nolist, String type, String day) {
         Integer start = Integer.valueOf(LocalDate.parse(day, yyyyMMddDF).minusDays(29L).format(yyyyMMddDF));
         Integer end = Integer.valueOf(day);
         List<String> custNums = list.stream().map(t -> t.getCustNum()).collect(Collectors.toList());
@@ -467,11 +466,11 @@ public class PushDataServiceImpl implements PushDataService{
         Set<String> custs = repeatData.stream().map(t -> t.getCustNum()).collect(Collectors.toSet());
         Set<String> custNumNow = new HashSet<>();
         for (HaierData haierData : list) {
-            if(custs.contains(haierData.getCustNum())){
+            if (custs.contains(haierData.getCustNum())) {
                 nolist.add(haierData);
                 continue;
             }
-            if(custNumNow.contains(haierData.getCustNum())){
+            if (custNumNow.contains(haierData.getCustNum())) {
                 nolist.add(haierData);
                 continue;
             }
@@ -481,8 +480,8 @@ public class PushDataServiceImpl implements PushDataService{
 
     }
 
-    void updateHaierFalse(List<HaierData> list){
-        if(list.size()>0) {
+    void updateHaierFalse(List<HaierData> list) {
+        if (list.size() > 0) {
             List<Long> ids = list.stream().map(t -> t.getId()).collect(Collectors.toList());
             HaierDataExample updateExample = new HaierDataExample();
             updateExample.createCriteria().andIdIn(ids);
@@ -493,8 +492,7 @@ public class PushDataServiceImpl implements PushDataService{
     }
 
 
-
-    String getHaierRequestId(String type){
+    String getHaierRequestId(String type) {
         String yyyyMMddHHmmss = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         String s = RandomUtils.randomStr(4);
         return yyyyMMddHHmmss.concat("_").concat(type).concat(s);
