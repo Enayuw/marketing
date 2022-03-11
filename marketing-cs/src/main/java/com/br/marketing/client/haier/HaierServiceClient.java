8cc50240467395a5e751c0907468edbd72103d5c
juman.wang <juman.wang@brgroup.com>
2022-03-11 18:26:26 +0800

    修改打印的日志

--- marketing-cs/src/main/java/com/br/marketing/client/haier/HaierServiceClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/haier/HaierServiceClient.java
@@ -104,12 +104,12 @@ public class HaierServiceClient {
         List<Long> ids = haierReqDTO.getIds();
         Result<Response2Entity> result = new Result<>();
         Assert.notNull(formData, "\"List\" is not null");
-        log.warn("##地址：{}；apicode：{}；apikey：{}", url, apiCode, apiKey);
-        if(log.isInfoEnabled()){
-            log.info(String.format("推送海尔数据：%s",JSON.toJSONString(formData)));
+//        log.warn("##地址：{}；apicode：{}；apikey：{}", url, apiCode, apiKey);
+        if(log.isWarnEnabled()){
+            log.warn(String.format("推送海尔数据：%s",JSON.toJSONString(formData)));
         }
         PushDTO pushDTO = new PushDTO(apiCode, formData, apiKey);
-        log.warn("&&发送内容：[{}]", pushDTO);
+//        log.warn("&&发送内容：[{}]", pushDTO);
         final HashMap<String, String> stringStringHashMap = httpProxyClient.sendByCode(pushDTO, url, true, MediaType.APPLICATION_JSON_UTF8_VALUE, "");
         final String httpCode = stringStringHashMap.getOrDefault("httpcode", "5000");
         if (httpCode.equals("200")) {
