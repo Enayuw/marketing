c55eea01a52b4865468277b3f80ded2c697d4a68
juman.wang <juman.wang@brgroup.com>
2022-03-21 10:38:11 +0800

    放开校验

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/config/WebConfig.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/config/WebConfig.java
@@ -30,10 +30,10 @@ public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(sessionInterceptor)
-//                .addPathPatterns("/rule/**","/pushrulefilter/**");
-//        registry.addInterceptor(authInterceptor)
-//                .addPathPatterns("/rule/**","/pushrulefilter/**");
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/rule/**","/pushrulefilter/**");
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/rule/**","/pushrulefilter/**");
 //        registry.addInterceptor(sessionInterceptor)
 //                .addPathPatterns("/rule/**");
 //        registry.addInterceptor(authInterceptor)
