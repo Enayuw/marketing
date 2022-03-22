fa701d75537d3e64eede040fc573e647a2131586
juanjuan.song <juanjuan.song@brgroup.com>
2022-03-22 19:58:23 +0800

    最新1h内;促首借

--- marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeCustomerCallRecordToPhoneSale.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeCustomerCallRecordToPhoneSale.java
@@ -80,10 +80,17 @@ public class ShuHeCustomerCallRecordToPhoneSale implements AssembleData<RealTime
         String s = BrCipherMaker.getInstance().decode(marketingSyncUser.getCell());
         dassSingleImportDataDTO.setPhone(s);//b_marketing_sync_{apicode}的cell，明文
         dassSingleImportDataDTO.setName("");
-        dassSingleImportDataDTO.setOrgname("shuheshenwan");
-        dassSingleImportDataDTO.setSource("16");
-        dassSingleImportDataDTO.setUserType("2");
-        dassSingleImportDataDTO.setType("2");
+        if("促申完".equals(dto.getUserType())){
+            dassSingleImportDataDTO.setOrgname("shuheshenwan");
+            dassSingleImportDataDTO.setSource("16");
+            dassSingleImportDataDTO.setUserType("2");
+            dassSingleImportDataDTO.setType("2");
+        }else if("促首借".equals(dto.getUserType())){
+            dassSingleImportDataDTO.setOrgname("shuheshoujie");
+            dassSingleImportDataDTO.setSource("18");
+            dassSingleImportDataDTO.setUserType("1");
+            dassSingleImportDataDTO.setType("4");
+        }
         dassSingleImportDataDTO.setPrioritySymbol("2");
         Map extendMap = new HashMap();
         extendMap.put("face_recognitiion","0");
@@ -93,18 +100,19 @@ public class ShuHeCustomerCallRecordToPhoneSale implements AssembleData<RealTime
         extendMap.put("is_usr_lst_app_sta_tim","0");
         extendMap.put("typeSign","2");
         if(marketingTransferSyncUser!=null){
-            ////b_marketing_transfer_sync_{cid} 的login_time
-            dassSingleImportDataDTO.setLoginTime(StringUtils.isNotEmpty(marketingTransferSyncUser.getLoginTime())?marketingTransferSyncUser.getLoginTime():"");
             if(StringUtils.isNotEmpty(marketingTransferSyncUser.getReserveField1())) {
                 JSONObject json = JSON.parseObject(marketingTransferSyncUser.getReserveField1());
                 Date createTime = marketingTransferSyncUser.getCreateTime();
                 String time = new SimpleDateFormat("yyyy-MM-dd").format(createTime);
+                String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
                 //clc_usr_iso_pho_tim如果有值且为接收转化数据当天赋1 ，非1为0
                 extendMap.put("face_recognitiion",getValueByCreateTime(json.getString("clc_usr_iso_pho_tim"),time));
                 extendMap.put("is_usr_idt",getValueByCreateTime(json.getString("clc_usr_iso_idt_tim"),time));
                 extendMap.put("is_bindcard",getValueByCreateTime(json.getString("clc_usr_iso_crd_tim"),time));
                 extendMap.put("is_usr_inf",getValueByCreateTime(json.getString("clc_usr_iso_inf_tim"),time));
-                extendMap.put("is_usr_lst_app_sta_tim",getValueByCreateTime(json.getString("clc_usr_iso_inf_tim"),time));
+                extendMap.put("is_usr_lst_app_sta_tim",getValueByCreateTime(json.getString("clc_usr_lst_app_sta_tim"),today));
+                dassSingleImportDataDTO.setLoginTime(StringUtils.isNotEmpty(json.getString("clc_usr_lst_app_sta_tim"))?json.getString("clc_usr_lst_app_sta_tim"):"");
+                dassSingleImportDataDTO.setAuditAmount(StringUtils.isNotEmpty(json.getString("clc_usr_adt_lmt_itr"))?json.getString("clc_usr_adt_lmt_itr"):"");
             }
         }else {
             dassSingleImportDataDTO.setLoginTime("");
@@ -154,7 +162,7 @@ public class ShuHeCustomerCallRecordToPhoneSale implements AssembleData<RealTime
      */
 
     private String getValueByCreateTime(String reserveFieldTime, String createTime) {
-        if (StringUtils.isNotBlank(reserveFieldTime)&&reserveFieldTime.startsWith(createTime)){
+        if (StringUtils.isNotBlank(reserveFieldTime) && reserveFieldTime.startsWith(createTime)){
             return "1";
         }
         return "0";
@@ -169,32 +177,47 @@ public class ShuHeCustomerCallRecordToPhoneSale implements AssembleData<RealTime
         JSONObject userProperties = JSON.parseObject(bo.getDetail().getUserProperties());
         String userType = userProperties.get("groupType").toString();
         String tcid = bo.getCid().toString().replaceFirst("-", "");
-        MarketingTransferSyncUser newest = marketingTransferSyncUserMapper.getNewestByCusnumAndApicode(tcid, bo.getCaseNum(), bo.getApiCode(),userType);
+        Date dtoCreateTime = bo.getCreateTime();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtoCreateTime);
+        calendar.add(Calendar.HOUR_OF_DAY, 1);
+        String timeAddHour = DateUtils.format(calendar.getTime(), "yyyy-MM-dd HH:mm:ss");
+        MarketingTransferSyncUser newest = marketingTransferSyncUserMapper.getNewestByCusnumAndApicode(tcid, bo.getCaseNum(), bo.getApiCode(),userType,timeAddHour);
         if (StringUtils.isNotEmpty(newest)&&StringUtils.isNotBlank(newest.getReserveField1())){
             JSONObject json = JSON.parseObject(newest.getReserveField1());
             boolean isTurn = "Y".equals(json.getString("is_turn"));
             boolean isBlack = "Y".equals(json.getString("is_black"));
-            boolean isoAtoTimIsSatisfy = false;
-            String clcUsrIsoAtoTim = json.getString("clc_usr_iso_ato_tim");
-            if (StringUtils.isNotBlank(clcUsrIsoAtoTim)){
-                Date date = json.getDate("clc_usr_iso_ato_tim");
-                Calendar c = Calendar.getInstance();
-                c.setTime(date);
-                c.add(Calendar.DAY_OF_MONTH, 1);
-                String time = DateUtils.format(c.getTime(), "yyyy-MM-dd");
-                Date date2 = null;
-                try {
-                    date2 = DateUtils.parse(time, "yyyy-MM-dd");
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    log.warn("日期转换出错！");
-                }
-                CallRecordExample example = new CallRecordExample();
-                example.createCriteria().andIdEqualTo(bo.getId()).andCreateTimeLessThan(date2);
-                isoAtoTimIsSatisfy = callRecordMapper.countByExample(example)>0;
+            boolean isRemoveFlag = false;
+            if("促申完".equals(bo.getUserType())){
+                isRemoveFlag = isRemove(bo.getId(), json, "clc_usr_iso_ato_tim");
+            }else if("促首借".equals(bo.getUserType())){
+                isRemoveFlag = isRemove(bo.getId(), json, "clc_usr_frt_fq_ord_tim");
             }
-            return isTurn || isBlack || isoAtoTimIsSatisfy;
+            return isTurn || isBlack || isRemoveFlag;
         }
         return false;
+    }
+
+    private boolean isRemove(Long id, JSONObject json, String timeField) {
+        boolean isRemoveFlag = false;
+        String clcUsrIsoAtoTim = json.getString(timeField);
+        if (StringUtils.isNotBlank(clcUsrIsoAtoTim)){
+            Date date = json.getDate(timeField);
+            Calendar c = Calendar.getInstance();
+            c.setTime(date);
+            c.add(Calendar.DAY_OF_MONTH, 1);
+            String time = DateUtils.format(c.getTime(), "yyyy-MM-dd");
+            Date date2 = null;
+            try {
+                date2 = DateUtils.parse(time, "yyyy-MM-dd");
+            } catch (ParseException e) {
+                e.printStackTrace();
+                log.warn("日期转换出错！");
+            }
+            CallRecordExample example = new CallRecordExample();
+            example.createCriteria().andIdEqualTo(id).andCreateTimeLessThan(date2);
+            isRemoveFlag = callRecordMapper.countByExample(example)>0;
+        }
+        return isRemoveFlag;
     }
 }
