95504d2112fa2d0a7d0f405ea4a9e58fe5e1b524
juman.wang <juman.wang@brgroup.com>
2022-03-23 17:06:01 +0800

    修改海尔bug

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
@@ -408,19 +408,15 @@ public class PushDataServiceImpl implements PushDataService {
                     HaierReqDTO haierReqDTO = new HaierReqDTO();
                     haierReqDTO.setIds(ids);
                     haierReqDTO.setFormData(formData);
-                    if (datas.size() <= 0) {
-                        return new Result().setCode(ResultCode.SUCCESS.getValue());
-                    }
-                    try {
-                        Result<Response2Entity> response2EntityResult = haierServiceClient.pushToTeleSalesWithIds(haierReqDTO, 0);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    if(datas.size()>0) {
+                        try {
+                            Result<Response2Entity> response2EntityResult = haierServiceClient.pushToTeleSalesWithIds(haierReqDTO, 0);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-
             }
-
-
         }
         return new Result().setCode(ResultCode.SUCCESS.getValue());
     }
