42b2bafd4273cbef0f76467ec240458a576d504e
juman.wang <juman.wang@brgroup.com>
2022-03-30 10:13:30 +0800

    生成实体类

---
+++ marketing-check/src/main/java/com/br/marketing/check/job/YiXinTransferToDxJob.java
@@ -0,0 +1,19 @@
+package com.br.marketing.check.job;
+
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+public class YiXinTransferToDxJob extends AbstractSimpleElasticJob {
+
+    @Override
+    public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
+        /**
+         * 1、
+         */
+    }
+}
--- marketing-cs/src/main/java/com/br/marketing/entity/PhoneSaleExtendInfo.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/PhoneSaleExtendInfo.java
@@ -1,16 +1,11 @@
 package com.br.marketing.entity;
 
-import lombok.Data;
-
-import java.io.Serializable;
 import java.util.Date;
 
-/**
- * b_phone_sale_extend_info
- * @author  lizhen
- */
-@Data
-public class PhoneSaleExtendInfo implements Serializable {
+public class PhoneSaleExtendInfo {
+    /**
+     * 
+     */
     private Long id;
 
     /**
@@ -63,5 +58,156 @@ public class PhoneSaleExtendInfo implements Serializable {
      */
     private Date updateTime;
 
-    private static final long serialVersionUID = 1L;
+    /**
+     * 客户传输节点
+     */
+    private String type;
+
+    /**
+     * 电销节点
+     */
+    private String dxType;
+
+    /**
+     * 推送电销时间
+     */
+    private String pushDxTime;
+
+    /**
+     * 1-实时推送;0-非实时推送
+     */
+    private String transformType;
+
+    /**
+     * 源数据id
+     */
+    private Long sourceId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getApiCode() {
+        return apiCode;
+    }
+
+    public void setApiCode(String apiCode) {
+        this.apiCode = apiCode == null ? null : apiCode.trim();
+    }
+
+    public String getCustNum() {
+        return custNum;
+    }
+
+    public void setCustNum(String custNum) {
+        this.custNum = custNum == null ? null : custNum.trim();
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId == null ? null : taskId.trim();
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType == null ? null : userType.trim();
+    }
+
+    public String getAppletDate() {
+        return appletDate;
+    }
+
+    public void setAppletDate(String appletDate) {
+        this.appletDate = appletDate == null ? null : appletDate.trim();
+    }
+
+    public String getAppletTime() {
+        return appletTime;
+    }
+
+    public void setAppletTime(String appletTime) {
+        this.appletTime = appletTime == null ? null : appletTime.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Integer getpStatus() {
+        return pStatus;
+    }
+
+    public void setpStatus(Integer pStatus) {
+        this.pStatus = pStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getDxType() {
+        return dxType;
+    }
+
+    public void setDxType(String dxType) {
+        this.dxType = dxType == null ? null : dxType.trim();
+    }
+
+    public String getPushDxTime() {
+        return pushDxTime;
+    }
+
+    public void setPushDxTime(String pushDxTime) {
+        this.pushDxTime = pushDxTime == null ? null : pushDxTime.trim();
+    }
+
+    public String getTransformType() {
+        return transformType;
+    }
+
+    public void setTransformType(String transformType) {
+        this.transformType = transformType == null ? null : transformType.trim();
+    }
+
+    public Long getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(Long sourceId) {
+        this.sourceId = sourceId;
+    }
 }
