f5f86c7f504fc594a0c46a7cb234a78c483b08fc
juanjuan.song <juanjuan.song@brgroup.com>
2022-03-31 15:56:28 +0800

    getRequestTime

--- marketing-cs/src/main/java/com/br/marketing/rule/yixin/YiXinRealtimeTransferToCustomer.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/yixin/YiXinRealtimeTransferToCustomer.java
@@ -57,7 +57,7 @@ public class YiXinRealtimeTransferToCustomer implements AssembleData<ConversionD
         conversionData.setTransformType("1");
         conversionData.setTaskId(context.getTransferInfoId().toString());
         //生效截止时间
-        LocalDate requestDate = LocalDateTime.parse(transfer.getRequestData(), dateTimeFormatter).toLocalDate();
+        LocalDate requestDate = LocalDateTime.parse(transfer.getRequestTime(), dateTimeFormatter).toLocalDate();
         LocalDate plusDays = requestDate.plusDays(6);
         conversionData.setExpireDate(plusDays + " 23:59:59");
 
