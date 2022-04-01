56c656c6985a8076391953dc34a379c732cc9c70
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-04-01 11:45:06 +0800

    华强：宜信2.0转化推送逻辑新增-营销→客服 * 修改注释中message json结构中去掉source字段

--- marketing-cs/src/main/java/com/br/marketing/origin/impl/TransferDataSetImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/origin/impl/TransferDataSetImpl.java
@@ -34,9 +34,9 @@ public class TransferDataSetImpl implements OriginDataService {
     public List<Object> collect(MqFact mqFact, ProcessHandlerContext context) {
         /* 2022/3/31 14:47
          * message结构：
-         * {"last": 0,"tcId": tcid,"ids": [id,id1],"source": 5,"apiCode": "code"}
+         * {"last": 0,"tcId": tcid,"ids": [id,id1],"apiCode": "code"}
          * eg:
-         * {"last": 0,"tcId": 772,"ids": [607772,607771,607770,607769,607768],"source": 5,"apiCode": "7410430"}
+         * {"last": 0,"tcId": 772,"ids": [607772,607771,607770,607769,607768],"apiCode": "7410430"}
          */
         final JSONObject jsonObject = JSONObject.parseObject(mqFact.getMessage());
         String last = jsonObject.getString("last");
