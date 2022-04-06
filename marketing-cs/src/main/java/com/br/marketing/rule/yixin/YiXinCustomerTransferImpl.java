24011666333322d2460987fd6d3dd7205584e8b8
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-04-06 16:28:54 +0800

    华强：宜信2.0转化推送逻辑新增-营销→客服 * 添加上下文标签

--- marketing-cs/src/main/java/com/br/marketing/rule/yixin/YiXinCustomerTransferImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/yixin/YiXinCustomerTransferImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.br.common.util.DateUtils;
 import com.br.marketing.client.robotaiapi.input.ConversionData;
 import com.br.marketing.context.ProcessHandlerContext;
+import com.br.marketing.context.RuleDataCollectionEnum;
 import com.br.marketing.entity.MarketingTransferSyncUser;
 import com.br.marketing.rule.AssembleData;
 import com.br.marketing.strategy.InterfaceHandlerEnum;
@@ -78,6 +79,6 @@ public class YiXinCustomerTransferImpl implements AssembleData<ConversionData> {
 
     @Override
     public Integer ruleDataCollection() {
-        return null;
+        return RuleDataCollectionEnum.CUSTOMER_TRANSFER_DATA_COLLECTION.getCode();
     }
 }
