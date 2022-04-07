e529adcb50e1e36b35fd7517c224ec18fdbb949b
张广超 <50570550@qq.com>
2022-04-07 19:50:47 +0800

    Subject：<验证码样式>

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/auth/CaptchaController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/auth/CaptchaController.java
@@ -2,12 +2,10 @@ package com.br.marketing.innerapi.controller.auth;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.CircleCaptcha;
-import cn.hutool.captcha.LineCaptcha;
 import com.alibaba.fastjson.JSONObject;
 import com.br.cloud.web.MethodType;
 import com.br.cloud.web.PrometheusTimeMethod;
 import com.br.marketing.client.RedisAuthService;
-import com.br.marketing.client.RedisChgService;
 import com.br.marketing.common.commondto.ApiResult;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +14,6 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * -------------------------------
@@ -52,7 +48,7 @@ public class CaptchaController {
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS)
     public ApiResult<JSONObject> captcha(HttpSession session) {
         //定义图形验证码的长、宽、验证码字符数、干扰元素个数
-        LineCaptcha captcha = CaptchaUtil.createLineCaptcha(206, 41, 4, 20);
+        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(206, 41, 4, 0);
         String code = captcha.getCode();
         String image = captcha.getImageBase64();
         JSONObject jsonObject = new JSONObject();
