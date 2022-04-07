af8fb12e6e09f2389b07914f69e40b78c2dac501
张广超 <50570550@qq.com>
2022-04-07 15:40:05 +0800

    Subject：<产管数据类型>

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/auth/CustomerStrategyController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/auth/CustomerStrategyController.java
@@ -33,41 +33,58 @@ public class CustomerStrategyController {
             "apiCode={apiCode}&strategyCategory={strategyCategory}&distributeType={distributeType}&strategyType={strategyType}";
 
     @Value("${api.productManagement.url}")
-    private  String productUrl;
+    private String productUrl;
+
+    @Value("${api.productManagement.productTypeUrl}")
+    private String productTypeUrl;
+
     @GetMapping("distributeList")
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS)
-    public JSONObject distributeList(String apiCode,String strategyCategory,String distributeType,String strategyType) {
+    public JSONObject distributeList(String apiCode, String strategyCategory, String distributeType, String strategyType) {
         Map<String, Object> urlVariables = getStringObjectMap(apiCode, strategyCategory, distributeType, strategyType);
         String result = restTemplate.getForObject(STRATEGY_DISTRIBUTION_LIST, String.class, urlVariables);
         return (JSONObject) JSONObject.parse(result);
     }
+
     //productChineseName、productName、secondTypeName、spreadStatus、version、versions
     @GetMapping("createView")
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS)
     public ApiResult<Map<String, Object>> createView() {
         String result = restTemplate.getForObject(productUrl, String.class);
+        String productType = restTemplate.getForObject(productTypeUrl, String.class);
+        JSONObject productTypeJson = (JSONObject) JSONObject.parse(productType);
+        JSONArray productTypeData = productTypeJson.getJSONArray("data");
         JSONObject jsonObject = (JSONObject) JSONObject.parse(result);
         JSONArray datArray = jsonObject.getJSONArray("data");
         List<Map<String, Object>> dataList = new ArrayList<>();
         Set<String> productSet = new TreeSet<>();
 
-        for(int i=0;i<datArray.size();i++){
+        for (int i = 0; i < datArray.size(); i++) {
             Map<String, Object> productMap = new HashMap<>();
             String productionChineseName = datArray.getJSONObject(i).getString("productionChineseName");
             String productName = datArray.getJSONObject(i).getString("productionName");
-            String secondTypeName = datArray.getJSONObject(i).getString("productionTypeCode");
+            String productionTypeCode = datArray.getJSONObject(i).getString("productionTypeCode");
             String spreadStatus = datArray.getJSONObject(i).getString("spreadStatus");
             String version = datArray.getJSONObject(i).getString("version");
-            productMap.put("productionChineseName",productionChineseName);
-            productMap.put("productName",productName);
-            productMap.put("secondTypeName",secondTypeName);
-            productMap.put("spreadStatus",spreadStatus);
-            productMap.put("versions",version);
+            productMap.put("productChineseName", productionChineseName);
+            productMap.put("productName", productName);
+            productMap.put("spreadStatus", spreadStatus);
+            productMap.put("versions", version);
+            for (int j = 0; j < productTypeData.size(); j++) {
+                JSONObject jsonObject1 = productTypeData.getJSONObject(j);
+                String typeCode = jsonObject1.getString("typeCode");
+                String secondType = jsonObject1.getString("secondType");
+                if(productionTypeCode.equals(typeCode)){
+                    productMap.put("secondTypeName", secondType);
+                    productMap.put("secondTypeCode", typeCode);
+                }
+            }
             dataList.add(productMap);
             productSet.add(productName);
+
         }
         List<Map<String, Object>> resultList = new ArrayList<>();
-        for(String productSetName : productSet){
+        for (String productSetName : productSet) {
             List<String> versionList = new ArrayList<>();
             Map<String, Object> productMap = new HashMap<>();
             for (Map<String, Object> stringObjectMap : dataList) {
@@ -78,13 +95,14 @@ public class CustomerStrategyController {
                     productMap = stringObjectMap;
                 }
             }
-            productMap.put("versions",versionList);
+            productMap.put("versions", versionList);
             resultList.add(productMap);
         }
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("data",resultList);
+        resultMap.put("data", resultList);
         return new ApiResult<Map<String, Object>>().success(resultMap);
     }
+
     private Map<String, Object> getStringObjectMap(String apiCode, String strategyCategory, String distributeType, String strategyType) {
         Map<String, Object> urlVariables = new HashMap<>();
         urlVariables.put("apiCode", apiCode);
