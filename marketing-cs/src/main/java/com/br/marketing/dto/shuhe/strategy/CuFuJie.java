240a02443b4aa64df92be8ec8c42935fb584c9d1
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-04-13 15:39:31 +0800

    华强：数禾转化落库变更需求 * 添加促复借

---
+++ marketing-cs/src/main/java/com/br/marketing/dto/shuhe/strategy/CuFuJie.java
@@ -0,0 +1,67 @@
+package com.br.marketing.dto.shuhe.strategy;
+
+import com.br.marketing.client.dassservice.input.userdata.DassSingleImportDataDTO;
+import com.br.marketing.entity.CaseShuheUser;
+import com.br.marketing.service.IMarketingSyncUserService;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * 促复借 场景
+ *
+ * @author Guo Zeqiang
+ * @dateTime 2022/4/12 15:14
+ */
+public class CuFuJie extends IUserType {
+
+    public CuFuJie(String... api2Codes) {
+        super(api2Codes);
+        super.apiCodes.add("3710043");
+    }
+
+    @Override
+    protected void getCaseUser(Map<String, String> dataItem, CaseShuheUser caseUser) {
+
+    }
+
+    @Override
+    public boolean ifTransfer(CaseShuheUser caseShuheUser, Date creatTime) {
+        return false;
+    }
+
+    @Override
+    public boolean dataPeriodOfValidity(CaseShuheUser caseShuheUser, IMarketingSyncUserService iMarketingSyncUserService, Date creatTime) {
+        return false;
+    }
+
+    @Override
+    public boolean dataPeriodOfValidity(IMarketingSyncUserService iMarketingSyncUserService, Date creatTime) {
+        return false;
+    }
+
+    @Override
+    public boolean dataPeriodOfValidity(IMarketingSyncUserService iMarketingSyncUserService, Date tCreatTime, Date creatTime) {
+        return false;
+    }
+
+    @Override
+    public String getBlackExpireDate(Date creatTime) {
+        return null;
+    }
+
+    @Override
+    public boolean ifGiveUp(CaseShuheUser caseShuheUser, Date creatTime) {
+        return false;
+    }
+
+    @Override
+    public void getPrivateInfo(DassSingleImportDataDTO dataDTO) {
+
+    }
+
+    @Override
+    public boolean isSatisfyPhoneSale(CaseShuheUser caseShuheUser, Date creatTime) {
+        return false;
+    }
+}
