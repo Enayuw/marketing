e55122dcf284fd4714421f295228d96a59905ab5
juman.wang <juman.wang@brgroup.com>
2022-04-14 17:10:13 +0800

    添加日志

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/YiXinTransferServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/YiXinTransferServiceImpl.java
@@ -252,12 +252,17 @@ public class YiXinTransferServiceImpl implements IYiXinTransferService {
 
                     //region 黑名单查询
                     HashMap<String, String> blackData = new HashMap<>();
+                    //todo 上线删除
+                    if(log.isWarnEnabled()){
+                        List<Long> collect = dataFilter2.stream().map(t -> t.getId()).collect(Collectors.toList());
+                        log.warn(String.format("黑名单查询总数据 pushUid：%s,线程数：%d,黑名单数据：%s",pushUid,_threadValue, JSON.toJSONString(collect)));
+                    }
                     List<List<MarketingTransferSyncUser>> partition = Lists.partition(dataFilter2, 500);
                     for (List<MarketingTransferSyncUser> marketingTransferSyncUsers : partition) {
                         //todo 上线删除
                         if(log.isWarnEnabled()){
                             List<Long> collect = marketingTransferSyncUsers.stream().map(t -> t.getId()).collect(Collectors.toList());
-                            log.warn(String.format("黑名单查询 pushUid：%s,线程数：%d,黑名单数据：%s",pushUid,_threadValue, JSON.toJSONString(collect)));
+                            log.warn(String.format("黑名单查询分页数据 pushUid：%s,线程数：%d,黑名单数据：%s",pushUid,_threadValue, JSON.toJSONString(collect)));
                         }
                         Result<Map<String, String>> result = iDxService.getBlackByTransfer(marketingTransferSyncUsers, _tApicode);
                         if (ResultCode.SUCCESS.getValue().equals(result.getCode())) {
