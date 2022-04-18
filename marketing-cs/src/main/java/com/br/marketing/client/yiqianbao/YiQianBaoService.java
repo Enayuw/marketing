fbc3905a011dcd578a45e8343a629a83b4258a05
zhen.li1 <zhen.li1@brgroup.com>
2022-04-18 15:13:06 +0800

    fix:删除日志

--- marketing-cs/src/main/java/com/br/marketing/client/yiqianbao/YiQianBaoService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/yiqianbao/YiQianBaoService.java
@@ -82,7 +82,6 @@ public class YiQianBaoService {
                 }
                 ResponseYqbDTO content = JSON.parseObject(RSAUtil.decrypt(jsonResult.getString("bizContent"), brPrivateKey), new TypeReference<ResponseYqbDTO>() {
                 }.getType());
-                log.warn("壹钱包调用返回 response={}", JSON.toJSONString(content));
                 return new Result<>().setCode(ResultCode.SUCCESS.getValue()).setDate(content);
             } else {
                 return new Result<>().setCode(ResultCode.FAIL.getValue());
