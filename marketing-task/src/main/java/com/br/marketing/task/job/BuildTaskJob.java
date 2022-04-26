9a23c05e11f58a160d0dc7289a88166775195de4
juman.wang <juman.wang@brgroup.com>
2022-04-26 19:10:06 +0800

    暂存

--- marketing-task/src/main/java/com/br/marketing/task/job/BuildTaskJob.java
+++ marketing-task/src/main/java/com/br/marketing/task/job/BuildTaskJob.java
@@ -8,8 +8,17 @@ import org.springframework.stereotype.Component;
 public class BuildTaskJob  extends AbstractSimpleElasticJob {
 
 
+    /**
+     * 生成自动任务
+     * 1、获取有效跑分并且规则类型condition_type=1规则
+     * 2、判断当前规则的时间是否符合
+     * 3、判断当前是否有符合的数据
+     *
+     * @param jobExecutionMultipleShardingContext
+     */
     @Override
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
-
+        //region
+        //endregion
     }
 }
