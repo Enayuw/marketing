3aa9374571f3fd9205c0144d6d92dcc53e26edcf
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-04-26 14:43:32 +0800

    华强：促复借自动化转daas + 添加delayTime属性

--- marketing-cs/src/main/java/com/br/marketing/origin/MqFact.java
+++ marketing-cs/src/main/java/com/br/marketing/origin/MqFact.java
@@ -52,13 +52,19 @@ public class MqFact extends InterfaceParams {
     private Set<String> includeRules;
 
     /**
-     *  mq中消息内容
+     * mq中消息内容
      */
     private String message;
 
     /**
-     *  是否为延迟队列的消息 1:是
+     * 是否为延迟队列的消息 1:是
      */
     private Integer isDelay;
+
+    /**
+     * 延迟时间；单位小时
+     * eg:1或0.5
+     */
+    private Float delayTime;
 
 }
