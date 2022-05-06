334cf2fbd512fc45b7c861102da8a05cab494461
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-05-06 18:16:43 +0800

    华强：促复借自动化转Daas * 修改配置名称

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -151,7 +151,7 @@ public class MarketingCommonConfig {
     /**
      * 数禾推送电销情况;eg:{"促复借":["a","b","c"],"促首借":["a","b"]}
      */
-    private HashMap<String, List<String>> shuHePushDXSituationMap;
+    private HashMap<String, List<String>> shuHePushDXStatusMap;
 
     /**
      * 数禾用户可用额度区间;大于:>、小于:<、等于:==、不等于:!=、大于等于:>=、小于等于:<=;eg:[">=","1","<=","100"]
