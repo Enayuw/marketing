3b39c4a9e1fc2f8399167e6e309c891abf104b25
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-05-12 11:53:49 +0800

    华强：促复借自动化转Daas * 去掉等于条件

--- marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeArtificialRealTimeUserDataFromDelayImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeArtificialRealTimeUserDataFromDelayImpl.java
@@ -389,7 +389,7 @@ public class ShuHeArtificialRealTimeUserDataFromDelayImpl implements AssembleDat
         ShuheTransferStopPushRecordExample recordExample = new ShuheTransferStopPushRecordExample();
         recordExample.createCriteria().andApiCodeEqualTo(transfer.getApiCode())
                 .andCaseNumEqualTo(transfer.getCustNum()).andUserTypeEqualTo(transfer.getUserType())
-                .andFailureTimeGreaterThanOrEqualTo(ObjectUtils.isEmpty(transfer.getCreateTime())
+                .andFailureTimeGreaterThan(ObjectUtils.isEmpty(transfer.getCreateTime())
                         ? new Date() : transfer.getCreateTime()).andStatusEqualTo("b").andChannelEqualTo(0);
         int count = shuheTransferStopPushRecordMapper.countByExample(recordExample);
         return count > 0;
