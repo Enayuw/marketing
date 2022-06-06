807975b3862a41f55539ae6d1d7a45eb3988f254
张广超 <50570550@qq.com>
2022-06-06 15:52:43 +0800

    Subject：<跑分优化跑分状态 任务优先级>

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingTaskServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingTaskServiceImpl.java
@@ -122,6 +122,7 @@ public class MarketingTaskServiceImpl implements MarketingTaskService {
         if (StringUtils.isNotEmpty(search) && search.contains("_")) {
             search = search.replace("_", "\\_");
         }
+
         PageHelper.startPage(current, size);
         List<MarketingTaskVO> fastTaskRuleListVOS = marketingTaskMapper.selectList(search, status,
                 createTimeStart, createTimeEnd, updateTimeStart, updateTimeEnd, taskStatus, null);
@@ -136,7 +137,7 @@ public class MarketingTaskServiceImpl implements MarketingTaskService {
         marketingTask.setId(Long.valueOf(id));
         Integer exist = marketingTaskMapper.selectByPriority(priority);
         if (exist > 0) {
-            throw new AppException(CodeEnum.TASK_PRIORITY_EXIST);
+            return new ApiResult<Boolean>().fail(CodeEnum.TASK_PRIORITY_EXIST.getMessage());
         }
         marketingTaskMapper.updateByPrimaryKeySelective(marketingTask);
         return new ApiResult<Boolean>().success(true);
