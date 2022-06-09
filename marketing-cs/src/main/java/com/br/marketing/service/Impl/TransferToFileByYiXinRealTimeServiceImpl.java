1f516772ea39471452a92e76f09032d4d20a93ca
juanjuan.song <juanjuan.song@brgroup.com>
2022-06-09 10:30:58 +0800

    宜信hist文件数量统计bug

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/TransferToFileByYiXinRealTimeServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/TransferToFileByYiXinRealTimeServiceImpl.java
@@ -681,7 +681,7 @@ public class TransferToFileByYiXinRealTimeServiceImpl implements ITransferToFile
                 fw.append(sb.toString());
             }
             dataFilter.clear();
-            totalSize = resultFilter.size();
+            totalSize = totalSize + resultFilter.size();
             resultFilter.clear();
             data.clear();
         }
