e2458663376c62513a56e9f03bc6b6bcc1031e35
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-07-01 08:59:16 +0800

    华强：T20220616转化记录 + 查询接口

--- marketing-cs/src/main/java/com/br/marketing/mapper/TransferSyncReportMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/TransferSyncReportMapper.java
@@ -1,7 +1,11 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.TransferSyncReport;
+import com.br.marketing.vo.TransferSyncReportVO;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Mapper
@@ -13,6 +17,11 @@ public interface TransferSyncReportMapper extends TransferSyncReportMapperBase {
     /**
      * 获取萨摩耶开始时间、结束时间、数据量
      * 2022/6/29 19:26
+     *
+     * @param apiCode  apiCode
+     * @param dateStr  日期字符串
+     * @param userType userType
+     * @return TransferSyncReport
      */
     TransferSyncReport dateTimeMinMaxCountSMY(@Param("apiCode") String apiCode, @Param("dateStr") String dateStr
             , @Param("userType") String userType);
@@ -20,8 +29,31 @@ public interface TransferSyncReportMapper extends TransferSyncReportMapperBase {
     /**
      * 获取开始时间、结束时间、数据量
      * 2022/6/29 19:26
+     *
+     * @param tCid     tCid
+     * @param apiCode  apiCode
+     * @param dateStr  日期字符串
+     * @param userType userType
+     * @return TransferSyncReport
      */
     TransferSyncReport dateTimeMinMaxCount(@Param("tCid") String tCid, @Param("apiCode") String apiCode
             , @Param("dateStr") String dateStr, @Param("userType") String userType);
 
+    /**
+     * 转化数据统计报表列表
+     * 2022/6/29 19:26
+     *
+     * @param params params
+     * @return List
+     */
+    List<TransferSyncReportVO> selectList(Map<String, Object> params);
+
+    /**
+     * 转化数据统计报表总计
+     * 2022/6/29 19:26
+     *
+     * @param params params
+     * @return long
+     */
+    long getReportListTotal(Map<String, Object> params);
 }
