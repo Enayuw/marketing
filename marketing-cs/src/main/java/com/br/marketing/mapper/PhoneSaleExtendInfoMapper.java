77dc16a7902be40e37531311d90e3a9b5a8641c1
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-07-15 18:14:02 +0800

    华强：数禾促申完失效数据过滤（3710004） + 查询接口

--- marketing-cs/src/main/java/com/br/marketing/mapper/PhoneSaleExtendInfoMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/PhoneSaleExtendInfoMapper.java
@@ -33,9 +33,30 @@ public  interface PhoneSaleExtendInfoMapper extends PhoneSaleExtendInfoMapperBas
 
     /**
      * 手机号是否重复
+     *
      * @param phone
      * @param dxType
      * @param date
      */
-    int countByPhoneAndType(@Param("phone")String phone,@Param("dxType")String dxType,@Param("date")String date);
+    int countByPhoneAndType(@Param("phone") String phone, @Param("dxType") String dxType, @Param("date") String date);
+
+    /**
+     * 2022/7/13 17:36
+     * 获取推送电销人工电销的记录
+     *
+     * @param apiCode       apiCode
+     * @param userType      场景
+     * @param startDateTime 开始时间 闭
+     * @param endDateTime   结束时间 开
+     * @param pageNum       页号
+     * @param pageSize      页大小
+     * @return list
+     */
+    List<PhoneSaleExtendInfo> findPushPhoneSaleListPage(
+            @Param("apiCode") String apiCode,
+            @Param("userType") String userType,
+            @Param("startDateTime") String startDateTime,
+            @Param("endDateTime") String endDateTime,
+            @Param("pageNum") int pageNum,
+            @Param("pageSize") int pageSize);
 }
