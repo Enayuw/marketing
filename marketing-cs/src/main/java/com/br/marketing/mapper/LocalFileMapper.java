1994141aaa3c79e2dc0b9f683539434b8f24e641
张广超 <50570550@qq.com>
2022-09-15 17:52:05 +0800

    Subject：<修复查询历史bug>

--- marketing-cs/src/main/java/com/br/marketing/mapper/LocalFileMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/LocalFileMapper.java
@@ -2,6 +2,7 @@ package com.br.marketing.mapper;
 
 
 
+import com.br.marketing.mysqlInterceptor.AddDataAuth;
 import com.br.marketing.vo.LocalFileVo;
 import org.apache.ibatis.annotations.Param;
 
@@ -11,6 +12,7 @@ public interface LocalFileMapper extends LocalFileMapperBase {
 
     Integer insertFileData(@Param("insertSql") String insertSql);
 
+    @AddDataAuth
     List<LocalFileVo> selectList(@Param("search")String search,
                                  @Param("apiCode")String apiCode,
                                  @Param("uploadStartTime")String uploadStartTime,
