8135b400e832e0466a97157024a37edb2d5949a7
juanjuan.song <juanjuan.song@brgroup.com>
2022-09-22 13:57:10 +0800

    测试接口

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/PingController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/PingController.java
@@ -1,10 +1,7 @@
 package com.br.marketing.innerapi.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.br.common.log.AlertLog;
 import com.br.marketing.client.AlarmApiClient;
-import com.br.marketing.common.utils.Constants;
-import com.br.marketing.common.utils.StringUtils;
 import com.br.marketing.common.utils.net.IpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +19,9 @@ import javax.annotation.Resource;
 @RequestMapping("/ping")
 public class PingController {
 
-    @Value("${otherConfig.alarm.secretKey:00}")
-    private String secretKey;
-    @Value("${otherConfig.alarm.appName:00}")
-    private String appName;
+    @Resource
+    private AlarmApiClient alarmClient;
+
 
     private static final Logger log = LoggerFactory.getLogger(PingController.class);
 
@@ -45,27 +41,14 @@ public class PingController {
         json.put("host", IpUtil.getHostName());
         json.put("serverName", "MARKETING-INNER-API");
         json.put("message", "测试报警接口");
-        sendAlarm(json.toString(),"调用了测试报警接口,请忽略~",appName,secretKey, "1001");
+        alarmClient.sendAlarm(json.toString(),"调用了测试报警接口,请忽略~",null,null, "1001");
         return "111";
     }
 
-    private void sendAlarm(String content, String title, String appName, String secretKey, String exceptionCode) {
-        String enviroment= "预发";
-        String hostName = IpUtil.getHostName();
-        if(StringUtils.isNotEmpty(title)){
-            title="【"+enviroment+"】"+hostName +title;
-        }else{
-            title ="【"+enviroment+"】"+hostName+ JSONObject.parseObject(content).getString("serverName");
-        }
-        try{
-            //生成打告警标签的消息内容
-            //其中 AlertLog 只是为日志的内容添加了标签，打印日志时还是按照正常方式输入参数、异 常。
-            String msg = AlertLog.buildWarnMessage(exceptionCode, content, title);
-            log.warn(msg, 10, new RuntimeException("调用了测试报警接口,请忽略~"));
-        }catch (Exception e){
-            String msg = AlertLog.buildWarnMessage(exceptionCode, content, title);
-            log.warn(msg,10, new RuntimeException("调用新报警发生异常！"+e));
-        }
+    @GetMapping("/logErrorTest")
+    public String logErrorTest() {
+        log.error("调用了测试报警接口,请忽略~");
+        return "log.error()";
     }
 
 
