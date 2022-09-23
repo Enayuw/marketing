de9e849fcae8a0e98402233d95c9d40f00a4d821
张广超 <50570550@qq.com>
2022-09-23 10:59:07 +0800

    Subject：<apiCode 搜索>

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/FileUploadHistoryController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/FileUploadHistoryController.java
@@ -5,7 +5,6 @@ import com.br.marketing.commonentity.PageResultReturn;
 import com.br.marketing.mysqlInterceptor.AddDataAuthBusiness;
 import com.br.marketing.service.LocalFileService;
 import io.swagger.annotations.Api;
-import io.swagger.models.auth.In;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,7 +31,7 @@ import javax.annotation.Resource;
 public class FileUploadHistoryController {
 
     @Resource
-    LocalFileService localfileService;
+    private LocalFileService localfileService;
 
     @GetMapping("/list")
     @AddDataAuthBusiness
@@ -44,8 +43,9 @@ public class FileUploadHistoryController {
             , @RequestParam(required = false) String uploadTimeEnd
             , @RequestParam(required = false) String fileType
     ) {
-        return new ApiResult<PageResultReturn>().success(localfileService.list(current, size, search,apiCode, uploadTimeStart,uploadTimeEnd,fileType));
+        return new ApiResult<PageResultReturn>().success(localfileService.list(current, size, search, apiCode, uploadTimeStart, uploadTimeEnd, fileType));
     }
+
     @GetMapping("/allCount")
     @AddDataAuthBusiness
     public ApiResult<Integer> allCount() {
