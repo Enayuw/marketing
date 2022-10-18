74f054f279288e0ae5d63174ee438dce371510b2
张广超 <50570550@qq.com>
2022-10-18 10:03:46 +0800

    Subject：<携程提速>

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushDataServiceImpl.java
@@ -36,6 +36,7 @@ import com.br.marketing.entity.*;
 import com.br.marketing.mapper.*;
 import com.br.marketing.rabbitmq.RabbitMqProducter;
 import com.br.marketing.service.PushDataService;
+import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.google.common.collect.Lists;
@@ -48,6 +49,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -56,6 +58,8 @@ import java.util.*;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import static com.br.marketing.util.TimeUtils.isEffectiveDate;
 
 @Slf4j
 @Service
@@ -138,6 +142,9 @@ public class PushDataServiceImpl implements PushDataService {
 
     @Autowired
     XieChengService xieChengService;
+
+    @Autowired
+    MarketingCommonConfig marketingCommonConfig;
 
 
     final static DateTimeFormatter yyyyMMddDF = DateTimeFormatter.ofPattern("yyyyMMdd");
@@ -870,47 +877,56 @@ public class PushDataServiceImpl implements PushDataService {
 
     @Override
     public Result pushXieChengToDbData(Long id) {
-        LocalFile localFile = localFileMapper.selectByPrimaryKey(id);
-        if (localFile == null) {
-            return new Result().setCode(ResultCode.SUCCESS.getValue()).setMessage("文件不存在");
-        }
-        //携程推送营销数据
-        if ("xiecheng".equals(localFile.getFileType())) {
-            localFile.setPushStartTime(localFile.getPushStartTime()==null?new Date():localFile.getPushStartTime());
+        Integer xiechengDateSendThread = marketingCommonConfig.getXiechengDateSendThread();
+        ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(xiechengDateSendThread, xiechengDateSendThread);
+        // 判断当前时间是否在 9:30~20:00之间
+        String format = "HH:mm:ss";
+        try {
+            LocalFile localFile = localFileMapper.selectByPrimaryKey(id);
+            if (localFile == null) {
+                return new Result().setCode(ResultCode.SUCCESS.getValue()).setMessage("文件不存在");
+            }
             Boolean actionMark = true;
+            localFile.setPushStartTime(localFile.getPushStartTime()==null?new Date():localFile.getPushStartTime());
             Integer pushCount = 0;
             while (actionMark) {
-
+                Date date = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                Date nowTime = new SimpleDateFormat(format).parse(sdf.format(date));
+                Date startTime = new SimpleDateFormat(format).parse("09:00:00");
+                Date endTime = new SimpleDateFormat(format).parse("20:00:00");
+                actionMark = isEffectiveDate(nowTime, startTime, endTime);
+                if (!actionMark) continue;
                 List<XieChengData> xieChengDatalist = xieChengDataMapper.selectByLocalId(id);
-                pushCount = pushCount+xieChengDatalist.size();
-                if (xieChengDatalist.size() <= 0) {
+                if (xieChengDatalist.size() == 0) {
                     actionMark = false;
                     continue;
                 }
                 for (int i = 0; i < xieChengDatalist.size(); i++) {
-                    try {
-                        Thread.sleep(500L);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
                     XieChengData xieChengData = xieChengDatalist.get(i);
-                    String result = xieChengService.pushXieChengData(xieChengData);
-                    JSONObject resultJson = JSONObject.parseObject(result);
-                    Integer code = resultJson.getInteger("code");
-                    XieChengData resultData = new XieChengData();
-                    resultData.setId(xieChengData.getId());
-                    if (code == 0) {
-                        resultData.setPushStatus(2);
-                    }else {
-                        resultData.setPushStatus(3);
-                    }
-                    resultData.setDataMessage(result);
-                    xieChengDataMapper.updateByPrimaryKeySelective(resultData);
+                    pushCount = pushCount + xieChengDatalist.size();
+                    threadPool.submit(() -> {
+                        String result = xieChengService.pushXieChengData(xieChengData);
+                        JSONObject resultJson = JSONObject.parseObject(result);
+                        Integer code = resultJson.getInteger("code");
+                        XieChengData resultData = new XieChengData();
+                        resultData.setId(xieChengData.getId());
+                        if (code == 0) {
+                            resultData.setPushStatus(2);
+                        } else {
+                            resultData.setPushStatus(3);
+                        }
+                        resultData.setDataMessage(result);
+                        xieChengDataMapper.updateByPrimaryKeySelective(resultData);
+                    });
+
                 }
             }
             localFile.setPushEndTime(new Date());
             localFile.setPushNumber(localFile.getPushNumber()+pushCount);
             localFileMapper.updateByPrimaryKeySelective(localFile);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return new Result().setCode(ResultCode.SUCCESS.getValue()).setDate(Boolean.FALSE);
     }
