6955aacd40f7b0c13a30769c29a739c14b5063ab
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-10-18 11:43:49 +0800

    华强：D20220916宜信拒贷数据逻辑处理-3710012 * 修改 sql

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncUserMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncUserMapper.java
@@ -1,7 +1,6 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.MarketingSyncUser;
-import com.br.marketing.entity.MarketingTransferSyncUser;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -76,6 +75,6 @@ public interface MarketingSyncUserMapper {
      * @return list
      */
     List<MarketingSyncUser> getCellByCustNumsAndMaxCreateTime(@Param("apiCode") String apiCode
-            , @Param("list") List<MarketingTransferSyncUser> list);
+            , @Param("set") Set<String> set);
 
 }
