efd975029789bfa8bbc7f911aa49453df25233a2
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-10-20 19:55:04 +0800

    华强：D20221012桔子周期性自动化转Daas-3710037 * 添加SQL

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferSyncUserMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferSyncUserMapper.java
@@ -188,4 +188,24 @@ public interface MarketingTransferSyncUserMapper extends MarketingTransferSyncUs
             , @Param("caseEffective") String caseEffective
             , @Param("custNumSet") Set<String> custNumSet
     );
+
+    /**
+     * 2022/10/20 10:36
+     * 获取周期性推送dass的转化数据
+     *
+     * @param cid      cid
+     * @param apiCode  apiCode
+     * @param dateSet  日期集合
+     * @param status   情况
+     * @param pageNum  页号
+     * @param pageSize 页大小
+     * @return list
+     */
+    List<MarketingTransferSyncUser> findOrangeCyclicalTransferSyncPage(
+            @Param("cid") String cid,
+            @Param("apiCode") String apiCode,
+            @Param("dateSet") Set<String> dateSet,
+            @Param("status") String status,
+            @Param("pageNum") int pageNum,
+            @Param("pageSize") int pageSize);
 }
