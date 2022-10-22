fb5f5bb1546a2ff5208658149315c284cdfa15b5
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-10-22 11:30:58 +0800

    华强：D20221020数禾促首登apicode切换-3710071 * 促首登apicode切换-3710071

--- marketing-cs/src/main/java/com/br/marketing/dto/shuhe/strategy/CuShouDeng.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/shuhe/strategy/CuShouDeng.java
@@ -22,7 +22,8 @@ public class CuShouDeng extends IUserType {
 
     public CuShouDeng(String... api2Codes) {
         super(api2Codes);
-        super.apiCodes.add("3710004");
+        super.apiCodes.add("3710071");
+        super.apiCodes.add("7410875");
     }
 
     @Override
