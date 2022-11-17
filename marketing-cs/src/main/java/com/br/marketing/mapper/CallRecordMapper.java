fce737708c54987629714ded2beff6e4989e2bf9
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-11-17 18:22:28 +0800

    华强：D20221108众安信贷代运营营销明细 + 拨打明细查询

--- marketing-cs/src/main/java/com/br/marketing/mapper/CallRecordMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/CallRecordMapper.java
@@ -6,7 +6,15 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public interface CallRecordMapper extends CallRecordMapperBase {
     List<CallRecord> getLastCallRecordByCustNum(@Param("custNums") Collection<String> custNums, @Param("cid") String cid);
+
+    /**
+     * 2022/11/17 10:51
+     * 根据案件编号+外呼开始时间判断
+     */
+    Set<String> getBlackListSet(@Param("custNumSet") Set<String> custNumSet
+            , @Param("apiCode") String apiCode, @Param("callStartTimeStr") String callStartTimeStr);
 }
