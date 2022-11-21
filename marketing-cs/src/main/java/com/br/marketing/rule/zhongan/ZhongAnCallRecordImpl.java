7871bcfc724aa15683271c85555872ec46d24bad
juanjuan.song <juanjuan.song@brgroup.com>
2022-11-21 11:47:27 +0800

    加日志

--- marketing-cs/src/main/java/com/br/marketing/rule/zhongan/ZhongAnCallRecordImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/zhongan/ZhongAnCallRecordImpl.java
@@ -26,6 +26,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Set;
 
 /**
  * 众安拨打明细入库规则
@@ -87,6 +88,8 @@ public class ZhongAnCallRecordImpl implements AssembleData<ZaRosterLockingDataDT
             if(bo.getDetail() != null && bo.getDetail().getCallStatus() != null && 12 == bo.getDetail().getCallStatus()){
                 //黑名单
                 custNumCache(bo.getCaseNum());
+                Set<String> smembers = redisChgService.smembers(RedisKeyConstant.zhongAnblackCusNumToday);
+                log.warn("众安拨打明细黑名单redis数据："+ smembers);
                 return flag;
             }
             //上传表获取手机号，转为md5加密
