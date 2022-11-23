8afbbbbe694255a715734585b6085b25d6270cba
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-11-23 18:05:17 +0800

    华强：D20221108众安信贷代运营营销明细 - 方法

--- marketing-cs/src/main/java/com/br/marketing/mapper/ZhonganRosterLockingDataMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/ZhonganRosterLockingDataMapper.java
@@ -64,13 +64,6 @@ public interface ZhonganRosterLockingDataMapper extends ZhonganRosterLockingData
 
     /**
      * 2022/11/16 16:54
-     * 统计文件未推送数据量
-     */
-    List<SftpFilePushSuccessDTO> getSftpFilePushFailSum(@Param("apiCode") String apiCode
-            , @Param("dateStr") String dateStr);
-
-    /**
-     * 2022/11/16 16:54
      * 获取推送文件id
      */
     List<Long> getSftpFileIdList(@Param("apiCode") String apiCode
