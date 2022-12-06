d8b0919ad042d0328af6659165499e0949ad07a8
juanjuan.song <juanjuan.song@brgroup.com>
2022-12-06 11:42:28 +0800

    D20221129携程自动化过滤（营销→外呼）-3710058

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -1292,6 +1292,24 @@ public class PushRuleServiceImpl implements PushRuleService {
                         transferSyncUser.setReserveField1(JSON.toJSONString(json));
                     }
                 }
+                //携程特殊处理
+                if (marketingCommonConfig.getXieChengTransferInsertApiCodes().contains(transferSyncUser.getApiCode())){
+                    transferSyncUser.setCustNum(transferDataItemDTO.getCustNum().substring(18));
+                    String reserveField1 = transferDataItemDTO.getReserveField1();
+                    if (StringUtils.isNotBlank(reserveField1)) {
+                        try {
+                            JSONObject json = JSON.parseObject(reserveField1);
+                            json.put("initCustNum", transferDataItemDTO.getCustNum());
+                            transferSyncUser.setReserveField1(JSON.toJSONString(json));
+                        } catch (Exception e) {
+                            transferSyncUser.setReserveField1(reserveField1 + "," + transferDataItemDTO.getCustNum());
+                        }
+                    } else {
+                        JSONObject json = new JSONObject();
+                        json.put("initCustNum", transferDataItemDTO.getCustNum());
+                        transferSyncUser.setReserveField1(JSON.toJSONString(json));
+                    }
+                }
                 try {
                     marketingTransferSyncUserMapper.insertSelective(transferSyncUser);
                 } catch (Exception ex) {
