5d6e738e8083a09ce5e55a1e3915495f8b5864e4
juanjuan.song <juanjuan.song@brgroup.com>
2022-12-10 15:08:41 +0800

    众安提取

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/TransferToFileByZhongAnServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/TransferToFileByZhongAnServiceImpl.java
@@ -1,18 +1,14 @@
 package com.br.marketing.service.Impl;
 
-import com.alibaba.fastjson.JSON;
-import com.br.common.util.BrCipherMaker;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.utils.DateHelper;
 import com.br.marketing.common.utils.StringUtils;
-import com.br.marketing.entity.MarketingSyncUser;
-import com.br.marketing.entity.MarketingTransferSyncUser;
 import com.br.marketing.entity.TransferFileTask;
 import com.br.marketing.entity.TransferFileTaskExample;
-import com.br.marketing.mapper.MarketingSyncInfoMapper;
-import com.br.marketing.mapper.MarketingTransferSyncUserMapper;
+import com.br.marketing.entity.ZhonganMarketingBan;
 import com.br.marketing.mapper.TransferFileTaskMapper;
+import com.br.marketing.mapper.ZhonganMarketingBanMapper;
 import com.br.marketing.service.ITransferToFileService;
 import com.br.marketing.service.SyncConfigService;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
@@ -20,14 +16,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @Author songjuanjnuan
@@ -43,19 +37,15 @@ public class TransferToFileByZhongAnServiceImpl implements ITransferToFileServic
     @Autowired
     private TransferFileTaskMapper transferFileTaskMapper;
     @Autowired
-    private TableCreateServiceImpl tableCreateService;
-    @Autowired
     private RuleRedisServiceImpl ruleRedisService;
     @Resource
-    private MarketingTransferSyncUserMapper marketingTransferSyncUserMapper;
-    @Resource
-    private MarketingSyncInfoMapper marketingSyncInfoMapper;
+    private ZhonganMarketingBanMapper zhonganMarketingBanMapper;
     @Resource
     private MarketingCommonConfig marketingCommonConfig;
 
     final static String EXECUTE_TIME = " 10:00:00";
 
-    final static String JIUFU_TRANSFER_FILE = "zhongan_zhuanhua_";
+    final static String ZHONGAN_FILE = "zhongan_zhuanhua_";
 
     final static DateTimeFormatter YYYYMMDDSHORTDF = DateTimeFormatter.ofPattern(DateHelper.SHORT_DATE_FORMAT);
 
@@ -113,14 +103,14 @@ public class TransferToFileByZhongAnServiceImpl implements ITransferToFileServic
     public Result actionTransferToFile(TransferFileTask transferFileTask,String jobParameter) {
         log.warn("众安异业撞库数据提取-开始写入文件,apiCode ={}", transferFileTask.getApiCode());
         String apiCode = transferFileTask.getApiCode();
-        String recordDate = transferFileTask.getStartDate();//yyyyMMdd
+        String recordDate = transferFileTask.getStartDate();
         String descPath = syncConfigService.getPath().concat("transferToFile/").concat(apiCode).concat("/").concat(recordDate).concat("/");
         File writeDic = new File(descPath);
         if (!writeDic.exists()) {
             writeDic.mkdirs();
         }
         StringBuilder fileName = new StringBuilder();
-        fileName.append(JIUFU_TRANSFER_FILE).append(recordDate).append(".txt");
+        fileName.append(ZHONGAN_FILE).append(recordDate).append(".txt");
         String fileAllPath = descPath.concat(fileName.toString());
         transferFileTask.setFileName(fileName.toString());
         transferFileTask.setFilePath(descPath);
@@ -140,80 +130,32 @@ public class TransferToFileByZhongAnServiceImpl implements ITransferToFileServic
 
     private void writeZhongAnTransferToFile(Writer fw, String apiCode,TransferFileTask transferFileTask) throws IOException {
         Long start = System.currentTimeMillis();
-        String tcId = tableCreateService.getTcId(apiCode);
+        String recordDate = transferFileTask.getStartDate();
+        LocalDate localDate = LocalDate.parse(recordDate, YYYYMMDDSHORTDF);
         Integer page = 0;
         Boolean mark = Boolean.TRUE;
-        //去重后的Set
-        HashSet custNumResult = new HashSet();
         int totalSize = 0;
         while (mark) {
-            Result<List<MarketingTransferSyncUser>> transferData = getOrderTransferData(tcId,"", page);
+            Result<List<ZhonganMarketingBan>> transferData = getOrderTransferData(localDate.toString(), page);
             if (!ResultCode.SUCCESS.getValue().equals(transferData.getCode())) {
                 mark = Boolean.FALSE;
                 continue;
             }
             page++;
-            List<MarketingTransferSyncUser> data = transferData.getData();
-            List<MarketingTransferSyncUser> dataFilter = new ArrayList<>();
-            for (MarketingTransferSyncUser marketingTransferSyncUser : data) {
-                //过滤掉 同一custNum的其他insertTime数据，custNumResult
-                if (StringUtils.isNotEmpty(marketingTransferSyncUser.getCustNum()) && custNumResult.add(marketingTransferSyncUser.getCustNum())) {
-                    dataFilter.add(marketingTransferSyncUser);
-                }
-            }
-            if (dataFilter.size() <= 0) {
-                continue;
-            }
-            Set<String> set = dataFilter.stream().map(MarketingTransferSyncUser::getCustNum).collect(Collectors.toSet());
-            List<MarketingSyncUser> preUserByTask = marketingSyncInfoMapper.getPreUserByInCust(apiCode, set);
-            Map<String, MarketingSyncUser> preUserMap = preUserByTask.stream().collect(
-                    Collectors.groupingBy(MarketingSyncUser::getCustNum
-                            , Collectors.collectingAndThen(
-                                    Collectors.reducing((v1, v2) ->
-                                            v1.getCreateTime().compareTo(v2.getCreateTime()) > 0 ? v1 : v2)
-                                    , Optional::get)));
-            //requestId,orgName,custNum,source,userType,ifLogin,loginTime,ifApply,applyDt,applyResult,auditAmount,lentTime,lentAmount,insertTime,applyLoan,applyLoanTime,cell
-            for (MarketingTransferSyncUser transferFilterData : dataFilter) {
-                String custNum = transferFilterData.getCustNum();
-                String applyLoan = "";
-                String applyLoanTime = "";
-                String cell = "";
-                if (preUserMap.containsKey(custNum)) {
-                    String decode = BrCipherMaker.getInstance().decode(preUserMap.get(custNum).getCell());
-                    cell = StringUtils.isBlank(decode) ? preUserMap.get(custNum).getCell() : DigestUtils.md5DigestAsHex(decode.getBytes());
-                }
-                if(StringUtils.isNotEmpty(transferFilterData.getReserveField1())){
-                    applyLoan = StringUtils.isNotEmpty(JSON.parseObject(transferFilterData.getReserveField1()).getString("applyLoan"))?JSON.parseObject(transferFilterData.getReserveField1()).getString("applyLoan"):"";
-                    applyLoanTime = StringUtils.isNotEmpty(JSON.parseObject(transferFilterData.getReserveField1()).getString("applyLoanTime"))?JSON.parseObject(transferFilterData.getReserveField1()).getString("applyLoanTime"):"";
-                }
+            List<ZhonganMarketingBan> data = transferData.getData();
+            //cell,applet_date
+            for (ZhonganMarketingBan transferFilterData : data) {
+                String cell = transferFilterData.getCustNum();
                 StringBuilder sb = new StringBuilder();
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getRequestId())?transferFilterData.getRequestId():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getOrgName())?transferFilterData.getOrgName():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getCustNum())?transferFilterData.getCustNum():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getSource())?transferFilterData.getSource():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getUserType())?transferFilterData.getUserType():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getIfLogin())?transferFilterData.getIfLogin():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getLoginTime())?transferFilterData.getLoginTime().replace(":000",""):"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getIfApply())?transferFilterData.getIfApply():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getApplyDt())?transferFilterData.getApplyDt().replace(":000",""):"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getApplyResult())?transferFilterData.getApplyResult():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getAuditAmount())?transferFilterData.getAuditAmount():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getLentTime())?transferFilterData.getLentTime().replace(":000",""):"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getLentAmount())?transferFilterData.getLentAmount():"").concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getInsertTime())?transferFilterData.getInsertTime().replace(":000",""):"").concat(","));
-                sb.append(applyLoan.concat(","));
-                sb.append(applyLoanTime.replace(":000","").concat(","));
                 sb.append(cell.concat(","));
-                sb.append((StringUtils.isNotEmpty(transferFilterData.getRequestTime())?transferFilterData.getRequestTime().replace(":000",""):""));
+                sb.append(transferFilterData.getAppletDate());
                 sb.append("\r\n");
                 fw.append(sb.toString());
             }
-            totalSize = totalSize + dataFilter.size();
-            dataFilter.clear();
+            totalSize = totalSize + data.size();
             data.clear();
         }
 
-        custNumResult.clear();
         TransferFileTask updatetask = new TransferFileTask();
         updatetask.setId(transferFileTask.getId());
         updatetask.setStatus(2);
@@ -235,18 +177,17 @@ public class TransferToFileByZhongAnServiceImpl implements ITransferToFileServic
      * 获取转化数据
      * 按照inserttime排序
      *
-     * @param tcId
-     * @param endDate
+     * @param date
      * @param pageIndex
      * @return
      */
-    private Result<List<MarketingTransferSyncUser>> getOrderTransferData(String tcId,String endDate,Integer pageIndex) {
+    private Result<List<ZhonganMarketingBan>> getOrderTransferData(String date,Integer pageIndex) {
         Integer limitStart = pageIndex * 2000;
-        List<MarketingTransferSyncUser> transferOrderInsertTime = marketingTransferSyncUserMapper.getTransferByRequestData(tcId, endDate,limitStart);
-        if (transferOrderInsertTime.size() <= 0) {
+        List<ZhonganMarketingBan> zhonganMarketingBans = zhonganMarketingBanMapper.getByZKData(date,limitStart);
+        if (zhonganMarketingBans.size() <= 0) {
             return new Result<>().setCode(ResultCode.FAIL.getValue());
         }
-        return new Result<>().setCode(ResultCode.SUCCESS.getValue()).setDate(transferOrderInsertTime);
+        return new Result<>().setCode(ResultCode.SUCCESS.getValue()).setDate(zhonganMarketingBans);
     }
 
 }
