6997675b7ef0789d5d5abc56c89c1c0dd3c19add
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-12-12 17:59:36 +0800

    华强：D20221207众安信贷营销名单推送逻辑变更-3710048 + 查询手机号

--- marketing-cs/src/main/java/com/br/marketing/mapper/ZhonganMarketingBanMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/ZhonganMarketingBanMapper.java
@@ -1,13 +1,25 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.ZhonganMarketingBan;
+import com.br.marketing.monkeydata.query.ZhongAnCellZkDateQuery;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
-public interface ZhonganMarketingBanMapper extends ZhonganMarketingBanMapperBase{
+public interface ZhonganMarketingBanMapper extends ZhonganMarketingBanMapperBase {
 
-    List<ZhonganMarketingBan> getByZKData(@Param("date")String date,@Param("limitStart") Integer limitStart);
+    List<ZhonganMarketingBan> getByZKData(@Param("date") String date, @Param("limitStart") Integer limitStart);
+
+    /**
+     * 2022-12-12 16:17
+     * 获取不营销手机号
+     *
+     * @param apiCode code
+     * @param queries 众安名单锁定列表
+     * @return list
+     */
+    List<ZhongAnCellZkDateQuery> getNotMarketingCell(@Param("apiCode") String apiCode
+            , @Param("list") List<ZhongAnCellZkDateQuery> queries);
 }
