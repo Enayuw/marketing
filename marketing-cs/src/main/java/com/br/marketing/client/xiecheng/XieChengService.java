67001d17afebd14658c7cdaaf14717ed9dce3f1f
张广超 <50570550@qq.com>
2022-12-12 10:22:57 +0800

    Subject：<重试>

--- marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
@@ -99,7 +99,8 @@ public class XieChengService {
         retMap.put("sign", FinanceAESUtils.signLocal(retMap, singKey));
         log.warn("携程发送参数 para={}", JSON.toJSONString(retMap));
 //        String result = httpProxyClient.send(JSON.toJSONString(retMap), openUrl, isProxy);
-        String result = "{\"code\":500,\"msg\":\"测试成功\",\"data\":null}";
+//        String result = "{\"code\":200,\"msg\":\"测试成功\",\"data\":null}";
+        String result = "{\"code\":500,\"msg\":\"测试重试成功\",\"data\":null}";
         JSONObject resultJson = JSONObject.parseObject(result);
         Integer code = resultJson.getInteger("code");
         log.warn("携程数据返回信息：{}", result);
