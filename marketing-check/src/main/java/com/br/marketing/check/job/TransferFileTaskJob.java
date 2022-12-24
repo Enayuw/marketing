944ecb19e1abcbcae77089fbab830f472c7177c7
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2022-12-24 15:22:05 +0800

    华强：转化数据提取到文件优化 * 根据apicode查询

--- marketing-check/src/main/java/com/br/marketing/check/job/TransferFileTaskJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/TransferFileTaskJob.java
@@ -24,9 +24,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Component
 @Slf4j
@@ -85,66 +83,96 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
     @Override
     public void process(JobExecutionMultipleShardingContext context) {
         String jobParameter = context.getJobParameter();
+        BindApiCodeServiceImplBean bindApiCodeServiceImplBean = bindApiCode();
+        Map<String, Set<ITransferToFileService>> bind = bindApiCodeServiceImplBean.bind;
         MarketingCustomerExample customerExample = new MarketingCustomerExample();
-        customerExample.createCriteria().andStatusEqualTo(Byte.valueOf("1"));
+        customerExample.createCriteria().andStatusEqualTo(Byte.valueOf("1"))
+                .andApiCodeIn(new ArrayList<>(bind.keySet()));
         List<MarketingCustomer> marketingCustomers = customerMapper.selectByExampleAndShard(customerExample
                 , context.getShardingTotalCount()
                 , context.getShardingItems());
-        log.warn("###分片：{},分片获取的数据量：{}", Arrays.toString(context.getShardingItems().toArray()), marketingCustomers.size());
         for (MarketingCustomer marketingCustomer : marketingCustomers) {
-            try {
-                ITransferToFileService serviceImpl = getServiceImpl(marketingCustomer);
-                if (serviceImpl == null) {
-                    continue;
-                }
-                //自定义参数传入格式举例 7410785#20220711,true;7412003#123;.....
-                String myParam = serviceImpl.isMyParam(marketingCustomer.getApiCode(), jobParameter);
-                log.warn("apicode={}获取的自定义参数为{}", marketingCustomer.getApiCode(), myParam);
-                Result<List<TransferFileTask>> listResult = serviceImpl.buildTransferTask(marketingCustomer.getApiCode(), myParam);
-                if (ResultCode.SUCCESS.getValue().equals(listResult.getCode()) && listResult.getData().size() > 0) {
-                    List<TransferFileTask> data = listResult.getData();
-                    for (TransferFileTask datum : data) {
-                        Result result = serviceImpl.actionTransferToFile(datum, myParam);
-                        if (ResultCode.SUCCESS.getValue().equals(result.getCode())) {
-                            Result res = sftpInnerService.pushInnerSftp(datum);
-                            if (!ResultCode.SUCCESS.getValue().equals(res.getCode())) {
-                                RetryMainLog retryMainLog = new RetryMainLog();
-                                retryMainLog.setRetryType(1);
-                                retryMainLog.setRetryParam(JSON.toJSONString(datum));
-                                retryMainLog.setRetryParamType(datum.getClass().getName());
-                                retryMainLog.setRetryService("sftpInnerServiceImpl");
-                                retryMainLog.setRetryMethod("pushInnerSftp");
-                                retryMainLog.setRetryNum(0);
-                                retryMainLog.setRetryMaxNum(3);
-                                retryMainLog.setRetryStatus(1);
-                                retryMainLog.setCreateTime(new Date());
-                                retryMainLog.setIncrId(redisChgService.incr(RedisKeyConstant.retryid));
-                                retryMainLogMapper.insertSelective(retryMainLog);
-                            } else {
-                                //第一次执行，查询为空，不会进行删除，直接返回
-                                //第二次执行，删除b_sync_log的记录
-                                List<SyncLog> syncLogList = loanSyncLogMapper.querySyncLog(ImmutableMap.of("apiCode", marketingCustomer.getApiCode(), "fileName", datum.getFileName()));
-                                if (!CollectionUtils.isEmpty(syncLogList)) {
-                                    if (syncLogList.size() != 1) {
-                                        log.warn("重新执行数据提取异常，apiCode={},fileName={},syncLogSize={}", marketingCustomer.getApiCode(), datum.getFileName(), syncLogList.size());
-                                        return;
+            Set<ITransferToFileService> serviceImplSet = bind.get(marketingCustomer.getApiCode());
+            for (ITransferToFileService serviceImpl : serviceImplSet) {
+                try {
+                    //自定义参数传入格式举例 7410785#20220711,true;7412003#123;.....
+                    String myParam = serviceImpl.isMyParam(marketingCustomer.getApiCode(), jobParameter);
+                    log.warn("apicode={}获取的自定义参数为{}", marketingCustomer.getApiCode(), myParam);
+                    Result<List<TransferFileTask>> listResult = serviceImpl.buildTransferTask(marketingCustomer.getApiCode(), myParam);
+                    if (ResultCode.SUCCESS.getValue().equals(listResult.getCode()) && listResult.getData().size() > 0) {
+                        List<TransferFileTask> data = listResult.getData();
+                        for (TransferFileTask datum : data) {
+                            Result result = serviceImpl.actionTransferToFile(datum, myParam);
+                            if (ResultCode.SUCCESS.getValue().equals(result.getCode())) {
+                                Result res = sftpInnerService.pushInnerSftp(datum);
+                                if (!ResultCode.SUCCESS.getValue().equals(res.getCode())) {
+                                    RetryMainLog retryMainLog = new RetryMainLog();
+                                    retryMainLog.setRetryType(1);
+                                    retryMainLog.setRetryParam(JSON.toJSONString(datum));
+                                    retryMainLog.setRetryParamType(datum.getClass().getName());
+                                    retryMainLog.setRetryService("sftpInnerServiceImpl");
+                                    retryMainLog.setRetryMethod("pushInnerSftp");
+                                    retryMainLog.setRetryNum(0);
+                                    retryMainLog.setRetryMaxNum(3);
+                                    retryMainLog.setRetryStatus(1);
+                                    retryMainLog.setCreateTime(new Date());
+                                    retryMainLog.setIncrId(redisChgService.incr(RedisKeyConstant.retryid));
+                                    retryMainLogMapper.insertSelective(retryMainLog);
+                                } else {
+                                    //第一次执行，查询为空，不会进行删除，直接返回
+                                    //第二次执行，删除b_sync_log的记录
+                                    List<SyncLog> syncLogList = loanSyncLogMapper.querySyncLog(ImmutableMap.of("apiCode", marketingCustomer.getApiCode(), "fileName", datum.getFileName()));
+                                    if (!CollectionUtils.isEmpty(syncLogList)) {
+                                        if (syncLogList.size() != 1) {
+                                            log.warn("重新执行数据提取异常，apiCode={},fileName={},syncLogSize={}", marketingCustomer.getApiCode(), datum.getFileName(), syncLogList.size());
+                                            return;
+                                        }
+                                        SyncLogExample syncLogExample = new SyncLogExample();
+                                        syncLogExample.createCriteria().andApiCodeEqualTo(marketingCustomer.getApiCode())
+                                                .andFileNameIn(Lists.newArrayList(datum.getFileName(), datum.getFileName() + ".success"));
+                                        loanSyncLogMapper.deleteByExample(syncLogExample);
                                     }
-                                    SyncLogExample syncLogExample = new SyncLogExample();
-                                    syncLogExample.createCriteria().andApiCodeEqualTo(marketingCustomer.getApiCode())
-                                            .andFileNameIn(Lists.newArrayList(datum.getFileName(), datum.getFileName() + ".success"));
-                                    loanSyncLogMapper.deleteByExample(syncLogExample);
                                 }
                             }
                         }
                     }
+                } catch (Exception ex) {
+                    log.error(String.format("客户转化文件提取报错：%s,报错信息：%s", marketingCustomer.getApiCode(), ex.getMessage()), ex);
                 }
-            } catch (Exception ex) {
-                log.error(String.format("客户转化文件提取报错：%s,报错信息：%s", marketingCustomer.getApiCode(), ex.getMessage()), ex);
             }
         }
     }
 
+    /**
+     * 2022-12-24 15:09
+     * apicCode 绑定 业务实现
+     */
+    private BindApiCodeServiceImplBean bindApiCode() {
+        return BindApiCodeServiceImplBean.BindApiCodeServiceImplBeanBuilder.create()
+                // 萨摩耶转化数据提取
+                .addBind(transferToFileBySamoyeServiveImpl, marketingCommonConfig.getSaMoYeTransferFileApiCodes())
+                // 哈啰转化数据提取
+                .addBind(transferToFileByHaluoServiceImpl, marketingCommonConfig.getHaLuoTransferFileApiCodes())
+                // 数禾转化数据提取
+                .addBind(transferToFileByShuHeService, marketingCommonConfig.getShuHeTransferExtractApiCodes().keySet())
+                // 宜信实时转化数据提取
+                .addBind(transferToFileByYiXinRealTimeService, marketingCommonConfig.getYinXinTransferRealTimeApiCodes())
+                // 玖富转化数据提取
+                .addBind(transferToFileByJiuFuService, marketingCommonConfig.getJiuFuTransferApiCodes())
+                //拍拍贷新客实时转化数据提取
+                .addBind(transferToFileByPPDService, marketingCommonConfig.getPPDTransferFileApiCodes())
+                //同程转化数据提取
+                .addBind(transferToFileByTongChengService, marketingCommonConfig.getTongChengTransferFileApiCodes())
+                // 小赢转化数据提取
+                .addBind(xiaoYingRealTimeService, marketingCommonConfig.getXiaoYingTransferExtractApiCodes())
+                // 众安异业撞库数据提取
+                .addBind(transferToFileByZhongAnService, marketingCommonConfig.getZhongAnTransferApiCodes())
+                //携程转化数据提取
+                .addBind(transferToFileByXieChengService, marketingCommonConfig.getXieChengTransferApiCodes())
+                .build();
+    }
 
+    @Deprecated
     ITransferToFileService getServiceImpl(MarketingCustomer customer) {
         if (marketingCommonConfig.getSaMoYeTransferFileApiCodes().contains(customer.getApiCode())) {
             return transferToFileBySamoyeServiveImpl;
@@ -172,8 +200,67 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
         }
         if (marketingCommonConfig.getXieChengTransferApiCodes().contains(customer.getApiCode())) {
             return transferToFileByXieChengService;
-        }else {
+        } else {
             return null;
+        }
+    }
+
+
+    private static class BindApiCodeServiceImplBean {
+        Map<String, Set<ITransferToFileService>> bind;
+
+        private BindApiCodeServiceImplBean(Map<String, Set<ITransferToFileService>> bind) {
+            this.bind = bind;
+        }
+
+        private static class BindApiCodeServiceImplBeanBuilder {
+            private LinkedList<Collection<String>> apiCodesLinkedList;
+            private LinkedList<ITransferToFileService> serviceLinkedList;
+
+            private static BindApiCodeServiceImplBeanBuilder create() {
+                return new BindApiCodeServiceImplBeanBuilder();
+            }
+
+            private BindApiCodeServiceImplBeanBuilder addBind(ITransferToFileService service
+                    , Collection<String> apiCodes) {
+                if (service != null && apiCodes != null) {
+                    if (this.serviceLinkedList == null) {
+                        this.serviceLinkedList = new LinkedList<>();
+                    }
+                    serviceLinkedList.add(service);
+                    if (this.apiCodesLinkedList == null) {
+                        this.apiCodesLinkedList = new LinkedList<>();
+                    }
+                    apiCodesLinkedList.add(apiCodes);
+                }
+                return this;
+            }
+
+            private BindApiCodeServiceImplBean build() {
+                if (this.serviceLinkedList == null) {
+                    this.serviceLinkedList = new LinkedList<>();
+                }
+                if (this.apiCodesLinkedList == null) {
+                    this.apiCodesLinkedList = new LinkedList<>();
+                }
+                int size = apiCodesLinkedList.size();
+                Map<String, Set<ITransferToFileService>> bind = new HashMap<>();
+                for (int i = 0; i < size; i++) {
+                    Collection<String> apiCodes = apiCodesLinkedList.get(i);
+                    ITransferToFileService iTransferToFileService = serviceLinkedList.get(i);
+                    for (String apiCode : apiCodes) {
+                        if (bind.containsKey(apiCode)) {
+                            Set<ITransferToFileService> serviceSet = bind.get(apiCode);
+                            serviceSet.add(iTransferToFileService);
+                        } else {
+                            HashSet<ITransferToFileService> serviceSet = new HashSet<>();
+                            serviceSet.add(iTransferToFileService);
+                            bind.put(apiCode, serviceSet);
+                        }
+                    }
+                }
+                return new BindApiCodeServiceImplBean(bind);
+            }
         }
     }
 
