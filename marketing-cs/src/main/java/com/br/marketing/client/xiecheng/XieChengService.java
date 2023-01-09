8f84b6fb9ecd854e5a25afb2e29d2edaef78cff3
张广超 <50570550@qq.com>
2023-01-09 14:11:08 +0800

    Subject：<携程推送恢复>

--- marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
@@ -123,19 +123,12 @@ public class XieChengService {
         retMap.put("channel", channel);
         retMap.put("data", FinanceAESUtils.encryptStr(JSON.toJSONString(thirdAdOuterReq), key, iv));
         retMap.put("sign", FinanceAESUtils.signLocal(retMap, singKey));
-        //
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        HashMap<String, String> resMap = httpProxyClient.sendByCodeWithLog(retMap, openUrl, isProxy, MediaType.APPLICATION_JSON_UTF8_VALUE, JSON.toJSONString(thirdAdOuterReq),true,false);
+        if (!"200".equals(resMap.get("httpcode")) || StringUtils.isBlank(resMap.get("content"))) {
+            log.error("携程广告上报接口发送参数:ThirdAdOuterReq={} para={}", JSON.toJSONString(thirdAdOuterReq),JSON.toJSONString(retMap));
+            return new Result().setCode(ResultCode.INTERNAL_SERVER_ERROR.getValue());
         }
-//        HashMap<String, String> resMap = httpProxyClient.sendByCodeWithLog(retMap, openUrl, isProxy, MediaType.APPLICATION_JSON_UTF8_VALUE, JSON.toJSONString(thirdAdOuterReq),true,false);
-//        if (!"200".equals(resMap.get("httpcode")) || StringUtils.isBlank(resMap.get("content"))) {
-//            log.error("携程广告上报接口发送参数:ThirdAdOuterReq={} para={}", JSON.toJSONString(thirdAdOuterReq),JSON.toJSONString(retMap));
-//            return new Result().setCode(ResultCode.INTERNAL_SERVER_ERROR.getValue());
-//        }
-//        String content = resMap.get("content");
-        String content = "{\"code\":0,\"msg\":\"测试成功\",\"data\":null}";
+        String content = resMap.get("content");
         JSONObject resultJson = JSONObject.parseObject(content);
         Integer code = resultJson.getInteger("code");
         if(code==0){
