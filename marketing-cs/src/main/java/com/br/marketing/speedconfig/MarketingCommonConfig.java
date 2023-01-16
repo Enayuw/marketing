0f194aa8fd7471ce8a1f8a97997d461c803b0174
juman <juman.wang@brgroup.com>
2023-01-16 18:14:13 +0800

    锁定名单提取

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -411,9 +411,9 @@ public class MarketingCommonConfig {
     private List<String> XieChengTransferApiCodes;
 
     /**
-     * 携程转化数据提取执行时间
+     * 携程转化数据提取执行时间,携程撞库提取时间
      */
-    private String XieChengTransferExecuteTime;
+    private List<String> XieChengTransferExecuteTime;
 
     /**
      * 携程短信撞库轮询时间间隔
