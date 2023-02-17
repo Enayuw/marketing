bb3888eb4b542690648555fc0700f883d29149b9
张广超 <50570550@qq.com>
2023-02-17 15:44:43 +0800

    Subject：<携程>

--- marketing-check/src/main/java/com/br/marketing/check/job/XieChengSmsDataCollidingToSendJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/XieChengSmsDataCollidingToSendJob.java
@@ -79,13 +79,20 @@ public class XieChengSmsDataCollidingToSendJob extends AbstractSimpleElasticJob 
                 if (LocalDate.now().isEqual(fileDate) &&!lf.getFileName().contains("sup") ) {
                     // 整点推 // 补偿推
                     // 当期那时间是否符合推送时间 当前时间 >= 推送时间
-                    push(lf, true);
+                    if (!LocalTime.now().isBefore(getSendTime())) {
+                        push(lf, true);
+                    }
                 }
                 if (LocalDate.now().isAfter(fileDate)) {
                     //周期推
                     // 当前时间大约文件推送时间 旧文件
-                    push(lf, false);
-
+                    if(lf.getFileName().contains("sup")){
+                        push(lf, false);
+                    }else {
+                        if (!LocalTime.now().isBefore(getSendTime())) {
+                            push(lf, false);
+                        }
+                    }
                 }
             });
         }
