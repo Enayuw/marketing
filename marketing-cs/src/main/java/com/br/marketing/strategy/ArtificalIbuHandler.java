ebd776a2b6cd73ca0aada79b1fa8d3dcdc97f579
zhen.li1 <zhen.li1@brgroup.com>
2023-02-20 11:41:20 +0800

    时间格式

--- marketing-cs/src/main/java/com/br/marketing/strategy/ArtificalIbuHandler.java
+++ marketing-cs/src/main/java/com/br/marketing/strategy/ArtificalIbuHandler.java
@@ -24,6 +24,7 @@ import org.springframework.util.CollectionUtils;
 import javax.annotation.Resource;
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -118,7 +119,7 @@ public class ArtificalIbuHandler extends AbstractExternalInterfaceHandler<IbuAda
             insert.setCell(extendInfo.getCell());
             insert.setCustNum(extendInfo.getCustNum());
             insert.setPushDaasDate(nowDate);
-            insert.setPushDaasTime(new Date().toString());
+            insert.setPushDaasTime(LocalDateTime.now().toString());
             if (marketingCommonConfig.getRongShuPushDaasSwitch()) {
                 insert.setPStatus(1);
             } else {
@@ -133,7 +134,7 @@ public class ArtificalIbuHandler extends AbstractExternalInterfaceHandler<IbuAda
             update.setId(rongshuCycleDataList.get(0).getId());
             update.setPushDaasDate(nowDate);
             update.setPhoneExtendId(extendInfo.getId());
-            update.setPushDaasTime(new Date().toString());
+            update.setPushDaasTime(LocalDateTime.now().toString());
             if (marketingCommonConfig.getRongShuPushDaasSwitch()) {
                 update.setPStatus(1);
             } else {
