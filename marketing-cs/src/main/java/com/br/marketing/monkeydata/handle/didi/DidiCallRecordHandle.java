75c9857e53251f51402c67a7b6a2afa68d05d647
张广超 <50570550@qq.com>
2023-05-09 21:12:27 +0800

    Subject：<代码注释>

--- marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/didi/DidiCallRecordHandle.java
+++ marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/didi/DidiCallRecordHandle.java
@@ -1,38 +1,25 @@
 package com.br.marketing.monkeydata.handle.didi;
 
-import com.br.marketing.client.RedisChgService;
-import com.br.marketing.client.didi.output.DiDiResponseTO;
-import com.br.marketing.client.marketingapi.input.UploadDataDTO;
-import com.br.marketing.common.commondto.Result;
-import com.br.marketing.common.commondto.ResultCode;
-import com.br.marketing.common.constants.rediskey.RedisKeyConstant;
 import com.br.marketing.common.utils.BrExecutors;
-import com.br.marketing.common.utils.StringUtils;
 import com.br.marketing.entity.DidiCallRecord;
 import com.br.marketing.entity.DidiCallRecordExample;
-import com.br.marketing.entity.MarketingSyncUser;
-import com.br.marketing.entity.MarketingTransferSyncUser;
 import com.br.marketing.mapper.DidiCallRecordMapper;
-import com.br.marketing.service.TransferDataValidityPeriodService;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.br.marketing.strategy.MethodRetryHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.CharSequenceUtils;
-import org.apache.poi.ss.formula.functions.Now;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author GuangChao.Zhang
  * @version 1.0
+ * @Description: 滴滴触达逻辑说明：滴滴触达需要判断数据推送当天是否有效，并判断数据当天是否推送过，
+ * 由于是并发处理，数据去重需要加Redis 锁。
  * @date 2023/4/27 15:41
  */
 
