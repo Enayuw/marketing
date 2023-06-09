d089180f46d2035d1089d6349387bb0c0d206723
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2023-06-09 18:12:19 +0800

    华强：D20230608中原通话明细自动化转人工-3710087 * 处理空值

--- marketing-cs/src/main/java/com/br/marketing/rule/zhongyuan/ZhongYuanCallRecordToDass.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/zhongyuan/ZhongYuanCallRecordToDass.java
@@ -45,7 +45,7 @@ public class ZhongYuanCallRecordToDass implements AssembleData<BatchRealTimeUser
         }
         BatchRealTimeUserDataDTO dataDTO = new BatchRealTimeUserDataDTO();
         dataDTO.setDassImportDataDTO(packageDassImportData(marketingSyncUser));
-        dataDTO.getDassImportDataDTO().setId(dataDTO.getDassImportDataDTO().getId());
+        dataDTO.getDassImportDataDTO().setId(dto.getId());
         dataDTO.setPhoneSaleExtendInfo(packagePhoneSaleExtendInfo(dto, marketingSyncUser));
         return dataDTO;
     }
