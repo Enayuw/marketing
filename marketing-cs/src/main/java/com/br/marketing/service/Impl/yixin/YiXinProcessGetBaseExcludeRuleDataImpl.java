3aebe5fe8db264529e5f8b17ee30458f17ee588f
张广超 <50570550@qq.com>
2023-06-30 20:09:41 +0800

    Subject：<情况B 剔除>

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/yixin/YiXinProcessGetBaseExcludeRuleDataImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/yixin/YiXinProcessGetBaseExcludeRuleDataImpl.java
@@ -129,7 +129,7 @@ public class YiXinProcessGetBaseExcludeRuleDataImpl implements YiXinProcessGetBa
                         today,
                         before,
                         custNums);
-        log.warn("宜信推送决策,符合推送条件:custNum在30天内有type!=12的基础数据,custNum集合为{}", Arrays.toString(includeList.toArray()));
+        log.warn("宜信推送决策,符合剔除条件:custNum在30天内有type!=12的基础数据,custNum集合为{}", Arrays.toString(includeList.toArray()));
         marketingTransferSyncUsers.removeIf(t ->  includeList.contains(t.getCustNum()));
     }
 
