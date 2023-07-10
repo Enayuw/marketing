71f227d2d5af27a1ca8447a3eeff6f669251aae6
juman <juman.wang@brgroup.com>
2023-07-10 15:55:27 +0800

    兼容离线文件回传处理

--- marketing-cs/src/main/java/com/br/marketing/client/FtpClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/FtpClient.java
@@ -131,6 +131,9 @@ public class FtpClient extends BaseFtpClient {
      * @param localFile   本地文件路径名称
      */
     public boolean download(String ftpFileName, File localFile)  {
+        if(!localFile.getParentFile().exists()){
+            localFile.getParentFile().mkdirs();
+        }
         try (OutputStream out = new BufferedOutputStream( java.nio.file.Files.newOutputStream(localFile.toPath()));){
             FTPFile[] fileInfoArray = ftp.listFiles(ftpFileName);
             if (fileInfoArray == null || fileInfoArray.length == 0) {
