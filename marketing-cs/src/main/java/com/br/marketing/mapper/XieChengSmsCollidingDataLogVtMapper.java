616ec081e7a4b35de5c12859e9a4b579b9478e2c
hong.chen <hong.chen@brgroup.com>
2023-07-12 18:21:28 +0800

    update:xiecheng实体

---
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengSmsCollidingDataLogVtMapper.java
@@ -0,0 +1,4 @@
+package com.br.marketing.mapper;
+
+public interface XieChengSmsCollidingDataLogVtMapper extends XieChengSmsCollidingDataLogVtMapperBase {
+}
---
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengSmsCollidingDataVtMapper.java
@@ -0,0 +1,5 @@
+package com.br.marketing.mapper;
+
+
+public interface XieChengSmsCollidingDataVtMapper extends XieChengSmsCollidingDataVtMapperBase{
+}
