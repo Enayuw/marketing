13705d189d5dd400a2b7e91c724467114ae78db3
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2023-07-13 15:16:25 +0800

    华强：众安转化数据清洗 * 查询手机号的上传数据

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncUserMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingSyncUserMapper.java
@@ -1,5 +1,6 @@
 package com.br.marketing.mapper;
 
+import com.br.marketing.bo.CellValidityPeriodBO;
 import com.br.marketing.entity.MarketingDataValidConfig;
 import com.br.marketing.entity.MarketingSyncUser;
 import com.br.marketing.entity.MarketingTransferSyncUser;
@@ -205,9 +206,12 @@ public interface MarketingSyncUserMapper {
             , @Param("custNums") Set<String> custNumSet);
 
 
-    List<MarketingSyncUser> getCellByAppletDateAndUserType(@Param("apiCode") String apiCode,  @Param("appletDate")String executeDate,  @Param("minId")Long minId,@Param("userType") String userType);
+    List<MarketingSyncUser> getCellByAppletDateAndUserType(@Param("apiCode") String apiCode, @Param("appletDate") String executeDate, @Param("minId") Long minId, @Param("userType") String userType);
 
-    List<MarketingSyncUser> getSyncUserByAppletDatePage(@Param("apiCode") String apiCode,  @Param("appletDate")String executeDate, @Param("userType") String userType, @Param("limitStart") Integer limitStart);
+    List<MarketingSyncUser> getSyncUserByAppletDatePage(@Param("apiCode") String apiCode, @Param("appletDate") String executeDate, @Param("userType") String userType, @Param("limitStart") Integer limitStart);
 
+    List<MarketingSyncUser> getSyncUserLastByCellAndInAppletDateUserTypeList(@Param("apiCode") String apiCode
+            , @Param("configList") List<MarketingDataValidConfig> configList
+            , @Param("cellValidityPeriodBOList") List<CellValidityPeriodBO> cellValidityPeriodBOList);
 
 }
