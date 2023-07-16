8b5b03be53ff0b4061e533029a36b073d460a4c9
hong.chen <hong.chen@brgroup.com>
2023-07-16 12:45:39 +0800

    update:优化

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -792,5 +792,10 @@ public class MarketingCommonConfig {
      * xieChengSmsApiCode=3710090
      */
     private String xieChengSmsApiCode;
+
+    /**
+     * 携程新场景短信撞库,mq推送客服转化线程数
+     */
+    private Integer xieChengSmsMqPushCustomerThreadNum;
 }
 
