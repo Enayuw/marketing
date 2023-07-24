4c80b6d49ed12f2cb22a5fb444a8f21cebacb260
zhen.li1 <zhen.li1@brgroup.com>
2023-07-24 19:17:13 +0800

    fix:去掉测试mock

--- marketing-cs/src/main/java/com/br/marketing/client/zhongan/ZhongAnClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/zhongan/ZhongAnClient.java
@@ -157,15 +157,14 @@ public class ZhongAnClient {
      */
     public Result<ZkReponseVO> zkXd(ZkReqDTO zkReqDTO) {
 //        上线需要去除掉,模拟调用不可营销的数据
-        if(true){
+       /* if(Arrays.asList("baca0e6571447795d04c60b9388c5a9d","9acb88dc59a878889d25aba379a844c5").contains(zkReqDTO.getCustMobileMd5())){
             ZkReponseVO zkReponseVO = new ZkReponseVO();
             zkReponseVO.setAccess(false);
             zkReponseVO.setRespNo("bbbe484f3bd94cb296fa3127be55d0c8");
             zkReponseVO.setRespCode("1");
             zkReponseVO.setStatus("SUCCESS");
-            log.warn("调用撞库接口返回"+JSON.toJSONString(zkReponseVO));
             return new Result<>().setCode(ResultCode.SUCCESS.getValue()).setDate(zkReponseVO);
-        }
+        }*/
         return zk(zkReqDTO, xinDaiZKApiKey);
     }
 
