fa2630bec0c9e49fa32528d89d5743be6d1d1ae6
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2023-08-01 16:48:52 +0800

    华强：D20230727同程自动化过滤-3710062（营销→外呼） * 有效期优化

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingDataValidConfigMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingDataValidConfigMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper
 public interface MarketingDataValidConfigMapper extends MarketingDataValidConfigMapperBase {
@@ -18,4 +19,17 @@ public interface MarketingDataValidConfigMapper extends MarketingDataValidConfig
     List<MarketingDataValidConfig> selectInfoFirstVersion(@Param("apiCode") String apiCode, @Param("userType") String userType);
 
     List<MarketingDataValidConfig> findListByApiCodeAndUserType(@Param("apiCode") String apiCode);
+
+    /**
+     * 2023-08-01 14:17
+     * 分页获取规则
+     *
+     * @param apiCode     code
+     * @param dateStr     日期,格式yyyy-MM-dd
+     * @param userTypeSet 场景集合
+     * @param page        页号，首页页号为0
+     */
+    List<MarketingDataValidConfig> findListByApiCodeAndUserTypeSetPage(@Param("apiCode") String apiCode
+            , @Param("dateStr") String dateStr
+            , @Param("userTypeSet") Set<String> userTypeSet, int page);
 }
