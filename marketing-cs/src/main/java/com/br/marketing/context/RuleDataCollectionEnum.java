3f8c7a32c81044c107d0c61538d9f6d5b4d132e2
zhen.li1 <zhen.li1@brgroup.com>
2023-08-01 20:00:08 +0800

    众邦通话明细推人工

--- marketing-cs/src/main/java/com/br/marketing/context/RuleDataCollectionEnum.java
+++ marketing-cs/src/main/java/com/br/marketing/context/RuleDataCollectionEnum.java
@@ -42,7 +42,9 @@ public enum RuleDataCollectionEnum {
 
     GOME_DATA_COLLECTION(21, "国美规则所需数据收集"),
 
-    ZHONGYUAN_DATA_COLLECTION(22, "中原规则所需数据收集");
+    ZHONGYUAN_DATA_COLLECTION(22, "中原规则所需数据收集"),
+
+    ZHONGBANG_DATA_COLLECTION(23, "众邦规则所需数据收集");
 
 
     RuleDataCollectionEnum(Integer code, String name) {
