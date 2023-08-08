b0a9f2fd173fd76b1e86a27d2bbb732fd5e2d872
‘zeqiang.guo <zeqiang.guo@brgroup.com>
2023-08-08 16:13:42 +0800

    华强：D20230807滴滴触达成功接口3710083-（sftp→api） * 继承

--- marketing-cs/src/main/java/com/br/marketing/entity/DidiCallRecord.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/DidiCallRecord.java
@@ -1,8 +1,10 @@
 package com.br.marketing.entity;
 
+import com.br.marketing.rule.InterfaceParams;
+
 import java.util.Date;
 
-public class DidiCallRecord {
+public class DidiCallRecord extends InterfaceParams {
     /**
      *
      */
