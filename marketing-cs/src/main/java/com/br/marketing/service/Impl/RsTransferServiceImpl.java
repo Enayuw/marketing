033d819ce6720471d8f87f6ddf70b05e5a646f79
李广秀 <guangxiu.li@brgroup.com>
2023-08-17 16:38:31 +0800

    D20230814榕树老客代运营自动化转决策-4004643：规则变更userType=1或userType=2userType=201或userType=202

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/RsTransferServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/RsTransferServiceImpl.java
@@ -97,7 +97,7 @@ public class RsTransferServiceImpl implements RsTransferService {
         Integer sort = 1;
         while (actionMark){
             List<PushMarketingUserDetailDTO> list = new ArrayList<>();
-            List<MarketingTransferSyncUser> rsToPolicyData = transferSyncUserMapper.getRsToPolicyData(date, tcId, Arrays.asList("1", "2"), "1", date, ifApply, applyDt, minId, 500);
+            List<MarketingTransferSyncUser> rsToPolicyData = transferSyncUserMapper.getRsToPolicyData(date, tcId, Arrays.asList("1", "201" ,"202"), "1", date, ifApply, applyDt, minId, 500);
             if(rsToPolicyData.size()<=0){
                 actionMark = Boolean.FALSE;
                 continue;
