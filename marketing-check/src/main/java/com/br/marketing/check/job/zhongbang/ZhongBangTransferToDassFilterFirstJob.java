c04a4d83e3067be96adb34eac8ca0487182f8e64
hong.chen <hong.chen@brgroup.com>
2023-08-23 11:47:15 +0800

    update:众邦自动化过滤

---
+++ marketing-check/src/main/java/com/br/marketing/check/job/zhongbang/ZhongBangTransferToDassFilterFirstJob.java
@@ -0,0 +1,35 @@
+package com.br.marketing.check.job.zhongbang;
+
+
+import com.br.marketing.service.ZhongBangToDassFilterProcessService;
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+
+/**
+ * @Author chenh
+ * @Date 2023/8/23 17:16
+ * @Description: 众邦转化数据推人工转化过滤接口(首次)
+ **/
+@Component
+@Slf4j
+public class ZhongBangTransferToDassFilterFirstJob extends AbstractSimpleElasticJob {
+    @Resource
+    private ZhongBangToDassFilterProcessService zhongBangToDassFilterProcessService;
+
+
+    @Override
+    public void process(JobExecutionMultipleShardingContext context) {
+        try {
+            zhongBangToDassFilterProcessService.doProcessFirst();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
+    }
+}
+
---
+++ marketing-cs/src/main/java/com/br/marketing/service/ZhongBangToDassFilterProcessService.java
@@ -0,0 +1,14 @@
+package com.br.marketing.service;
+
+
+/**
+ * 众邦转化数据推人工转化过滤接口
+ * @author chenh
+ * @version 1.0
+ * @date 2023/6/16 17:19
+ */
+public interface ZhongBangToDassFilterProcessService {
+    void doProcessFirst();
+    void doProcessNoFirst();
+
+}
