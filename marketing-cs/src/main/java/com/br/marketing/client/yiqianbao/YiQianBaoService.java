5341c181a56a89d51f8d4b82d892ba49893b2f92
juman <juman.wang@brgroup.com>
2023-09-19 11:30:51 +0800

    修复任务调度

--- marketing-cs/src/main/java/com/br/marketing/client/yiqianbao/YiQianBaoService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/yiqianbao/YiQianBaoService.java
@@ -55,6 +55,7 @@ public class YiQianBaoService {
 
     @RetryMethod(retryNowNum = 3)
     public Result<ResponseYqbDTO> pushMarketingData(YqbDetailVo yqbDetailVo) {
+        org.bouncycastle.jcajce.provider.asymmetric.rsa.RSAUtil
         try {
             log.warn("壹钱包明文参数 para={}", JSON.toJSONString(yqbDetailVo));
             RequestYqbDTO requestYqbDTO = new RequestYqbDTO();
