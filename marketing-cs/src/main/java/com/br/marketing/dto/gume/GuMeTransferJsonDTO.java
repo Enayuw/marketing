c9570bde21cf5b6ee82116e5e55dbfabebd8882d
郭泽强 <zeqaing.guo@brgroup.com>
2023-10-18 14:30:55 +0800

    华强：D20231012国美定制api转化接口-3710076 *

--- marketing-cs/src/main/java/com/br/marketing/dto/gume/GuMeTransferJsonDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/gume/GuMeTransferJsonDTO.java
@@ -1,7 +1,8 @@
 package com.br.marketing.dto.gume;
 
+import com.alibaba.fastjson.JSONArray;
+
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * 数禾数据
@@ -36,10 +37,26 @@ public class GuMeTransferJsonDTO implements Serializable {
     /**
      * 2023-10-16 17:43
      * 业务数据，必填
+     * 初始已知字段：
+     * group 分组 非必填
+     * userId userId 必填
+     * registrationDate 注册日期 非必填
+     * isLogin 是否登录 1: 是 0: 否 非必填
+     * loginTime 登录时间 yyyy-mm-dd 非必填
+     * isApplyCredit 是否申请授信 1: 是 0: 否 非必填
+     * applyCreditTime 申请授信时间 yyyy-mm-dd 非必填
+     * isCreditPass 是否授信通过 1: 是 0: 否 非必填
+     * creditPassTime 授信通过时间 yyyy-mm-dd 非必填
+     * creditAmount 授信金额 非必填
+     * isApplyWithdrawals 是否申请提现 1: 是 0: 否 非必填
+     * withdrawalsTime 提现时间 yyyy-mm-dd 非必填
+     * isRiskPass 是否风控通过 1: 是 0: 否 非必填
+     * riskPassAmount 风控通过金额 非必填
+     * lendersDate 放款日期 yyyy-mm-dd 非必填
      */
-    private List<BizData> data;
+    private JSONArray data;
 
-    public GuMeTransferJsonDTO(String sign, String requestId, String channelCode, List<BizData> data) {
+    public GuMeTransferJsonDTO(String sign, String requestId, String channelCode, JSONArray data) {
         this.sign = sign;
         this.requestId = requestId;
         this.channelCode = channelCode;
@@ -74,11 +91,11 @@ public class GuMeTransferJsonDTO implements Serializable {
         this.channelCode = channelCode;
     }
 
-    public List<BizData> getData() {
+    public JSONArray getData() {
         return data;
     }
 
-    public void setData(List<BizData> data) {
+    public void setData(JSONArray data) {
         this.data = data;
     }
 
@@ -91,268 +108,4 @@ public class GuMeTransferJsonDTO implements Serializable {
                 ", data=" + data +
                 '}';
     }
-
-    /**
-     * 2023-10-16 17:57
-     * 业务数据
-     */
-    public static class BizData {
-        /**
-         * 2023-10-16 17:55
-         * 分组
-         * 非必填
-         */
-        private String group;
-        /**
-         * 2023-10-16 17:55
-         * userId
-         * 必填
-         */
-        private String userId;
-        /**
-         * 2023-10-16 17:55
-         * 注册日期
-         * 非必填
-         */
-        private String registrationDate;
-        /**
-         * 2023-10-16 17:55
-         * 是否登录 1: 是 0: 否
-         * 非必填
-         */
-        private String isLogin;
-        /**
-         * 2023-10-16 17:55
-         * 登录时间 yyyy-mm-dd
-         * 非必填
-         */
-        private String loginTime;
-        /**
-         * 2023-10-16 17:55
-         * 是否申请授信 1: 是 0: 否
-         * 非必填
-         */
-        private String isApplyCredit;
-        /**
-         * 2023-10-16 17:55
-         * 申请授信时间 yyyy-mm-dd
-         * 非必填
-         */
-        private String applyCreditTime;
-        /**
-         * 2023-10-16 17:55
-         * 是否授信通过 1: 是 0: 否
-         * 非必填
-         */
-        private String isCreditPass;
-        /**
-         * 2023-10-16 17:55
-         * 授信通过时间 yyyy-mm-dd
-         * 非必填
-         */
-        private String creditPassTime;
-        /**
-         * 2023-10-16 17:55
-         * 授信金额
-         * 非必填
-         */
-        private String creditAmount;
-        /**
-         * 2023-10-16 17:55
-         * 是否申请提现 1: 是 0: 否
-         * 非必填
-         */
-        private String isApplyWithdrawals;
-        /**
-         * 2023-10-16 17:55
-         * 提现时间 yyyy-mm-dd
-         * 非必填
-         */
-        private String withdrawalsTime;
-        /**
-         * 2023-10-16 17:55
-         * 是否风控通过 1: 是 0: 否
-         * 非必填
-         */
-        private String isRiskPass;
-        /**
-         * 2023-10-16 17:55
-         * 风控通过金额
-         * 非必填
-         */
-        private String riskPassAmount;
-        /**
-         * 2023-10-16 17:55
-         * 放款日期 yyyy-mm-dd
-         * 非必填
-         */
-        private String lendersDate;
-
-        public BizData() {
-        }
-
-        public BizData(String group, String userId, String registrationDate, String isLogin, String loginTime
-                , String isApplyCredit, String applyCreditTime, String isCreditPass, String creditPassTime
-                , String creditAmount, String isApplyWithdrawals, String withdrawalsTime, String isRiskPass
-                , String riskPassAmount, String lendersDate) {
-            this.group = group;
-            this.userId = userId;
-            this.registrationDate = registrationDate;
-            this.isLogin = isLogin;
-            this.loginTime = loginTime;
-            this.isApplyCredit = isApplyCredit;
-            this.applyCreditTime = applyCreditTime;
-            this.isCreditPass = isCreditPass;
-            this.creditPassTime = creditPassTime;
-            this.creditAmount = creditAmount;
-            this.isApplyWithdrawals = isApplyWithdrawals;
-            this.withdrawalsTime = withdrawalsTime;
-            this.isRiskPass = isRiskPass;
-            this.riskPassAmount = riskPassAmount;
-            this.lendersDate = lendersDate;
-        }
-
-        public String getGroup() {
-            return group;
-        }
-
-        public void setGroup(String group) {
-            this.group = group;
-        }
-
-        public String getUserId() {
-            return userId;
-        }
-
-        public void setUserId(String userId) {
-            this.userId = userId;
-        }
-
-        public String getRegistrationDate() {
-            return registrationDate;
-        }
-
-        public void setRegistrationDate(String registrationDate) {
-            this.registrationDate = registrationDate;
-        }
-
-        public String getIsLogin() {
-            return isLogin;
-        }
-
-        public void setIsLogin(String isLogin) {
-            this.isLogin = isLogin;
-        }
-
-        public String getLoginTime() {
-            return loginTime;
-        }
-
-        public void setLoginTime(String loginTime) {
-            this.loginTime = loginTime;
-        }
-
-        public String getIsApplyCredit() {
-            return isApplyCredit;
-        }
-
-        public void setIsApplyCredit(String isApplyCredit) {
-            this.isApplyCredit = isApplyCredit;
-        }
-
-        public String getApplyCreditTime() {
-            return applyCreditTime;
-        }
-
-        public void setApplyCreditTime(String applyCreditTime) {
-            this.applyCreditTime = applyCreditTime;
-        }
-
-        public String getIsCreditPass() {
-            return isCreditPass;
-        }
-
-        public void setIsCreditPass(String isCreditPass) {
-            this.isCreditPass = isCreditPass;
-        }
-
-        public String getCreditPassTime() {
-            return creditPassTime;
-        }
-
-        public void setCreditPassTime(String creditPassTime) {
-            this.creditPassTime = creditPassTime;
-        }
-
-        public String getCreditAmount() {
-            return creditAmount;
-        }
-
-        public void setCreditAmount(String creditAmount) {
-            this.creditAmount = creditAmount;
-        }
-
-        public String getIsApplyWithdrawals() {
-            return isApplyWithdrawals;
-        }
-
-        public void setIsApplyWithdrawals(String isApplyWithdrawals) {
-            this.isApplyWithdrawals = isApplyWithdrawals;
-        }
-
-        public String getWithdrawalsTime() {
-            return withdrawalsTime;
-        }
-
-        public void setWithdrawalsTime(String withdrawalsTime) {
-            this.withdrawalsTime = withdrawalsTime;
-        }
-
-        public String getIsRiskPass() {
-            return isRiskPass;
-        }
-
-        public void setIsRiskPass(String isRiskPass) {
-            this.isRiskPass = isRiskPass;
-        }
-
-        public String getRiskPassAmount() {
-            return riskPassAmount;
-        }
-
-        public void setRiskPassAmount(String riskPassAmount) {
-            this.riskPassAmount = riskPassAmount;
-        }
-
-        public String getLendersDate() {
-            return lendersDate;
-        }
-
-        public void setLendersDate(String lendersDate) {
-            this.lendersDate = lendersDate;
-        }
-
-        @Override
-        public String toString() {
-            return "bizData{" +
-                    "group='" + group + '\'' +
-                    ", userId='" + userId + '\'' +
-                    ", registrationDate='" + registrationDate + '\'' +
-                    ", isLogin='" + isLogin + '\'' +
-                    ", loginTime='" + loginTime + '\'' +
-                    ", isApplyCredit='" + isApplyCredit + '\'' +
-                    ", applyCreditTime='" + applyCreditTime + '\'' +
-                    ", isCreditPass='" + isCreditPass + '\'' +
-                    ", creditPassTime='" + creditPassTime + '\'' +
-                    ", creditAmount='" + creditAmount + '\'' +
-                    ", isApplyWithdrawals='" + isApplyWithdrawals + '\'' +
-                    ", withdrawalsTime='" + withdrawalsTime + '\'' +
-                    ", isRiskPass='" + isRiskPass + '\'' +
-                    ", riskPassAmount='" + riskPassAmount + '\'' +
-                    ", lendersDate='" + lendersDate + '\'' +
-                    '}';
-        }
-    }
-
-
 }
