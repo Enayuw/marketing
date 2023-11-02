d54b1786a0498434e0bfc9902f39a1051bad4e67
juman.wang <juman.wang@brgroup.com>
2023-11-02 12:53:14 +0800

    去掉历史redis调用代码

--- marketing-api/src/test/java/com/br/marketing/api/RedisOldChgClientTest.java
+++ marketing-api/src/test/java/com/br/marketing/api/RedisOldChgClientTest.java
@@ -1,8 +1,6 @@
 package com.br.marketing.api;
 
 import com.alibaba.fastjson.JSON;
-import com.br.marketing.client.RedisChgService;
-import com.br.marketing.client.RedisOldChgClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class RedisOldChgClientTest {
