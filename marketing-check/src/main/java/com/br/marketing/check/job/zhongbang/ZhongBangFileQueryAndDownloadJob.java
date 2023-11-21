f373beb18e402e7747fc966ce997c9156ab279f1
郭泽强 <zeqaing.guo@brgroup.com>
2023-11-21 10:31:23 +0800

    华强：D20231106众邦财富api接口联调-3710099（营销、外呼） + 调度任务

--- marketing-check/src/main/java/com/br/marketing/check/job/zhongbang/ZhongBangFileQueryAndDownloadJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/zhongbang/ZhongBangFileQueryAndDownloadJob.java
@@ -2,7 +2,13 @@ package com.br.marketing.check.job.zhongbang;
 
 import com.alibaba.fastjson.JSONObject;
 import com.br.marketing.common.utils.BrExecutors;
+import com.br.marketing.entity.LocalFileExample;
+import com.br.marketing.entity.TransferActionFront;
+import com.br.marketing.mapper.LocalFileMapper;
+import com.br.marketing.service.Impl.JobManager;
+import com.br.marketing.service.Impl.TableCreateServiceImpl;
 import com.br.marketing.service.Impl.zhongbang.ZhongBangService;
+import com.br.marketing.service.SyncConfigService;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
@@ -11,12 +17,19 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.File;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 
 /**
+ * 众邦财富文件拉取
  * D20231102众邦财富FileSDK文件查询-3710099（营销→客户）
  * http://c.100credit.cn/pages/viewpage.action?pageId=125078844
  * <p>
@@ -36,54 +49,185 @@ public class ZhongBangFileQueryAndDownloadJob extends AbstractSimpleElasticJob {
     @Resource
     private MarketingCommonConfig marketingCommonConfig;
 
+    @Resource
+    private TableCreateServiceImpl tableCreateService;
+
+    @Resource
+    private SyncConfigService syncConfigService;
+
+    @Resource
+    private JobManager jobManager;
+
+    @Resource
+    private LocalFileMapper localFileMapper;
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final LocalTime repeatPullAsOfTime = LocalTime.parse("12:00");
+
     @Override
     public void process(JobExecutionMultipleShardingContext context) {
+        String okFileExtension = ".ok";
+        String txtFileExtension = ".txt";
         long start = System.currentTimeMillis();
-        /* param 格式：apiCode或{"apiCode":"yyyy-MM-dd或yyyy-MM-ddTHH:mm:ss,yyyy-MM-ddTHH:mm:ss"}
-         * eg1：7410994
-         * 或
-         * eg2：{"7410994":"2023-08-25"}
-         * 或
-         * eg2：{"7410994":"2023-08-25T00:00:00,2023-08-25T23:59:59"}
+        /*
+         * param格式：{"apiCode":"yyyyMMdd,yyyy-MM-dd HH:mm:ss,yyyy-MM-dd HH:mm:ss"}
          */
         String parameter = context.getJobParameter();
-        JSONObject jsonObject = new JSONObject();
-        int poolSize = marketingCommonConfig.getZhongBangTransferPushDaasThreadPoolSize();
-        ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(poolSize, poolSize, 5);
-        try {
-            if (StringUtils.isBlank(parameter)) {
-                jsonObject.put("3710094", LocalDate.now().toString());
-
-            } else {
-                try {
-                    jsonObject = JSONObject.parseObject(parameter);
-                } catch (Exception e) {
-                    jsonObject.put(parameter, LocalDate.now().toString());
-                    log.warn(e.getMessage(), e);
+        JSONObject paramJson = StringUtils.isBlank(parameter) ? null : JSONObject.parseObject(parameter);
+        String localDate = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+        Integer status = 2;
+        Map<String, List<Map<String, String>>> config2Map = marketingCommonConfig.getZhongBangPullFileDataConfigMap();
+        Map<String, LinkedHashMap<String, String>> configMap = zhongBangPullFileDataConfigDefault(config2Map);
+        int size = configMap.size();
+        if (size < 1) {
+            return;
+        }
+        configMap.forEach((apiCode, v) -> {
+            List<String> dateStrList = getDateStrList(apiCode, paramJson, localDate);
+            String dateStr = dateStrList.get(0);
+            String beginDateTime = dateStrList.get(1);
+            String endDateTime = dateStrList.get(2);
+            String cId = tableCreateService.getCId(apiCode);
+            String filePath = syncConfigService.getPullCustomerFilePath(apiCode).concat(LocalDate.now()
+                    .format(DateTimeFormatter.BASIC_ISO_DATE)).concat(File.separator);
+            ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(1, 50, new SynchronousQueue<>());
+            v.forEach((fileName, tableHead) -> {
+                String fileNameNew = fileName.endsWith(txtFileExtension) ? fileName.replace(txtFileExtension
+                        , "") : fileName.endsWith("_") ? fileName.concat(dateStr) : fileName;
+                TransferActionFront frontData = jobManager.getFrontData(apiCode, localDate, 1
+                        , fileNameNew.concat(txtFileExtension));
+                Long id;
+                boolean b;
+                if (frontData == null) {
+                    // 创建任务记录
+                    frontData = newTransferActionFront(localDate, apiCode, fileNameNew.concat(txtFileExtension));
+                    id = jobManager.saveFrontData(frontData);
+                    b = zhongBangService.zhongBangFileQueryAndDownload(apiCode, cId, fileNameNew.concat(okFileExtension)
+                            , tableHead, filePath.concat(fileNameNew).concat(File.separator), beginDateTime, endDateTime
+                            , threadPool);
+                } else if (frontData.getStatus().equals(status)) {
+                    // 已成任务已经完成
+                    return;
+                } else {
+                    id = frontData.getId();
+                    if (LocalTime.now().isBefore(repeatPullAsOfTime) || getLocalFileCount(apiCode, cId, fileNameNew.concat(txtFileExtension)) < 1) {
+                        // 任务未完成且没有到截至时间
+                        b = zhongBangService.zhongBangFileQueryAndDownload(apiCode, cId, fileNameNew.concat(okFileExtension)
+                                , tableHead, filePath.concat(fileNameNew).concat(File.separator), beginDateTime, endDateTime
+                                , threadPool);
+                    } else {
+                        // 任务未完成已到截至时间,如果已存在文件记录，就标记任务完成了
+                        b = true;
+                    }
                 }
-            }
-            jsonObject.forEach((k, v) -> {
-                String[] dateTimeStr = v.toString().split(",");
-                zhongBangService.pushTransferToDaasRealTimeUserOneAndCustomer(k, threadPool, dateTimeStr);
-
+                if (b) {
+                    jobManager.updateFrontDataStatus(id, status);
+                }
             });
             threadPool.shutdown();
-            long taskCount = -1;
-            long timeout = 10;
-            while (!threadPool.awaitTermination(timeout, TimeUnit.SECONDS)) {
-                long completedTask2Count = threadPool.getCompletedTaskCount();
-                if (taskCount == completedTask2Count) {
-                    break;
+            try {
+                while (!threadPool.awaitTermination(5, TimeUnit.MINUTES)) {
+                    log.warn("众邦财富FileSDK文件下载任务等待入库任务结束，预估还有任务{}", threadPool.getTaskCount());
                 }
-                taskCount = completedTask2Count;
+            } catch (InterruptedException e) {
+                log.error(e.getMessage(), e);
+                Thread.currentThread().interrupt();
             }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            threadPool.shutdownNow();
-        }
+        });
         long end = System.currentTimeMillis();
-        log.warn("众邦众邦财富FileSDK文件下载任务结束，参数信息:{}，运行耗时:{}"
-                , jsonObject, end - start);
+        log.warn("众邦财富FileSDK文件下载任务结束，job参数信息:{};speed参数信息:{}，运行耗时:{}s"
+                , parameter, configMap, (end - start) / 1000);
+    }
+
+    /**
+     * 2023-11-18 17:49
+     * 任务执行的默认参数
+     */
+    private Map<String, LinkedHashMap<String, String>> zhongBangPullFileDataConfigDefault(Map<String, List<Map<String, String>>> zhongBangPullFileDataConfigMap) {
+        Map<String, LinkedHashMap<String, String>> map = new HashMap<>(4);
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        if (zhongBangPullFileDataConfigMap == null) {
+            linkedHashMap.put("original_caifu_"
+                    , "custNum|@|id|@|cell|@|name|@|gender|@|age|@|region|@|registerTime|@|ifRegister|@|ifApply|@|ifLogin|@|loginTime|@|applyResult|@|productStartTime|@|productEndTime|@|ifApplyAmount|@|ifLent|@|userType");
+            linkedHashMap.put("original_daikuan_"
+                    , "custNum|@|id|@|cell|@|name|@|gender|@|age|@|region|@|registerTime|@|ifRegister|@|ifApply|@|ifLogin|@|loginTime|@|userType");
+            linkedHashMap.put("transform_"
+                    , "custNum|@|ifLogin1|@|ifApply1|@|applyTime|@|applyproductName|@|applyAmount|@|ifLent1|@|lentTime|@|lentAmount|@|pushTime|@|userType");
+            map.put("3710099", linkedHashMap);
+        } else {
+            zhongBangPullFileDataConfigMap.forEach((k, l) -> {
+                l.forEach(m -> m.forEach(linkedHashMap::put));
+                map.put(k, linkedHashMap);
+            });
+        }
+        return map;
+    }
+
+
+    /**
+     * 2023-11-18 17:49
+     * 查询文件下载情况
+     */
+    private int getLocalFileCount(String apiCode, String cId, String fileName) {
+        LocalFileExample example = new LocalFileExample();
+        example.createCriteria().andApiCodeEqualTo(apiCode).andCidEqualTo(cId)
+                .andFileNameEqualTo(fileName)
+                .andCreateTimeGreaterThanOrEqualTo(Date.from(
+                        LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()))
+                .andFileTypeEqualTo("zhongbang_caifu");
+        return localFileMapper.countByExample(example);
+    }
+
+    /**
+     * 2023-11-18 17:49
+     * 创建任务
+     */
+    private TransferActionFront newTransferActionFront(String loaclDate, String apiCode, String fileName) {
+        TransferActionFront frontData = new TransferActionFront();
+        frontData.setActionData(loaclDate);
+        frontData.setRemark(fileName);
+        frontData.setActionType(1);
+        frontData.setApiCode(apiCode);
+        return frontData;
+    }
+
+    /**
+     * 2023-11-18 17:47
+     * 根据apiCode获取参数中的时间
+     * 时间数组中index位置0为文件名称中拼接的日期(yyyyMMdd),1为查询文件的开始时间(yyyy-MM-dd HH:mm:ss),2为查询文件的结束时间(yyyy-MM-dd HH:mm:ss)
+     */
+    private List<String> getDateStrList(String apiCode, JSONObject parameJson, String localDate) {
+        List<String> list = new ArrayList<>();
+        String dateStr;
+        String beginDateTime;
+        String endDateTime;
+        String regex = ",";
+        int lengthIs3 = 3;
+        int lengthIs2 = 2;
+        String[] dateTimeStr;
+        if (parameJson == null || (dateTimeStr = parameJson.getString(apiCode).split(regex)).length == 0) {
+            dateStr = localDate;
+            beginDateTime = LocalDate.now().plusDays(-1).atStartOfDay().format(DATE_TIME_FORMATTER);
+            endDateTime = LocalDate.now().atTime(23, 59, 59)
+                    .atZone(ZoneId.systemDefault()).format(DATE_TIME_FORMATTER);
+        } else if (lengthIs3 == dateTimeStr.length) {
+            dateStr = dateTimeStr[0];
+            beginDateTime = dateTimeStr[1];
+            endDateTime = dateTimeStr[2];
+        } else if (lengthIs2 == dateTimeStr.length) {
+            dateStr = localDate;
+            beginDateTime = dateTimeStr[0];
+            endDateTime = dateTimeStr[1];
+        } else {
+            dateStr = dateTimeStr[0];
+            beginDateTime = LocalDate.now().plusDays(-1).atStartOfDay().format(DATE_TIME_FORMATTER);
+            endDateTime = LocalDate.now().atTime(23, 59, 59)
+                    .atZone(ZoneId.systemDefault()).format(DATE_TIME_FORMATTER);
+        }
+        list.add(dateStr);
+        list.add(beginDateTime);
+        list.add(endDateTime);
+        return list;
     }
 }
 
