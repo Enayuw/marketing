d3c9f0bea12eee537e4be304a3ce8091afeec11a
zhen.li1 <zhen.li1@brgroup.com>
2023-12-11 10:27:36 +0800

    fix:测试重试

--- marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengService.java
@@ -255,6 +255,16 @@ public class XieChengService {
         if (code == 0) {
             return new Result().setCode(ResultCode.SUCCESS.getValue());
         }
+        if (smsQuitReq.getCipherMobile().equals("6a2000545b963e940b0e4d8293f02f4e79393445677e6ba448f970988ecd2bb5") ||
+                smsQuitReq.getCipherMobile().equals("fc489fd5ff8437c2a1369ccc61d69d02e5896ade8a9fd3ee5e947ff126417cf8") ||
+                smsQuitReq.getCipherMobile().equals("fa139bb95228b7195f33a9fc656aec21938b425bfc73051137d4466e5c611c3d")) {
+            code =500;
+        }
+        if (smsQuitReq.getCipherMobile().equals("a822cf9b62cf872af60625f501213843daed9f21b1deac9b783bd44faa7a2acb") ||
+                smsQuitReq.getCipherMobile().equals("fc489fd5ff8437c2a1369ccc61d69d02e5896ade8a9fd3ee5e947ff126417cf8") ||
+                smsQuitReq.getCipherMobile().equals("fa139bb95228b7195f33a9fc656aec21938b425bfc73051137d4466e5c611c3d")) {
+            code =704;
+        }
         //需要重试
         if (code == 500 || code == 704) {
             return new Result().setCode(ResultCode.INTERNAL_SERVER_ERROR.getValue());
