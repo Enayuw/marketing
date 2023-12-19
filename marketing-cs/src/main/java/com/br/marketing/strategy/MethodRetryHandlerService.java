e042e7d0b55f3d0949e9bc069a1d4071e0161344
郭泽强 <zeqaing.guo@brgroup.com>
2023-12-19 19:10:41 +0800

    华强：通用有效期改造一期 * 滴滴拨打明细推送DidiCallingRecordJob有效期改造

--- marketing-cs/src/main/java/com/br/marketing/strategy/MethodRetryHandlerService.java
+++ marketing-cs/src/main/java/com/br/marketing/strategy/MethodRetryHandlerService.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.br.marketing.bo.SaveReachDeleteRecordReqBO;
+import com.br.marketing.bo.SyncUserValidityPeriodsBO;
 import com.br.marketing.bo.ZaMarketDataBO;
 import com.br.marketing.client.RedisChgService;
 import com.br.marketing.client.dassservice.DassServiceClient;
@@ -892,9 +893,12 @@ public class MethodRetryHandlerService {
                 marketingTransferSyncUser.setRequestData(LocalDate.now().toString());
                 marketingTransferSyncUser.setCustNum(custNum);
                 // 判断是否有效
-                MarketingSyncUser newValidityPeriodData = transferDataValidityPeriodService.getMarketingSyncUserDidi(marketingTransferSyncUser, null);
-                if (newValidityPeriodData != null) {
-                    updateDidiCallRecord.setCell(newValidityPeriodData.getCell());
+                Map<String, SyncUserValidityPeriodsBO> validityPeriodsBOMap = transferDataValidityPeriodService
+                        .getValidityPeriodsByCustNum(Collections.singleton(custNum), apiCode, null);
+                SyncUserValidityPeriodsBO bo;
+                List<MarketingSyncUser> syncUsers;
+                if ((bo = validityPeriodsBOMap.get(custNum)) != null && (syncUsers = bo.getSyncUsers()).size() > 0) {
+                    updateDidiCallRecord.setCell(syncUsers.get(0).getCell());
                     // 调接口推送
                     DiDiReqVO diDiReqVO = new DiDiReqVO();
                     diDiReqVO.setMediaName(meidaName);
