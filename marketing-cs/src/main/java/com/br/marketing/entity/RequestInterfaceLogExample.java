c545e0fb4e114b7069abd3ec33e1038691433262
guangchao.zhang <505740550@qq.com>
2023-12-26 13:46:44 +0800

    解决扫描问题

--- marketing-cs/src/main/java/com/br/marketing/entity/RequestInterfaceLogExample.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/RequestInterfaceLogExample.java
@@ -64,10 +64,10 @@ public class RequestInterfaceLogExample {
         distinct = false;
     }
 
-    protected abstract static class GeneratedCriteria {
+    protected abstract static class AbstractGeneratedCriteria {
         protected List<Criterion> criteria;
 
-        protected GeneratedCriteria() {
+        protected AbstractGeneratedCriteria() {
             super();
             criteria = new ArrayList<Criterion>();
         }
@@ -686,7 +686,7 @@ public class RequestInterfaceLogExample {
         }
     }
 
-    public static class Criteria extends GeneratedCriteria {
+    public static class Criteria extends AbstractGeneratedCriteria {
 
         protected Criteria() {
             super();
