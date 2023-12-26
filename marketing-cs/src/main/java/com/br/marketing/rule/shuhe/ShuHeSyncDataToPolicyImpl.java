5745432593118eb15f7c5099a74a17a711f10bcf
hong.chen <hong.chen@brgroup.com>
2023-12-26 11:26:14 +0800

    update:修复：只处理上传类型数据

--- marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeSyncDataToPolicyImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeSyncDataToPolicyImpl.java
@@ -5,6 +5,7 @@ import com.br.common.util.BrCipherMaker;
 import com.br.marketing.client.intelligentcustomerservice.input.PushMarketingUserDetailByRuleDTO;
 import com.br.marketing.context.ProcessHandlerContext;
 import com.br.marketing.entity.MarketingSyncUser;
+import com.br.marketing.entity.MarketingTransferSyncUser;
 import com.br.marketing.rule.AssembleData;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.br.marketing.strategy.InterfaceHandlerEnum;
@@ -53,7 +54,11 @@ public class ShuHeSyncDataToPolicyImpl implements AssembleData<PushMarketingUser
 
     @Override
     public boolean isNeedAssemble(Object transmitFact, ProcessHandlerContext context) throws Exception {
-        return true;
+        if (transmitFact instanceof MarketingSyncUser) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
