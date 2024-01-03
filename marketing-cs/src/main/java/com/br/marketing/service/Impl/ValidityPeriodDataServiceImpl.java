9f0f9938547b34d39ecd758e45caef7449eea611
yu.xia <yu.xia@brgroup.com>
2024-01-03 17:05:59 +0800

    ice代码下线

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/ValidityPeriodDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/ValidityPeriodDataServiceImpl.java
@@ -1,6 +1,5 @@
 package com.br.marketing.service.Impl;
 
-import IceInternal.Ex;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.br.marketing.common.commondto.ApiNoDataResult;
