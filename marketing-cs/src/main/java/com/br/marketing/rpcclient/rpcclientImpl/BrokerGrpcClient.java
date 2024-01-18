0fb326957546b4b3a7f094d1c34ed108070c5fa8
yu.xia <yu.xia@brgroup.com>
2024-01-18 17:42:50 +0800

    升级guava版本，解决有冲突的服务

--- marketing-cs/src/main/java/com/br/marketing/rpcclient/rpcclientImpl/BrokerGrpcClient.java
+++ marketing-cs/src/main/java/com/br/marketing/rpcclient/rpcclientImpl/BrokerGrpcClient.java
@@ -142,13 +142,13 @@ public class BrokerGrpcClient {
             public void onSuccess(ResponseBean result) {
                 //此处提供测试验证，开发人员、测试人员通过这块看ELK/消费端对应索引是否有日志判断验证整个流程是否正常
                 //可通过yaml中增加环境变量MOM_TEST=true来触发此处打印日志仅用来验证用，注意header包引入后如果DLF=off则此处的info日志才可触发，如果预发验证无问题，上线此处可去掉
-//                momLog.warn("[test verify mom log result]{}", result);
-
+                momLog.warn("[test verify mom log result]{}message:{}code:{}", result.getResult(),result.getMessage(),result.getCode());
+                log.warn("[test verify mom log result]{}message:{}code:{}", result.getResult(),result.getMessage(),result.getCode());
                 if (!"0".equals(result.getCode())) {
                     //建议返回码非0的时候配置响应的阶梯告警
                     String msgLog = AlertLog.buildWarnMessage(AlarmSendCodeEnum.EXCEPTION_COMMON.getCode()
                             , "返回的异常码值："+result.getCode()+"发送的消息："+msg
-                            , "MOM日志发送成功，但是码值异常");
+                            , "MOM日志发送完成，但是码值异常");
                     log.warn(msgLog);
                 }
             }
