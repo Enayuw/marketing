f87f62db2a5006ea8365561dabc2e1c97359cc83
xiang.li4 <xiang.li4@brgroup.com>
2024-01-22 18:19:18 +0800

    D20240102亿联3710065转化数据入库-更新

---
+++ marketing-check/src/main/java/com/br/marketing/check/enums/FileTypeToAssemblerEnum.java
@@ -0,0 +1,29 @@
+package com.br.marketing.check.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+@AllArgsConstructor
+public enum FileTypeToAssemblerEnum {
+
+    YILIAN_TRANSFER_CSV("yilian_transfer_csv", "csvToDbAssembler"),
+    ;
+
+    private String fileType;
+
+    private String assemblerName;
+
+    public static String getByFileType(String fileType){
+        if(StringUtils.isEmpty(fileType)){
+            return "";
+        }
+        for (FileTypeToAssemblerEnum e: FileTypeToAssemblerEnum.values()) {
+            if (e.getFileType().equals(fileType)) {
+                return e.getAssemblerName();
+            }
+        }
+        return "";
+    }
+}
