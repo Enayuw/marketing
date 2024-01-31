f2ec9ce9e425c35e62e7a438a36c05179931a09f
guangxiu.li <guangxiu.li@brgroup.com>
2024-01-31 14:36:18 +0800

    同程集团迁移可营销名单推送客户

--- marketing-data-monkey/src/main/java/com/br/marketing/monkey/job/tongcheng/TongChengOperationPushToCustomerJob.java
+++ marketing-data-monkey/src/main/java/com/br/marketing/monkey/job/tongcheng/TongChengOperationPushToCustomerJob.java
@@ -46,7 +46,7 @@ public class TongChengOperationPushToCustomerJob extends AbstractSimpleElasticJo
             String formatted = now.format(formatter);
             Integer today = Integer.parseInt(formatted);
             //查询待推数据 查询条件b_tongcheng_agent_mtk_data：is_delete=0 、 status=1 、 push_status=0
-            example.createCriteria().andApiCodeEqualTo(apiCode).andCreateDateEqualTo(today).andIsDeleteEqualTo(0).andStatusEqualTo(1).andPushStatusEqualTo(0);
+            example.createCriteria().andApiCodeEqualTo(apiCode).andIsDeleteEqualTo(0).andStatusEqualTo(1).andPushStatusEqualTo(0);
             List<TongChengAgent> tongChengAgents = tongChengAgentMapper.selectByExample(example);
             if (CollectionUtils.isEmpty(tongChengAgents)) {
                 log.warn("同程集团运营名单推送客户量级为空！");
