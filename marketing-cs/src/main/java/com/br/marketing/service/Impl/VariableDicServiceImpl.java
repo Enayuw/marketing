751c2571ea037177e9b61d88f27abeea15cb53fc
郭泽强 <zeqaing.guo@brgroup.com>
2024-03-07 22:49:57 +0800

    华强：T20240222中台场景字段自动添加&报警 + 兼容null 值问题

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/VariableDicServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/VariableDicServiceImpl.java
@@ -419,7 +419,8 @@ public class VariableDicServiceImpl implements VariableDicService {
         marketingSyncUser.setAppletDate(parseTime.toLocalDate().toString());
         String basicDate = parseTime.format(DateTimeFormatter.BASIC_ISO_DATE);
         // 添加有效期范围
-        if (marketingCommonConfig.getCustomizeConfigValidDefaultApiCodes().contains(apiCode)) {
+        if (marketingCommonConfig.getCustomizeConfigValidDefaultApiCodes().contains(apiCode)
+                && StringUtils.hasText(collectionDTO.getTaskId())) {
             marketingSyncUser.setCusBatch(collectionDTO.getTaskId());
             // 定制生成有效期
             configValidDateDefault(marketingSyncUser
