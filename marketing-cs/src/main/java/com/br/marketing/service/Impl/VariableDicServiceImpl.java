b65543facd8c7ea92331fdcf0779b52ad2e8b4d9
郭泽强 <zeqaing.guo@brgroup.com>
2024-03-11 13:49:34 +0800

    华强：T20240222中台场景字段自动添加&报警 + 优化

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/VariableDicServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/VariableDicServiceImpl.java
@@ -235,6 +235,10 @@ public class VariableDicServiceImpl implements VariableDicService {
                 log.error("未获取到apiCode，消息内容：{}", msgStr);
                 return result;
             }
+            if (CollectionUtils.isEmpty(apiDataInfoDTO.getArgList())) {
+                log.error("场景内容新增中，新增数量为0，消息内容：{}", msgStr);
+                return result;
+            }
             String cId = StringUtils.hasText(apiDataInfoDTO.getCid()) ? apiDataInfoDTO.getCid()
                     : tableCreateService.getCId(apiCode);
             if (StringUtils.isEmpty(cId)) {
