a58680259c6d3629784caa90977fa9be2ec2659b
hong.chen <hong.chen@brgroup.com>
2024-03-12 14:18:40 +0800

    update:sonar扫描

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingSyncReportServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingSyncReportServiceImpl.java
@@ -125,7 +125,8 @@ public class MarketingSyncReportServiceImpl implements MarketingSyncReportServic
                                         .plusDays(1L).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
                                 String appletDateStart = LocalDate.parse(uploadDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
                                         .minusDays(1L).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                                List<String> appletDateList = syncReportMapper.getAppletDatetikv_(apiCode, userType, createStartDate, createEndDate, appletDateStart);
+                                List<String> appletDateList = syncReportMapper.getAppletDatetikv_(apiCode, userType, createStartDate, createEndDate
+                                        , appletDateStart);
                                 for (String appletDate : appletDateList) {
                                     //上传开始时间
                                     String appletBeginTime = getAppletTime(apiCode, userType, appletDate, Boolean.TRUE);
