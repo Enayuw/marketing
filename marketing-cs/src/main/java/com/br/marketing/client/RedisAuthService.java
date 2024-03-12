0450d6992a7ace1058ce5d5540a33f52046c87ca
juman.wang <juman.wang@brgroup.com>
2024-03-12 11:01:39 +0800

    redis和pulsar升级

--- marketing-cs/src/main/java/com/br/marketing/client/RedisAuthService.java
+++ marketing-cs/src/main/java/com/br/marketing/client/RedisAuthService.java
@@ -17,42 +17,63 @@ import java.util.Map;
 public class RedisAuthService {
 
     public void set(String key, String value, String typeNo) {
-        BrRedisClient<String, Object> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        marketingRedisClient.set(getUnionKey(key, typeNo), value);
+        try {
+            BrRedisClient<String, Object> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            marketingRedisClient.set(getUnionKey(key, typeNo), value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * 写入一个key并且添加过期时间
+     *
      * @param key
      * @param value
      * @param period 秒
      * @param typeNo
      */
     public void set(String key, String value, Integer period, String typeNo) {
-        BrRedisClient<String, Object> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        marketingRedisClient.setex(getUnionKey(key, typeNo), period, value);
+        try {
+            BrRedisClient<String, Object> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            marketingRedisClient.setex(getUnionKey(key, typeNo), period, value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public String get(String key, String typeNo) {
-        BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        String str = marketingRedisClient.get(getUnionKey(key, typeNo));
-        return str;
+        try {
+            BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            String str = marketingRedisClient.get(getUnionKey(key, typeNo));
+            return str;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     //删除key
     public long del(String key) {
-        BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        long size = marketingRedisClient.del(key);
-        return size;
+        try {
+            BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            long size = marketingRedisClient.del(key);
+            return size;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     //删除key
     public long del(String key, String typeNo) {
-        BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        long size = marketingRedisClient.del(getUnionKey(key, typeNo));
-        return size;
+        try {
+            BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            long size = marketingRedisClient.del(getUnionKey(key, typeNo));
+            return size;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     //设置联合主键
@@ -66,14 +87,22 @@ public class RedisAuthService {
     }
 
     public boolean exists(String key, String typeNo) {
-        BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        Long flag = marketingRedisClient.exists(getUnionKey(key, typeNo));
-        return !new Long(0L).equals(flag);
+        try {
+            BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            Long flag = marketingRedisClient.exists(getUnionKey(key, typeNo));
+            return !new Long(0L).equals(flag);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Boolean expire(String key, String typeNo, int seconds) {
-        BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
-        Boolean result = marketingRedisClient.expire(getUnionKey(key, typeNo), seconds);
-        return result;
+        try {
+            BrRedisClient<String, String> marketingRedisClient = BrRedisClients.getRedisClient("marketing_redis");
+            Boolean result = marketingRedisClient.expire(getUnionKey(key, typeNo), seconds);
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
