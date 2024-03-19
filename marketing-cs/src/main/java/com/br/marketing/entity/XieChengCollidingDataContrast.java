97a92a83800a789896a83ad36ccb90bd79ae82f6
yu.xia <yu.xia@brgroup.com>
2024-03-19 21:22:13 +0800

    新增周期表、非周期表、日志表、对比表的 entity、mapper、xml

---
+++ marketing-cs/src/main/java/com/br/marketing/entity/XieChengCollidingDataContrast.java
@@ -0,0 +1,96 @@
+package com.br.marketing.entity;
+
+import java.util.Date;
+
+public class XieChengCollidingDataContrast {
+    /**
+     * 
+     */
+    private Long id;
+
+    /**
+     * 规则条件 1 ，2，,3
+     */
+    private Integer ruleTypeFlag;
+
+    /**
+     * 手机号
+     */
+    private String cellSha256CodeList;
+
+    /**
+     * 状态 0-正常 1-删除
+     */
+    private Integer isDelete;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 扩展字段
+     */
+    private String extend;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRuleTypeFlag() {
+        return ruleTypeFlag;
+    }
+
+    public void setRuleTypeFlag(Integer ruleTypeFlag) {
+        this.ruleTypeFlag = ruleTypeFlag;
+    }
+
+    public String getCellSha256CodeList() {
+        return cellSha256CodeList;
+    }
+
+    public void setCellSha256CodeList(String cellSha256CodeList) {
+        this.cellSha256CodeList = cellSha256CodeList == null ? null : cellSha256CodeList.trim();
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getExtend() {
+        return extend;
+    }
+
+    public void setExtend(String extend) {
+        this.extend = extend == null ? null : extend.trim();
+    }
+}
