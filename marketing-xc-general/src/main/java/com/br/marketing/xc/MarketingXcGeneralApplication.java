41b92bd686bdaeffed5250bb0180b322613fb4c2
guangchao.zhang <505740550@qq.com>
2024-03-19 18:26:17 +0800

    修改配置文件

--- marketing-xc-general/src/main/java/com/br/marketing/xc/MarketingXcGeneralApplication.java
+++ marketing-xc-general/src/main/java/com/br/marketing/xc/MarketingXcGeneralApplication.java
@@ -1,7 +1,6 @@
 package com.br.marketing.xc;
 
 import com.br.cloud.boot.EnablePrometheusEndpoint;
-import com.br.cloud.counter.EnableBrCounter;
 import com.br.cloud.hystrix.EnableHystrixPrometheus;
 import com.br.cloud.jvm.EnablePrometheusJvm;
 import com.br.cloud.web.EnablePrometheusTiming;
