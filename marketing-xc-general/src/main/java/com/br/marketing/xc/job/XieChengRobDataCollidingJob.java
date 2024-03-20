541af757f0ed51e6606805d34d4e4f11ab3a293c
senyang.zheng <senyang.zheng@brgroup.com>
2024-03-20 20:03:21 +0800

    feat(中邮数据提取任务):
    1、中邮数据提取任务新增提取字段

---
+++ marketing-xc-general/src/main/java/com/br/marketing/xc/job/XieChengRobDataCollidingJob.java
@@ -0,0 +1,31 @@
+package com.br.marketing.xc.job;
+
+import com.br.marketing.service.Impl.xc.XieChengRobDataCollidingService;
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Random;
+
+@Slf4j
+@Component
+public class XieChengRobDataCollidingJob extends AbstractSimpleElasticJob {
+
+    @Resource
+    private XieChengRobDataCollidingService xieChengRobDataCollidingService;
+
+    @Override
+    public void process(JobExecutionMultipleShardingContext context) {
+        //TODO 增加强制开关和条件开关
+        Boolean strongSwitch = new Random().nextInt(10) % 2 == 0 ? Boolean.FALSE : Boolean.TRUE;
+        Boolean conditionSwitch = new Random().nextInt(10) % 2 == 0 ? Boolean.FALSE : Boolean.TRUE;
+        //强制开关开启强制撞库，强制开关关闭且条件开关关闭开始撞库
+        if (strongSwitch || !conditionSwitch) {
+            xieChengRobDataCollidingService.collidingData();
+        } else {
+            log.info("不进行撞库");
+        }
+    }
+}
