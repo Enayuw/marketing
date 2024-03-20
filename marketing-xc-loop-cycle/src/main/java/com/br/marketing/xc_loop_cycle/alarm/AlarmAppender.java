e67226cfa774aacf8a904d4df6725f0a924e98e1
hong.chen <hong.chen@brgroup.com>
2024-03-20 10:51:10 +0800

    携程true数据撞库新服务部署

--- marketing-xc-loop-cycle/src/main/java/com/br/marketing/xc_loop_cycle/alarm/AlarmAppender.java
+++ marketing-xc-loop-cycle/src/main/java/com/br/marketing/xc_loop_cycle/alarm/AlarmAppender.java
@@ -23,7 +23,7 @@ public class AlarmAppender<E> extends RollingFileAppender<E>  {
             }
             try {
                 SystemExceptionServiceImpl emailService= MarketingXcLoopCycleApplication.ac.getBean(SystemExceptionServiceImpl.class);
-                emailService.sendAlarmPrintStack(loggerName+":</br>"+content.toString(), "MARKETING_XC_GENERAL",throwableProxy);
+                emailService.sendAlarmPrintStack(loggerName+":</br>"+content.toString(), "marketing-xc-loop-cycle",throwableProxy);
             } catch (Exception e) {
                 log.warn("Exception",e);
             }
