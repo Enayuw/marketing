9e2be7ce43e72ef2a76accdb003307ac0ad2c240
yu.xia <yu.xia@brgroup.com>
2024-03-20 21:27:10 +0800

    剔除多余import，解决报错

--- marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataLogMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataLogMapper.java
@@ -1,8 +1,6 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.XieChengCollidingDataLog;
-import com.br.marketing.entity.XieChengCollidingDataLogExample;
-import com.br.marketing.entity.XieChengCollidingDataRob;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
--- marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataLoopCycleArchiveMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataLoopCycleArchiveMapper.java
@@ -1,8 +1,6 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.XieChengCollidingDataLoopCycleArchive;
-import com.br.marketing.entity.XieChengCollidingDataLoopCycleArchiveExample;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
