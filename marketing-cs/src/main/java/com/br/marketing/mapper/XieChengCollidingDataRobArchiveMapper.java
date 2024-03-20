816e2f2586c31893c88dbc337a3595620decd214
yu.xia <yu.xia@brgroup.com>
2024-03-20 14:26:16 +0800

    新增周期表、非周期表、日志表、对比表的 base

--- marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataRobArchiveMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataRobArchiveMapper.java
@@ -6,32 +6,5 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface XieChengCollidingDataRobArchiveMapper {
-    int countByExample(XieChengCollidingDataRobArchiveExample example);
-
-    int deleteByExample(XieChengCollidingDataRobArchiveExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(XieChengCollidingDataRobArchive record);
-
-    int insertSelective(XieChengCollidingDataRobArchive record);
-
-    List<XieChengCollidingDataRobArchive> selectByExampleWithBLOBs(XieChengCollidingDataRobArchiveExample example);
-
-    List<XieChengCollidingDataRobArchive> selectByExample(XieChengCollidingDataRobArchiveExample example);
-
-    XieChengCollidingDataRobArchive selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") XieChengCollidingDataRobArchive record, @Param("example") XieChengCollidingDataRobArchiveExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") XieChengCollidingDataRobArchive record, @Param("example") XieChengCollidingDataRobArchiveExample example);
-
-    int updateByExample(@Param("record") XieChengCollidingDataRobArchive record, @Param("example") XieChengCollidingDataRobArchiveExample example);
-
-    int updateByPrimaryKeySelective(XieChengCollidingDataRobArchive record);
-
-    int updateByPrimaryKeyWithBLOBs(XieChengCollidingDataRobArchive record);
-
-    int updateByPrimaryKey(XieChengCollidingDataRobArchive record);
+public interface XieChengCollidingDataRobArchiveMapper extends XieChengCollidingDataRobArchiveMapperBase{
 }
--- marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataRobMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengCollidingDataRobMapper.java
@@ -6,32 +6,6 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface XieChengCollidingDataRobMapper {
-    int countByExample(XieChengCollidingDataRobExample example);
+public interface XieChengCollidingDataRobMapper extends XieChengCollidingDataRobMapperBase{
 
-    int deleteByExample(XieChengCollidingDataRobExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(XieChengCollidingDataRob record);
-
-    int insertSelective(XieChengCollidingDataRob record);
-
-    List<XieChengCollidingDataRob> selectByExampleWithBLOBs(XieChengCollidingDataRobExample example);
-
-    List<XieChengCollidingDataRob> selectByExample(XieChengCollidingDataRobExample example);
-
-    XieChengCollidingDataRob selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") XieChengCollidingDataRob record, @Param("example") XieChengCollidingDataRobExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") XieChengCollidingDataRob record, @Param("example") XieChengCollidingDataRobExample example);
-
-    int updateByExample(@Param("record") XieChengCollidingDataRob record, @Param("example") XieChengCollidingDataRobExample example);
-
-    int updateByPrimaryKeySelective(XieChengCollidingDataRob record);
-
-    int updateByPrimaryKeyWithBLOBs(XieChengCollidingDataRob record);
-
-    int updateByPrimaryKey(XieChengCollidingDataRob record);
 }
