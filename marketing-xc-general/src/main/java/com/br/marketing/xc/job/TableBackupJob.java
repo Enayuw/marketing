cc5fad5c84e25955e95505cbe622bba38026bd94
yu.xia <yu.xia@brgroup.com>
2024-03-21 11:22:43 +0800

    代码优化

--- marketing-xc-general/src/main/java/com/br/marketing/xc/job/TableBackupJob.java
+++ marketing-xc-general/src/main/java/com/br/marketing/xc/job/TableBackupJob.java
@@ -31,12 +31,17 @@ public class TableBackupJob extends AbstractSimpleElasticJob {
     @Override
     public void process(JobExecutionMultipleShardingContext context) {
         String uuid = UUID.randomUUID().toString();
-        log.warn("TableBackupJob-start-{}",uuid);
         String jobParameter = context.getJobParameter();
+        log.warn("TableBackupJob-start-{}-jobParam:[{}]",uuid,jobParameter);
         Boolean loopCycleSkipFlag = Boolean.FALSE;
         Boolean robSkipFlag = Boolean.FALSE;
         Boolean logSkipFlag = Boolean.FALSE;
         Boolean contrastSkipFlag = Boolean.FALSE;
+        LocalDateTime currentTime = LocalDateTime.now();
+        // 获取当前时间
+        String nowString = currentTime.format(ymd);
+        // 获取当前时间前14天
+        String daysAgo14 = currentTime.minusDays(7).format(ymd);
         if (StringUtils.isNotBlank(jobParameter)) {
             JSONObject param = JSON.parseObject(jobParameter);
             if(null != param){
@@ -56,28 +61,28 @@ public class TableBackupJob extends AbstractSimpleElasticJob {
                 if(null != contrast){
                     contrastSkipFlag = contrast;
                 }
+                String currentDate = param.getString("currentDate");
+                if(StringUtils.isNotBlank(currentDate)){
+                    nowString = currentDate;
+                    daysAgo14 = currentDate;
+                }
             }
         }
-        LocalDateTime currentTime = LocalDateTime.now();
-        // 获取当前时间
-        String nowString = currentTime.format(ymd);
-        // 获取当前时间前14天
-        String daysAgo14 = currentTime.minusDays(7).format(ymd);
         int limit = 10000;
 
-        //1.周期表 b_xiecheng_colliding_data_loop_cycle 备份代码
+        //1.周期表
         if(!loopCycleSkipFlag){
             tableBackupService.loopCycleHandle(daysAgo14,limit);
         }
-        //2.非周期表b_xiecheng_colliding_data_rob 备份代码
+        //2.非周期表
         if(!robSkipFlag){
             tableBackupService.robHandle(daysAgo14,limit);
         }
-        //3.撞库结果日志表b_xiecheng_colliding_data_log 备份代码
+        //3.撞库结果日志表
         if(!logSkipFlag){
             tableBackupService.logHandle(daysAgo14,limit);
         }
-        //4.对比表b_xiecheng_colliding_data_contrast 数据删除代码
+        //4.对比表
         if(!contrastSkipFlag){
             tableBackupService.contrastHandle(nowString,limit);
         }
