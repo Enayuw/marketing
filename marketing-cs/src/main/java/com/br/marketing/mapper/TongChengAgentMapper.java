79e8fa11ec0f520bc03cc53092b572354d37e087
xiang.li4 <xiang.li4@brgroup.com>
2024-03-26 15:45:49 +0800

    20240322-上传记录增加文件类型-增加对应对客推送量级更新

--- marketing-cs/src/main/java/com/br/marketing/mapper/TongChengAgentMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/TongChengAgentMapper.java
@@ -33,6 +33,6 @@ public interface TongChengAgentMapper {
 
     List<TongChengAgent> tongChengGroupOperationDataPage(@Param("minId") Long minId, @Param("apiCode") String apiCode, @Param("num") int num);
 
-    List<Map<String, Object>> queryQuantityGroupByLocalId(TongChengPushQueryQuantityDTO queryParams);
+    List<Map<String, Object>> queryQuantityGroupByLocalId(TongChengPushQueryQuantityDTO params);
 
 }
