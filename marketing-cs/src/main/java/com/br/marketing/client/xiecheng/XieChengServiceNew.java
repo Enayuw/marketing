f13003f1dbca2907580f294981737e1f9c87f981
hong.chen <hong.chen@brgroup.com>
2024-03-27 00:22:42 +0800

    update:增加707直接报警

--- marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengServiceNew.java
+++ marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengServiceNew.java
@@ -11,6 +11,7 @@ import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.constants.rediskey.RedisKeyConstant;
 import com.br.marketing.common.utils.StringUtils;
 import com.br.marketing.entity.XieChengSmsCollidingReq;
+import com.br.marketing.service.Impl.xc.XcExceptionDataRetryService;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
@@ -28,7 +30,6 @@ import java.util.*;
  * 携程处理
  * <p>
  * --------------------------------
- *
  * @BelongsProject: marketing
  * @BelongsPackage: com.br.marketing.client.xiecheng
  * @Description: 携程处理
@@ -74,41 +75,44 @@ public class XieChengServiceNew {
     @Autowired
     private RedisChgService redisChgService;
 
-    private HashMap<String,String> getTestMap(List<String> sha256CodeList){
+    @Resource
+    XcExceptionDataRetryService retryService;
+
+    private HashMap<String, String> getTestMap(List<String> sha256CodeList) {
         JSONObject map = new JSONObject();
-        if(marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(2)){
-            map.put("code",707);
-            map.put("msg","测试挡板非0异常");
-        }else {
-            map.put("code",0);
-            map.put("msg","success");
+        if (marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(2)) {
+            map.put("code", 707);
+            map.put("msg", "测试挡板非0异常");
+        } else {
+            map.put("code", 0);
+            map.put("msg", "success");
         }
 
         JSONArray jsonArray = new JSONArray();
-        for(int i=0;i<sha256CodeList.size();i++){
+        for (int i = 0; i < sha256CodeList.size(); i++) {
             JSONObject dataMap = new JSONObject();
             String s = sha256CodeList.get(i);
-            dataMap.put("sha256Code",s);
-            if(i%2==0){
-                dataMap.put("result",true);
-                dataMap.put("releaseTime", DateUtil.formatDateTime(DateUtil.offsetDay(new Date(),7)));
-            }else {
-                dataMap.put("result",false);
+            dataMap.put("sha256Code", s);
+            if (i % 2 == 0) {
+                dataMap.put("result", true);
+                dataMap.put("releaseTime", DateUtil.formatDateTime(DateUtil.offsetDay(new Date(), 7)));
+            } else {
+                dataMap.put("result", false);
             }
-            dataMap.put("orgChannel","测试orgChannel");
-            dataMap.put("mktLevel","测试mktLevel");
-            dataMap.put("info","测试info");
+            dataMap.put("orgChannel", "测试orgChannel");
+            dataMap.put("mktLevel", "测试mktLevel");
+            dataMap.put("info", "测试info");
             jsonArray.add(dataMap);
         }
-        map.put("data",jsonArray);
+        map.put("data", jsonArray);
         HashMap<String, String> resMap = new HashMap<>();
-        if(marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(1)){
-            resMap.put("httpcode","201");
-        }else {
-            resMap.put("httpcode","200");
+        if (marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(1)) {
+            resMap.put("httpcode", "201");
+        } else {
+            resMap.put("httpcode", "200");
         }
 
-        resMap.put("content",map.toString());
+        resMap.put("content", map.toString());
         return resMap;
 
     }
@@ -116,7 +120,6 @@ public class XieChengServiceNew {
     /**
      * 携程撞库方法
      * 使用范围：TRUE数据撞库、FALSE数据撞库、异常数据重试撞库
-     *
      * @param sha256CodeList
      * @return
      */
@@ -135,9 +138,9 @@ public class XieChengServiceNew {
         retMap.put("data", FinanceAESUtils.encryptStr(JSON.toJSONString(xieChengSmsCollidingReq), smsCollidingKey, smsCollidingIv));
         retMap.put("sign", FinanceAESUtils.signLocal(retMap, smsCollidingSingKey));
         HashMap<String, String> resMap;
-        if(marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(0)){
+        if (marketingCommonConfig.getXieChengSmsCollidingRetrySwitch().get(0)) {
             resMap = getTestMap(sha256CodeList);
-        }else {
+        } else {
             resMap = httpProxyClient.sendByCodeWithLog(retMap, smsCollidingOpenUrl, smsCollidingIsProxy,
                     MediaType.APPLICATION_JSON_UTF8_VALUE, JSON.toJSONString(xieChengSmsCollidingReq), true, false);
         }
@@ -151,12 +154,23 @@ public class XieChengServiceNew {
             return new Result().setCode(ResultCode.SUCCESS.getValue()).setDate(JSON.toJSONString(resMap));
         } else {
             if (code == 707) {
-                shutDownConditionSwitch();
+                shutDownConditionSwitchAndAlert("携程撞库暂停通知:code返回707");
             }
 
             return new Result().setCode(ResultCode.FAIL.getValue()).setDate(JSON.toJSONString(resMap));
         }
 
+    }
+
+    /**
+     * 关闭条件开关并发送钉钉告警
+     * @param msg
+     */
+    public void shutDownConditionSwitchAndAlert(String msg) {
+        // 关闭条件开关
+        shutDownConditionSwitch();
+        // 钉钉告警
+        retryService.sendDingDingAlert("携程撞库暂停通知", msg);
     }
 
     /**
@@ -168,9 +182,9 @@ public class XieChengServiceNew {
         // 当前时间至23:59:59
         LocalDateTime endOfDay = now.with(LocalTime.MAX);
         // 计算当前时间至23:59:59的秒数
-        int secondsUntilEndOfDay = (int)ChronoUnit.SECONDS.between(now, endOfDay);
+        int secondsUntilEndOfDay = (int) ChronoUnit.SECONDS.between(now, endOfDay);
         try {
-            redisChgService.setex(RedisKeyConstant.XIECHENG_CONDITIONSWITCH, "false",secondsUntilEndOfDay);
+            redisChgService.setex(RedisKeyConstant.XIECHENG_CONDITIONSWITCH, "false", secondsUntilEndOfDay);
         } catch (Exception e) {
             log.error("携程数据撞库，关闭redis条件开关失败:" + e.getMessage(), e);
         }
