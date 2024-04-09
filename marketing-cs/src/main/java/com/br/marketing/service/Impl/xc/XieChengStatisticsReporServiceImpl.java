0ec24c5aed8344ca9e9c11c164ab98718171eddf
yu.xia <yu.xia@brgroup.com>
2024-04-09 13:39:04 +0800

    新增Excel数据排序

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/xc/XieChengStatisticsReporServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/xc/XieChengStatisticsReporServiceImpl.java
@@ -103,6 +103,7 @@ public class XieChengStatisticsReporServiceImpl implements XieChengStatisticsRep
                 .andApiCodeEqualTo(apiCode)
                 .andIsDeleteEqualTo(0)
                 .andReportTimeBetween(firstDayString,requestData);
+        xieChengStatisticsReportExample.setOrderByClause(" report_time asc");
         List<XieChengStatisticsReport> xieChengStatisticsReports =
                 xieChengStatisticsReportMapper.selectByExample(xieChengStatisticsReportExample);
         return xieChengStatisticsReports;
