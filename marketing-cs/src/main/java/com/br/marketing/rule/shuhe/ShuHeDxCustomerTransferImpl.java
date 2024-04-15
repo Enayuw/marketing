fe0702ce6a5e78b993f633dc9ceb4a90098a47db
郭泽强 <zeqaing.guo@brgroup.com>
2024-04-15 16:10:09 +0800

    华强：D20240408数禾电销转化过滤-3710117（营销→外呼） + 新增条件

---
+++ marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeDxCustomerTransferImpl.java
@@ -0,0 +1,142 @@
+package com.br.marketing.rule.shuhe;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.br.common.util.BrCipherMaker;
+import com.br.marketing.client.robotaiapi.input.ConversionData;
+import com.br.marketing.common.utils.DateHelper;
+import com.br.marketing.context.ProcessHandlerContext;
+import com.br.marketing.context.impl.ShuHeRuleCollectDataImpl;
+import com.br.marketing.entity.MarketingSyncUser;
+import com.br.marketing.entity.MarketingTransferSyncUser;
+import com.br.marketing.rule.AssembleData;
+import com.br.marketing.strategy.InterfaceHandlerEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 数禾电销全场景推送客服转化 业务
+ * <p>
+ * 需求标题：D20240408数禾电销转化过滤-3710117（营销→外呼）
+ * 需求地址：https://c.100credit.cn/pages/viewpage.action?pageId=155694316
+ * <p>
+ * 方案地址：https://c.100credit.cn/pages/viewpage.action?pageId=155697965
+ *
+ * @author Guo Zeqiang
+ * @dateTime 2024/4/15 14:35
+ */
+@Service
+@Slf4j
+public class ShuHeDxCustomerTransferImpl implements AssembleData<ConversionData> {
+
+    private static final List<String> USER_TYPE_LIST = new ArrayList<>(Arrays.asList(
+            "轻资产", "促复借", "促首借", "促申完", "促首登"));
+    /**
+     * 2024-04-15 15:48
+     * 已转化
+     */
+    private final static String HAS_TRANSFER = "0";
+    /**
+     * 2024-04-15 15:47
+     * 已失效
+     */
+    private final static String HAS_EXPIRE = "2";
+
+    @Override
+    public ConversionData assemble(Object transmitFact, ProcessHandlerContext context) throws Exception {
+        MarketingTransferSyncUser transfer = (MarketingTransferSyncUser) transmitFact;
+        ConversionData conversionData = new ConversionData();
+        conversionData.setDataId(transfer.getId().toString());
+        conversionData.setInversionStatus(USER_TYPE_LIST.contains(transfer.getUserType()) ? HAS_TRANSFER : HAS_EXPIRE);
+        JSONObject reserveFieldObject = JSONObject.parseObject(transfer.getReserveField1());
+        String usrForbidCallEndTim = reserveFieldObject.getString("usr_forbid_call_end_tim");
+        if (StringUtils.hasText(usrForbidCallEndTim)) {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(
+                    DateHelper.LINE_DATE_COLON_TIME_FORMAT);
+            try {
+                LocalDateTime localDateTime = LocalDateTime.parse(usrForbidCallEndTim, DateTimeFormatter.ofPattern(
+                        DateHelper.LINE_DATE_COLON_TIME_FORMAT_SSS)).atZone(ZoneId.systemDefault()).toLocalDateTime();
+                conversionData.setExpireDate(localDateTime.format(dateTimeFormatter));
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                try {
+                    conversionData.setExpireDate(LocalDate.parse(usrForbidCallEndTim)
+                            .atTime(23, 59, 59).atZone(ZoneId.systemDefault()).format(dateTimeFormatter));
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        ShuHeRuleCollectDataImpl.ShuHeRuleNecessaryData shuHeRuleNecessaryData =
+                (ShuHeRuleCollectDataImpl.ShuHeRuleNecessaryData) context.getRuleNecessaryData();
+        Map<String, MarketingSyncUser> map = shuHeRuleNecessaryData.getCustomerMap();
+        if (map != null && map.containsKey(transfer.getCustNum())) {
+            MarketingSyncUser marketingSyncUser = map.get(transfer.getCustNum());
+            conversionData.setPhone(BrCipherMaker.getInstance().decode(marketingSyncUser.getCell()));
+        } else {
+            conversionData.setPhone("");
+        }
+        return conversionData;
+    }
+
+    @Override
+    public boolean isNeedAssemble(Object transmitFact, ProcessHandlerContext context) {
+        if (transmitFact instanceof MarketingTransferSyncUser) {
+            Integer isDelay = context.getMqFact().getIsDelay();
+            if (isDelay == null || isDelay != 1) {
+                MarketingTransferSyncUser transfer = (MarketingTransferSyncUser) transmitFact;
+                String reserveField1 = transfer.getReserveField1();
+                if (StringUtils.hasText(reserveField1) && JSON.isValid(reserveField1)) {
+                    JSONObject reserveFieldObject = JSONObject.parseObject(reserveField1);
+                    String usrForbidCallEndTim = reserveFieldObject.getString("usr_forbid_call_end_tim");
+                    if (StringUtils.hasText(usrForbidCallEndTim)) {
+                        return true;
+                    }
+                    String userType = transfer.getUserType();
+                    switch (userType) {
+                        case "轻资产":
+                        case "促复借":
+                        case "促首借":
+                            String usrLoanSucBtcashLimt1st = reserveFieldObject.getString("usr_loan_suc_btcash_limt_1st");
+                            if (StringUtils.hasText(usrLoanSucBtcashLimt1st)) {
+                                return true;
+                            }
+                        case "促申完":
+                        case "促首登":
+                            String usrCompAplAiClSpUse = reserveFieldObject.getString("usr_comp_apl_ai_cl_sp_use");
+                            if (StringUtils.hasText(usrCompAplAiClSpUse)) {
+                                return true;
+                            }
+                        default:
+                            return false;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String label() {
+        return "ShuHe_Dx_TransferData_CustomerTransfer";
+    }
+
+    @Override
+    public Integer dataDirection() {
+        return InterfaceHandlerEnum.CUSTOMER_TRANSFER.getCode();
+    }
+
+    @Override
+    public Integer ruleDataCollection() {
+        return null;
+    }
+}
