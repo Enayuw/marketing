b490392258cee3a6d7e902eb65c255d506ea79e5
郭泽强 <zeqaing.guo@brgroup.com>
2024-04-17 15:51:31 +0800

    华强：D20240408数禾电销转化过滤-3710117（营销→外呼） * 异常抛出

--- marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeDxCustomerTransferImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/shuhe/ShuHeDxCustomerTransferImpl.java
@@ -71,13 +71,9 @@ public class ShuHeDxCustomerTransferImpl implements AssembleData<ConversionData>
                 conversionData.setExpireDate(localDateTime.format(dateTimeFormatter));
             } catch (Exception e) {
                 log.warn(e.getMessage(), e);
-                try {
-                    // 兼容时间格式错误情况，默认到当天结束
-                    conversionData.setExpireDate(LocalDate.parse(usrForbidCallEndTim)
-                            .atTime(23, 59, 59).atZone(ZoneId.systemDefault()).format(dateTimeFormatter));
-                } catch (Exception exception) {
-                    log.error(e.getMessage(), e);
-                }
+                // 兼容时间格式错误情况，默认到当天结束
+                conversionData.setExpireDate(LocalDate.parse(usrForbidCallEndTim).atTime(23, 59, 59)
+                        .atZone(ZoneId.systemDefault()).format(dateTimeFormatter));
             }
         } else {
             conversionData.setInversionStatus(HAS_TRANSFER);
