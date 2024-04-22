97072f6cd4a6a12852db8259efcaf159cf2cb042
hong.chen <hong.chen@brgroup.com>
2024-04-22 18:43:20 +0800

    update:携程跑分数据同步

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/xc/XieChengRuleScoreToDbServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/xc/XieChengRuleScoreToDbServiceImpl.java
@@ -2,10 +2,14 @@ package com.br.marketing.service.Impl.xc;
 
 import com.br.marketing.entity.StraHisFile;
 import com.br.marketing.entity.StraHisFileExample;
+import com.br.marketing.entity.XieChengRuleScoreRecord;
+import com.br.marketing.entity.XieChengRuleScoreRecordExample;
 import com.br.marketing.mapper.StraHisFileMapper;
+import com.br.marketing.mapper.XieChengRuleScoreRecordMapper;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
@@ -25,18 +29,35 @@ public class XieChengRuleScoreToDbServiceImpl implements XieChengRuleScoreToDbSe
     private MarketingCommonConfig marketingCommonConfig;
     @Resource
     StraHisFileMapper straHisFileMapper;
+    @Resource
+    XieChengRuleScoreRecordMapper scoreRecordMapper;
     @Override
     public void process() {
         marketingCommonConfig.getXieChengCollidingDataProcessApiCodes().forEach(apiCode -> {
-            // XieChengRuleScoreToDbJob查询跑分记录表，条件：api_code=3710058 && stra_his_file.status=2 && stra_his_file.create_time>当前日期-3天
             LocalDate createTimeStartLocalDate = LocalDate.now().minusDays(marketingCommonConfig.getXieChengRuleScoreToDbLastDays());
             Date createTimeStartDate = Date.from(createTimeStartLocalDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
             StraHisFileExample straHisFileExample = new StraHisFileExample();
-            straHisFileExample.createCriteria().andApiCodeEqualTo(apiCode).andStatusEqualTo(2).andCreateTimeGreaterThanOrEqualTo(createTimeStartDate);
+            straHisFileExample.createCriteria().andApiCodeEqualTo(apiCode).andStatusEqualTo(2).andCreateTimeGreaterThanOrEqualTo(createTimeStartDate).andTypeEqualTo(2);
             List<StraHisFile> straHisFiles = straHisFileMapper.selectByExample(straHisFileExample);
             straHisFiles.forEach(straHisFile -> {
+                XieChengRuleScoreRecordExample scoreRecordExample = new XieChengRuleScoreRecordExample();
+                scoreRecordExample.createCriteria().andIsDeleteEqualTo(0).andBatchNumberEqualTo(straHisFile.getBatchNumber());
+                List<XieChengRuleScoreRecord> xieChengRuleScoreRecords = scoreRecordMapper.selectByExample(scoreRecordExample);
 
+                if (CollectionUtils.isEmpty(xieChengRuleScoreRecords)) {
+                    XieChengRuleScoreRecord scoreRecord = new XieChengRuleScoreRecord();
+                    scoreRecord.setApiCode(apiCode);
+                    scoreRecord.setRecordStatus(1);
+                    scoreRecord.setBatchNumber(straHisFile.getBatchNumber());
+                    scoreRecord.setFileName(straHisFile.getFileName());
+                    scoreRecord.setCreateTime(new Date());
+                    scoreRecord.setUpdateTime(new Date());
+
+                    scoreRecordMapper.insertSelective(scoreRecord);
+
+
+                }
             });
         });
 
