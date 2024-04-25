7768d3b22f4004ad62227513578402bee1796348
senyang.zheng <senyang.zheng@brgroup.com>
2024-04-25 22:03:44 +0800

    feat(携程自动化撞库V1.1): 携程非周期撞库
    XieChengCollidingDataRob entry相关新增collidingCount字段

--- marketing-cs/src/main/java/com/br/marketing/entity/XieChengCollidingDataRobExample.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/XieChengCollidingDataRobExample.java
@@ -485,6 +485,66 @@ public class XieChengCollidingDataRobExample {
             return (Criteria) this;
         }
 
+        public Criteria andCollidingCountIsNull() {
+            addCriterion("colliding_count is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountIsNotNull() {
+            addCriterion("colliding_count is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountEqualTo(Integer value) {
+            addCriterion("colliding_count =", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountNotEqualTo(Integer value) {
+            addCriterion("colliding_count <>", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountGreaterThan(Integer value) {
+            addCriterion("colliding_count >", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountGreaterThanOrEqualTo(Integer value) {
+            addCriterion("colliding_count >=", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountLessThan(Integer value) {
+            addCriterion("colliding_count <", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountLessThanOrEqualTo(Integer value) {
+            addCriterion("colliding_count <=", value, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountIn(List<Integer> values) {
+            addCriterion("colliding_count in", values, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountNotIn(List<Integer> values) {
+            addCriterion("colliding_count not in", values, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountBetween(Integer value1, Integer value2) {
+            addCriterion("colliding_count between", value1, value2, "collidingCount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCollidingCountNotBetween(Integer value1, Integer value2) {
+            addCriterion("colliding_count not between", value1, value2, "collidingCount");
+            return (Criteria) this;
+        }
+
         public Criteria andIsDeleteIsNull() {
             addCriterion("is_delete is null");
             return (Criteria) this;
@@ -542,6 +602,76 @@ public class XieChengCollidingDataRobExample {
 
         public Criteria andIsDeleteNotBetween(Integer value1, Integer value2) {
             addCriterion("is_delete not between", value1, value2, "isDelete");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendIsNull() {
+            addCriterion("extend is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendIsNotNull() {
+            addCriterion("extend is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendEqualTo(String value) {
+            addCriterion("extend =", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendNotEqualTo(String value) {
+            addCriterion("extend <>", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendGreaterThan(String value) {
+            addCriterion("extend >", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendGreaterThanOrEqualTo(String value) {
+            addCriterion("extend >=", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendLessThan(String value) {
+            addCriterion("extend <", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendLessThanOrEqualTo(String value) {
+            addCriterion("extend <=", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendLike(String value) {
+            addCriterion("extend like", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendNotLike(String value) {
+            addCriterion("extend not like", value, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendIn(List<String> values) {
+            addCriterion("extend in", values, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendNotIn(List<String> values) {
+            addCriterion("extend not in", values, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendBetween(String value1, String value2) {
+            addCriterion("extend between", value1, value2, "extend");
+            return (Criteria) this;
+        }
+
+        public Criteria andExtendNotBetween(String value1, String value2) {
+            addCriterion("extend not between", value1, value2, "extend");
             return (Criteria) this;
         }
 
