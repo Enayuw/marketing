6cb85d769cc8b34581febf7e98ba1cf910195aee
xiang.li4 <xiang.li4@brgroup.com>
2024-05-21 10:11:49 +0800

    营销服务拆分-marketing-data-monkey-增加job开关配置

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -282,6 +282,11 @@ public class MarketingCommonConfig {
     private Boolean checkJobOnlineSwitch;
 
     /**
+     * 服务上线job开关-Monkey
+     */
+    private Boolean dataMonkeyJobOnlineSwitch;
+
+    /**
      * 离线跑批入es文件 线程数
      */
     private Integer OffLineInserEsThreadNum;
