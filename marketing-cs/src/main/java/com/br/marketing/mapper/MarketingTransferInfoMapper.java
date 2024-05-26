2030d04c2798258131d2f14d5e76d6c233902407
xiang.li4 <xiang.li4@brgroup.com>
2024-05-26 17:24:13 +0800

    D20240523宜信ai调用转化过滤自动化推送-查询转化数据标志

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferInfoMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferInfoMapper.java
@@ -52,4 +52,7 @@ public interface MarketingTransferInfoMapper extends MarketingTransferInfoMapper
 
     int getTransferUnresolvedCount(@Param("apiCode") String apiCode, @Param("startDate") String startDate,@Param("endDate") String endDate);
 
+    @Select("SELECT request_id FROM b_marketing_transfer_info WHERE  api_code=#{apiCode} and date_format(create_time,'%Y-%m-%d') = #{bizDate} and last=#{last} limit 1")
+    String  queryByApiCodAndLast(@Param("apiCode") String apiCode, @Param("bizDate") String bizDate, @Param("last") String last);
+
 }
