84e50962acf9baae59ef48e3f9049cdb0f4db533
yu.xia <yu.xia@brgroup.com>
2024-06-07 16:32:08 +0800

    数据清洗提交修改报错

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/DataCleaningGeneralServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/DataCleaningGeneralServiceImpl.java
@@ -218,8 +218,8 @@ public class DataCleaningGeneralServiceImpl implements IDataCleaningGeneralServi
                                         // 根据动态配置赋值
                                         if(StringUtils.isNotBlank(fieldVO.getDynamicData())){
                                             value = tableMap.get(fieldVO.getDynamicData());
-                                        }else if(StringUtils.isNotBlank(fieldVO.getDefalutValue())) {
-                                            value = fieldVO.getDefalutValue();
+                                        }else if(StringUtils.isNotBlank(fieldVO.getDefaultValue())) {
+                                            value = fieldVO.getDefaultValue();
                                         }
                                     }
                                     // 字典项不为空 则进行字典项映射
