f3b4092f37dcd3a36a3fd924ee36b7571858c23d
hong.chen <hong.chen@brgroup.com>
2024-06-11 10:52:40 +0800

    update:跑分每日定时叠加有效期、优先级插队

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTaskAutoBuildConfigMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTaskAutoBuildConfigMapper.java
@@ -1,31 +1,4 @@
 package com.br.marketing.mapper;
 
-import com.br.marketing.entity.MarketingTaskAutoBuildConfig;
-import com.br.marketing.entity.MarketingTaskAutoBuildConfigExample;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
-public interface MarketingTaskAutoBuildConfigMapper {
-    int countByExample(MarketingTaskAutoBuildConfigExample example);
-
-    int deleteByExample(MarketingTaskAutoBuildConfigExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(MarketingTaskAutoBuildConfig record);
-
-    int insertSelective(MarketingTaskAutoBuildConfig record);
-
-    List<MarketingTaskAutoBuildConfig> selectByExample(MarketingTaskAutoBuildConfigExample example);
-
-    MarketingTaskAutoBuildConfig selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") MarketingTaskAutoBuildConfig record, @Param("example") MarketingTaskAutoBuildConfigExample example);
-
-    int updateByExample(@Param("record") MarketingTaskAutoBuildConfig record, @Param("example") MarketingTaskAutoBuildConfigExample example);
-
-    int updateByPrimaryKeySelective(MarketingTaskAutoBuildConfig record);
-
-    int updateByPrimaryKey(MarketingTaskAutoBuildConfig record);
+public interface MarketingTaskAutoBuildConfigMapper extends MarketingTaskAutoBuildConfigMapperBase{
 }
