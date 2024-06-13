a219803c89965038538e820534cd23eebd39d7a5
hong.chen <hong.chen@brgroup.com>
2024-06-13 22:00:21 +0800

    update:增加注释

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/SoleDbStrategyImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/SoleDbStrategyImpl.java
@@ -528,7 +528,7 @@ public class SoleDbStrategyImpl implements SoleStrategyService {
         if (StringUtils.isBlank(conditionStr)) {
             return new Result<String>().setCode(ResultCode.FAIL.getValue()).setMessage("规则不能传空");
         }
-        RuleConditionVo conditionVo;
+        RuleConditionVo conditionVo = new RuleConditionVo();
         try {
             conditionVo = JSON.parseObject(conditionStr, new TypeReference<RuleConditionVo>() {
             }.getType());
