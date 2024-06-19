288fc3bfd3aaafb39bc658ac21438c21559e14c6
guangxiu.li <guangxiu.li@brgroup.com>
2024-06-19 12:17:23 +0800

    榕树转化数据提取紧急需求代码优化

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
@@ -234,7 +234,7 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
                             listResult = serviceImpl.buildTransferTask(marketingCustomer.getApiCode(), myParam);
                         }
                     } catch (Exception e) {
-                        log.error("该apiCode:{}执行数据提取任务获取锁:{}异常", marketingCustomer.getApiCode(), redisKey);
+                        log.error("该apiCode:{}执行数据提取任务获取锁:{}异常", marketingCustomer.getApiCode(), redisKey, e);
                     } finally {
                         redisChgService.unlock(redisKey, value);
                     }
@@ -260,10 +260,12 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
                                 } else {
                                     //第一次执行，查询为空，不会进行删除，直接返回
                                     //第二次执行，删除b_sync_log的记录
-                                    List<SyncLog> syncLogList = loanSyncLogMapper.querySyncLog(ImmutableMap.of("apiCode", marketingCustomer.getApiCode(), "fileName", datum.getFileName()));
+                                    List<SyncLog> syncLogList = loanSyncLogMapper
+                                            .querySyncLog(ImmutableMap.of("apiCode", marketingCustomer.getApiCode(), "fileName", datum.getFileName()));
                                     if (!CollectionUtils.isEmpty(syncLogList)) {
                                         if (syncLogList.size() != 1) {
-                                            log.warn("重新执行数据提取异常，apiCode={},fileName={},syncLogSize={}", marketingCustomer.getApiCode(), datum.getFileName(), syncLogList.size());
+                                            log.warn("重新执行数据提取异常，apiCode={},fileName={},syncLogSize={}"
+                                                    , marketingCustomer.getApiCode(), datum.getFileName(), syncLogList.size());
                                             return;
                                         }
                                         SyncLogExample syncLogExample = new SyncLogExample();
