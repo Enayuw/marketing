26b53d2e37bc69320f5919fd3bde1303b99456fc
senyang.zheng <senyang.zheng@brgroup.com>
2024-07-01 10:52:09 +0800

    feat(360): 360促完件上传数据推决策
    添加上传数据基本字段

--- marketing-cs/src/main/java/com/br/marketing/rule/qifu/QiFuCuWanJianSyncDataToPolicyImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/qifu/QiFuCuWanJianSyncDataToPolicyImpl.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
 
+import com.br.marketing.client.halo.EncryptUtil;
+import com.github.pagehelper.util.StringUtil;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
@@ -52,7 +54,24 @@ public class QiFuCuWanJianSyncDataToPolicyImpl implements AssembleData<PushMarke
         dto.setPhone(syncUser.getCellMd5());
         JSONObject jsonObject = JSONObject.parseObject(syncUser.getReserveField1());
         JSONObject variables = new JSONObject();
-        variables.put("taskId", syncUser.getCusBatch());
+        if (StringUtil.isNotEmpty(syncUser.getCusBatch())) {
+            variables.put("taskId", syncUser.getCusBatch());
+        }
+        if (StringUtil.isNotEmpty(syncUser.getRequestBatch())) {
+            variables.put("requestBatch", syncUser.getRequestBatch());
+        }
+        if (StringUtil.isNotEmpty(syncUser.getIdCard())) {
+            variables.put("idCard", EncryptUtil.getMd5Str(syncUser.getIdCard()));
+        }
+        if (StringUtil.isNotEmpty(syncUser.getName())) {
+            variables.put("name", EncryptUtil.getMd5Str(syncUser.getName()));
+        }
+        if (StringUtil.isNotEmpty(syncUser.getGroupType())) {
+            variables.put("groupType", syncUser.getGroupType());
+        }
+        if (StringUtil.isNotEmpty(syncUser.getRegisterDate())) {
+            variables.put("registerDate", syncUser.getRegisterDate());
+        }
         for (String key : jsonObject.keySet()) {
             variables.put(key, jsonObject.get(key));
         }
