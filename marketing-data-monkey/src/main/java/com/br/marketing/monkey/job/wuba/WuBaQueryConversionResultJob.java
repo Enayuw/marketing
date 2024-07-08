3f37720f228215d3b7ca337b84253e2db47e2c9a
xiang.li4 <xiang.li4@brgroup.com>
2024-07-08 17:55:36 +0800

    D20240704-58新客提交营销名单-3710155-job提交

---
+++ marketing-data-monkey/src/main/java/com/br/marketing/monkey/job/wuba/WuBaQueryConversionResultJob.java
@@ -0,0 +1,32 @@
+package com.br.marketing.monkey.job.wuba;
+
+import com.br.marketing.service.Impl.tongcheng.TongChengOperationPushToCustomerService;
+import com.br.marketing.speedconfig.MarketingCommonConfig;
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * 58新客提交营销名单结果查询
+ *
+ * @Author lixiang
+ * @Date 2024-07-08
+ */
+@Component
+@Slf4j
+public class WuBaQueryConversionResultJob extends AbstractSimpleElasticJob {
+
+    @Resource
+    MarketingCommonConfig marketingCommonConfig;
+
+    @Resource
+    TongChengOperationPushToCustomerService service;
+
+    @Override
+    public void process(JobExecutionMultipleShardingContext context) {
+
+    }
+}
