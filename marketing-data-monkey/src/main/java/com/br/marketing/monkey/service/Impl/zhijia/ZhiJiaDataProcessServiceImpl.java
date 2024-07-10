c72931f881ea2f9819c1ef29479e40d33d1a66a8
zhen.li1 <zhen.li1@brgroup.com>
2024-07-10 17:16:24 +0800

    之家：获取配置job

---
+++ marketing-data-monkey/src/main/java/com/br/marketing/monkey/service/Impl/zhijia/ZhiJiaDataProcessServiceImpl.java
@@ -0,0 +1,27 @@
+package com.br.marketing.monkey.service.Impl.zhijia;
+
+import com.br.marketing.monkey.service.zhijia.ZhiJiaDataProcessService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName ZhiJiaDataProcessServiceImpl
+ * @Description 之家数据处理service
+ * @Author zhen.li1
+ * @Date 2024/7/10 16:40
+ */
+@Service
+@Slf4j
+public class ZhiJiaDataProcessServiceImpl implements ZhiJiaDataProcessService {
+
+
+    @Override
+    public void getCityAndCounty() {
+
+    }
+
+    @Override
+    public void getBrandAndseries() {
+
+    }
+}
