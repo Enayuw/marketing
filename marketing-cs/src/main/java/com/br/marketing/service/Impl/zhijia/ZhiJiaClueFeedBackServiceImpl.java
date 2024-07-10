9e7f7b2489f95b6e8b0707b9fa79ec344f4a2dbe
bingxu.kong <bingxu.kong@brgroup.com>
2024-07-10 20:56:59 +0800

    之家线索v1.1第三版

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/zhijia/ZhiJiaClueFeedBackServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/zhijia/ZhiJiaClueFeedBackServiceImpl.java
@@ -1,17 +1,29 @@
 package com.br.marketing.service.Impl.zhijia;
 
+import com.br.common.encryption.Md5Utils;
 import com.br.marketing.client.zhijia.ZhiJiaClient;
 import com.br.marketing.client.zhijia.input.ReqAddZhiJiaClueDTO;
+import com.br.marketing.client.zhongan.utils.Md5OfZanUtils;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.utils.BrExecutors;
 import com.br.marketing.entity.*;
 import com.br.marketing.mapper.ZhiJiaClueBackDataMapper;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
+import com.google.api.client.util.Base64;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -37,14 +49,8 @@ public class ZhiJiaClueFeedBackServiceImpl implements ZhiJiaClueFeedBackService{
     @Resource
     private ZhiJiaClient zhiJiaClient;
 
-    @Value("${api.zhijia.zhiJiaClientId:00}")
-    private String zhiJiaClientId;
-
-    @Value("${api.zhijia.zhiJiaClientSecret:00}")
-    private String zhiJiaClientSecret;
-
     @Value("${api.zhijia.zhiJiaClientAppid:00}")
-    private Integer zhiJiaClientAppid;
+    private String zhiJiaClientAppid;
 
     @Override
     public void process() {
@@ -80,7 +86,6 @@ public class ZhiJiaClueFeedBackServiceImpl implements ZhiJiaClueFeedBackService{
             log.error("之家创建线索接口线程池关闭！异常", ex);
             Thread.currentThread().interrupt();
         }
-
     }
 
     public void pushZhiJiaCollidingSync(List<ZhiJiaClueBackData> zhiJiaClueBackDataList){
@@ -91,16 +96,18 @@ public class ZhiJiaClueFeedBackServiceImpl implements ZhiJiaClueFeedBackService{
             // 调用车辆信息接口
             try {
                 // 调用高质线索创建接口
-                Result result = zhiJiaClient.addZhiJiaClue(buildAddZhiJiaClue());
+                Result result = zhiJiaClient.addZhiJiaClue(buildAddZhiJiaClue(zhiJiaClueBackData));
                 // 更新结果
                 if (ResultCode.SUCCESS.getValue().equals(result.getCode())) {
-                    //更新成功
+                    //匹配成功更新
                     updatePushStatus(id, 2, result.getMessage());
                 } else {
-                    //更新失败
-                    updatePushStatus(id, 3, result.getMessage());
+                    //匹配失败更新
+                    updatePushStatus(id, 4, result.getMessage());
                 }
             }catch (Exception e){
+                //匹配失败更新
+                updatePushStatus(id, 3, e.getMessage());
                 log.error("调用高质线索创建接口异常！", e.getMessage());
             }
         }
@@ -115,23 +122,45 @@ public class ZhiJiaClueFeedBackServiceImpl implements ZhiJiaClueFeedBackService{
         zhiJiaClueBackDataMapper.updateByExampleSelective(record,example);
     }
 
-    private ReqAddZhiJiaClueDTO buildAddZhiJiaClue() {
-        // 获取access_token
+    private ReqAddZhiJiaClueDTO buildAddZhiJiaClue(ZhiJiaClueBackData zhiJiaClueBackData) {
         // 组装入参
         ReqAddZhiJiaClueDTO dto = new ReqAddZhiJiaClueDTO();
         dto.setAccess_token("");
-        dto.setMobile("");
-        dto.setMobilecode("");
+        dto.setMobile(zhiJiaClueBackData.getCell());
+        dto.setMobilecode(encryptCell(zhiJiaClueBackData.getCell()));
         dto.setCid(1);
         dto.setCountyid(1);
         dto.setBrandid("");
         dto.setSeriesid("");
         dto.setSpecid("");
-        dto.setFirstregtime("");
-        dto.setPlatenum("");
-        dto.setMileage("");
-        dto.setAppid(zhiJiaClientAppid);
+        dto.setFirstregtime(zhiJiaClueBackData.getFirstRegTime());
+        //dto.setPlatenum("");
+        dto.setMileage(zhiJiaClueBackData.getMileAge());
+        dto.setAppid(Integer.valueOf(zhiJiaClientAppid));
         return dto;
+    }
+
+    public String encryptCell(String cell) {
+        String keyStr = StringUtils.substring(Md5Utils.cell32(zhiJiaClientAppid), 0, 16);
+        String ivStr = reverseString(keyStr);
+        return encrypt(cell, keyStr, ivStr);
+    }
+
+    public static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static String encrypt(String plaintext, String keyStr, String ivStr) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec keySpec = new SecretKeySpec(keyStr.getBytes(), "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(ivStr.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+            byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
+            return new String(Base64.encodeBase64(encryptedBytes));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
