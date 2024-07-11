e666d11b423fb333be09827bee6f8c381d7024da
xiang.li4 <xiang.li4@brgroup.com>
2024-07-11 15:23:55 +0800

    58新客-mapper

--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaSubmitConversionDataLogMapperBase.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaSubmitConversionDataLogMapperBase.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface WubaSubmitConversionDataLogMapperBase {
+
     int countByExample(WubaSubmitConversionDataLogExample example);
 
     int deleteByExample(WubaSubmitConversionDataLogExample example);
--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaSubmitConversionDataMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaSubmitConversionDataMapper.java
@@ -1,5 +1,18 @@
 package com.br.marketing.mapper;
 
-public interface WubaSubmitConversionDataMapper extends WubaSubmitConversionDataLogMapperBase{
+import com.br.marketing.entity.WubaSubmitConversionData;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface WubaSubmitConversionDataMapper extends WubaSubmitConversionDataMapperBase{
+
+    List<WubaSubmitConversionData> findByConditionAndPage(
+            @Param("apiCode") String apiCode,
+            @Param("status") Integer requestDate,
+            @Param("pushStatus") Integer pushStatus,
+            @Param("extendSql") String extendSql,
+            @Param("indexId") Long indexId,
+            @Param("pageSize") Integer pageSize);
 
 }
