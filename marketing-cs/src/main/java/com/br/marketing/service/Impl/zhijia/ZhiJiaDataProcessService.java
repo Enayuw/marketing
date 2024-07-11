4aaadd8db7bb754da3a5a0ef4354f79fc25b6c46
guangxiu.li <guangxiu.li@brgroup.com>
2024-07-11 14:20:39 +0800

    车辆信息落库与获取车辆品牌id、车系id——v1.4

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/zhijia/ZhiJiaDataProcessService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/zhijia/ZhiJiaDataProcessService.java
@@ -2,10 +2,7 @@ package com.br.marketing.service.Impl.zhijia;
 
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.dto.zhijia.CityCountyDataDTO;
-import com.br.marketing.entity.ZhiJiaCarSeriesInfo;
-import com.br.marketing.entity.ZhiJiaClueBackData;
-import com.br.marketing.entity.ZhijiaCityConfig;
-import com.br.marketing.entity.ZhijiaCountyConfig;
+import com.br.marketing.entity.*;
 
 import java.util.List;
 
@@ -22,12 +19,19 @@ public interface ZhiJiaDataProcessService {
     CityCountyDataDTO matchCityAndCounty(List<ZhijiaCityConfig>cityList, List<ZhijiaCountyConfig>countyList,ZhiJiaClueBackData zhiJiaClueBackInfo);
 
     /**
-     * 根据sftp信息获取车辆品牌以及车系信息
-     * @author guangxiu.li
-     * @date 2024/7/9 14:15
+     * 根据sftp信息获取车辆品牌
      * @param zhiJiaClueBackInfo
-     * @return java.util.List<ZhiJiaClueBackInfo>
+     * @param zhiJiaCarBrandInfos
+     * @return
      */
-    Result<ZhiJiaCarSeriesInfo> getZhiJiaCarInfo(ZhiJiaClueBackData zhiJiaClueBackInfo);
+    Result<ZhiJiaCarSeriesInfo> getZhiJiaCarBrandInfo(ZhiJiaClueBackData zhiJiaClueBackInfo, List<ZhiJiaCarBrandInfo> zhiJiaCarBrandInfos);
+
+    /**
+     * 根据sftp信息获取车系信息
+     * @param zhiJiaClueBackInfo
+     * @param zhiJiaCarSeriesInfos
+     * @return
+     */
+    Result<ZhiJiaCarSeriesInfo> getZhiJiaCarSeriesInfo(ZhiJiaClueBackData zhiJiaClueBackInfo, List<ZhiJiaCarSeriesInfo> zhiJiaCarSeriesInfos);
 
 }
