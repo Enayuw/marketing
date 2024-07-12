ece9bae065cac29b5d1aae09a73162cc1ab0a12e
hong.chen <hong.chen@brgroup.com>
2024-07-12 21:47:18 +0800

    update:全表增加apicode

--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataLogMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataLogMapper.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface WubaCollidingDataLogMapper extends WubaCollidingDataLogMapperBase {
     void batchSaveByBatchNo(@Param("list") List<WubaCollidingDataLog> list);
-    void batchUpdateResultById(@Param("logs") List<WubaCollidingDataLog> list);
+
+    void batchUpdateResultById(@Param("logs") List<WubaCollidingDataLog> list, @Param("result") Boolean result);
 }
