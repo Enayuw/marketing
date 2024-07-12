27ef4910a30fa5af7d8f1d8791c1f8277ca8b9d8
hong.chen <hong.chen@brgroup.com>
2024-07-12 17:44:41 +0800

    update:全表增加apicode

--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataFrontMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataFrontMapper.java
@@ -6,8 +6,8 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface WubaCollidingDataFrontMapper extends WubaCollidingDataFrontMapperBase {
-    List<WubaCollidingDataFront> selectNoDupDataByCurDate(@Param("localId") Long localId, @Param("apiCode") String apiCode,
-                                                          @Param("minId") Long minId, @Param("pageSize") Integer pageSize);
+    List<WubaCollidingDataFront> selectNoDupDataByCurDatetikv_(@Param("localId") Long localId, @Param("apiCode") String apiCode,
+                                                               @Param("minId") Long minId, @Param("pageSize") Integer pageSize);
 
     void batchUpdatePushStatusByCell(@Param("list") List<WubaCollidingDataFront> wubaCollidingDataFronts,
                                      @Param("localId") Long localId, @Param("apiCode") String apiCode);
