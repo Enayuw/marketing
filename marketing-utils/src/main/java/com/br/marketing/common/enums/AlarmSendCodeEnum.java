377aacdb1ea2c9b24af2c1740c8b68ef57ee0ffe
guangxiu.li <guangxiu.li@brgroup.com>
2024-07-16 10:57:04 +0800

    苏商自动化数据回传提取

--- marketing-utils/src/main/java/com/br/marketing/common/enums/AlarmSendCodeEnum.java
+++ marketing-utils/src/main/java/com/br/marketing/common/enums/AlarmSendCodeEnum.java
@@ -39,10 +39,14 @@ public enum AlarmSendCodeEnum {
     EXCEPTION_NEW_FIELD_CHECK("62006", "接口字段新增检查"),
     // 一般通知,阶梯推送，一般
     EXCEPTION_USUAL_NOTICE("62007", "通知"),
+    //众安通话明细回调,
+    EXCEPTION_ZHONGAN_CALL_RECORD("62008", "众安通话明细回调"),
+    //携程业务报错
+    XIECHENG_RECORD("62009", "携程业务报错"),
     //业务未知错误,立即推送，63000
     SERVICEERROR_UNKNOWN("63000", "业务未知错误"),
-    //众安通话明细回调
-    EXCEPTION_ZHONGAN_CALL_RECORD("62008", "众安通话明细回调"),
+    //三方接口错误,立即推送，64000
+    INTERFACE_ERROR("64000", "三方接口错误"),
 
     //数据治理平台调用marketing-inner-api邮件发送接口使用
     DATA_GOVERNANCE_PLATFORM_SEND_EMAIL("70000", "数据治理平台邮件发送"),
