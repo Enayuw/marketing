3005af8b3a7b4afea791fdaa63073d733f220f93
hong.chen <hong.chen@brgroup.com>
2024-07-18 16:38:06 +0800

    update:sonar扫描

--- marketing-cs/src/main/java/com/br/marketing/mock/custom/wuba/WuBaMockService.java
+++ marketing-cs/src/main/java/com/br/marketing/mock/custom/wuba/WuBaMockService.java
@@ -65,7 +65,7 @@ public class WuBaMockService {
             for (WubaSubmitConversionDataLog log : logs) {
                 JSONObject jsonObject = new JSONObject();
                 //
-                String randomNumber = new Random().ints(1, 10)
+                String randomNumber = RANDOM.ints(1, 10)
                         .limit(10).mapToObj(String::valueOf).collect(Collectors.joining()) + "0";
                 jsonObject.put("id", randomNumber);
                 jsonObject.put("mobileEncrypt", log.getCell());
