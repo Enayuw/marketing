780bf699b3762b0d8b097f7844e02c955fff0a02
juman.wang <juman.wang@brgroup.com>
2024-07-23 11:17:55 +0800

    格式化注释

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -1931,7 +1931,7 @@ public class MarketingCommonConfig {
      */
     private HashMap<String, JSONObject> wbxkExtDataConfig;
 
-    /*
+    /**
      * 58撞库接口参数配置
      */
     private JSONObject wuBaCollidingUrlConfig;
