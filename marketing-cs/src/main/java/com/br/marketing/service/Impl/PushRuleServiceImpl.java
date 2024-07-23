17b3ca65b1f89a1ae1366360ed08c74005397333
zhen.li1 <zhen.li1@brgroup.com>
2024-07-23 14:05:57 +0800

    fix:规则集名称 时分秒

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -499,9 +499,10 @@ public class PushRuleServiceImpl implements PushRuleService {
             batchName = dto.getBatchName();
         } else {  //默认名称
             if (StringUtils.isNotEmpty(dto.getRuleModelName())) {
-                batchName = LocalDate.now().toString().concat("-").concat(dto.getRuleModelName()).concat("-").concat(LocalTime.now().toString());
+                batchName = LocalDate.now().toString().concat("-").concat(dto.getRuleModelName()).concat("-").concat(LocalTime.now().withNano(0).toString());
             } else {
-                batchName = LocalDate.now().toString().concat("-").concat(customerInfoPushMain.getId().toString()).concat("-").concat(LocalTime.now().toString());
+                batchName = LocalDate.now().toString().concat("-").concat(customerInfoPushMain.getId().toString()).concat("-").
+                        concat(LocalTime.now().withNano(0).toString());
             }
         }
         CustomerInfoPushMain updatePushMain = new CustomerInfoPushMain();
