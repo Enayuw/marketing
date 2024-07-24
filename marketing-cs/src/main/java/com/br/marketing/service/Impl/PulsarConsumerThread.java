95431c06d9d3a33d3d220cfa330fc440a4fcab13
senyang.zheng <senyang.zheng@brgroup.com>
2024-07-24 10:53:27 +0800

    feat(marketing-cs): sonar fix
    1.sonar fix

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PulsarConsumerThread.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PulsarConsumerThread.java
@@ -77,6 +77,7 @@ public class PulsarConsumerThread extends Thread {
                         try {
                             Thread.sleep(5000L);
                         } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
                             throw new RuntimeException(e);
                         }
                         continue;
