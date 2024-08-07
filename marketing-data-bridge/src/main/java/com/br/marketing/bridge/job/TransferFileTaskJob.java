ac67740012d8a190595a4f3137732a0d7e3ba0cd
guangxiu.li <guangxiu.li@brgroup.com>
2024-08-07 10:15:39 +0800

    sonar问题处理

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
@@ -305,7 +305,10 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
                                 } else {
                                     //第一次执行，查询为空，不会进行删除，直接返回
                                     //第二次执行，删除b_sync_log的记录
-                                    String fileChildDir = datum.getFileChildDir().concat("/");
+                                    String fileChildDir = "";
+                                    if (StringUtils.isNotEmpty(datum.getFileChildDir())) {
+                                        fileChildDir =  datum.getFileChildDir().concat("/");
+                                    }
                                     String childDir = StringUtils.isNotEmpty(datum.getFileChildDir()) ? fileChildDir : "";
                                     String srcPath = sftpHost.concat(":")
                                             .concat(upLoadPath)
