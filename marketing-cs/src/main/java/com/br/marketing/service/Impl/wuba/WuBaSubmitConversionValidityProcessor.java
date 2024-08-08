9f3cb3864f36261d20e14f2f0afd431f5a02849e
xiang.li4 <xiang.li4@brgroup.com>
2024-08-08 19:01:29 +0800

    58新客-营销名单上报-增加判断有效期

---
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/wuba/WuBaSubmitConversionValidityProcessor.java
@@ -0,0 +1,54 @@
+package com.br.marketing.service.Impl.wuba;
+
+import com.br.marketing.entity.WubaSubmitConversionData;
+import com.br.marketing.mapper.MarketingSyncUserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class WuBaSubmitConversionValidityProcessor {
+
+    private final static String TITLE = "【58新客提交营销名单】";
+
+    @Resource
+    private MarketingSyncUserMapper marketingSyncUserMapper;
+
+    public List<Long> validate(List<WubaSubmitConversionData> pushList, WubaSubmitConversionData param){
+        List<Long> notValidIds = new ArrayList<>();
+        if(CollectionUtils.isEmpty(pushList)) {
+            return notValidIds;
+        }
+        String apiCode = param.getApiCode();
+        Integer createDate = param.getCreateDate();
+        LocalDate curLocalDate = LocalDate.parse(String.valueOf(createDate), DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate startLocalDate = curLocalDate.plusDays(-6);
+
+        String appletDateStart = startLocalDate.toString();
+        String appletDateEnd = curLocalDate.toString();
+
+        List<String> custNums = pushList.stream().map(WubaSubmitConversionData::getCell).collect(Collectors.toList());
+        Set<String> validCustNums = marketingSyncUserMapper.getCustNumSetByAppletDateInterval(apiCode, custNums, appletDateStart, appletDateEnd);
+
+        Iterator<WubaSubmitConversionData> iterator = pushList.iterator();
+        while (iterator.hasNext()){
+            WubaSubmitConversionData next = iterator.next();
+            String cell = next.getCell();
+            if(!validCustNums.contains(cell)){
+                notValidIds.add(next.getId());
+                iterator.remove();
+            }
+        }
+        return notValidIds;
+    }
+}
