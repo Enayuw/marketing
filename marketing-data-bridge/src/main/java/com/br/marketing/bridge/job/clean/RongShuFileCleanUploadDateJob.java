06b65ccbaa3ca7aa00c3d66d8ccd7a76b8f785b9
zeqiang.guo <zeqaing.guo@brgroup.com>
2024-08-19 14:21:19 +0800

    华强：D20240729榕树上传数据更新-4004643 * 告警消息调整

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/clean/RongShuFileCleanUploadDateJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/clean/RongShuFileCleanUploadDateJob.java
@@ -178,7 +178,7 @@ public class RongShuFileCleanUploadDateJob extends AbstractSimpleElasticJob {
                             dingDingRobotHookService.sendDingDingTextMessage("榕树上传数据更新-" + apiCode + "结束["
                                     + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
                                     + "]\n文件：" + fileName + "\n清洗" + (bool ? "成功^_^\n清洗量级：" + sum : "失败!!!")
-                                    + (sum < 1 ? "\n文件无内容或只存在表头" : ""), map);
+                                    + (sum < 1 ? "\n文件无内容" : ""), map);
                         } catch (Exception e) {
                             log.warn(e.getMessage(), e);
                         }
