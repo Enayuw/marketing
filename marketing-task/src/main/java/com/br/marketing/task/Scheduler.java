9bc29ebb9b8de1ff3f64c2795ae49b1280277258
guangchao.zhang <guangchao.zhang@brgroup.com>
2024-08-26 10:48:47 +0800

    测试回滚日志打印添加

--- marketing-task/src/main/java/com/br/marketing/task/Scheduler.java
+++ marketing-task/src/main/java/com/br/marketing/task/Scheduler.java
@@ -38,6 +38,7 @@ public class Scheduler {
      */
     public static void main(String[] args) {
         Long start=System.currentTimeMillis();
+        log.warn("Scheduler开始启动！");
         ac= new SpringApplicationBuilder().sources(Scheduler.class).run(args);
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
