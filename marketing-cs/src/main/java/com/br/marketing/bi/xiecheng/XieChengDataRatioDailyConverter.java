fd33f24e1c594ae8ab960d6253f97b0c22992558
hong.chen <hong.chen@brgroup.com>
2024-09-05 17:43:50 +0800

    update:7日撞库结果报表

--- marketing-cs/src/main/java/com/br/marketing/bi/xiecheng/XieChengDataRatioDailyConverter.java
+++ marketing-cs/src/main/java/com/br/marketing/bi/xiecheng/XieChengDataRatioDailyConverter.java
@@ -1,20 +1,9 @@
 package com.br.marketing.bi.xiecheng;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.alibaba.fastjson.JSONObject;
 import com.br.marketing.aspect.BiReportType;
 import com.br.marketing.bi.AbstractBiReportConverter;
 import com.br.marketing.dto.report.xiecheng.XiechengDataRatioDailyReportDTO;
-import com.br.marketing.entity.DwsXcDataRatioD;
 import com.br.marketing.enums.report.BiReportChartTypeEnum;
 import com.br.marketing.enums.report.BiReportTypeEnum;
 import com.br.marketing.mapper.XieChengBiReportMapper;
@@ -22,12 +11,19 @@ import com.br.marketing.vo.bi.BiReportVO;
 import com.br.marketing.vo.bi.WrapDataVO;
 import com.br.marketing.vo.bi.param.BiReportParam;
 import com.google.api.client.util.Lists;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 携程数据使用率报表适配实现
- *
  * @author senyang.zheng
  * @date 2024/08/28
  */
@@ -40,7 +36,6 @@ public class XieChengDataRatioDailyConverter extends AbstractBiReportConverter<B
 
     /**
      * 获取数据
-     *
      * @param param 查询条件
      * @return {@link List }<{@link XiechengDataRatioDailyReportDTO }>
      * @author senyang.zheng
@@ -51,33 +46,20 @@ public class XieChengDataRatioDailyConverter extends AbstractBiReportConverter<B
         List<XiechengDataRatioDailyReportDTO> dtos = Lists.newArrayList();
         // 近30天数据列表
         String reportDateStart = LocalDate.now().minusDays(30).toString();
-        List<DwsXcDataRatioD> dwsXcDataRatioDS = xieChengBiReportMapper.selectXcDataRatioListbI_(reportDateStart);
-        for (DwsXcDataRatioD dwsXcDataRatioD : dwsXcDataRatioDS) {
-            long collidingBackNumber = dwsXcDataRatioD.getCollidingBackNum();
-            long extractionNumber = dwsXcDataRatioD.getExtractionNum();
-            long callableNumber = dwsXcDataRatioD.getCallableNum();
-            BigDecimal extractionRatioOrg = (BigDecimal)dwsXcDataRatioD.getExtractionRatio();
-            BigDecimal callableRatioOrg = (BigDecimal)dwsXcDataRatioD.getCallableRatio();
-
-            BigDecimal extractionRatio = extractionRatioOrg.multiply(new BigDecimal(100)).setScale(3, RoundingMode.FLOOR);
-            BigDecimal callableRatio = callableRatioOrg.multiply(new BigDecimal(100)).setScale(3, RoundingMode.FLOOR);
-            // 创建DailyReportDTO实例并设置数据
-            XiechengDataRatioDailyReportDTO report = new XiechengDataRatioDailyReportDTO();
-            report.setReportDate(dwsXcDataRatioD.getReportDate());
-            report.setCollidingBackNum(collidingBackNumber);
-            report.setExtractionNum(extractionNumber);
-            report.setCallableNum(callableNumber);
-            report.setExtractionRatio(extractionRatio);
-            report.setCallableRatio(callableRatio);
-            dtos.add(report);
+        List<XiechengDataRatioDailyReportDTO> dwsXcDataRatioDS = xieChengBiReportMapper.selectXcDataRatioListbI_(reportDateStart);
+        for (XiechengDataRatioDailyReportDTO dwsXcDataRatioD : dwsXcDataRatioDS) {
+            BigDecimal extractionRatio = dwsXcDataRatioD.getExtractionRatio().multiply(new BigDecimal(100)).setScale(3, RoundingMode.FLOOR);
+            BigDecimal callableRatio = dwsXcDataRatioD.getCallableRatio().multiply(new BigDecimal(100)).setScale(3, RoundingMode.FLOOR);
+            dwsXcDataRatioD.setExtractionRatio(extractionRatio);
+            dwsXcDataRatioD.setCallableRatio(callableRatio);
+            dtos.add(dwsXcDataRatioD);
         }
         return dtos;
     }
 
     /**
      * 数据处理
-     *
-     * @param dtos 数据
+     * @param dtos   数据
      * @param extend 延长
      * @return {@link BiReportVO }
      * @author senyang.zheng
@@ -91,7 +73,7 @@ public class XieChengDataRatioDailyConverter extends AbstractBiReportConverter<B
         biReportVO.setType(BiReportChartTypeEnum.TABLE.getType());
         // 根据时间排序
         List<XiechengDataRatioDailyReportDTO> sortedData = dtos.stream()
-            .sorted(Comparator.comparing(XiechengDataRatioDailyReportDTO::getReportDate, Comparator.naturalOrder())).collect(Collectors.toList());
+                .sorted(Comparator.comparing(XiechengDataRatioDailyReportDTO::getReportDate, Comparator.naturalOrder())).collect(Collectors.toList());
         // 构造横坐标数据
         List<String> xAxis = sortedData.stream().map(XiechengDataRatioDailyReportDTO::getReportDate).distinct().collect(Collectors.toList());
         biReportVO.setXAxisName("日期");
--- marketing-cs/src/main/java/com/br/marketing/mapper/XieChengBiReportMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/XieChengBiReportMapper.java
@@ -2,15 +2,15 @@ package com.br.marketing.mapper;
 
 import com.br.marketing.dto.report.xiecheng.XiechengCollidingDailyReportDTO;
 import com.br.marketing.dto.report.xiecheng.XiechengCollidingWeeklyReportDTO;
+import com.br.marketing.dto.report.xiecheng.XiechengDataRatioDailyReportDTO;
 import com.br.marketing.dto.report.xiecheng.XiechengTransferDailyReportDTO;
 import com.br.marketing.dto.report.xiecheng.XiechengTransferWeeklyReportDTO;
-import com.br.marketing.entity.DwsXcDataRatioD;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface XieChengBiReportMapper {
-    List<DwsXcDataRatioD> selectXcDataRatioListbI_(@Param("reportDateStart") String reportDateStart);
+    List<XiechengDataRatioDailyReportDTO> selectXcDataRatioListbI_(@Param("reportDateStart") String reportDateStart);
 
     List<XiechengCollidingDailyReportDTO> selectXcColldingDistrubuteDayListbI_(@Param("reportDateStart") String reportDateStart, @Param(
             "reportDateEnd") String reportDateEnd);
