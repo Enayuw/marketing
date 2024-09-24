ad8a7892000b8fde62cef78e97c801aeb8d7fe14
bingxu.kong <bingxu.kong@brgroup.com>
2024-09-24 13:44:47 +0800

    增加分组名称

--- marketing-cs/src/main/java/com/br/marketing/bi/zhongan/ZhonganMultiHeadGroupConverter.java
+++ marketing-cs/src/main/java/com/br/marketing/bi/zhongan/ZhonganMultiHeadGroupConverter.java
@@ -1,5 +1,6 @@
 package com.br.marketing.bi.zhongan;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -12,6 +13,7 @@ import com.br.marketing.dto.report.zhongan.ZhongAnDistributionStatisticDTO;
 import com.br.marketing.dto.report.zhongan.ZhongAnGroupedScoreDistributionDTO;
 import com.br.marketing.entity.ReportStatisticTransfer;
 import com.br.marketing.entity.ReportStatisticTransferExample;
+import com.br.marketing.entity.ReportTask;
 import com.br.marketing.enums.report.BiReportChartTypeEnum;
 import com.br.marketing.enums.report.BiReportTypeEnum;
 import com.br.marketing.mapper.ReportStatisticTransferMapper;
@@ -21,6 +23,8 @@ import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.br.marketing.vo.bi.BiReportVO;
 import com.br.marketing.vo.bi.WrapDataVO;
 import com.br.marketing.vo.bi.param.BiReportParam;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,10 +32,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -57,6 +58,11 @@ public class ZhonganMultiHeadGroupConverter extends AbstractBiReportConverter<Bi
     @Override
     public List<ZhongAnGroupedScoreDistributionDTO> fetchData(BiReportParam param) {
         String taskId = param.getCondition().getString("taskId");
+        if(StringUtils.isEmpty(taskId)){
+            return new ArrayList<>();
+        }
+        // 查询规则分组名称
+        Map<String, String> groupNameMap = formatReportRules(taskId);
 
         ReportStatisticTransferExample reportStatisticTransferExample = new ReportStatisticTransferExample();
         reportStatisticTransferExample.createCriteria().andReportTaskIdEqualTo(taskId);
@@ -81,16 +87,17 @@ public class ZhonganMultiHeadGroupConverter extends AbstractBiReportConverter<Bi
             if(StringUtils.isNotEmpty(dimensionValue) && StringUtils.isNotEmpty(dimensionField)){
                 // 分组多头查询
                 for (String value : formatField(dimensionValue)) {
+                    String groupName = groupNameMap.get(value);
                     for (String itemName : formatField(multiHeadField)) {
                         List<ZhongAnDistributionStatisticDTO> dtoList = zhongAnBiReportMapper.selectZaMultiHeadGroupListbI_(reportId, field, dimensionField, value, itemName);
-                        bulidMultiHead(dtoList,field,step,dtos);
+                        bulidMultiHead(dtoList,field,groupName,step,dtos);
                     }
                 }
             }else if(StringUtils.isNotEmpty(multiHeadField)){
                 // 多头查询
                 for (String itemName : formatField(multiHeadField)) {
                     List<ZhongAnDistributionStatisticDTO> dtoList = zhongAnBiReportMapper.selectZaMultiHeadGroupListbI_(reportId, field, null, null, itemName);
-                    bulidMultiHead(dtoList,field,step,dtos);
+                    bulidMultiHead(dtoList,field,null,step,dtos);
                 }
             }
         }
@@ -100,13 +107,10 @@ public class ZhonganMultiHeadGroupConverter extends AbstractBiReportConverter<Bi
     @Override
     public List<BiReportVO> process(List<ZhongAnGroupedScoreDistributionDTO> dtos, JSONObject extend) {
         List<BiReportVO> biReportVOS = Lists.newArrayList();
-        //先根据产品分组，一个分一个报表
-        Map<String, List<ZhongAnGroupedScoreDistributionDTO>> scoreMap =
-                dtos.stream().collect(Collectors.groupingBy(ZhongAnGroupedScoreDistributionDTO::getProduct));
 
         Map<String, Map<String, List<ZhongAnGroupedScoreDistributionDTO>>> groupedByProductAndGroupName =
-                dtos.stream().collect(Collectors.groupingBy(ZhongAnGroupedScoreDistributionDTO::getProduct, // 外层分组键：product
-                                Collectors.groupingBy(ZhongAnGroupedScoreDistributionDTO::getGroup) // 内层分组键：groupName
+                dtos.stream().collect(Collectors.groupingBy(ZhongAnGroupedScoreDistributionDTO::getProduct,
+                                Collectors.groupingBy(ZhongAnGroupedScoreDistributionDTO::getGroup)
                         ));
 
         for (Map.Entry<String, Map<String, List<ZhongAnGroupedScoreDistributionDTO>>> entry1 : groupedByProductAndGroupName.entrySet()) {
@@ -182,18 +186,46 @@ public class ZhonganMultiHeadGroupConverter extends AbstractBiReportConverter<Bi
     }
 
     public void bulidMultiHead(List<ZhongAnDistributionStatisticDTO> dtoList, String field,
-                               Integer step, List<ZhongAnGroupedScoreDistributionDTO> dtos) {
+                               String groupName, Integer step, List<ZhongAnGroupedScoreDistributionDTO> dtos) {
         for (ZhongAnDistributionStatisticDTO zhongAnDistributionStatisticDTO : dtoList) {
             ZhongAnGroupedScoreDistributionDTO zhongAnGroupedScoreDistributionDTO = new ZhongAnGroupedScoreDistributionDTO();
             zhongAnGroupedScoreDistributionDTO.setProduct(field);
             zhongAnGroupedScoreDistributionDTO.setInterval(zhongAnDistributionStatisticDTO.getScoreValue());
-            zhongAnGroupedScoreDistributionDTO.setGroup(zhongAnDistributionStatisticDTO.getDimensionField()+zhongAnDistributionStatisticDTO.getDimensionValue());
+            zhongAnGroupedScoreDistributionDTO.setGroup(groupName == null ? "0":groupName);
             zhongAnGroupedScoreDistributionDTO.setName(zhongAnDistributionStatisticDTO.getItemName());
             zhongAnGroupedScoreDistributionDTO.setNum(Long.valueOf(zhongAnDistributionStatisticDTO.getItemValue()));
             zhongAnGroupedScoreDistributionDTO.setStep(step);
             dtos.add(zhongAnGroupedScoreDistributionDTO);
         }
         fillProportion(dtos,ZhongAnGroupedScoreDistributionDTO::getNum,ZhongAnGroupedScoreDistributionDTO::setProportion);
+    }
+
+    public Map<String, String> formatReportRules(String taskId){
+        ReportTask reportTask = reportTaskMapper.selectByPrimaryKey(Long.valueOf(taskId));
+        String reportRules = reportTask.getReportRules();
+
+        Map<String, String> resultMap = new HashMap<>();
+        if(reportRules.contains("upload")){
+            JSONObject param = JSONObject.parseObject(reportRules);
+            String upload = param.getString("upload");
+            JSONObject uploadJson = JSONObject.parseObject(upload);
+            String dimensions = uploadJson.getString("dimensionsValue");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                // 解析JSON数组为JsonNode
+                JsonNode jsonNode = objectMapper.readTree(dimensions);
+                resultMap = new HashMap<>();
+                for (JsonNode item : jsonNode) {
+                    String code = item.get("code").asText();
+                    String desc = item.get("desc").asText();
+                    resultMap.put(code, desc);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultMap;
     }
 
 }
