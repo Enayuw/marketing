ab114a0a0280635136c7f3a03e6faa4ed23c7b8f
guangxiu.li <guangxiu.li@brgroup.com>
2024-09-25 11:22:22 +0800

    宜信紧急上线

--- marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/yixin/YiXinBlackPushToBaiYingHandler.java
+++ marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/yixin/YiXinBlackPushToBaiYingHandler.java
@@ -273,7 +273,7 @@ public class YiXinBlackPushToBaiYingHandler extends IMonkeyDataHandle<MarketingS
             blackBlklistDataDTO.setCaseNum(syncUser.getCustNum());
             blackBlklistDataDTO.setExpireDate(LocalDate.now()
                     .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-                    .concat(" 23:59:59"));
+                    .concat(" 11:20:00"));
             blackBlklistDataDTO.setPhone(syncUser.getCellMd5());
             pushBlkList.add(blackBlklistDataDTO);
 
@@ -287,7 +287,7 @@ public class YiXinBlackPushToBaiYingHandler extends IMonkeyDataHandle<MarketingS
                     reqBlacklistDTO.setMethod(pushMethod);
                     reqBlacklistDTO.setApiCode(condition.getSynApiCode());
                     reqBlacklistDTO.setData(finalList);
-                    byApiServiceClient.pushBaiying(reqBlacklistDTO,0);
+//                    byApiServiceClient.pushBaiying(reqBlacklistDTO,0);
                     // 紧急需求，增加了推送百可录逻辑（后续逻辑变更请注意）
                     ReqBlacklistDTO reqBklBlacklistDTO = new ReqBlacklistDTO();
                     reqBklBlacklistDTO.setMethod(pushMethod);
