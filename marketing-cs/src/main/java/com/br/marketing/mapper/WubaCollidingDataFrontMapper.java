8a1d1f322088ec4ea4c7d6dfb065483f4824661e
hong.chen <hong.chen@brgroup.com>
2024-10-09 18:17:27 +0800

    update:58高价值数据重置

--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataFrontMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingDataFrontMapper.java
@@ -14,4 +14,8 @@ public interface WubaCollidingDataFrontMapper extends WubaCollidingDataFrontMapp
 
     void batchUpdatePushStatusByCell(@Param("list") List<WubaCollidingDataFront> wubaCollidingDataFronts,
                                      @Param("localId") Long localId, @Param("apiCode") String apiCode);
+
+    void updatePushStatusByHighValueFileIds(@Param("highValueIds") String highValueIds);
+
+    void updatePushStatusByLocalId(@Param("highValueIds") String highValueIds);
 }
