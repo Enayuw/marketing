67f728c8a4ddb403567a61fc8bfe53c085a945fb
hong.chen <hong.chen@brgroup.com>
2024-10-11 17:48:40 +0800

    update:增加部分成功判断

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/PhoneSaleDataPushDassIbuJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/PhoneSaleDataPushDassIbuJob.java
@@ -7,6 +7,7 @@ import com.br.marketing.mapper.LocalFileMapper;
 import com.br.marketing.service.PushDataService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,7 +33,7 @@ public class PhoneSaleDataPushDassIbuJob extends AbstractSimpleElasticJob {
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
         LocalFileExample localFileExample = new LocalFileExample();
         localFileExample.createCriteria().andFileTypeEqualTo(SftpFileTypeEnum.DXIBU.getValue()).andPushStatusEqualTo("0")
-                .andStatusEqualTo("1").andCompleteEqualTo("1");
+                .andStatusEqualTo("1").andCompleteEqualTo("1").andCompleteIn(Lists.newArrayList("1", "3"));
         List<LocalFile> localFiles = localFileMapper.selectByExample(localFileExample);
         if (CollectionUtils.isEmpty(localFiles)) {
             return;
--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/PhoneSaleDataPushDassJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/PhoneSaleDataPushDassJob.java
@@ -9,6 +9,7 @@ import com.br.marketing.service.ZhongYuanService;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -41,7 +42,7 @@ public class PhoneSaleDataPushDassJob extends AbstractSimpleElasticJob {
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
         LocalFileExample localFileExample = new LocalFileExample();
         localFileExample.createCriteria().andFileTypeEqualTo(SftpFileTypeEnum.DX.getValue()).andPushStatusEqualTo("0")
-                .andStatusEqualTo("1").andCompleteEqualTo("1");
+                .andStatusEqualTo("1").andCompleteIn(Lists.newArrayList("1", "3"));
         List<LocalFile> localFiles = localFileMapper.selectByExample(localFileExample);
         if (CollectionUtils.isEmpty(localFiles)) {
             return;
