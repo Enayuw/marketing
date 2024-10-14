b64e39b3fef0bbff9668444b460b1a084364d19e
hong.chen <hong.chen@brgroup.com>
2024-10-14 19:30:06 +0800

    update:修改挡板

--- marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengServiceNew.java
+++ marketing-cs/src/main/java/com/br/marketing/client/xiecheng/XieChengServiceNew.java
@@ -71,13 +71,14 @@ public class XieChengServiceNew {
             JSONObject dataMap = new JSONObject();
             String s = sha256CodeList.get(i);
             dataMap.put("sha256Code",s);
-            dataMap.put("hitRequestNo", RandomUtil.randomString(29).toUpperCase());
             if(i%2==0){
                 dataMap.put("result",true);
                 dataMap.put("releaseTime", DateUtil.formatDateTime(DateUtil.offsetDay(new Date(),7)));
                 dataMap.put("releaseDate", null);
+                dataMap.put("hitRequestNo", RandomUtil.randomString(29).toUpperCase());
             }else {
                 dataMap.put("result",false);
+                dataMap.put("hitRequestNo", null);
                 dataMap.put("releaseDate", DateUtil.formatDate(DateUtil.offsetDay(new Date(), RandomUtil.getRandom().nextInt(7)+1)));
             }
             dataMap.put("orgChannel","测试orgChannel");
