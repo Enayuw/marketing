bb32f2fdab17479c5974c45bd27fb2ca7033feb0
guangxiu.li <guangxiu.li@brgroup.com>
2024-10-17 16:00:01 +0800

    携程报表优化

--- marketing-cs/src/main/java/com/br/marketing/bi/xiecheng/XiechengTransferMonthlySwitchOnConverter.java
+++ marketing-cs/src/main/java/com/br/marketing/bi/xiecheng/XiechengTransferMonthlySwitchOnConverter.java
@@ -76,7 +76,8 @@ public class XiechengTransferMonthlySwitchOnConverter extends AbstractBiReportCo
         yAxis.add(buildWrapDataVO("实际外呼量", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getOutboundNum, FormatType.THOUSAND_SEPARATOR));
         yAxis.add(buildWrapDataVO("累计运营量", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getOperateNum, FormatType.THOUSAND_SEPARATOR));
         yAxis.add(buildWrapDataVO("累计接通量级（未去重）", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getCallNum, FormatType.THOUSAND_SEPARATOR));
-        yAxis.add(buildWrapDataVO("累计接通量级（去量）", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getDistinctCallNum, FormatType.THOUSAND_SEPARATOR));
+        yAxis.add(buildWrapDataVO("累计接通量级（去重）", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getDistinctCallNum,
+                FormatType.THOUSAND_SEPARATOR));
         yAxis.add(buildWrapDataVO("身份认证量", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getCertifyNum, FormatType.THOUSAND_SEPARATOR));
         yAxis.add(buildWrapDataVO("申请量", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getApplyNum, FormatType.THOUSAND_SEPARATOR));
         yAxis.add(buildWrapDataVO("授信量", sortedData, XiechengTransferMonthlySwitchOnReportDTO::getCreditNum, FormatType.THOUSAND_SEPARATOR));
