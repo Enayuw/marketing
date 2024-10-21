de474f6f859f7da5c10a9a2c7fc1ccaf0315a9f6
xiang.li4 <xiang.li4@brgroup.com>
2024-10-21 18:19:15 +0800

    技术方案变更-删除

--- marketing-cs/src/main/java/com/br/marketing/strategy/InterfaceHandlerEnum.java
+++ marketing-cs/src/main/java/com/br/marketing/strategy/InterfaceHandlerEnum.java
@@ -37,8 +37,6 @@ public enum InterfaceHandlerEnum {
     CUSTOMER_TRANSFER_SOLE_USE_STATUS(32, "客服转化去重+状态自定义"),
 
     BIOCLOO_BLACK_LIST(33, "百可录黑名单"),
-
-    QIFU_CUWANJIAN_BAT_QRY_USER_REAL(34, "360促完件用户信息批量查询"),
     ;
 
 
