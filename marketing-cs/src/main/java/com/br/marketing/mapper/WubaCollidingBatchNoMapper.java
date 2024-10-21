05ec744478e806bd089141883323d0d7043d7f72
dongshuo.he <dongshuo.he@brgroup.com>
2024-10-21 10:42:15 +0800

    58撞库status=-2历史数据从interface_log清洗至撞库log表-v1.0

--- marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingBatchNoMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/WubaCollidingBatchNoMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface WubaCollidingBatchNoMapper extends WubaCollidingBatchNoMapperBase {
     void saveDataByBatchNo(@Param("batchNo") String batchNo, @Param("batchType") Integer type, @Param("apiCode") String apiCode, @Param(
@@ -13,4 +14,7 @@ public interface WubaCollidingBatchNoMapper extends WubaCollidingBatchNoMapperBa
     List<WubaCollidingDataBatchNo> selectCollidingDataResult(@Param("pushTime") Date pushTime,
                                                              @Param("pageSize") Integer pageSize,
                                                              @Param("apiCode") String apiCode);
+
+    List<Map<String, Object>> selectBatchNoByTimeRange(@Param("apiCode") String apiCode,
+                                                       @Param("condition") String condition);
 }
