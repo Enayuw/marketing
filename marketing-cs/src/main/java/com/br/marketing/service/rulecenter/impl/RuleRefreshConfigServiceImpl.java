1c81a9e6ea4fab8383eca554133410c6ccdc0cc5
bingxu.kong <bingxu.kong@brgroup.com>
2024-10-22 17:31:57 +0800

    奇富360配置项更新

--- marketing-cs/src/main/java/com/br/marketing/service/rulecenter/impl/RuleRefreshConfigServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/rulecenter/impl/RuleRefreshConfigServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -63,60 +62,37 @@ public class RuleRefreshConfigServiceImpl implements IRuleRefreshConfigService {
             PushDecisionsExample pushDecisionsExample = new PushDecisionsExample();
             pushDecisionsExample.createCriteria().andApiCodeEqualTo(apiCode).andRuleNumberEqualTo(ruleNumber).andIsDelEqualTo(Constants.DATA_VALID);
             List<PushDecisions> pushDecisionsList = pushDecisionsMapper.selectByExample(pushDecisionsExample);
-
+            log.warn("决策规则编号查询决策配置:{}" ,JSONObject.toJSONString(pushDecisionsList));
             if(CollectionUtil.isEmpty(pushDecisionsList)){
                 return;
             }
             PushDecisions pushDecisions = pushDecisionsList.get(0);
 
             // aipCode + 推决策配置id + 日期 查询是否生成推决策任务
-
             DecisionsTaskLogExample decisionsTaskLogExample = new DecisionsTaskLogExample();
             decisionsTaskLogExample.createCriteria()
+                    .andApiCodeEqualTo(apiCode)
                     .andPushConfigIdEqualTo(pushDecisions.getId())
                     .andIsDelEqualTo(Constants.DATA_VALID)
-                    .andCreateTimeGreaterThanOrEqualTo(new Date());
-            List<DecisionsTaskLog> decisionsTaskLogs = decisionsTaskLogMapper.selectByExample(decisionsTaskLogExample);
+                    .andCreateTimeLessThanOrEqualTo(new Date());
+            //List<DecisionsTaskLog> decisionsTaskLogs = decisionsTaskLogMapper.selectByExample(decisionsTaskLogExample);
+            int i = decisionsTaskLogMapper.countByExample(decisionsTaskLogExample);
+            if(i > 0){
+                // 判断推决策配置是否失效
+                if(pushDecisions.getStatus() != 2){
+                    ScoreSearchCondition scoreSearchCondition = new ScoreSearchCondition();
+                    scoreSearchCondition.setId(pushDecisions.getId());
+                    scoreSearchCondition.setStatus(2);
+                    scoreSearchConditionMapper.updateByPrimaryKeySelective(scoreSearchCondition);
+                }
+                return;
+            }
 
-
-            // 判断推决策配置是否失效
             // apiCode + 日期 + 跑分状态 + 数据范围 + 跑分规则编号 查询当日跑分任务是否完成
+            //
+            //marketingTaskService.queryCompletStatus(apiCode,createTimeStart,createTimeEnd,taskStatus,);
+            ////根据规则配置id，更新跑分id和生效状态
 
-            //根据规则配置id，更新跑分id和生效状态
-
-
-
-            //
-            //
-            //
-            //PageResultReturn list = marketingTaskService.list(1, 10, key,
-            //        null, null, null, null, null, null, null);
-            //
-            //List<MarketingTaskVO> fastTaskRuleListVOS = list.getRecords();
-            //if(CollectionUtil.isEmpty(fastTaskRuleListVOS)){
-            //    return;
-            //}
-            //MarketingTaskVO marketingTaskVO = fastTaskRuleListVOS.get(0);
-            //
-            //Long hisFileId = marketingTaskVO.getHisFileId();
-            //
-            ////根据value查询模板
-            //ScoreSearchConditionExample scoreSearchConditionExample = new ScoreSearchConditionExample();
-            //scoreSearchConditionExample.createCriteria().andNameEqualTo(value).andIsDelEqualTo(0).andStatusEqualTo(1);
-            //List<ScoreSearchCondition> scoreSearchConditions = scoreSearchConditionMapper.selectByExample(scoreSearchConditionExample);
-            //if(CollectionUtil.isEmpty(scoreSearchConditions)){
-            //    return;
-            //}
-            ////若不为空，则更新改模板的source_condition字段为hisFileId
-            //ScoreSearchCondition scoreSearchCondition1 = new ScoreSearchCondition();
-            //scoreSearchCondition1.setSourceCondition(String.valueOf(hisFileId));
-            //ScoreSearchConditionExample scoreSearchConditionExample1 = new ScoreSearchConditionExample();
-            //scoreSearchConditionExample1.createCriteria().andNameEqualTo(value).andIsDelEqualTo(0).andStatusEqualTo(1);
-            //int updateCount = scoreSearchConditionMapper.updateByExampleSelective(scoreSearchCondition1, scoreSearchConditionExample1);
-            //if(updateCount == 0){
-            //    log.warn("自动刷新模板失败:{}" ,key);
-            //    return;
-            //}
 
         }
 
