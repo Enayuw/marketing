bc4871698e5829bae75fefe527d619a13ed5f88f
bingxu.kong <bingxu.kong@brgroup.com>
2024-10-22 22:25:55 +0800

    优化代码

--- marketing-cs/src/main/java/com/br/marketing/service/rulecenter/impl/RuleRefreshConfigServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/rulecenter/impl/RuleRefreshConfigServiceImpl.java
@@ -2,8 +2,11 @@ package com.br.marketing.service.rulecenter.impl;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSONObject;
+import com.br.common.log.AlertLog;
+import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.Constants;
 import com.br.marketing.entity.*;
+import com.br.marketing.enums.ScoreStatusEnum;
 import com.br.marketing.mapper.DecisionsTaskLogMapper;
 import com.br.marketing.mapper.MarketingTaskMapper;
 import com.br.marketing.mapper.PushDecisionsMapper;
@@ -31,29 +34,30 @@ public class RuleRefreshConfigServiceImpl implements IRuleRefreshConfigService {
 
     @Autowired
     MarketingTaskService marketingTaskService;
-
     @Resource
     MarketingTaskMapper marketingTaskMapper;
-
     @Resource
     PushDecisionsMapper pushDecisionsMapper;
-
     @Resource
     DecisionsTaskLogMapper decisionsTaskLogMapper;
-
     @Resource
     ScoreSearchConditionMapper scoreSearchConditionMapper;
-
     @Resource
     private MarketingCommonConfig marketingCommonConfig;
+    private static final String TITLE = "【自动刷新推决策配置】";
 
     @Override
     public void autoRefreshConfig() {
 
-        // autoRefreshConfig={"3710128":{"ruleNameShort":"R20240905-01","ruleNumber":"JC20241021_7410908_001"}}
         Map<String, JSONObject> map = marketingCommonConfig.getAutoRefreshConfig();
-
         for (Map.Entry<String, JSONObject> entry : map.entrySet()) {
+            buildRefreshConfig(entry);
+        }
+    }
+
+    private void buildRefreshConfig(Map.Entry<String, JSONObject> entry) {
+
+        try {
             String apiCode = entry.getKey();
             JSONObject jsonObject = entry.getValue();
             // 跑分配置规则编号
@@ -62,67 +66,112 @@ public class RuleRefreshConfigServiceImpl implements IRuleRefreshConfigService {
             String ruleNumber = jsonObject.getString("ruleNumber");
 
             // 1- 根据决策规则编号查询决策配置
-            PushDecisionsExample pushDecisionsExample = new PushDecisionsExample();
-            pushDecisionsExample.createCriteria().andApiCodeEqualTo(apiCode).andRuleNumberEqualTo(ruleNumber).andIsDelEqualTo(Constants.DATA_VALID);
-            List<PushDecisions> pushDecisionsList = pushDecisionsMapper.selectByExample(pushDecisionsExample);
-            log.warn("决策规则编号查询决策配置:{}" ,JSONObject.toJSONString(pushDecisionsList));
-            if(CollectionUtil.isEmpty(pushDecisionsList)){
+            PushDecisions pushDecisions = queryPushDecisions(apiCode, ruleNumber);
+            if(pushDecisions == null){
                 return;
             }
-            PushDecisions pushDecisions = pushDecisionsList.get(0);
 
             // 2- aipCode + 推决策配置id + 日期 查询是否生成推决策任务
-            DecisionsTaskLogExample decisionsTaskLogExample = new DecisionsTaskLogExample();
-            decisionsTaskLogExample.createCriteria()
-                    .andApiCodeEqualTo(apiCode)
-                    .andPushConfigIdEqualTo(pushDecisions.getId())
-                    .andIsDelEqualTo(Constants.DATA_VALID)
-                    .andCreateTimeLessThanOrEqualTo(new Date());
-            int i = decisionsTaskLogMapper.countByExample(decisionsTaskLogExample);
-            if(i > 0){
-                // 判断推决策配置是否失效
-                if(pushDecisions.getStatus() != 2){
-                    PushDecisions pushDecisions1 = new PushDecisions();
-                    pushDecisions1.setId(pushDecisions.getId());
-                    pushDecisions1.setStatus(2);
-                    pushDecisionsMapper.updateByPrimaryKeySelective(pushDecisions1);
-                }
+            Boolean b = queryDecisionsTaskLog(apiCode, pushDecisions);
+            if(b){
                 return;
             }
-            // 3- apiCode + 日期 + 跑分状态 + 数据范围 + 跑分规则编号 查询当日跑分任务是否完成
-            // 获取今天的日期
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDate today = LocalDate.now();
-            LocalDateTime startTime = today.atStartOfDay();
-            String createTimeStart = startTime.format(formatter);
-            // 获取当前时间
-            LocalDateTime now = LocalDateTime.now();
-            String createTimeEnd = now.format(formatter);
-            Integer taskStatus = 2;
-            String conditionType = "1";
-            List<MarketingTaskVO> marketingTaskVOS = marketingTaskMapper.queryCompletStatus(apiCode,
-                    createTimeStart, createTimeEnd, taskStatus, conditionType, ruleNameShort);
-            if(CollectionUtil.isEmpty(marketingTaskVOS)){
-                return;
-            }
-            //List<Long> ids = marketingTaskVOS.stream() // 创建一个 Stream
-            //        .map(MarketingTaskVO::getId) // 将每个 MarketingTaskVO 映射到其 id
-            //        .collect(Collectors.toList());
 
-            MarketingTaskVO marketingTaskVO = marketingTaskVOS.get(0);
+            // 3- apiCode + 日期 + 跑分状态 + 数据范围 + 跑分规则编号 查询当日跑分任务是否完成
+            MarketingTaskVO marketingTaskVO = queryMarketingTask(apiCode, ruleNameShort);
+            if(marketingTaskVO == null){
+                return;
+            }
 
             // 4- 根据规则配置id，更新跑分id
-            ScoreSearchCondition scoreSearchCondition = new ScoreSearchCondition();
-            scoreSearchCondition.setId(pushDecisions.getDependencyTemplateId());
-            scoreSearchCondition.setSourceCondition(String.valueOf(marketingTaskVO.getHisFileId()));
-            scoreSearchConditionMapper.updateByPrimaryKeySelective(scoreSearchCondition);
-            // 5- 更新生效状态
-            PushDecisions pushDecisions1 = new PushDecisions();
-            pushDecisions1.setId(pushDecisions.getId());
-            pushDecisions1.setStatus(1);
-            pushDecisionsMapper.updateByPrimaryKeySelective(pushDecisions1);
-        }
+            updateScoreSearch(pushDecisions,marketingTaskVO);
 
+        }catch (Exception e){
+            String title = "自动化刷新推送决策任务 刷新失败【未知异常】";
+            String text = String.format("异常【%s】", e.getMessage());
+            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), text, title), e);
+        }
+    }
+
+    private void updateScoreSearch(PushDecisions pushDecisions, MarketingTaskVO marketingTaskVO) {
+        // 查询是否已经更新过跑分id
+        ScoreSearchConditionExample scoreSearchConditionExample = new ScoreSearchConditionExample();
+        scoreSearchConditionExample.createCriteria().andIdEqualTo(pushDecisions.getDependencyTemplateId()).andSourceConditionEqualTo(String.valueOf(marketingTaskVO.getHisFileId()));
+        int i1 = scoreSearchConditionMapper.countByExample(scoreSearchConditionExample);
+        log.warn(TITLE + "查询是否已经更新过跑分:{}", i1);
+        if(i1> 0){
+            return;
+        }
+        // 更新跑分id
+        ScoreSearchCondition scoreSearchCondition = new ScoreSearchCondition();
+        scoreSearchCondition.setId(pushDecisions.getDependencyTemplateId());
+        scoreSearchCondition.setSourceCondition(String.valueOf(marketingTaskVO.getHisFileId()));
+        scoreSearchConditionMapper.updateByPrimaryKeySelective(scoreSearchCondition);
+        // 更新生效状态
+        PushDecisions pushDecisions1 = new PushDecisions();
+        pushDecisions1.setId(pushDecisions.getId());
+        pushDecisions1.setStatus(1);
+        pushDecisionsMapper.updateByPrimaryKeySelective(pushDecisions1);
+    }
+
+    private MarketingTaskVO queryMarketingTask(String apiCode, String ruleNameShort) {
+
+        MarketingTaskVO marketingTaskVO = new MarketingTaskVO();
+        // 获取今天的日期
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDate today = LocalDate.now();
+        LocalDateTime startTime = today.atStartOfDay();
+        String createTimeStart = startTime.format(formatter);
+        // 获取当前时间
+        LocalDateTime now = LocalDateTime.now();
+        String createTimeEnd = now.format(formatter);
+        Integer taskStatus = ScoreStatusEnum.FINISH.getValue();
+        String conditionType = "1";
+        List<MarketingTaskVO> marketingTaskVOS = marketingTaskMapper.queryCompletStatus(apiCode,
+                createTimeStart, createTimeEnd, taskStatus, conditionType, ruleNameShort);
+        log.warn(TITLE + "查询当日跑分任务是否完成:{}", JSONObject.toJSONString(marketingTaskVOS));
+        if(CollectionUtil.isEmpty(marketingTaskVOS)){
+            return marketingTaskVO;
+        }
+        marketingTaskVO = marketingTaskVOS.get(0);
+        return marketingTaskVO;
+    }
+
+    private Boolean queryDecisionsTaskLog(String apiCode, PushDecisions pushDecisions) {
+        Boolean aTrue = Boolean.FALSE;
+        DecisionsTaskLogExample decisionsTaskLogExample = new DecisionsTaskLogExample();
+        decisionsTaskLogExample.createCriteria()
+                .andApiCodeEqualTo(apiCode)
+                .andPushConfigIdEqualTo(pushDecisions.getId())
+                .andIsDelEqualTo(Constants.DATA_VALID)
+                .andCreateTimeLessThanOrEqualTo(new Date());
+        int i = decisionsTaskLogMapper.countByExample(decisionsTaskLogExample);
+        log.warn(TITLE + "查询是否生成推决策任务:{}", i);
+        if(i > 0){
+            // 判断推决策配置是否失效
+            if(pushDecisions.getStatus() != 2){
+                PushDecisions pushDecisions1 = new PushDecisions();
+                pushDecisions1.setId(pushDecisions.getId());
+                pushDecisions1.setStatus(2);
+                pushDecisionsMapper.updateByPrimaryKeySelective(pushDecisions1);
+            }
+            aTrue = Boolean.TRUE;
+            return aTrue;
+        }
+        return aTrue;
+    }
+
+    private PushDecisions queryPushDecisions(String apiCode, String ruleNumber) {
+        PushDecisions pushDecisions = new PushDecisions();
+        PushDecisionsExample pushDecisionsExample = new PushDecisionsExample();
+        pushDecisionsExample.createCriteria().andApiCodeEqualTo(apiCode).andRuleNumberEqualTo(ruleNumber).andIsDelEqualTo(Constants.DATA_VALID);
+        List<PushDecisions> pushDecisionsList = pushDecisionsMapper.selectByExample(pushDecisionsExample);
+        log.warn(TITLE + "决策规则编号查询决策配置:{}", JSONObject.toJSONString(pushDecisionsList));
+        if(CollectionUtil.isEmpty(pushDecisionsList)){
+            return pushDecisions;
+        }
+        pushDecisions = pushDecisionsList.get(0);
+        return pushDecisions;
     }
 
 }
