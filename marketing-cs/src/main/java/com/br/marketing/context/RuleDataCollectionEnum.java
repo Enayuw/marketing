857c37c0e965a30cb6602be7762fded153133d5b
yu.xia <yu.xia@brgroup.com>
2024-10-23 20:46:45 +0800

    微聚自动化过滤-3710157修改WEIJU_TRANSFER_FILTER_COLLECTION为33，解决冲突

--- marketing-cs/src/main/java/com/br/marketing/context/RuleDataCollectionEnum.java
+++ marketing-cs/src/main/java/com/br/marketing/context/RuleDataCollectionEnum.java
@@ -61,7 +61,8 @@ public enum RuleDataCollectionEnum {
     YILIAN_TRANSFER_FILTER_COLLECTION(29, "亿联转化数据过滤规则所需数据收集"),
     SHUHE_CUSHOUDENG_RULE_DATA_COLLECTION(30, "数禾促首登规则所需数据收集"),
     YISHI_TRANSFER_FILTER_COLLECTION(31, "医时转化规则所需数据收集"),
-    WEIJU_TRANSFER_FILTER_COLLECTION(32, "微聚转化规则所需数据收集"),
+    WUBA_TRANSFER_FILTER_COLLECTION(32, "58新客转化规则所需数据收集"),
+    WEIJU_TRANSFER_FILTER_COLLECTION(33, "微聚转化规则所需数据收集"),
     ;
 
 
