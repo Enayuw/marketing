4e3ffb753403c8abc8ab7e0c7982146b1394bca9
hong.chen <hong.chen@brgroup.com>
2024-10-28 13:57:33 +0800

    update:1.增加异常处理

--- marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/UploadDataController.java
+++ marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/UploadDataController.java
@@ -1,32 +1,23 @@
 package com.br.marketing.datarelayservice.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.br.cloud.web.MethodType;
 import com.br.cloud.web.PrometheusTimeMethod;
-import com.br.common.log.AlertLog;
 import com.br.marketing.aspect.LogAnnotation;
 import com.br.marketing.client.qifu.enums.CodeEnum;
 import com.br.marketing.client.qifu.enums.FlagEnum;
-import com.br.marketing.client.qifu.util.AESUtil;
-import com.br.marketing.client.qifu.util.RSAUtil;
-import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.datarelayservice.client.QiFuAiReqDTO;
 import com.br.marketing.datarelayservice.client.QiFuAiResDTO;
 import com.br.marketing.datarelayservice.service.QiFuAiUploadDataService;
-import com.br.marketing.speedconfig.MarketingCommonConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @Description UploadDataController
@@ -40,52 +31,19 @@ import java.nio.charset.StandardCharsets;
 public class UploadDataController {
     @Resource
     private QiFuAiUploadDataService qiFuAiUploadDataService;
-    @Resource
-    private MarketingCommonConfig marketingCommonConfig;
 
     @ApiOperation(value = "奇富AI上传数据接入接口")
     @PostMapping("/uploadData/24152")
     @LogAnnotation
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS, to = 0)
     public QiFuAiResDTO qiFuAiUploadData(@RequestBody QiFuAiReqDTO requestBody) {
-        String qiFuPublicKey = marketingCommonConfig.getQiFuAIServerConfig().getString("qiFuPublicKey");
-        String brPrivateKey = marketingCommonConfig.getQiFuAIServerConfig().getString("brPrivateKey");
-        System.out.println("==================== 解密开始 ====================");
-        String requestStr = JSON.toJSONString(requestBody);
-        // 服务端1. SHA256withRSA验签
-        String originSign = requestBody.getSign();
-        JSONObject requestJson = JSONObject.parseObject(requestStr);
-        String signAgain = RSAUtil.generateContent(requestJson);
-        boolean verifyResult = RSAUtil.verifySignByPublicKey(qiFuPublicKey, originSign, signAgain);
-        if (!verifyResult) {
-            System.out.println("验签结果：失败！！！");
-            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), requestStr,
-                    "奇富AI上传数据，验签失败！！！"));
-            // 返回异常
-            return qiFuAiUploadDataService.getResult(CodeEnum.GWS209, FlagEnum.F);
-        }
-        System.out.println("验签结果：成功");
+        Pair<CodeEnum, FlagEnum> pair = qiFuAiUploadDataService.handle(requestBody);
 
-        // 服务端2. RSA解密（客户端公钥加密，服务端私钥解密）AESKey和IV
-        String originKey = requestBody.getEncryptKey();
-        String originIv = requestBody.getEncryptIV();
-        String decryptKey = RSAUtil.decryptByPrivateKey(brPrivateKey, originKey);
-        String decryptIv = RSAUtil.decryptByPrivateKey(brPrivateKey, originIv);
-        System.out.println("经过RSA解密后的AESKey：" + decryptKey);
-        System.out.println("经过RSA解密后的IV：" + decryptIv);
-
-        // 服务端3. AES-CBC解密业务数据
-        String originData = requestBody.getBizData();
-        String decryptData = new String(Base64.decodeBase64(AESUtil.decrypt(decryptKey, decryptIv, originData))
-                , StandardCharsets.UTF_8);
-        System.out.println("经过AES解密后的业务数据：" + decryptData);
-
-        System.out.println("==================== 解密结束 ====================");
-        // ******************** 解密 结束 ********************
-
-        // 服务端解密后，会进行相应的业务处理
-        // 处理完业务后，服务端会对最终的业务响应数据进行加密（加密过程同客户端加密过程，所以客户端处理服务端的Response可以参考上述服务端解密过程）
-        Pair<CodeEnum, FlagEnum> pair = qiFuAiUploadDataService.bizHandle(decryptData);
-        return qiFuAiUploadDataService.getResult(pair.getKey(), pair.getValue());
+        QiFuAiResDTO qiFuAiResDTO = new QiFuAiResDTO();
+        qiFuAiResDTO.setCode(pair.getKey().getCode());
+        qiFuAiResDTO.setMsg(pair.getKey().getDesc());
+        qiFuAiResDTO.setFlag(pair.getValue().toString());
+        qiFuAiResDTO.setData(new QiFuAiResDTO.DataResult());
+        return qiFuAiResDTO;
     }
 }
--- marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/service/QiFuAiUploadDataService.java
+++ marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/service/QiFuAiUploadDataService.java
@@ -5,20 +5,22 @@ import com.alibaba.fastjson.JSONObject;
 import com.br.common.log.AlertLog;
 import com.br.marketing.client.qifu.enums.CodeEnum;
 import com.br.marketing.client.qifu.enums.FlagEnum;
+import com.br.marketing.client.qifu.util.AESUtil;
 import com.br.marketing.client.qifu.util.RSAUtil;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
+import com.br.marketing.datarelayservice.client.QiFuAiReqDTO;
 import com.br.marketing.entity.DrsCustomizeUploadData;
 import com.br.marketing.mapper.DrsCustomizeUploadDataMapper;
 import com.br.marketing.datarelayservice.client.QiFuAiBizDataDTO;
-import com.br.marketing.datarelayservice.client.QiFuAiResDTO;
 import com.br.marketing.service.Impl.TableCreateServiceImpl;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
@@ -42,41 +44,90 @@ public class QiFuAiUploadDataService {
     @Resource
     private MarketingCommonConfig marketingCommonConfig;
 
+    public Pair<CodeEnum, FlagEnum> handle(QiFuAiReqDTO requestBody) {
+        String decryptData;
+        try {
+            // 奇富侧公钥
+            String qiFuPublicKey = marketingCommonConfig.getQiFuAIServerConfig().getString("qiFuPublicKey");
+            // 百融侧私钥
+            String brPrivateKey = marketingCommonConfig.getQiFuAIServerConfig().getString("brPrivateKey");
+            String requestStr = JSON.toJSONString(requestBody);
+
+            // 服务端1. SHA256withRSA验签
+            String originSign = requestBody.getSign();
+            JSONObject requestJson = JSONObject.parseObject(requestStr);
+            String signAgain = RSAUtil.generateContent(requestJson);
+            boolean verifyResult = RSAUtil.verifySignByPublicKey(qiFuPublicKey, originSign, signAgain);
+            if (!verifyResult) {
+                log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), requestStr,
+                        "奇富AI上传数据，验签失败！！！"));
+                // 返回异常
+                return new Pair<>(CodeEnum.GWS209, FlagEnum.F);
+            }
+
+            // 服务端2. RSA解密（客户端公钥加密，服务端私钥解密）AESKey和IV
+            String originKey = requestBody.getEncryptKey();
+            String originIv = requestBody.getEncryptIV();
+            String decryptKey = RSAUtil.decryptByPrivateKey(brPrivateKey, originKey);
+            String decryptIv = RSAUtil.decryptByPrivateKey(brPrivateKey, originIv);
+
+            // 服务端3. AES-CBC解密业务数据
+            String originData = requestBody.getBizData();
+            decryptData = new String(Base64.decodeBase64(AESUtil.decrypt(decryptKey, decryptIv, originData))
+                    , StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), e.getMessage(),
+                    "奇富AI上传数据，验签解密失败！！！"), e);
+            // 返回异常
+            return new Pair<>(CodeEnum.GWS208, FlagEnum.F);
+        }
+
+        // 服务端解密后，会进行相应的业务处理
+        return bizHandle(decryptData);
+    }
+
     public Pair<CodeEnum, FlagEnum> bizHandle(String decryptData) {
         String apiCode = marketingCommonConfig.getQiFuAIUploadDataApiCode();
-        String tCid = tableCreateService.getTcId(apiCode);
-        String suffix = null;
-        if (Objects.nonNull(tCid)) {
-            suffix = "_" + tCid;
-        }
-        DrsCustomizeUploadData uploadData = new DrsCustomizeUploadData();
-        uploadData.setApiCode(apiCode);
-        uploadData.setTCid(suffix);
-        drsCustomizeUploadDataMapper.createDrsCustomizeUploadDataTable(suffix);
-
-        QiFuAiBizDataDTO qiFuAiBizDataDTO;
         try {
-            qiFuAiBizDataDTO = JSONObject.parseObject(decryptData, QiFuAiBizDataDTO.class);
-        } catch (Exception e) {
-            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), decryptData,
-                    "奇富AI上传数据，JSON解析失败！！！"));
+            String tCid = tableCreateService.getTcId(apiCode);
+            String suffix = null;
+            if (Objects.nonNull(tCid)) {
+                suffix = "_" + tCid;
+            }
+            DrsCustomizeUploadData uploadData = new DrsCustomizeUploadData();
+            uploadData.setApiCode(apiCode);
+            uploadData.setTCid(suffix);
+            drsCustomizeUploadDataMapper.createDrsCustomizeUploadDataTable(suffix);
 
-            String requestId =
-                    yyyyMMdd.concat("_").concat(apiCode).concat("_").concat(UUID.randomUUID().toString().substring(0, 5)) + System.currentTimeMillis();
-            uploadData.setRequestId(requestId);
-            uploadData.setRequestJsonData(decryptData);
-            uploadData.setBizDataNumber(0);
-            uploadData.setReceiveDate(LocalDate.now().toString());
-            uploadData.setCreateTime(new Date());
-            uploadData.setUpdateTime(new Date());
-            uploadData.setResponseCode(CodeEnum.GWS200.getCode());
-            uploadData.setResponseData(null);
-            uploadData.setExtend("JSON解析失败");
-            uploadData.setStatus(0);
-            return new Pair<>(CodeEnum.GWS200, FlagEnum.F);
-        }
+            QiFuAiBizDataDTO qiFuAiBizDataDTO;
+            try {
+                qiFuAiBizDataDTO = JSONObject.parseObject(decryptData, QiFuAiBizDataDTO.class);
+            } catch (Exception e) {
+                log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), decryptData,
+                        "奇富AI上传数据，JSON解析失败！！！"));
 
-        try {
+                String requestId =
+                        yyyyMMdd.concat("_").concat(apiCode).concat("_").concat(UUID.randomUUID().toString().substring(0, 5)) + System.currentTimeMillis();
+                uploadData.setRequestId(requestId);
+                uploadData.setRequestJsonData(decryptData);
+                uploadData.setBizDataNumber(0);
+                uploadData.setReceiveDate(LocalDate.now().toString());
+                uploadData.setCreateTime(new Date());
+                uploadData.setUpdateTime(new Date());
+                uploadData.setResponseCode(CodeEnum.GWS200.getCode());
+                uploadData.setResponseData(null);
+                uploadData.setExtend("JSON解析失败");
+                uploadData.setStatus(0);
+                // 保存前置数据
+                int i = drsCustomizeUploadDataMapper.insertSelective(uploadData);
+                if (i != 1) {
+                    log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(),
+                            "jsonData:" + decryptData), "奇富AI上传数据入库失败！！！");
+                }
+                return new Pair<>(CodeEnum.GWS200, FlagEnum.F);
+            }
+
+
             uploadData.setRequestId(qiFuAiBizDataDTO.getFlowNo());
             List<QiFuAiBizDataDTO.DataList> dataList = qiFuAiBizDataDTO.getDataList();
             uploadData.setRequestJsonData(decryptData);
@@ -103,14 +154,5 @@ public class QiFuAiUploadDataService {
                     "该apiCode:" + apiCode + "定制上传数据接入异常！！！，jsonData:" + decryptData), e);
             return new Pair<>(CodeEnum.GWS208, FlagEnum.F);
         }
-    }
-
-    public QiFuAiResDTO getResult(CodeEnum codeEnum, FlagEnum flagEnum) {
-        QiFuAiResDTO qiFuAiResDTO = new QiFuAiResDTO();
-        qiFuAiResDTO.setCode(codeEnum.getCode());
-        qiFuAiResDTO.setMsg(codeEnum.getDesc());
-        qiFuAiResDTO.setFlag(flagEnum.toString());
-        qiFuAiResDTO.setData(new QiFuAiResDTO.DataResult());
-        return qiFuAiResDTO;
     }
 }
