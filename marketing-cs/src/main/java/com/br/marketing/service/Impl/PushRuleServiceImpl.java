4bf80ff37064b392cd324541ed7473ab8d032711
dongshuo.he <dongshuo.he@brgroup.com>
2024-10-29 23:04:49 +0800

    规则中心支持评分分布圈选范围&客群分组 v1.6

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -1142,7 +1142,7 @@ public class PushRuleServiceImpl implements PushRuleService {
                                 List<MarketingCondition> conditions = marketingHistory.getCondition();
                                 if (!CollectionUtils.isEmpty(conditions)) {
                                     Map<String, Double> scoreMap = conditions.stream()
-                                            .filter(condition -> StringUtils.isNotEmpty(condition.getCode()) && condition.getDValue() != null)
+                                            .filter(condition -> StringUtils.isNotEmpty(condition.getCode()))
                                             .collect(Collectors.toMap(MarketingCondition::getCode
                                                     , MarketingCondition::getDValue
                                                     , (existing, replacement) -> replacement));
