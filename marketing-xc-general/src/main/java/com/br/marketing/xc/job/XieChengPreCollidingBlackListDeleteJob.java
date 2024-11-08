4e566a617ca2f497155c07788df00ea9feb351f0
peng.kang <KPnewsoft@123>
2024-11-08 16:30:32 +0800

    携程撞库黑名单-黑名单log解密sha256加密job

---
+++ marketing-xc-general/src/main/java/com/br/marketing/xc/job/XieChengPreCollidingBlackListDeleteJob.java
@@ -0,0 +1,27 @@
+package com.br.marketing.xc.job;
+
+import com.br.marketing.service.Impl.xc.XieChengPreCollidingBlackListDeleteService;
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:携程撞库黑名单log加密转sha256加密
+ * @Author: Ethan.Kang
+ */
+@Component
+@Slf4j
+public class XieChengPreCollidingBlackListDeleteJob extends AbstractSimpleElasticJob {
+    @Resource
+    XieChengPreCollidingBlackListDeleteService service;
+
+    @Override
+    public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
+        long curTime = System.currentTimeMillis();
+        service.process();
+        log.warn("携程处理撞库黑名单cell log解密转sha256加密完成, 耗时:{}s", (System.currentTimeMillis() - curTime) / 1000);
+    }
+}
