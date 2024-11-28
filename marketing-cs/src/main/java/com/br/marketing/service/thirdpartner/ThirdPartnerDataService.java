91041941c89774358874dd6330aabce1e25f0ab8
hong.chen <hong.chen@brgroup.com>
2024-11-28 18:11:21 +0800

    update:增加三方数据处理

---
+++ marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataService.java
@@ -0,0 +1,9 @@
+package com.br.marketing.service.thirdpartner;
+
+/**
+ * @Description 三方数据处理接口
+ * @Author hong.chen
+ * @CreateTime 2024/11/28
+ */
+public interface ThirdPartnerDataService {
+}
---
+++ marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataServiceImpl.java
@@ -0,0 +1,14 @@
+package com.br.marketing.service.thirdpartner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description 三方数据处理实现类
+ * @Author hong.chen
+ * @CreateTime 2024/11/28
+ */
+@Service
+@Slf4j
+public class ThirdPartnerDataServiceImpl {
+}
