f811246f25c56cf786ed81c4b37fb3779c0e6ad3
dongshuo.he <dongshuo.he@brgroup.com>
2024-11-28 21:49:53 +0800

    三方流程自动化 代码暂存

--- marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataService.java
@@ -6,4 +6,14 @@ package com.br.marketing.service.thirdpartner;
  * @CreateTime 2024/11/28
  */
 public interface ThirdPartnerDataService {
+
+
+    /**
+     * @Description 有效期变更数据回传
+     * @Author dongshuo.he
+     * @CreateTime 2024/11/28
+     * @Param data 三方数据
+     * @Return void
+     */
+    void validChangeDataBack();
 }
