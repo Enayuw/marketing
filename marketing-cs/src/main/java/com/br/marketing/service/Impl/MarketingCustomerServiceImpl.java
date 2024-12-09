9430d179f88684cf8175f8dedacef12991d7d25f
zhen.li1 <zhen.li1@brgroup.com>
2024-12-09 19:23:58 +0800

    fix:分隔符默认 ,

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingCustomerServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/MarketingCustomerServiceImpl.java
@@ -112,7 +112,7 @@ public class MarketingCustomerServiceImpl implements MarketingCustomerService {
                 customerListVo.setCheckType(marketingCustomerConfig == null
                         ? CustomerTagsValue.CheckTypeEnum.CHECKCELL.getValue()
                         : marketingCustomerConfig.getCheckType());
-                customerListVo.setScoreSeparator(marketingCustomerConfig.getScoreSeparator());
+                customerListVo.setScoreSeparator(marketingCustomerConfig == null ? "," : marketingCustomerConfig.getScoreSeparator());
                 customerListVos.add(customerListVo);
             }
             PageInfo<MarketingCustomer> marketingCustomerPageInfo = new PageInfo<>(marketingCustomersList);
