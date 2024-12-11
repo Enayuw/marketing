231f792855d3bc03aef0ab596b2301da53c03a65
hong.chen <hong.chen@brgroup.com>
2024-12-11 21:49:38 +0800

    update:兼容问题数据

--- marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/thirdpartner/ThirdPartnerDataServiceImpl.java
@@ -72,7 +72,6 @@ public class ThirdPartnerDataServiceImpl implements ThirdPartnerDataService {
                 if (Objects.isNull(apiCode)) {
                     log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.YINGXIAO_SERVICEERROR.getCode(), "入参apiCode：" + orgApiCode,
                             "外呼推送三方上传数据接口，入参apiCode没有映射关系。数据不落库，需要关注"));
-                    return;
                 }
 
                 Long taskId = cleaningAutoService.saveCleanTask(apiCode, 0, "三方数据_上传清洗规则勿动");
