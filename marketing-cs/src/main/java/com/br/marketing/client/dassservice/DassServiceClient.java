f5159267d5bb426297e1126b0d3985243006bd93
guangxiu.li <guangxiu.li@brgroup.com>
2024-12-13 14:36:13 +0800

    sftpToDass

--- marketing-cs/src/main/java/com/br/marketing/client/dassservice/DassServiceClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/dassservice/DassServiceClient.java
@@ -191,7 +191,12 @@ public class DassServiceClient {
             interfaceLog.setResult("程序异常：" + ex.getMessage());
             log.warn(AlertLog.buildErrorMessage(AlarmSendCodeEnum.PUSHING_DAASERROR.getCode(), ex.getMessage()), ex);
         }
-        interfaceLogMapper.insertSelective(interfaceLog);
+        try {
+            interfaceLogMapper.insertSelective(interfaceLog);
+        } catch (Exception e) {
+            log.warn(AlertLog.buildErrorMessage(AlarmSendCodeEnum.PUSHING_DAASERROR.getCode(),
+                    "推送Dass数据写入日志异常，异常日志：" + e.getMessage()), e);
+        }
         return result;
     }
 
