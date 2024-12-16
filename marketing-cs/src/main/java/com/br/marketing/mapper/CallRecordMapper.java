1e9be274aa2cd5e73466a42e03dc93972edb6f64
bin.li1 <bin.li1@brgroup.com>
2024-12-16 19:25:47 +0800

    优化众安名单推送redis不可用时业务兼容2

--- marketing-cs/src/main/java/com/br/marketing/mapper/CallRecordMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/CallRecordMapper.java
@@ -4,7 +4,10 @@ package com.br.marketing.mapper;
 import com.br.marketing.entity.CallRecord;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface CallRecordMapper extends CallRecordMapperBase {
     List<CallRecord> getLastCallRecordByCustNum(@Param("custNums") Collection<String> custNums, @Param("cid") String cid);
