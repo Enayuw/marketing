10e187066f9d47b0ab114c2aa78361b424b28bd9
senyang.zheng <senyang.zheng@brgroup.com>
2024-12-24 21:55:34 +0800

    feat(marketing-check): 萨摩耶定制上传、转化数据清洗任务

--- marketing-cs/src/main/java/com/br/marketing/service/clean/smy/SmyDataCleanService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/clean/smy/SmyDataCleanService.java
@@ -2,4 +2,6 @@ package com.br.marketing.service.clean.smy;
 
 public interface SmyDataCleanService {
     void cleanCustomizedUploadData(String apiCode, String date);
+
+    void cleanCustomizedTransferData(String apiCode, String date);
 }
--- marketing-cs/src/main/java/com/br/marketing/service/clean/smy/impl/SmyDataCleanServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/clean/smy/impl/SmyDataCleanServiceImpl.java
@@ -1,8 +1,12 @@
 package com.br.marketing.service.clean.smy.impl;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.UUID;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.br.common.log.AlertLog;
+import com.br.marketing.client.marketingapi.input.PushTransferDataDetailDTO;
 import com.br.marketing.client.marketingapi.input.UploadDataDTO;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
@@ -10,9 +14,14 @@ import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.BrExecutors;
 import com.br.marketing.dto.MarketingPreUserDTO;
 import com.br.marketing.dto.MarketingPreUserDetailDTO;
+import com.br.marketing.dto.TransferDataDTO;
+import com.br.marketing.dto.TransferDataItemDTO;
 import com.br.marketing.dto.smy.request.NameValueDTO;
+import com.br.marketing.dto.smy.request.SmyTransferRequestDTO;
 import com.br.marketing.dto.smy.request.SmyUploadRequestDTO;
+import com.br.marketing.entity.CustomizeTransferDataSmy;
 import com.br.marketing.entity.CustomizeUploadDataSmy;
+import com.br.marketing.mapper.CustomizeTransferDataSmyMapper;
 import com.br.marketing.mapper.CustomizeUploadDataSmyMapper;
 import com.br.marketing.service.Impl.TableCreateServiceImpl;
 import com.br.marketing.service.PushInfoService;
@@ -21,6 +30,7 @@ import com.br.marketing.speedconfig.MarketingCommonConfig;
 import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -33,13 +43,15 @@ public class SmyDataCleanServiceImpl implements SmyDataCleanService {
     @Resource
     private CustomizeUploadDataSmyMapper customizeUploadDataSmyMapper;
     @Resource
+    private CustomizeTransferDataSmyMapper customizeTransferDataSmyMapper;
+
+    @Resource
     private PushInfoService pushInfoService;
     @Resource
     private TableCreateServiceImpl tableCreateService;
 
     @Override
     public void cleanCustomizedUploadData(String apiCode, String date) {
-
         String tCid = tableCreateService.getTcId(apiCode);
         JSONObject smyCustomizeDataConfig = marketingCommonConfig.getSmyCustomizeDataCleanConfig();
         Integer limit = smyCustomizeDataConfig.getInteger("limit");
@@ -79,11 +91,12 @@ public class SmyDataCleanServiceImpl implements SmyDataCleanService {
                         }
                     } catch (Exception e) {
                         log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.SAMOYE_CUSTOMIZE_UPLOAD_SERVICEERROR.getCode(),
-                                "微聚定制上传数据清洗，子线程处理异常，前置表id：" + customizeUploadDataSmy.getId()), e);
+                                "萨摩耶定制上传数据清洗，子线程处理异常，前置表id：" + customizeUploadDataSmy.getId()), e);
                     }
                 }
             });
         }
+        shutDownThreadPool(threadPool);
     }
 
     private List<MarketingPreUserDetailDTO> buildUploadDataItems(SmyUploadRequestDTO uploadRequestDTO) {
@@ -102,5 +115,111 @@ public class SmyDataCleanServiceImpl implements SmyDataCleanService {
             dataItems.add(marketingPreUserDetailDTO);
         });
         return dataItems;
+    }
+
+    @Override
+    public void cleanCustomizedTransferData(String apiCode, String date) {
+        String tCid = tableCreateService.getTcId(apiCode);
+        JSONObject smyCustomizeDataConfig = marketingCommonConfig.getSmyCustomizeDataCleanConfig();
+        Integer limit = smyCustomizeDataConfig.getInteger("limit");
+        Integer threadNum = smyCustomizeDataConfig.getInteger("threadNum");
+        ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(threadNum, threadNum, "SMY_CUSTOMIZED_TRANSFER_DATA_CLEAN_THREAD_POOL", 200);
+        boolean mark = Boolean.TRUE;
+        Long minId = customizeTransferDataSmyMapper.smyCleanCustomizedUploadDataOfMinId(tCid, apiCode, date);
+        if (minId == null) {
+            return;
+        }
+        minId = minId - 1;
+        while (mark) {
+            List<CustomizeTransferDataSmy> transferDataSmyList = customizeTransferDataSmyMapper.smyCleanCustomizedUploadDataByMinId(tCid, apiCode,
+                    date, minId, limit);
+            if (transferDataSmyList.size() <= 0) {
+                mark = Boolean.FALSE;
+                continue;
+            }
+            minId = transferDataSmyList.get(transferDataSmyList.size() - 1).getId();
+            threadPool.submit(() -> {
+                for (CustomizeTransferDataSmy customizeTransferDataSmy : transferDataSmyList) {
+                    SmyTransferRequestDTO smyUploadRequestDTO = JSONObject.parseObject(customizeTransferDataSmy.getRequestJsonData(),
+                            SmyTransferRequestDTO.class);
+                    Result<Boolean> result;
+                    try {
+                        TransferDataDTO<TransferDataItemDTO> transferDataDTO = new TransferDataDTO<>();
+                        transferDataDTO.setRequestId(apiCode + "_" + UUID.fastUUID().toString(true));
+                        List<TransferDataItemDTO> dataTransferItems = buildTransferDataItems(smyUploadRequestDTO);
+                        transferDataDTO.setDataItems(dataTransferItems);
+                        PushTransferDataDetailDTO dto = new PushTransferDataDetailDTO();
+                        dto.setApiCode(apiCode);
+                        dto.setJsonData(JSON.toJSONString(transferDataDTO));
+                        result = pushInfoService.pushTransferByRetry(dto, null);
+                        if (ResultCode.SUCCESS.getValue().equals(result.getCode())) {
+                            customizeTransferDataSmyMapper.updateSyncStatusById(tCid, customizeTransferDataSmy.getId(), 1);
+                        }
+                    } catch (Exception e) {
+                        log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.SAMOYE_CUSTOMIZE_UPLOAD_SERVICEERROR.getCode(),
+                                "萨摩耶定制转化数据清洗，子线程处理异常，前置表id：" + customizeTransferDataSmy.getId()), e);
+                    }
+                }
+            });
+        }
+        shutDownThreadPool(threadPool);
+    }
+
+    private List<TransferDataItemDTO> buildTransferDataItems(SmyTransferRequestDTO smyUploadRequestDTO) {
+        List<TransferDataItemDTO> dataItems = Lists.newArrayList();
+        TransferDataItemDTO transferDataItemDTO = new TransferDataItemDTO();
+        transferDataItemDTO.setCustNum(smyUploadRequestDTO.getCid());
+        DateTime date = DateUtil.date(smyUploadRequestDTO.getEventTime());
+        String formatDateTime = DateUtil.formatDateTime(date);
+        if ("login".equals(smyUploadRequestDTO.getEventType())) {
+            transferDataItemDTO.setIfLogin("1");
+            transferDataItemDTO.setLoginTime(formatDateTime);
+        }
+        if ("finish".equals(smyUploadRequestDTO.getEventType())) {
+            transferDataItemDTO.setIfApply("1");
+            transferDataItemDTO.setApplyDt(formatDateTime);
+        }
+        if ("approve".equals(smyUploadRequestDTO.getEventType())) {
+            transferDataItemDTO.setApplyResult("1");
+            transferDataItemDTO.setApplyTime(formatDateTime);
+        }
+        if ("approve".equals(smyUploadRequestDTO.getEventType())) {
+            transferDataItemDTO.setApplyResult("1");
+            transferDataItemDTO.setAuditTime(formatDateTime);
+        }
+        if ("loan".equals(smyUploadRequestDTO.getEventType())) {
+            transferDataItemDTO.setIfLent("1");
+            transferDataItemDTO.setLentTime(formatDateTime);
+        }
+        JSONObject reserveField1 = new JSONObject();
+        if ("blacklist".equals(smyUploadRequestDTO.getEventType())) {
+            reserveField1.put("isBlack", "1");
+        }
+        if ("F1".equals(smyUploadRequestDTO.getEventType())) {
+            reserveField1.put("F1", "1");
+        }
+        if ("F2".equals(smyUploadRequestDTO.getEventType())) {
+            reserveField1.put("F2", "1");
+        }
+        if ("F3".equals(smyUploadRequestDTO.getEventType())) {
+            reserveField1.put("F3", "1");
+        }
+        reserveField1.putAll(JSONObject.parseObject(smyUploadRequestDTO.getExtendFields()));
+        dataItems.add(transferDataItemDTO);
+        return dataItems;
+    }
+
+
+    public void shutDownThreadPool(ThreadPoolExecutor threadPool) {
+        // 关闭线程池
+        threadPool.shutdown();
+        try {
+            while (!threadPool.awaitTermination(10L, TimeUnit.SECONDS)) {
+                log.warn("萨摩耶数据清洗 等待线程池结束");
+            }
+        } catch (InterruptedException e) {
+            threadPool.shutdownNow();
+            log.error("萨摩耶数据清洗 线程池关闭异常,直接关闭线程池", e);
+        }
     }
 }
