9101944e7b5e559a3c11a022b163b887fc14a289
bin.li1 <bin.li1@brgroup.com>
2024-12-25 18:27:01 +0800

    众安推送锁定名单 redis异常优化，添加异常挡板

--- marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/zhongan/ZhongAnPushRosterDataHandler.java
+++ marketing-cs/src/main/java/com/br/marketing/monkeydata/handle/zhongan/ZhongAnPushRosterDataHandler.java
@@ -678,6 +678,10 @@ public class ZhongAnPushRosterDataHandler extends IMonkeyDataHandle<ZhonganRoste
             Iterator<Map.Entry<String, String>> iterator = custNumMap.entrySet().iterator();
             while (iterator.hasNext()) {
                 Map.Entry<String, String> ob = iterator.next();
+                //模拟redis异常
+                if(marketingCommonConfig.getZhongAnPushRosterDataMock() == 1){
+                    throw new RuntimeException("众安推送名单锁定模拟redis异常");
+                }
                 if (nowDayStr.equals(ob.getValue()) && redisChgService.sismember(RedisKeyConstant.zhongAnblackCusNumToday,ob.getKey())) {
                     iterator.remove();
                     custNumBlackListSet.add(ob.getKey() + nowDayStr);
@@ -693,6 +697,10 @@ public class ZhongAnPushRosterDataHandler extends IMonkeyDataHandle<ZhonganRoste
      * 兼容redis不可用场景，查询db判断众安当天拨打记录黑名单
      */
     public void processTodayZhongaAnBlackDataFromDb(Map<String,String> custNumMap,Set custNumBlackListSet,String nowDayStr){
+        //禁用db查询功能开关
+        if(marketingCommonConfig.getZhongAnPushRosterDataMock() == 2){
+            return ;
+        }
         if (CollectionUtils.isEmpty(custNumMap)) {
             return ;
         }
