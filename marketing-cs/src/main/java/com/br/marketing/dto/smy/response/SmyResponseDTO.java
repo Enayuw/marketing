a2d0a3815a58ff1cc48939e8a3544d3d1afc1b99
senyang.zheng <senyang.zheng@brgroup.com>
2024-12-25 16:15:43 +0800

    feat(marketing-check): 萨摩耶定转化数据清洗任务
    sonar fix

--- marketing-cs/src/main/java/com/br/marketing/dto/smy/response/SmyResponseDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/smy/response/SmyResponseDTO.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 
 @Data
 public class SmyResponseDTO implements Serializable {
+    private static final long serialVersionUID = 8376361895732046747L;
 
     @ApiModelProperty("响应码")
     private Integer code;
