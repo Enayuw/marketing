532e9fa500607838a06f53871de6d49a1e8cfdc6
senyang.zheng <senyang.zheng@brgroup.com>
2024-12-27 12:44:02 +0800

    feat(marketing-data-relay-service): 萨摩耶
    1.萨摩耶定制接口 联调问题优化

--- marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/SmyCustomizeController.java
+++ marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/SmyCustomizeController.java
@@ -1,21 +1,21 @@
 package com.br.marketing.datarelayservice.controller;
 
-import com.br.cloud.web.MethodType;
-import com.br.cloud.web.PrometheusTimeMethod;
-import com.br.marketing.datarelayservice.client.QiFuAiResDTO;
-import com.br.marketing.datarelayservice.dto.smy.request.SmyTransferRequestDTO;
-import com.br.marketing.datarelayservice.dto.smy.request.SmyUploadRequestDTO;
-import com.br.marketing.datarelayservice.dto.smy.response.SmyResponseDTO;
-import com.br.marketing.datarelayservice.service.SmyTransferDataService;
-import com.br.marketing.datarelayservice.service.SmyUploadDataService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import javax.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.cloud.web.MethodType;
+import com.br.cloud.web.PrometheusTimeMethod;
+import com.br.marketing.datarelayservice.dto.smy.response.SmyResponseDTO;
+import com.br.marketing.datarelayservice.service.SmyTransferDataService;
+import com.br.marketing.datarelayservice.service.SmyUploadDataService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 @Api(value = "萨摩耶定制传输数据接口")
 @RequestMapping("/marketing/v1/smy")
@@ -33,14 +33,15 @@ public class SmyCustomizeController {
     @ApiOperation(value = "萨摩耶代运营数据上传")
     @PostMapping("/upload")
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS, to = 0)
-    public SmyResponseDTO receiveSmyUploadData(@RequestBody SmyUploadRequestDTO dto) {
-        return smyUploadDataService.receiveSmyUploadData(dto);
+    public SmyResponseDTO receiveSmyUploadData(@RequestBody String jsonData) {
+
+        return smyUploadDataService.receiveSmyUploadData(jsonData);
     }
 
     @ApiOperation(value = "萨摩耶代运营数据上传")
     @PostMapping("/transfer")
     @PrometheusTimeMethod(buckets = {0.05d, 0.1d, 0.2d, 0.5d}, methodType = MethodType.ACCESS, to = 0)
-    public SmyResponseDTO receiveSmyTransferData(@RequestBody SmyTransferRequestDTO dto) {
-        return smyTransferDataService.receiveSmyTransferData(dto);
+    public SmyResponseDTO receiveSmyTransferData(@RequestBody String jsonData) {
+        return smyTransferDataService.receiveSmyTransferData(jsonData);
     }
 }
--- marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/service/SmyTransferDataService.java
+++ marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/service/SmyTransferDataService.java
@@ -28,42 +28,50 @@ public class SmyTransferDataService {
     @Resource
     private CustomizeTransferDataSmyMapper customizeTransferDataSmyMapper;
 
-    public SmyResponseDTO receiveSmyTransferData(SmyTransferRequestDTO dto) {
+    public SmyResponseDTO receiveSmyTransferData(String jsonData) {
         SmyResponseDTO smyResponseDTO = new SmyResponseDTO();
         smyResponseDTO.success();
-        CustomizeTransferDataSmy customizeTransferDataSmy = new CustomizeTransferDataSmy();
-        JSONObject smyCustomizeDataConfig = marketingCommonConfig.getSmyCustomizeDataConfig();
-        customizeTransferDataSmy.setApiCode(smyCustomizeDataConfig == null ? null : smyCustomizeDataConfig.getString("transferApiCode"));
-        customizeTransferDataSmy.setRequestId(UUID.fastUUID().toString(true));
-        customizeTransferDataSmy.setReceiveDate(LocalDate.now().toString());
-        customizeTransferDataSmy.setRequestJsonData(JSONObject.toJSONString(dto));
-        customizeTransferDataSmy.setStatus(1);
-        // Check Field
-        StringBuilder errorMessage = new StringBuilder();
-        if (StringUtils.isBlank(dto.getEventType())) {
-            errorMessage.append(", event_type 不可为空");
+        try {
+            SmyTransferRequestDTO dto = JSONObject.parseObject(jsonData, SmyTransferRequestDTO.class);
+            CustomizeTransferDataSmy customizeTransferDataSmy = new CustomizeTransferDataSmy();
+            JSONObject smyCustomizeDataConfig = marketingCommonConfig.getSmyCustomizeDataConfig();
+            customizeTransferDataSmy.setApiCode(smyCustomizeDataConfig == null ? null : smyCustomizeDataConfig.getString("transferApiCode"));
+            customizeTransferDataSmy.setRequestId(UUID.fastUUID().toString(true));
+            customizeTransferDataSmy.setReceiveDate(LocalDate.now().toString());
+            customizeTransferDataSmy.setRequestJsonData(jsonData);
+            customizeTransferDataSmy.setStatus(1);
+            // Check Field
+            StringBuilder errorMessage = new StringBuilder();
+            if (StringUtils.isBlank(dto.getEventType())) {
+                errorMessage.append(", event_type 不可为空");
+            }
+            if (dto.getEventTime() == null) {
+                errorMessage.append(", event_time 不可为空");
+            }
+            if (StringUtils.isBlank(dto.getCid())) {
+                errorMessage.append(", cid 不可为空");
+            }
+            if (StringUtils.isNotBlank(dto.getExtendFields()) && !JSONObject.isValid(dto.getExtendFields())) {
+                errorMessage.append(", extend_fields 非Json格式");
+            }
+            if (errorMessage.length() > 0) {
+                customizeTransferDataSmy.setStatus(0);
+                smyResponseDTO = smyResponseDTO.failed(SmyResponseDTO.ResultEnum.FAILED_PARAM_ERROR, errorMessage.toString());
+            }
+            customizeTransferDataSmy.setBizDataNumber(1);
+            customizeTransferDataSmy.setResponseCode(String.valueOf(smyResponseDTO.getCode()));
+            customizeTransferDataSmy.setResponseData(smyResponseDTO.getMessage());
+            int i = customizeTransferDataSmyMapper.insertSelective(customizeTransferDataSmy);
+            if (i != 1) {
+                log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.SAMOYE_CUSTOMIZE_TRANSFER_SERVICEERROR.getCode(), "jsonData:" + jsonData,
+                    "萨摩耶定制转化数据入库失败！！！"));
+            }
+        } catch (Exception e) {
+            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.SAMOYE_CUSTOMIZE_TRANSFER_SERVICEERROR.getCode(), "jsonData:" + jsonData,
+                "萨摩耶定制转化数据接入异常！！！"));
+            smyResponseDTO = smyResponseDTO.failed(SmyResponseDTO.ResultEnum.FAILED_SYSTEM_ERROR);
         }
-        if (dto.getEventTime() == null) {
-            errorMessage.append(", event_time 不可为空");
-        }
-        if (StringUtils.isBlank(dto.getCid())) {
-            errorMessage.append(", cid 不可为空");
-        }
-        if (StringUtils.isNotBlank(dto.getExtendFields()) && !JSONObject.isValid(dto.getExtendFields())) {
-            errorMessage.append(", extend_fields 非Json格式");
-        }
-        if (errorMessage.length() > 0) {
-            customizeTransferDataSmy.setStatus(0);
-            smyResponseDTO = smyResponseDTO.failed(SmyResponseDTO.ResultEnum.FAILED_PARAM_ERROR, errorMessage.toString());
-        }
-        customizeTransferDataSmy.setBizDataNumber(1);
-        customizeTransferDataSmy.setResponseCode(String.valueOf(smyResponseDTO.getCode()));
-        customizeTransferDataSmy.setResponseData(smyResponseDTO.getMessage());
-        int i = customizeTransferDataSmyMapper.insertSelective(customizeTransferDataSmy);
-        if (i != 1) {
-            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.SAMOYE_CUSTOMIZE_TRANSFER_SERVICEERROR.getCode(),
-                "dtoJson:" + JSONObject.toJSONString(dto), "萨摩耶定制转化数据入库失败！！！"));
-        }
+
         return smyResponseDTO;
     }
 }
