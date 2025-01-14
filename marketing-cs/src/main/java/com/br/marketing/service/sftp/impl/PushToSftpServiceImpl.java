e57fba80568c8cf3152dfafda4a069e69c8a1d0d
bingxu.kong <bingxu.kong@brgroup.com>
2025-01-14 20:57:16 +0800

    文件推送SFTP异常重试

---
+++ marketing-cs/src/main/java/com/br/marketing/service/sftp/impl/PushToSftpServiceImpl.java
@@ -0,0 +1,47 @@
+package com.br.marketing.service.sftp.impl;
+
+import com.br.marketing.client.RedisChgService;
+import com.br.marketing.common.commondto.Result;
+import com.br.marketing.common.commondto.ResultCode;
+import com.br.marketing.entity.LoanFile;
+import com.br.marketing.service.sftp.PushFinishService;
+import com.br.marketing.service.sftp.PushService;
+import com.br.marketing.service.sftp.PushToSftpService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+/**
+ * @ClassName PushToSftpServiceImpl
+ * @Description TODO
+ * @Author kongbx
+ * @Date 2025/1/14 16:28
+ */
+@Service
+@Slf4j
+public class PushToSftpServiceImpl implements PushToSftpService {
+
+    @Autowired
+    PushService pushService;
+    @Autowired
+    RedisChgService redisChgService;
+    @Autowired
+    PushFinishService pushFinishService;
+
+    @Override
+    public Result pushFiles(List<LoanFile> pushList) {
+
+        try {
+            pushService.push(pushList);
+        }catch (Exception e){
+            return new Result().setCode(ResultCode.FAIL.getValue()).setMessage("推送文件至SFTP失败！");
+        }
+        for (LoanFile loanFile : pushList) {
+            pushFinishService.pushFinish(loanFile.getId());
+        }
+        return new Result().setCode(ResultCode.SUCCESS.getValue()).setMessage("推送文件至SFTP成功");
+    }
+}
