3ed01c31dd984bc7f811da097c78ee9356a52730
juman.wang <juman.wang@brgroup.com>
2025-01-20 22:43:37 +0800

    线索回调接口

--- marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/HxCarClueController.java
+++ marketing-data-relay-service/src/main/java/com/br/marketing/datarelayservice/controller/HxCarClueController.java
@@ -1,9 +1,11 @@
 package com.br.marketing.datarelayservice.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.datarelayservice.vo.CarClueResponse;
 import com.br.marketing.dto.HxClueCallBackReqDTO;
 import com.br.marketing.service.carclue.ICarClueService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @RequestMapping("/hxcarclue")
 @RestController
 public class HxCarClueController {
@@ -20,7 +23,12 @@ public class HxCarClueController {
 
     @PostMapping("/callBack")
     public CarClueResponse callBack(@RequestBody HxClueCallBackReqDTO reqDTO) {
-        Result result = iCarClueService.callBackClue(reqDTO);
-        return CarClueResponse.fromResult(result);
+        try {
+            Result result = iCarClueService.callBackClue(reqDTO);
+            return CarClueResponse.fromResult(result);
+        } catch (Exception ex) {
+            log.error(String.format("请求信息：%s;异常信息：%s", JSON.toJSONString(reqDTO), ex.getMessage()), ex);
+            return new CarClueResponse().setResultCode(0).setMessage("内部异常请稍后在试");
+        }
     }
 }
