5d4dea6d0870d696ccb9d1854acd362de079b010
xiang.li4 <xiang.li4@brgroup.com>
2025-01-22 15:31:22 +0800

    58新客转化推送客服-增加phone

--- marketing-cs/src/main/java/com/br/marketing/rule/wuba/WuBaTransferDataToCustomerFilter.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/wuba/WuBaTransferDataToCustomerFilter.java
@@ -1,6 +1,7 @@
 package com.br.marketing.rule.wuba;
 
 import com.alibaba.fastjson.JSON;
+import com.br.common.util.BrCipherMaker;
 import com.br.common.util.DateUtils;
 import com.br.common.util.StringUtils;
 import com.br.marketing.bo.PeriodOfValidityBO;
@@ -9,6 +10,7 @@ import com.br.marketing.client.robotaiapi.input.ConversionData;
 import com.br.marketing.context.ProcessHandlerContext;
 import com.br.marketing.context.RuleDataCollectionEnum;
 import com.br.marketing.context.impl.WuBaRuleCollectDataImpl;
+import com.br.marketing.entity.MarketingSyncUser;
 import com.br.marketing.entity.MarketingTransferSyncUser;
 import com.br.marketing.rule.AssembleData;
 import com.br.marketing.strategy.InterfaceHandlerEnum;
@@ -17,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -44,9 +47,9 @@ public class WuBaTransferDataToCustomerFilter implements AssembleData<Conversion
         conversionData.setInversionStatus("0");
         Map<String, SyncUserValidityPeriodsBO> userValidityPeriodsBOMap = ruleNecessaryData.getSyncUserValidityPeriodMap();
         SyncUserValidityPeriodsBO syncUserValidityPeriodsBO = userValidityPeriodsBOMap.get(marketingTransferSyncUser.getCustNum());
-//        List<MarketingSyncUser> syncUsers = syncUserValidityPeriodsBO.getSyncUsers();
-//        conversionData.setPhone(BrCipherMaker.getInstance().decode(syncUsers.get(0).getCell()));
-//        conversionData.setGroupType(syncUsers.get(0).getUserType());
+        List<MarketingSyncUser> syncUsers = syncUserValidityPeriodsBO.getSyncUsers();
+        conversionData.setPhone(BrCipherMaker.getInstance().decode(syncUsers.get(0).getCell()));
+        // conversionData.setGroupType(syncUsers.get(0).getUserType());
 
         PeriodOfValidityBO periodOfValidityBO = syncUserValidityPeriodsBO.getBuilders().get(0).addDateString().addOfDayTimeStrString().builder();
         conversionData.setExpireBeginDate(periodOfValidityBO.getBeginDateStr());
