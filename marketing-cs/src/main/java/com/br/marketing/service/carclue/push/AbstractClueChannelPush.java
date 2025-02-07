ee1d1ebaac344c867c44a1c37e569d3b1b8e668c
bingxu.kong <bingxu.kong@brgroup.com>
2025-02-07 14:13:54 +0800

    fix

--- marketing-cs/src/main/java/com/br/marketing/service/carclue/push/AbstractClueChannelPush.java
+++ marketing-cs/src/main/java/com/br/marketing/service/carclue/push/AbstractClueChannelPush.java
@@ -2,19 +2,10 @@ package com.br.marketing.service.carclue.push;
 
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.entity.CarClueInfo;
-import com.br.marketing.mapper.CarClueInfoMapper;
-import com.br.marketing.service.carclue.clueenums.CarClueDataStatusEnum;
-import com.br.marketing.service.carclue.clueenums.CarCluePushStatusEnum;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service
 public abstract class AbstractClueChannelPush {
-
-
-    @Resource
-    CarClueInfoMapper carClueInfoMapper;
 
     /**
      * 组装和推送逻辑
@@ -22,13 +13,6 @@ public abstract class AbstractClueChannelPush {
      * @return
      */
     public abstract Result push(CarClueInfo carClueInfo);
-
-    void updateClueStatus(Long id, CarCluePushStatusEnum carCluePushStatusEnum) {
-        CarClueInfo updateEntity = new CarClueInfo();
-        updateEntity.setId(id);
-        updateEntity.setClueDataStatus(carCluePushStatusEnum.getValue());
-        carClueInfoMapper.updateByPrimaryKeySelective(updateEntity);
-    }
 
     /**
      * 过滤规则的名称
