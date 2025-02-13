33bf958be3941fd3f8d28893fcd254ca812f9cd0
guangxiu.li <guangxiu.li@brgroup.com>
2025-02-13 20:26:19 +0800

    外呼传输字段更改

--- marketing-cs/src/main/java/com/br/marketing/service/carclue/todb/impl/CarCluesDataCleanServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/carclue/todb/impl/CarCluesDataCleanServiceImpl.java
@@ -128,11 +128,11 @@ public class CarCluesDataCleanServiceImpl implements CarCluesDataToDBService {
                                             "车线索数据入库异常：通话明细用户信息或手机号为空！"));
                                     continue;
                                 }
-                                String carBrand = getPhoneFromJsonObject(jsonObject, "carBrand");
-                                String carSeries = getPhoneFromJsonObject(jsonObject, "carSeries");
+                                String carBrand = getPhoneFromJsonObject(jsonObject, "brandName");
+                                String carSeries = getPhoneFromJsonObject(jsonObject, "seriesName");
                                 String province = getPhoneFromJsonObject(jsonObject, "province");
                                 String city = getPhoneFromJsonObject(jsonObject, "city");
-                                String member = getPhoneFromJsonObject(jsonObject, "firstName");
+                                String member = getPhoneFromJsonObject(jsonObject, "cusName");
                                 String resourceType = getPhoneFromJsonObject(jsonObject, "resourceType");
                                 String intentionGrade = callRecord.getIntentionGrade();
                                 if (ObjectUtil.isNotEmpty(carClueIntentionGrades) && carClueIntentionGrades.contains(intentionGrade)) {
