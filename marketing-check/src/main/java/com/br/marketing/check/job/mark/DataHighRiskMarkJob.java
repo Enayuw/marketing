b52eac9f4e7437eeb037bbf1d3e81cf76a73e805
guangchao.zhang <505740550@qq.com>
2025-02-18 22:21:35 +0800

    job 名字修改

--- marketing-check/src/main/java/com/br/marketing/check/job/mark/DataHighRiskMarkJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/mark/DataHighRiskMarkJob.java
@@ -1,18 +1,9 @@
 package com.br.marketing.check.job.mark;
 
-import com.br.marketing.common.utils.StringUtils;
-import com.br.marketing.service.IPPDTransferService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import javax.annotation.Resource;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Map;
 
 /**
  * pp停车-榕树数据打标规则-高风险打标
@@ -22,7 +13,7 @@ import java.util.Map;
  */
 @Component
 @Slf4j
-public class PPDataHighRiskMarkJob extends AbstractSimpleElasticJob {
+public class DataHighRiskMarkJob extends AbstractSimpleElasticJob {
 
     @Override
     public void process(JobExecutionMultipleShardingContext shardingContext) {
