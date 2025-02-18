b8a2addc218e94b2a08d7878e5a829fba3f7c802
dongshuo.he <dongshuo.he@brgroup.com>
2025-02-18 19:57:13 +0800

    众安自动化过滤v3-v1.2

--- marketing-cs/src/main/java/com/br/marketing/rule/zhongan/ZhongAnTransferDataByUserType4Filter.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/zhongan/ZhongAnTransferDataByUserType4Filter.java
@@ -97,7 +97,7 @@ public class ZhongAnTransferDataByUserType4Filter implements AssembleData<Conver
 
     @Override
     public String label() {
-        return "ZhongAn_TransferData_To_CustomerFilter7";
+        return "ZhongAn_TransferData_To_CustomerFilter4";
     }
 
     @Override
