c5439b7442e0c38f85ad3124c377b87c96f9bc50
bingxu.kong <bingxu.kong@brgroup.com>
2025-02-25 18:18:51 +0800

    fix

--- marketing-cs/src/main/java/com/br/marketing/service/mark/Impl/DataUpdateEsMarkServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/mark/Impl/DataUpdateEsMarkServiceImpl.java
@@ -4,16 +4,12 @@ import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.br.cloud.web.MethodType;
-import com.br.cloud.web.PrometheusTimeMethod;
 import com.br.common.log.AlertLog;
 import com.br.marketing.client.RedisChgService;
-import com.br.marketing.common.annoation.RetryMethod;
 import com.br.marketing.common.constants.rediskey.RedisKeyConstant;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.BrExecutors;
 import com.br.marketing.dto.mark.FlagDataEsMark;
-import com.br.marketing.entity.FlagDataExample;
 import com.br.marketing.entity.StraHisFile;
 import com.br.marketing.enums.EsSyncStatusEnum;
 import com.br.marketing.es.bean.MarketingCondition;
@@ -61,6 +57,11 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
     @Resource
     MarketingCommonConfig marketingCommonConfig;
     private static final String TITLE = "【pp停车数据更新es】";
+    private static final String fieldKeys = "flag_new_cust,flag_riskgroup,flag_interest,flag_age,flag_province," +
+            "flag_special_small,flag_specialrisklevel_rule,flag_applyloan,flag_scoreysbase,flag_scorefxsbbaseb," +
+            "flag_scorescashonregisternologin,flag_scorescashonyxxy,flag_scorencashonzawswyyym," +
+            "flag_intellaudio_blacklist,flag_without_willingness,flag_whitelist";
+
 
     @Override
     public void process() {
@@ -69,10 +70,11 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
             if (null == straHisFile) {
                 return;
             }
-            Integer threadPoolSize = marketingCommonConfig.getDataMarkThreadNum();
+            Integer threadPoolSize = marketingCommonConfig.getDataMarkESThreadNum();
             int dataMarkPageSize = marketingCommonConfig.getDataMarkPageSize() == null?2000:marketingCommonConfig.getDataMarkPageSize();
             ThreadPoolExecutor threadPool = BrExecutors.getThreadPool(threadPoolSize, threadPoolSize);
             String key = RedisKeyConstant.DATA_UPDATE_ES_MARK.concat(":").concat(apiCode);
+            List<Long> ids = new ArrayList<>();
             while (true) {
                 String lockValue = UUID.randomUUID().toString();
                 try {
@@ -80,10 +82,11 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
                     List<FlagDataEsMark> flagDataEsMarkList = flagDataMapper.queryEsMarkByDate(apiCode, LocalDate.now().toString(), dataMarkPageSize);
                     if (CollectionUtil.isEmpty(flagDataEsMarkList)) {
                         redisChgService.unlock(key, lockValue);
+                        threadPoolShutDown(threadPool);
                         break;
                     }
                     //更新打标表状态
-                    List<Long> ids = flagDataEsMarkList.stream().map(FlagDataEsMark::getId).collect(Collectors.toList());
+                    ids = flagDataEsMarkList.stream().map(FlagDataEsMark::getId).collect(Collectors.toList());
                     flagDataMapper.batchUpdateEsStatusById(ids, EsSyncStatusEnum.SYNCING.getValue());
                     //释放锁
                     redisChgService.unlock(key, lockValue);
@@ -95,6 +98,7 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
                 } catch (Exception e) {
                     log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.PP_MARKING_SERVICEERROR.getCode(),
                             TITLE + "抢锁出现异常，" + "errorMessage=" + e.getMessage()), e);
+                    flagDataMapper.batchUpdateEsStatusById(ids, EsSyncStatusEnum.INITIAL.getValue());
                     redisChgService.unlock(key, lockValue);
                     threadPoolShutDown(threadPool);
                     break;
@@ -104,9 +108,8 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
         });
     }
 
-    @RetryMethod(retryNowNum = 3, isOrNoDbRetry = true)
-    @PrometheusTimeMethod(buckets = {0.02d, 0.05d, 0.2d, 0.5d, 1d}, methodType = MethodType.REMOTE)
     private void updateEsMarkData(List<FlagDataEsMark> flagDataList, StraHisFile straHisFile) {
+        List<Long> ids = flagDataList.stream().map(FlagDataEsMark::getId).collect(Collectors.toList());
         try {
             String index = EsHandleUtil.getDateFromBatchNumber(straHisFile.getBatchNumber());
 
@@ -144,6 +147,7 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
                 for (Map.Entry<String, MarketingHistory> entry : marketingHistoryMap.entrySet()) {
                     MarketingHistory marketingHistory = entry.getValue();
                     List<MarketingCondition> marketingConditions = marketingHistory.getCondition();
+                    // todo marketingHistory.getCell() 是否加密
                     FlagDataEsMark flagData = groupedByCellLog.get(marketingHistory.getCell());
                     buildParams(marketingConditions, flagData);
                     JSONObject params = JSON.parseObject(JSON.toJSONString(marketingHistory));
@@ -153,23 +157,23 @@ public class DataUpdateEsMarkServiceImpl implements DataUpdateEsMarkService {
                 }
             }
             //更新打标表状态
-            List<Long> ids = flagDataList.stream().map(FlagDataEsMark::getId).collect(Collectors.toList());
             if (!CollectionUtil.isEmpty(ids)) {
                 flagDataMapper.batchUpdateEsStatusById(ids, EsSyncStatusEnum.COMPLETE.getValue());
             }
         } catch (Exception e) {
+            flagDataMapper.batchUpdateEsStatusById(ids, EsSyncStatusEnum.INITIAL.getValue());
             log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.PP_MARKING_SERVICEERROR.getCode(),
                     TITLE + "出现异常，" + "errorMessage=" + e.getMessage()), e);
         }
     }
 
     private void buildParams(List<MarketingCondition> conditions, FlagDataEsMark flagData) {
-        List<String> fieldKeys = marketingCommonConfig.getDataMarkField();
+        String[] split = fieldKeys.split(",");
         Map<String, MarketingCondition> map = conditions.stream()
                 .collect(Collectors.toMap(MarketingCondition::getFieldKey, data -> data, (oldValue, newValue) -> newValue));
-        for (String fieldKey : fieldKeys) {
+        for (String fieldKey : split) {
             String value = "";
-            switch (fieldKey) {
+            switch (fieldKey.trim()) {
                 case "flag_new_cust":
                     value = String.valueOf(flagData.getFlagNewCust());
                     break;
--- marketing-cs/src/main/java/com/br/marketing/service/mark/Impl/DataWriteBackFileMarkServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/mark/Impl/DataWriteBackFileMarkServiceImpl.java
@@ -9,6 +9,8 @@ import com.br.marketing.client.SftpClient;
 import com.br.marketing.common.annoation.RetryMethod;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.BrExecutors;
+import com.br.marketing.common.utils.Constants;
+import com.br.marketing.common.utils.file.ZipUtil;
 import com.br.marketing.entity.FlagData;
 import com.br.marketing.entity.FlagDataExample;
 import com.br.marketing.entity.StraHisFile;
@@ -32,6 +34,8 @@ import java.util.*;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @ClassName DataWriteBackFileMarkServiceImpl
@@ -90,7 +94,8 @@ public class DataWriteBackFileMarkServiceImpl implements DataWriteBackFileMarkSe
         flagDataExample.createCriteria()
                 .andApiCodeEqualTo(apiCode)
                 .andAppletDateEqualTo(LocalDate.now().toString())
-                .andEsSyncStatusNotEqualTo(EsSyncStatusEnum.COMPLETE.getValue());
+                .andEsSyncStatusNotEqualTo(EsSyncStatusEnum.COMPLETE.getValue())
+                .andIsDeleteEqualTo(0);
         int i = flagDataMapper.countByExample(flagDataExample);
         if (i > 0) {
             log.warn(TITLE + "es数据未补充完毕");
@@ -146,8 +151,6 @@ public class DataWriteBackFileMarkServiceImpl implements DataWriteBackFileMarkSe
         }
     }
 
-    @RetryMethod(retryNowNum = 3, isOrNoDbRetry = true)
-    @PrometheusTimeMethod(buckets = {0.02d, 0.05d, 0.2d, 0.5d, 1d}, methodType = MethodType.REMOTE)
     private void writeBackFileMark(List<FlagData> flagDataList, String batchNumber,
                                    String descPath, String fileName) {
         // 写入doris
@@ -209,8 +212,6 @@ public class DataWriteBackFileMarkServiceImpl implements DataWriteBackFileMarkSe
         return flagDataResult;
     }
 
-    private final Object fileLock = new Object();
-
     private void writeDataToFile(List<Map<String, Object>> dataList, String descPath, String fileName) {
         if (CollectionUtil.isEmpty(dataList)) {
             return;
@@ -220,25 +221,20 @@ public class DataWriteBackFileMarkServiceImpl implements DataWriteBackFileMarkSe
         if (!file.exists()) {
             file.mkdirs();
         }
-        File decodeFile = new File(descPath.concat(fileName));
+        File decodeFile = new File(fileAllPath);
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(decodeFile, true), StandardCharsets.UTF_8))) {
             // 判断是否添加文件头
             checkHeaderExists(fileAllPath, dataList, writer);
             // 写入每行数据
-            synchronized (fileLock) {
-                for (Map<String, Object> resultMap : dataList) {
-                    StringBuilder row = new StringBuilder();
-                    Collection<Object> values = resultMap.values();
-                    for (Object v : values) {
-                        if (v == null) {
-                            row.append(",");
-                        } else {
-                            row.append(v).append(",");
-                        }
-                    }
-                    // 写入数据行
-                    writer.append(row.toString()).append(System.lineSeparator());
-                }
+            for (Map<String, Object> resultMap : dataList) {
+                // 将Map的值转换为一个List<String>，同时处理null值
+                List<String> values = resultMap.values().stream()
+                        .map(v -> v != null ? v.toString() : "")
+                        .collect(Collectors.toList());
+
+                String row = String.join(",", values);
+                // 写入数据行
+                writer.append(row).append(System.lineSeparator());
             }
         } catch (IOException e) {
             log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.PP_MARKING_SERVICEERROR.getCode(),
@@ -262,11 +258,22 @@ public class DataWriteBackFileMarkServiceImpl implements DataWriteBackFileMarkSe
         SftpClient sftpClient = new SftpClient(sftpHost, sftpPort, sftpUsername, sftpPwd);
         String syncDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
         String remotePath = "/UploadFiles/marketing/" + apiCode + "/output/" + syncDate;
+
+        String zipFileName = fileName.replace(".txt", ".zip");
         String localFileName = descPath.concat(fileName);
-        log.warn(TITLE + "原文件路径：" + localFileName + " | 推送文件路径:" + remotePath);
+        String localFileNameZip = descPath.concat(zipFileName);
+        log.warn(TITLE + "原文件路径：" + localFileNameZip + " | 推送文件路径:" + remotePath);
         try {
+            ZipUtil.compress(localFileName, localFileNameZip);
             sftpClient.connect();
-            sftpClient.uploadFile(remotePath, fileName, localFileName);
+            boolean upload = sftpClient.uploadFile(remotePath, zipFileName, localFileNameZip);
+            if (upload) {
+                File successFile = new File(localFileNameZip + ".success");
+                successFile.createNewFile();
+                if (successFile.exists()) {
+                    sftpClient.uploadFile(remotePath, zipFileName + ".success", localFileNameZip + ".success");
+                }
+            }
         } catch (Exception e) {
             log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.PUSH_TO_SFTP.getCode(),
                     TITLE + "文件推送SFTP异常，apiCode：" + apiCode), e);
