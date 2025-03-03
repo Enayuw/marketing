1464e1360f839d0d7ca73e5c2a7178d4c1a7806c
guangxiu.li <guangxiu.li@brgroup.com>
2025-03-03 12:22:31 +0800

    数据计算代码优化

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/qifu/QiFuServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/qifu/QiFuServiceImpl.java
@@ -360,15 +360,13 @@ public class QiFuServiceImpl implements IQiFuService {
         String rTaTemporaryAmountExpireDate = reserField1.getString("rTaTemporaryAmountExpireDate");
         // 计算新的字段值
         String oldLowAmountys = getAmount(null, lowAmountys, rTaLastAdjustmentAmount);
-        String oldHighAmountys = getAmount(highAmountys, null, rTaLastAdjustmentAmount);
         String newHighAmountys = getAmount(highAmountys, null, rTotalAvailableAmt);
-        String newLowAmountys = getAmount(null, lowAmountys, rTotalAvailableAmt);
-        String changeAmountys = calculateDifference(newHighAmountys, oldHighAmountys);
+        String changeAmountys = calculateDifference(newHighAmountys, oldLowAmountys);
         String remainDayys = calculateDaysDifference(rTaTemporaryAmountExpireDate);
         String changeIncrease = calculateIncreaseRate(newHighAmountys, oldLowAmountys);
 
         reserField1.put("highAmount_derived", newHighAmountys);
-        reserField1.put("lowAmount_derived", newLowAmountys);
+        reserField1.put("lowAmount_derived", oldLowAmountys);
         reserField1.put("changeAmount_derived", changeAmountys);
         reserField1.put("remainDayys_derived", remainDayys);
         reserField1.put("changeIncrease_derived", changeIncrease);
