2b9af0c11ccfcd4a229c1cb0ea5faadf1efe5c4e
yu.xia <yu.xia@brgroup.com>
2025-03-03 19:02:47 +0800

    RabbitMQ新增virtual-host隔离配置

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/config/RabbitMqConfig.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/config/RabbitMqConfig.java
@@ -211,15 +211,23 @@ public class RabbitMqConfig {
             @Value("${spring.rabbitmq.yz.addresses:11}") String yzAddresses,
             @Value("${spring.rabbitmq.yz.username:11}") String yzUsername,
             @Value("${spring.rabbitmq.yz.password:11}") String yzPassword,
-            @Value("${spring.rabbitmq.yz.virtual-host:11}") String yzVirtualHost) {
+            @Value("${spring.rabbitmq.yz.virtual-host:11}") String yzVirtualHost,
+            @Value("${spring.rabbitmq.sim.virtual-host:11}") String simVirtualHost
+    ) {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
         String enumName = ClusterEnum.CLUSTER_PROD_C.getName();
-        log.warn("clusterConfig:{},enumName:{}", clusterConfig, enumName);
+        String simEnumName = ClusterEnum.CLUSTER_PROD_B.getName();
+        log.warn("clusterConfig:{},enumName:{},simEnumName:{}", clusterConfig, enumName, simEnumName);
         if (StringUtils.isNotBlank(clusterConfig) && enumName.equals(clusterConfig)) {
             connectionFactory.setAddresses(yzAddresses);
             connectionFactory.setUsername(yzUsername);
             connectionFactory.setPassword(yzPassword);
             connectionFactory.setVirtualHost(yzVirtualHost);
+        } else if (StringUtils.isNotBlank(clusterConfig) && simEnumName.equals(clusterConfig)){
+            connectionFactory.setAddresses(yzAddresses);
+            connectionFactory.setUsername(yzUsername);
+            connectionFactory.setPassword(yzPassword);
+            connectionFactory.setVirtualHost(simVirtualHost);
         } else {
             connectionFactory.setAddresses(zwAddresses);
             connectionFactory.setUsername(zwUsername);
