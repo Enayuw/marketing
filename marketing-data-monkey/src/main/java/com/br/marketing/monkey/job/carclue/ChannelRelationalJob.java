96b09811f0c3808557f62328115ccd3d5e581ad5
bingxu.kong <bingxu.kong@brgroup.com>
2025-03-17 14:30:12 +0800

    车线索二期-渠道商更新job修改

--- marketing-data-monkey/src/main/java/com/br/marketing/monkey/job/carclue/ChannelRelationalJob.java
+++ marketing-data-monkey/src/main/java/com/br/marketing/monkey/job/carclue/ChannelRelationalJob.java
@@ -1,5 +1,8 @@
 package com.br.marketing.monkey.job.carclue;
 
+import com.br.marketing.common.utils.Constants;
+import com.br.marketing.entity.CarClueRelationalMappingExample;
+import com.br.marketing.mapper.CarClueRelationalMappingMapper;
 import com.br.marketing.service.carclue.ChannelRelationalService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
@@ -7,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
 
 /**
  * @ClassName ChannelRelationalJob
@@ -19,10 +23,23 @@ import javax.annotation.Resource;
 public class ChannelRelationalJob extends AbstractSimpleElasticJob {
     @Resource
     private ChannelRelationalService channelRelationalService;
+    @Resource
+    CarClueRelationalMappingMapper carClueRelationalMappingMapper;
     private static final String TITLE = "【外采渠道商信息维护】";
 
     @Override
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
+
+        CarClueRelationalMappingExample carClueRelationalMappingExample = new CarClueRelationalMappingExample();
+        carClueRelationalMappingExample.createCriteria()
+                .andAppletDateEqualTo(LocalDate.now().toString())
+                .andIsDelEqualTo(Constants.DATA_VALID);
+        int i = carClueRelationalMappingMapper.countByExample(carClueRelationalMappingExample);
+        if(i > 0){
+            log.warn(TITLE + "今日外采渠道信息已维护！");
+            return;
+        }
+
         log.warn(TITLE + "start");
         long start = System.currentTimeMillis();
         //获取当天的 易车KA 外采初始配置
