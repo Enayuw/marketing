aefc117c8bd3792cb840f161b63f35223f19a0b7
bingxu.kong <bingxu.kong@brgroup.com>
2025-03-18 11:07:30 +0800

    车线索二期-去掉立即重试

--- marketing-cs/src/main/java/com/br/marketing/service/carclue/push/impl/YiCarKAClueChannelPush.java
+++ marketing-cs/src/main/java/com/br/marketing/service/carclue/push/impl/YiCarKAClueChannelPush.java
@@ -45,7 +45,7 @@ public class YiCarKAClueChannelPush extends AbstractClueChannelPush {
     MarketingCommonConfig marketingCommonConfig;
 
     @Override
-    @RetryMethod(retryNowNum = 3, isOrNoDbRetry = true)
+    @RetryMethod(isOrNoDbRetry = true)
     @PrometheusTimeMethod(buckets = {0.02d, 0.05d, 0.2d, 0.5d, 1d}, methodType = MethodType.REMOTE)
     public Result push(CarClueInfo carClueInfo, Integer retry) {
         try {
