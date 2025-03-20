a3f95c29581c9c0e07155c4c028df1aee7febb5c
bingxu.kong <bingxu.kong@brgroup.com>
2025-03-20 10:01:54 +0800

    fix

--- marketing-cs/src/main/java/com/br/marketing/client/tag/AntaiosResourceClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/tag/AntaiosResourceClient.java
@@ -10,7 +10,7 @@ import com.br.marketing.client.tag.dto.AntaiosResourceDTO;
 import com.br.marketing.client.tag.vo.AntaiosResourceVo;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.net.ThirdApiResultTransfer;
-//import com.br.marketing.mapper.InterfaceLogMapper;
+import com.br.marketing.mapper.datasource.log.InterfaceLogMapper;
 import com.br.marketing.speedconfig.MarketingCommonConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,27 +47,27 @@ public class AntaiosResourceClient {
     public ThreadPoolExecutor logDbpool;
 
     @Autowired
-    //InterfaceLogMapper interfaceLogMapper;
+    InterfaceLogMapper interfaceLogMapper;
 
     @PrometheusTimeMethod(buckets = {0.02d, 0.05d, 0.2d, 0.5d, 1d}, methodType = MethodType.REMOTE)
     public AntaiosResourceVo getTagLibrary(AntaiosResourceDTO dto) {
         AntaiosResourceVo result = new AntaiosResourceVo();
-        //try{
-        //    ThirdApiResultTransfer thirdApiResult = new ApiCallerUtil(restTemplate,interfaceLogMapper,logDbpool)
-        //            .setUrl(tagUrl)
-        //            .setContentType(MediaType.APPLICATION_FORM_URLENCODED)
-        //            .setRequestParam(dto).postTransferStr();
-        //    if(!Integer.valueOf(200).equals(thirdApiResult.getHttpCode())){
-        //        throw new RuntimeException("客服中心：".concat(String.valueOf(thirdApiResult.getHttpCode())));
-        //    }
-        //    result = JSON.parseObject(thirdApiResult.getResult()
-        //            , new TypeReference<AntaiosResourceVo>() {}.getType());
-        //    return result;
-        //}catch (Exception ex){
-        //    log.warn(AlertLog.buildErrorMessage(AlarmSendCodeEnum.PUSHING_CUSTOMERERROR.getCode(), ex.getMessage()), ex);
-        //    result.setCode("9999");
-        //    result.setMessage(ex.getMessage());
-        //}
+        try{
+            ThirdApiResultTransfer thirdApiResult = new ApiCallerUtil(restTemplate,interfaceLogMapper,logDbpool)
+                    .setUrl(tagUrl)
+                    .setContentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .setRequestParam(dto).postTransferStr();
+            if(!Integer.valueOf(200).equals(thirdApiResult.getHttpCode())){
+                throw new RuntimeException("客服中心：".concat(String.valueOf(thirdApiResult.getHttpCode())));
+            }
+            result = JSON.parseObject(thirdApiResult.getResult()
+                    , new TypeReference<AntaiosResourceVo>() {}.getType());
+            return result;
+        }catch (Exception ex){
+            log.warn(AlertLog.buildErrorMessage(AlarmSendCodeEnum.PUSHING_CUSTOMERERROR.getCode(), ex.getMessage()), ex);
+            result.setCode("9999");
+            result.setMessage(ex.getMessage());
+        }
         return result;
     }
 
