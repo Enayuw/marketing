703ef79f290978e336b1b9e81a90d7361461d2cc
guangxiu.li <guangxiu.li@brgroup.com>
2025-03-21 21:45:28 +0800

    查询接口逻辑修改

--- marketing-cs/src/main/java/com/br/marketing/dto/tag/TagQueryDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/tag/TagQueryDTO.java
@@ -23,7 +23,7 @@ public class TagQueryDTO {
     private Integer size = 10;
 
     @ApiModelProperty("标签名称")
-    private List<String> tagName;
+    private String tagName;
 
     @ApiModelProperty("授权APICode列表")
     private List<String> apiCodes;
