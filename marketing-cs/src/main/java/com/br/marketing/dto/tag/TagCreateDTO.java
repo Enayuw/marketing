4a8d0edc5bd0b96210360e352e54ca438c6694b3
guangxiu.li <guangxiu.li@brgroup.com>
2025-03-21 15:13:49 +0800

    接口代码优化

--- marketing-cs/src/main/java/com/br/marketing/dto/tag/TagCreateDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/tag/TagCreateDTO.java
@@ -1,5 +1,7 @@
 package com.br.marketing.dto.tag;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -21,7 +23,7 @@ public class TagCreateDTO {
 
     @NotNull(message = "条件树不能为空")
     @ApiModelProperty(value = "条件树配置", required = true)
-    private TagConditionTreeDTO conditionTree;
+    private JSONObject conditionTree;
 
     @NotBlank(message = "时间范围不能为空")
     @ApiModelProperty(value = "时间范围(YESTERDAY-昨天,LAST_THREE_DAYS-最近三天,LAST_WEEK-最近一周,LAST_MONTH-最近一月,LAST_THREE_MONTHS-最近三月)", required = true)
