724fc735c9ab9b13944cb927e52f3396a7b446f9
guangxiu.li <guangxiu.li@brgroup.com>
2025-03-26 17:06:35 +0800

    权限更改

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TagController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TagController.java
@@ -45,8 +45,8 @@ public class TagController {
     @AddDataAuthBusiness
     public ApiResult<PageResultReturn> getTagList(@RequestBody @Valid TagQueryDTO request) {
         try {
-//            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
-//            request.setCurrentUserId(Long.valueOf(userDetail.getId()));
+            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
+            request.setCurrentUserId(Long.valueOf(userDetail.getId()));
             PageResultReturn result = tagService.getTagList(request);
             return new ApiResult<PageResultReturn>().success(result);
         } catch (Exception ex) {
@@ -79,9 +79,9 @@ public class TagController {
     @AddDataAuthBusiness
     public ApiResult<Boolean> updateTag(@RequestBody @Validated TagUpdateDTO request) {
         try {
-//            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
-//            request.setOptUserId(Long.valueOf(userDetail.getId()));
-//            request.setOptUserName(userDetail.getUserName());
+            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
+            request.setOptUserId(Long.valueOf(userDetail.getId()));
+            request.setOptUserName(userDetail.getUserName());
             Boolean result = tagService.updateTag(request);
             return new ApiResult<Boolean>().success(result);
         } catch (BusinessException be) {
@@ -169,8 +169,8 @@ public class TagController {
     @AddDataAuthBusiness
     public ApiResult<Boolean> batchDelete(@RequestBody @Valid TagBatchDeleteDTO request) {
         try {
-//            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
-//            request.setCurrentUserId(Long.valueOf(userDetail.getId()));
+            MarketingUserDetail userDetail = ThreadContextInfo.getUser();
+            request.setCurrentUserId(Long.valueOf(userDetail.getId()));
             Boolean result = tagService.batchDelete(request);
             return new ApiResult<Boolean>().success(result);
         } catch (BusinessException be) {
