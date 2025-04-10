3d1ebc81cecbb81d7752bb3a89b89194c3486c66
hong.chen <hong.chen@brgroup.com>
2025-04-10 19:31:46 +0800

    update:datapushdown新服务搭建

---
+++ marketing-ai-data-push-down/src/main/java/com/br/marketing/marketingaidatapushdown/api/ConsumerApp.java
@@ -0,0 +1,95 @@
+package com.br.marketing.marketingaidatapushdown.api;
+
+import com.br.marketing.common.enums.SwitchMessageQueueEnum;
+import com.br.marketing.common.utils.AiMQConstants;
+import com.br.marketing.common.utils.MQConstants;
+import com.br.marketing.service.IPushShuheDataService;
+import com.br.marketing.service.Impl.ConsumerService;
+import com.br.marketing.service.PushDataService;
+import com.br.marketing.service.PushRuleService;
+import com.br.marketing.strategy.InterfaceHandlerService;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * rabbitmq 消费端
+ */
+@Component
+public class ConsumerApp {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerApp.class);
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @Autowired
+    PushRuleService pushRuleService;
+
+    @Autowired
+    IPushShuheDataService pushShuheDataService;
+
+    @Autowired
+    PushDataService pushDataService;
+
+    @Resource
+    private InterfaceHandlerService interfaceHandlerService;
+
+    /**
+     * 消费 AI推送下游数据消费端
+     * @param channel 通道
+     * @param message 消息体
+     */
+    @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE, durable = "true")
+            , exchange = @Exchange(type = "topic", value = MQConstants.MARKETINGEXCHANGER_NAME, durable = "true")
+            , key = AiMQConstants.ROUTING_KEY_MARKETING_AI_UNIVERSAL_RECEIVE)}, containerFactory = "concurrentContainerFactory")
+    public void consumerUniversalTransfer(Channel channel, Message message) {
+        String o = new String(message.getBody(), StandardCharsets.UTF_8);
+        /*消费逻辑*/
+        consumerService.consumerRunAndSwitchQueue(channel, message, interfaceHandlerService::handleDataDirection, o,
+                MQConstants.ROUTING_KEY_UNIVERSAL_TRANSFER_ERROR_DELAY, SwitchMessageQueueEnum.MARKETING_AI_UNIVERSAL_RECEIVE.getQueueType(),
+                AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE);
+    }
+
+    /**
+     * 消费 AI推送下游数据消费端-备用1
+     * @param channel 通道
+     * @param message 消息体
+     */
+    @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE_1, durable = "true")
+            , exchange = @Exchange(type = "topic", value = MQConstants.MARKETINGEXCHANGER_NAME, durable = "true")
+            , key = AiMQConstants.ROUTING_KEY_MARKETING_AI_UNIVERSAL_RECEIVE_1)}, containerFactory = "concurrentContainerFactory")
+    public void consumerUniversalTransfer1(Channel channel, Message message) {
+        String o = new String(message.getBody(), StandardCharsets.UTF_8);
+        /*消费逻辑*/
+        consumerService.consumerRunAndSwitchQueue(channel, message, interfaceHandlerService::handleDataDirection, o,
+                MQConstants.ROUTING_KEY_UNIVERSAL_TRANSFER_ERROR_DELAY, SwitchMessageQueueEnum.MARKETING_AI_UNIVERSAL_RECEIVE.getQueueType(),
+                AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE_1);
+    }
+
+    /**
+     * 消费 AI推送下游数据消费端-备用2
+     * @param channel 通道
+     * @param message 消息体
+     */
+    @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE_2, durable = "true")
+            , exchange = @Exchange(type = "topic", value = MQConstants.MARKETINGEXCHANGER_NAME, durable = "true")
+            , key = AiMQConstants.ROUTING_KEY_MARKETING_AI_UNIVERSAL_RECEIVE_2)}, containerFactory = "concurrentContainerFactory")
+    public void consumerUniversalTransfer2(Channel channel, Message message) {
+        String o = new String(message.getBody(), StandardCharsets.UTF_8);
+        /*消费逻辑*/
+        consumerService.consumerRunAndSwitchQueue(channel, message, interfaceHandlerService::handleDataDirection, o,
+                MQConstants.ROUTING_KEY_UNIVERSAL_TRANSFER_ERROR_DELAY, SwitchMessageQueueEnum.MARKETING_AI_UNIVERSAL_RECEIVE.getQueueType(),
+                AiMQConstants.MARKETING_AI_UNIVERSAL_RECEIVE_2);
+    }
+}
