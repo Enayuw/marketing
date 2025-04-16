b6bad9347acc9b77118d1ec789d5494b4972c56e
guangchao.zhang <505740550@qq.com>
2025-04-16 16:12:46 +0800

    携程队列拆除且黑名单 status =12

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferSyncUserMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTransferSyncUserMapper.java
@@ -459,7 +459,7 @@ public interface MarketingTransferSyncUserMapper extends MarketingTransferSyncUs
     MarketingTransferSyncUser getXcTransferNoAdDataByOnlyConvType(@Param("tCid") String tcId, @Param("custNum") String custNum, @Param("apiCodes") JSONArray apiCodes);
 
     MarketingTransferSyncUser getXcTransferNoAdDataByOnlyBlack(@Param("tCid") String tcId, @Param("custNum") String custNum, @Param("apiCodes") JSONArray apiCodes);
-    MarketingTransferSyncUser getXcTransferTodayNoAdDataByOnlyBlack(@Param("tCid") String tcId, @Param("custNum") String custNum, @Param("apiCodes") String apiCodes);
+    MarketingTransferSyncUser getXcTransferTodayNoAdDataByOnlyBlack(@Param("tCid") String tcId, @Param("custNum") String custNum, @Param("apiCode") String apiCode);
 
     List<MarketingTransferSyncUser> getZhongBangTransferByApiCodetikv_(@Param("tCid") String tCid,
                                                                  @Param("apiCode") String apiCode,
