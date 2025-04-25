fa745094bba39d5598be97a69fa7f8b38d5054dc
dongshuo.he <dongshuo.he@brgroup.com>
2025-04-25 19:03:51 +0800

    同程 提交代码

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingDataCleanConfigMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingDataCleanConfigMapper.java
@@ -1,11 +1,15 @@
 package com.br.marketing.mapper;
 
 import com.br.marketing.entity.MarketingDataCleanConfig;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface MarketingDataCleanConfigMapper extends MarketingDataCleanConfigMapperBase{
 
-    List<MarketingDataCleanConfig> selectConfigs(String apiCode, Integer cleanType, String bizAction);
+    List<MarketingDataCleanConfig> selectConfigs(
+            @Param("apiCode")String apiCode,
+            @Param("cleanType")Integer cleanType,
+            @Param("bizAction")String bizAction);
 
 }
