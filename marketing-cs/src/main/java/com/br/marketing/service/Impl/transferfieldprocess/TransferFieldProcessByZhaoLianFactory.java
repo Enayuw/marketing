3b1ebe6462d93e395e910ed51703a1df965bb461
bingxu.kong <bingxu.kong@brgroup.com>
2025-05-29 14:25:11 +0800

    招联清洗逻辑

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/transferfieldprocess/TransferFieldProcessByZhaoLianFactory.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/transferfieldprocess/TransferFieldProcessByZhaoLianFactory.java
@@ -75,7 +75,7 @@ public class TransferFieldProcessByZhaoLianFactory  implements TransferFieldProc
                     log.warn("招联转化清洗匹配有效上传数据：" + JSONObject.toJSONString(marketingSyncUser));
                     dto.setUserType(marketingSyncUser.getUserType());
                     JSONObject jsonObject = JSONObject.parseObject(dto.getReserveField1());
-                    jsonObject.put("cell",marketingSyncUser.getCell());
+                    jsonObject.put("cell",marketingSyncUser.getCellSha256());
                     dto.setReserveField1(jsonObject.toJSONString());
                 }
             }
