db240494c16fd6cbb885ecdeb75acca7814e1604
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-05-29 18:46:36 +0800

    umeng 新分支-xml相关

---
+++ marketing-cs/src/main/java/com/br/marketing/mapper/UMengDataMapper.java
@@ -0,0 +1,24 @@
+package com.br.marketing.mapper;
+
+import com.br.marketing.entity.UMengData;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface UMengDataMapper extends UMengDataMapperBase{
+
+    List<UMengData> selectDeviceAddList(@Param("localId") Long localId, @Param("apiCode") String apiCode,
+                                        @Param("searchStartTime") LocalDateTime searchStartTime, @Param("lastSearchId") Long lastSearchId,
+                                        @Param("searchSize") Integer searchSize);
+
+    Integer updateDeviceAddStatus(@Param("idList") List<Long> idList,@Param("localId")Long localId,
+                                  @Param("deviceAddStatus") Integer deviceAddStatus);
+
+    List<UMengData> selectDevicePushList(@Param("localId") Long localId, @Param("apiCode") String apiCode,
+                                         @Param("lastSearchId") Long lastSearchId, @Param("searchSize") Integer searchSize);
+
+    List<UMengData> selectDeviceByCell(@Param("localId")Long localId, @Param("cell") String cell);
+
+    Integer updatePushStausByIds(@Param("idList")List<Long> idList, @Param("pushStatus")Integer pushStatus);
+}
