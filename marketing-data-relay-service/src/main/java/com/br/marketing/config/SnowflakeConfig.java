e5bc9ed87a1d847f47b65205cccc9ceec4650313
zeqiang.guo <zeqinag.guo@brgroup.com>
2025-06-10 00:14:26 +0800

    华强：rocketmq 迁移 + 初始化问题

---
+++ marketing-data-relay-service/src/main/java/com/br/marketing/config/SnowflakeConfig.java
@@ -0,0 +1,29 @@
+package com.br.marketing.config;
+
+import com.br.marketing.handle.SnowflakeRedisGeneratorHandle;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+/**
+ * @author Hua Qiang
+ * @date 2025/6/9 20:16
+ */
+@Configuration
+public class SnowflakeConfig {
+
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${snowflake.datacenter.id:0}")
+    private long datacenterId;
+
+    // 配置改进版雪花算法（摆脱时钟依赖）
+    @Bean
+    @Order()
+    public SnowflakeRedisGeneratorHandle snowflakeRedisGeneratorHandle() {
+        return new SnowflakeRedisGeneratorHandle(null, applicationName, datacenterId);
+    }
+}
