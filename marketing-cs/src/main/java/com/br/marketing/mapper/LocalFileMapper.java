61ab1be9c85f0a1a8aadadb17cbfed719c0cb947
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-06-10 14:13:03 +0800

    umeng 新分支-thread fix优化 sonar(单行过长)

--- marketing-cs/src/main/java/com/br/marketing/mapper/LocalFileMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/LocalFileMapper.java
@@ -96,5 +96,6 @@ public interface LocalFileMapper extends LocalFileMapperBase {
      * @param startTime
      * @return
      */
-    List<LocalFile> getLastDataByApiCode(@Param("apiCode") String apiCode,@Param("startTime") LocalDateTime startTime,@Param("endTime")LocalDateTime endTime);
+    List<LocalFile> getLastDataByApiCode(@Param("apiCode") String apiCode,@Param("startTime") LocalDateTime startTime
+            ,@Param("endTime")LocalDateTime endTime);
 }
