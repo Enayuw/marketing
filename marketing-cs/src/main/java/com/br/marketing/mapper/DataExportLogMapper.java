7c43ba69c8b209dcef55a0b2a1436c0e3df812b1
guangxiu.li <guangxiu.li@brgroup.com>
2025-06-12 15:14:58 +0800

    创建任务接口代码编写

--- marketing-cs/src/main/java/com/br/marketing/mapper/DataExportLogMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/DataExportLogMapper.java
@@ -1,21 +1,5 @@
 package com.br.marketing.mapper;
 
-import com.br.marketing.mysqlInterceptor.AddDataAuth;
-import com.br.marketing.vo.DataExportLogVO;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
 public interface DataExportLogMapper extends DataExportLogMapperBase {
 
-    @AddDataAuth
-    List<DataExportLogVO> getDataExportLogList(@Param("taskId") Long taskId, 
-                                               @Param("batchNo") String batchNo, 
-                                               @Param("executeStatus") Integer executeStatus,
-                                               @Param("startTimeStart") String startTimeStart,
-                                               @Param("startTimeEnd") String startTimeEnd);
-
-    List<DataExportLogVO> getLogsByTaskId(@Param("taskId") Long taskId);
-
-    int updateSftpStatus(@Param("id") Long id, @Param("sftpStatus") Integer sftpStatus);
 } 
--- marketing-cs/src/main/java/com/br/marketing/mapper/DataExportTaskMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/DataExportTaskMapper.java
@@ -1,17 +1,5 @@
 package com.br.marketing.mapper;
 
-import com.br.marketing.mysqlInterceptor.AddDataAuth;
-import com.br.marketing.vo.DataExportTaskVO;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
 public interface DataExportTaskMapper extends DataExportTaskMapperBase {
 
-    @AddDataAuth
-    List<DataExportTaskVO> getDataExportTaskList(@Param("taskName") String taskName, 
-                                                  @Param("dataSource") String dataSource, 
-                                                  @Param("status") Integer status);
-
-    List<DataExportTaskVO> getActiveTasksByDataSource(@Param("dataSource") String dataSource);
 } 
