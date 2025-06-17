56bf5f49196e5deeb29a517a438c3ab574a8a8ee
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-06-17 20:36:05 +0800

    tc speed优化: 关闭线程池

--- marketing-cs/src/main/java/com/br/marketing/service/tc/impl/TcSyncDataMatchServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tc/impl/TcSyncDataMatchServiceImpl.java
@@ -193,22 +193,15 @@ public class TcSyncDataMatchServiceImpl implements TcSyncDataMatchService {
 
     public  void shutdownThreadPool(ThreadPoolExecutor executor) {
         log.warn(TITLE + "shutdownThreadPool开始");
-        long taskCount = -1;
         executor.shutdown();
         try {
-            while (!executor.awaitTermination(2, TimeUnit.MINUTES)) {
-                long completedTaskCount = executor.getCompletedTaskCount();
-                if (taskCount == completedTaskCount) {
-                    log.warn(TITLE + "业务线程等待超时");
-                    break;
-                }
-                taskCount = completedTaskCount;
+            while (!executor.awaitTermination(60L, TimeUnit.SECONDS)) {
+                log.info("{},线程池关闭",TITLE);
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException ex) {
             executor.shutdownNow();
-            Thread.interrupted();
-        } catch (Throwable e) {
-            log.warn(TITLE + "ThreadPoolManager shutdown executor has error : ", e);
+            log.error("{},日志保存线程池结束异常！",TITLE,ex);
+            Thread.currentThread().interrupt();
         }
         log.warn(TITLE + "shutdownThreadPool结束");
     }
