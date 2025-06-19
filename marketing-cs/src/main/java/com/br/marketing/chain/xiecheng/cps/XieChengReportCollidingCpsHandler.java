a1b8f2b60af8a612bd92f28cf7885c9fa249d662
dongshuo.he <dongshuo.he@brgroup.com>
2025-06-19 10:27:13 +0800

    携程cps撞库 日志查询修改 channel赋值

--- marketing-cs/src/main/java/com/br/marketing/chain/xiecheng/cps/XieChengReportCollidingCpsHandler.java
+++ marketing-cs/src/main/java/com/br/marketing/chain/xiecheng/cps/XieChengReportCollidingCpsHandler.java
@@ -1,6 +1,7 @@
 package com.br.marketing.chain.xiecheng.cps;
 
 import com.br.marketing.chain.xiecheng.AbstractXieChengReportHandler;
+import com.br.marketing.common.utils.StringUtils;
 import com.br.marketing.context.XieChengReportContext;
 import com.br.marketing.entity.XieChengSmsCollidingDataLogVt;
 import com.br.marketing.enums.HandlerStageEnum;
@@ -22,6 +23,7 @@ public class XieChengReportCollidingCpsHandler extends AbstractXieChengReportHan
         //1.当日撞库返回为true
         XieChengSmsCollidingDataLogVt dataLogVt = xieChengSmsCollidingDataLogVtMapper.selectLatestVtLog(context.getSha256Tel());
         if(null == dataLogVt) return "撞库释放时间小于当前时间或无返回true的撞库日志";
+        if(StringUtils.isBlank(dataLogVt.getOrgChannel())) return "撞库日志获取orgChannel为空";
         context.getAdReqDTO().setMktChannel(dataLogVt.getOrgChannel());
         return null;
     }
