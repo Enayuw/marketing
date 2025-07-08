326bfce95c74dc12dba38be41c96b10ef11bd26c
dongshuo.he <dongshuo.he@brgroup.com>
2025-07-08 22:51:44 +0800

    线路&短信对账报表 新增接口 代码提交

--- marketing-cs/src/main/java/com/br/marketing/service/LineSmsAccountService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/LineSmsAccountService.java
@@ -4,9 +4,12 @@ import com.br.marketing.common.commondto.ApiResult;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.commonentity.PageResultReturn;
 import com.br.marketing.dto.account.SmsAccountDto;
+import com.br.marketing.entity.MarketingSmsAccountLog;
+import com.br.marketing.entity.MarketingSmsAccountRecord;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import javax.validation.Valid;
+import java.util.List;
 
 public interface LineSmsAccountService {
 
@@ -19,6 +22,10 @@ public interface LineSmsAccountService {
     PageResultReturn getSmsAccounts(Integer current, Integer size,String vendorName,String channelsName,Double price);
 
 
-    PageResultReturn getSmsAccountLogs(Integer current, Integer size, Long recordId,String vendorName,String optUserName,Integer optType);
+    List<MarketingSmsAccountLog> getSmsAccountLogs(Long configId);
+
+    List<MarketingSmsAccountRecord> getSmsAccountsByConfigId(Long configId);
+
+
 
 }
