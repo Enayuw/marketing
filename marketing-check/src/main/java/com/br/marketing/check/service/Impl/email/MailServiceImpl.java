1a4db31676f8b6f7c547b882e711eb9094f95ed4
xiong.luo <xiong.luo@brgroup.com>
2025-07-09 14:29:09 +0800

    宜信邮件统计信息入BI

--- marketing-check/src/main/java/com/br/marketing/check/service/Impl/email/MailServiceImpl.java
+++ marketing-check/src/main/java/com/br/marketing/check/service/Impl/email/MailServiceImpl.java
@@ -1,25 +1,19 @@
 package com.br.marketing.check.service.Impl.email;
 
-import cn.hutool.poi.excel.ExcelUtil;
 import com.br.marketing.check.service.email.IMailService;
 import com.br.marketing.common.utils.StringUtils;
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.mail.*;
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import javax.mail.search.SearchTerm;
-import javax.mail.search.SubjectTerm;
 import java.io.File;
-import java.util.List;
-import java.util.Properties;
 
 /**
  *
@@ -27,13 +21,11 @@ import java.util.Properties;
 @Slf4j
 @Service
 public class MailServiceImpl implements IMailService {
-
     /**
      * Spring Boot 提供了一个发送邮件的简单抽象，使用的是下面这个接口，这里直接注入即可使用
      */
     @Autowired
-    private JavaMailSenderImpl mailSender;
-
+    private JavaMailSender mailSender;
     /**
      * 配置文件中我的qq邮箱
      */
@@ -41,7 +33,7 @@ public class MailServiceImpl implements IMailService {
     private String from;
 
     static {
-        System.setProperty("mail.mime.splitlongparameters", "false");
+        System.setProperty("mail.mime.splitlongparameters","false");
     }
 
     @Override
@@ -83,9 +75,9 @@ public class MailServiceImpl implements IMailService {
                 //发送
                 mailSender.send(message);
                 //日志信息
-                MailServiceImpl.log.info("发送邮件给：{}。", to);
+                log.info("发送邮件给：{}。", to);
             } catch (Exception e) {
-                MailServiceImpl.log.error("发送邮件给：{} 时发生异常！", to, e);
+                log.error("发送邮件给：{} 时发生异常！", to, e);
             }
         }
     }
@@ -112,9 +104,9 @@ public class MailServiceImpl implements IMailService {
             //发送
             mailSender.send(message);
             //日志信息
-            MailServiceImpl.log.info("发送邮件给：{}。", toStr);
+            log.info("发送邮件给：{}。", toStr);
         } catch (Exception e) {
-            MailServiceImpl.log.error("发送邮件给：{} 时发生异常！", toStr, e);
+            log.error("发送邮件给：{} 时发生异常！", toStr, e);
         }
     }
 
@@ -134,18 +126,18 @@ public class MailServiceImpl implements IMailService {
                 helper.setText(content, true);
 
                 FileSystemResource file = new FileSystemResource(new File(filePath));
-                String fileName = filePath.substring(filePath.lastIndexOf(File.separator) + 1);
-                if (MailServiceImpl.log.isInfoEnabled()) {
-                    MailServiceImpl.log.info("attachmentFilename:[{}]", fileName);
+                String fileName = filePath.substring(filePath.lastIndexOf(File.separator)+1);
+                if(log.isInfoEnabled()){
+                    log.info("attachmentFilename:[{}]", fileName);
                 }
                 helper.addAttachment(fileName, file);
                 mailSender.send(message);
-                if (MailServiceImpl.log.isInfoEnabled()) {
+                if(log.isInfoEnabled()){
                     //日志信息
-                    MailServiceImpl.log.info("邮件已经发送。");
+                    log.info("邮件已经发送。");
                 }
             } catch (MessagingException e) {
-                MailServiceImpl.log.error("subject[{}]filePath[{}]to[{}]发送邮件时发生异常！", subject, filePath, to, e);
+                log.error("subject[{}]filePath[{}]to[{}]发送邮件时发生异常！", subject, filePath, to, e);
             }
         }
     }
@@ -159,7 +151,7 @@ public class MailServiceImpl implements IMailService {
         String[] toArray = toStr.split(",");
         for (String to : toArray) {
             try {
-                MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+                MimeMessageHelper helper = new MimeMessageHelper(message, true,"UTF-8");
                 helper.setFrom(from);
                 helper.setTo(to);
                 helper.setSubject(subject);
@@ -170,11 +162,11 @@ public class MailServiceImpl implements IMailService {
                 mailSender.send(message);
 
             } catch (MessagingException e) {
-                MailServiceImpl.log.error("发送邮件时发生异常！", e);
+                log.error("发送邮件时发生异常！", e);
             }
         }
         //日志信息
-        MailServiceImpl.log.warn("邮件已经发送。toArray:{}", toStr);
+        log.warn("邮件已经发送。toArray:{}",toStr);
     }
 
     @Override
@@ -184,10 +176,10 @@ public class MailServiceImpl implements IMailService {
         }
         MimeMessage message = mailSender.createMimeMessage();
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            MimeMessageHelper helper = new MimeMessageHelper(message, true,"UTF-8");
             helper.setFrom(from);
             helper.setTo(to.split(","));
-            if (StringUtils.isNotEmpty(cc)) {
+            if (StringUtils.isNotEmpty(cc)){
                 helper.setCc(cc.split(","));
             }
             helper.setSubject(subject);
@@ -197,33 +189,9 @@ public class MailServiceImpl implements IMailService {
             helper.addAttachment(attachmentFilename, file);
             mailSender.send(message);
             //日志信息
-            MailServiceImpl.log.info("邮件已经发送。to:{},cc:{}", to, cc);
+            log.info("邮件已经发送。to:{},cc:{}",to,cc);
         } catch (MessagingException e) {
-            MailServiceImpl.log.error("发送邮件时发生异常！", e);
-        }
-    }
-
-    public List<Message> readMailBySubject(String subject) {
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "imap");
-        props.put("mail.imap.ssl.enable", "true");
-        props.put("mail.imap.auth", "true");
-
-        Session session = Session.getInstance(props);
-
-        // 连接邮箱服务器
-        try {
-            Store store = session.getStore();
-            store.connect(mailSender.getHost(), mailSender.getUsername(), mailSender.getPassword());
-            // 打开收件箱
-            Folder inbox = store.getFolder("INBOX");
-            inbox.open(Folder.READ_ONLY);
-
-            SearchTerm term = new SubjectTerm(subject);
-            Message[] messages = inbox.search(term);
-            return Lists.newArrayList(messages);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.error("发送邮件时发生异常！", e);
         }
     }
 }
