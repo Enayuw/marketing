9d2f4b014c59ee7073f5a456393886386edbc9ba
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-07-15 22:37:12 +0800

    同程速率优化: 合并代码优化

--- marketing-utils/src/main/java/com/br/marketing/common/enums/ThreadPoolNameEnum.java
+++ marketing-utils/src/main/java/com/br/marketing/common/enums/ThreadPoolNameEnum.java
@@ -26,9 +26,9 @@ public enum ThreadPoolNameEnum {
     AI_UNIVERSAL_RECEIVE_2(6, MARKETING_AI_UNIVERSAL_RECEIVE_2, "ai推送下游数据队列消费备用2"),
 
     SWITCH_MESSAGE_QUEUE(7,"switch_message_queue", "mq队列动态切换任务"),
-    TCYR_QUICK_DEAL(10,"tcyr_quick_deal","同程易融quick_deal流程"),
-    TCYR_DB_DEAL(11,"tcyr_db_deal","同程易融db_deal流程"),
-    TCYR_CLEAN_CHECK(12,"tcyr_clean_check","同程易融clean_cleck流程"),
+    TCYR_QUICK_DEAL(13,"tcyr_quick_deal","同程易融quick_deal流程"),
+    TCYR_DB_DEAL(14,"tcyr_db_deal","同程易融db_deal流程"),
+    TCYR_CLEAN_CHECK(15,"tcyr_clean_check","同程易融clean_cleck流程"),
 
     ;
 
