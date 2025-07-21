42d966bcc1441a7ac42f24b756b934d11873edd9
juman.wang <juman.wang@brgroup.com>
2025-07-21 21:11:11 +0800

    去掉其他分支字段

--- marketing-cs/src/main/java/com/br/marketing/entity/MarketingSyncUser.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/MarketingSyncUser.java
@@ -143,11 +143,6 @@ public class MarketingSyncUser {
      */
     private Integer isRepeat;
 
-    /**
-     * 数据指纹(内部使用，不对外展示)
-     */
-    private Long fingerprint;
-
     public Long getId() {
         return id;
     }
@@ -370,13 +365,5 @@ public class MarketingSyncUser {
 
     public void setIsRepeat(Integer isRepeat) {
         this.isRepeat = isRepeat;
-    }
-
-    public Long getFingerprint() {
-        return fingerprint;
-    }
-
-    public void setFingerprint(Long fingerprint) {
-        this.fingerprint = fingerprint;
     }
 }
