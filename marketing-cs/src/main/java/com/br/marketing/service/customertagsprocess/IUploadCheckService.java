c1ba40d61a3377c8dcdad18dce7e9b716085e756
juman.wang <juman.wang@brgroup.com>
2025-07-23 21:30:34 +0800

    aes解密原值字段赋值逻辑错误

--- marketing-cs/src/main/java/com/br/marketing/service/customertagsprocess/IUploadCheckService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/customertagsprocess/IUploadCheckService.java
@@ -43,18 +43,13 @@ public interface IUploadCheckService {
                 user.setCell(BrCipherMaker.getInstance().encode(content));
             }
         } else if (StringUtils.isNotBlank(content) && "id".equals(type)) {
-            if (!userValidator.validateId(content)) {
-                user.setId(content);
-            } else {
+            if (userValidator.validateId(content)) {
                 user.setId(BrCipherMaker.getInstance().encode(content));
             }
         } else if (StringUtils.isNotBlank(content) && "name".equals(type)) {
-            if (!userValidator.validateName(content)) {
-                user.setName(content);
-            } else {
+            if (userValidator.validateName(content)) {
                 user.setName(BrCipherMaker.getInstance().encode(content));
             }
-
         }
     }
 
