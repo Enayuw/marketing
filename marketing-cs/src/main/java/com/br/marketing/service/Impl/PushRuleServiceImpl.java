360dfcbd531dceaf71db0e05383ef0d443dcad6f
bingxu.kong <bingxu.kong@brgroup.com>
2025-07-24 14:40:39 +0800

    上传数据log加密-id/name恢复旧逻辑

--- marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/PushRuleServiceImpl.java
@@ -2875,10 +2875,17 @@ public class PushRuleServiceImpl implements PushRuleService {
             return content;
         }
 
-        if (CustomerTagsValue.PushJc3keyTypeEnum.AES_COMMON.getValue().equals(encryptionType)
-                || CustomerTagsValue.PushJc3keyTypeEnum.AES_NMD.getValue().equals(encryptionType)) {
-            String decode = BrCipherMaker.getInstance().decode(content);
-            return StringUtils.isNotBlank(decode) ? DigestUtils.md5DigestAsHex(decode.getBytes()) : content;
+        Boolean isOpenNewEncrypt = marketingCommonConfig.getIsOpenNewEncrypt();
+        if(isOpenNewEncrypt){
+            if (CustomerTagsValue.PushJc3keyTypeEnum.PLAINTEXT.getValue().equals(encryptionType)) {
+                return StringUtils.isNotBlank(content) ? BrCipherMaker.getInstance().decode(content) : content;
+            }
+
+            if (CustomerTagsValue.PushJc3keyTypeEnum.AES_COMMON.getValue().equals(encryptionType)
+                    || CustomerTagsValue.PushJc3keyTypeEnum.AES_NMD.getValue().equals(encryptionType)) {
+                String decode = BrCipherMaker.getInstance().decode(content);
+                return StringUtils.isNotBlank(decode) ? DigestUtils.md5DigestAsHex(decode.getBytes()) : content;
+            }
         }
 
         if (CustomerTagsValue.PushJc3keyTypeEnum.MD5_ALL.getValue().equals(encryptionType)) {
