e406e4cc6c632fe74c2d4ed86398219774e46335
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-08-06 19:54:09 +0800

    携程促活:携程促活MQ切换及消费者迁移到xc-consumer开发,去除历史代码

--- marketing-mq-consumer/src/main/java/com/br/marketing/mq/consumer/rabbitmq/ConsumerApp.java
+++ marketing-mq-consumer/src/main/java/com/br/marketing/mq/consumer/rabbitmq/ConsumerApp.java
@@ -256,21 +256,6 @@ public class ConsumerApp {
                 , new String(message.getBody(), StandardCharsets.UTF_8), null);
     }
 
-//    /**
-//     * 消费 携程促活数据接入消费端
-//     * @param channel 通道
-//     * @param message 消息体
-//     */
-//    @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = MQConstants.MARKETING_XIECHENG_COLLIDING_ACTIVATE_QUEUE, durable = "true")
-//            , exchange = @Exchange(type = "topic", value = MQConstants.MARKETINGEXCHANGER_NAME, durable = "true")
-//            , key = MQConstants.ROUTING_KEY_MARKETING_XIECHENG_COLLIDING_ACTIVATE)}, containerFactory = "concurrentContainerFactory")
-//    public void consumerXieChengActivate(Channel channel, Message message) {
-//        XieChengActivateDTO xieChengActivateDTO = JSON.parseObject(new String(message.getBody(), StandardCharsets.UTF_8),
-//                new TypeReference<XieChengActivateDTO>() {
-//                }.getType());
-//        consumerService.consumerRun(channel, message, robDataCollidingService::activateDataHandle, xieChengActivateDTO, null);
-//    }
-
 
     /**
      * 消费 微聚数据清洗消费端
