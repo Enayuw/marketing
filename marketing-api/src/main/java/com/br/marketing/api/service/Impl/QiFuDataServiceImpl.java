436992443714f355ff2bdd9f4217c320b1a5ed9b
hang.zhou <hang.zhou@brgroup.com>
2025-08-07 11:46:47 +0800

    奇富促完件效果接口（营销）

--- marketing-api/src/main/java/com/br/marketing/api/service/Impl/QiFuDataServiceImpl.java
+++ marketing-api/src/main/java/com/br/marketing/api/service/Impl/QiFuDataServiceImpl.java
@@ -1,6 +1,7 @@
 package com.br.marketing.api.service.Impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.br.common.log.AlertLog;
 import com.br.marketing.api.service.QiFuDataService;
@@ -8,11 +9,12 @@ import com.br.marketing.common.commondto.ApiNoDataResult;
 import com.br.marketing.common.constants.MarketingErrorInfo;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.StringUtils;
+import com.br.marketing.entity.QiFuEffectReportData;
 import com.br.marketing.entity.QifuActuation;
 import com.br.marketing.entity.QifuStrategyReportData;
+import com.br.marketing.mapper.QiFuEffectReportDataMapper;
 import com.br.marketing.mapper.QifuActuationMapper;
 import com.br.marketing.mapper.QifuStrategyReportDataMapper;
-import com.br.marketing.speedconfig.MarketingCommonConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +27,11 @@ import java.util.Date;
 import java.util.List;
 
 import static com.br.marketing.common.constants.MarketingErrorInfo.SUCCESS;
+import com.br.marketing.entity.QiFuEffectReportDataExample;
 
 
 /**
  * This is a Javadoc comment
- * @param <T> the parameter of the class
  */
 @Service
 @Slf4j
@@ -40,6 +42,8 @@ public class QiFuDataServiceImpl implements QiFuDataService {
     private QifuStrategyReportDataMapper qifuStrategyReportDataMapper;
     @Resource
     private QifuActuationMapper qifuActuationMapper;
+    @Resource
+    private QiFuEffectReportDataMapper qifuEffectReportDataMapper;
 
     @Override
     public ApiNoDataResult strategyReportData(String apiCode, String jsonData) {
@@ -98,5 +102,39 @@ public class QiFuDataServiceImpl implements QiFuDataService {
         }
     }
 
+    @Override
+    public ApiNoDataResult effectReport(String apiCode, String jsonData) {
+        QiFuEffectReportData qiFuEffectReportData = JSONObject.parseObject(jsonData, QiFuEffectReportData.class);
+        //必填参数校验
+        List paramsCheckList = Lists.newArrayList(qiFuEffectReportData.getBelongingMonth(), qiFuEffectReportData.getStrategyMonth()
+                , qiFuEffectReportData.getCanvasName(), qiFuEffectReportData.getAgentOperator(), qiFuEffectReportData.getGroupName()
+                , qiFuEffectReportData.getUserCount(), qiFuEffectReportData.getLoginUserCount(), qiFuEffectReportData.getApplySubmitUserCount()
+                , qiFuEffectReportData.getCreditSuccessUserCount());
+        boolean paramNull = paramsCheckList.stream().anyMatch(param -> StringUtils.isEmpty(param));
+        if (paramNull) {
+            return new ApiNoDataResult().setCode(MarketingErrorInfo.PARAM_ISNULL_ERROR.getErrorCode()).
+                    setMessage(MarketingErrorInfo.PARAM_ISNULL_ERROR.getErrorMsg());
+        }
 
+        // 幂等校验：根据updateDate、canvasName、groupName检查是否已存在记录
+        QiFuEffectReportDataExample example = new QiFuEffectReportDataExample();
+        example.createCriteria()
+                .andUpdateDateEqualTo(qiFuEffectReportData.getUpdateDate())
+                .andCanvasNameEqualTo(qiFuEffectReportData.getCanvasName())
+                .andGroupNameEqualTo(qiFuEffectReportData.getGroupName())
+                .andIsDelEqualTo(1);
+        List<QiFuEffectReportData> existingRecords = qifuEffectReportDataMapper.selectByExample(example);
+
+        if (!CollectionUtils.isEmpty(existingRecords)) {
+            log.warn("奇富效果报告数据已存在，跳过重复插入。updateDate:{}, canvasName:{}, groupName:{}",
+                    qiFuEffectReportData.getUpdateDate(), qiFuEffectReportData.getCanvasName(), qiFuEffectReportData.getGroupName());
+            return new ApiNoDataResult().setCode(SUCCESS.getErrorCode()).setMessage(SUCCESS.getErrorMsg());
+        }
+
+        qiFuEffectReportData.setApiCode(apiCode);
+        qiFuEffectReportData.setCreateTime(new Date());
+        qiFuEffectReportData.setUpdateTime(new Date());
+        qifuEffectReportDataMapper.insertSelective(qiFuEffectReportData);
+        return new ApiNoDataResult().setCode(SUCCESS.getErrorCode()).setMessage(SUCCESS.getErrorMsg());
+    }
 }
