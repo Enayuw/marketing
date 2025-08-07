ea32926c6ee4693b07e466bf622ff7a0cfa68934
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-08-07 20:07:46 +0800

    携程促活MQ切换及消费者迁移: 修改注释

--- marketing-utils/src/main/java/com/br/marketing/common/constants/rocketmq/MarketingXieChengConstants.java
+++ marketing-utils/src/main/java/com/br/marketing/common/constants/rocketmq/MarketingXieChengConstants.java
@@ -7,7 +7,7 @@ package com.br.marketing.common.constants.rocketmq;
  */
 public class MarketingXieChengConstants {
 
-    //携程cpa 撞库日志
+    //携程促活 撞库
     public static final String TOPIC_MARKETING_XIECHENG_ACTIVE_COLLIDING_QUEUE = "marketingXieChengActivateCollidingQueue";
 
     public static final String GROUP_MARKETING_XIECHENG_ACTIVE_COLLIDING_QUEUE = "Marketing_XieCheng_Activate_Colliding_Queue";
