0be4cf17bff94eba45c17fd81ea800cb731ad045
hang.zhou <hang.zhou@brgroup.com>
2025-08-07 12:37:56 +0800

    奇富促完件效果接口（营销）

---
+++ marketing-cs/src/main/java/com/br/marketing/entity/QiFuEffectReportData.java
@@ -0,0 +1,230 @@
+package com.br.marketing.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/***
+ * @ClassName QiFuEffectDTO
+ * @Author hang.zhou
+ * @Date 2025/8/6
+ */
+@Data
+public class QiFuEffectReportData {
+
+    private Long id;
+
+    /**
+     * 商户编号
+     */
+    private String apiCode;
+
+    /**
+     * 归属月份 yyyy-MM-dd
+     */
+    private String belongingMonth;
+
+    /**
+     * 策略月份 yyyy-MM-dd
+     */
+    private String strategyMonth;
+
+    /**
+     * 更新日期 yyyy-MM-dd
+     */
+    private String updateDate;
+
+    /**
+     * 画布名称
+     */
+    private String canvasName;
+
+    /**
+     * 代运营供应商code
+     */
+    private String agentOperator;
+
+    /**
+     * 分组
+     */
+    private String groupName;
+
+    /**
+     * 名单量
+     */
+    private Integer userCount;
+
+    /**
+     * 登录用户数
+     */
+    private Integer loginUserCount;
+
+    /**
+     * 完件用户数
+     */
+    private Integer applySubmitUserCount;
+
+    /**
+     * 授信用户数
+     */
+    private Integer creditSuccessUserCount;
+
+    /**
+     * 登录率
+     */
+    private BigDecimal loginRate;
+
+    /**
+     * 完件率
+     */
+    private BigDecimal applySubmitRate;
+
+    /**
+     * 通过率
+     */
+    private BigDecimal passRate;
+
+    /**
+     * 授信率
+     */
+    private BigDecimal creditSuccessRate;
+
+    /**
+     * delta完件率
+     */
+    private BigDecimal deltaApplySubmitRate;
+
+    /**
+     * delta授信率
+     */
+    private BigDecimal deltaCreditSuccessRate;
+
+    /**
+     * delta完件量
+     */
+    private Integer deltaApplySubmitCount;
+
+    /**
+     * delta授信量
+     */
+    private Integer deltaCreditSuccessCount;
+
+    /**
+     * 归因完件用户数
+     */
+    private Integer attrApplyUserCount;
+
+    /**
+     * 归因授信用户数
+     */
+    private Integer attrCreditUserCount;
+
+    /**
+     * 归因授信用户数A
+     */
+    private Integer attrCreditUserCountA;
+
+    /**
+     * 归因授信用户数B
+     */
+    private Integer attrCreditUserCountB;
+
+    /**
+     * 归因授信用户数C
+     */
+    private Integer attrCreditUserCountC;
+
+    /**
+     * 归因完件占比
+     */
+    private BigDecimal attrApplyRatio;
+
+    /**
+     * 归因授信占比
+     */
+    private BigDecimal attrCreditRatio;
+
+    /**
+     * 归因完件率
+     */
+    private BigDecimal attrApplyRate;
+
+    /**
+     * 归因授信率
+     */
+    private BigDecimal attrCreditRate;
+
+    /**
+     * 归因授信用户量A占比
+     */
+    private BigDecimal attrCreditCountRatioA;
+
+    /**
+     * 归因授信用户量B占比
+     */
+    private BigDecimal attrCreditCountRatioB;
+
+    /**
+     * 归因授信用户量C占比
+     */
+    private BigDecimal attrCreditCountRatioC;
+
+    /**
+     * 归因人均授信额度
+     */
+    private BigDecimal attrAvgCreditLimit;
+
+    /**
+     * 归因人头发起率m0
+     */
+    private BigDecimal attrUserPerRate;
+
+    /**
+     * 归因人头动支率m0
+     */
+    private BigDecimal attrUserActRate;
+
+    /**
+     * 归因人头发起通过率
+     */
+    private BigDecimal attrUserPerAprlRate;
+
+    /**
+     * 归因金额发起率m0
+     */
+    private BigDecimal attrAmtPerRate;
+
+    /**
+     * 归因金额动支率m0
+     */
+    private BigDecimal attrAmtActRate;
+
+    /**
+     * 归因金额发起通过率m0
+     */
+    private BigDecimal attrAmtPerAprlRate;
+
+    /**
+     * 归因授信户均动支金额m0
+     */
+    private BigDecimal attrCreditAvgActAmt;
+
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 1-有效；9-无效
+     */
+    private Integer isDel;
+
+
+}
