aaa8b91175275c5537ad507c6da63b3a982c589f
xiong.luo <xiong.luo@brgroup.com>
2025-09-04 10:56:24 +0800

    哈啰硅基人回调

--- marketing-check/src/main/java/com/br/marketing/check/job/halo/HaloCallbackJob.java
+++ marketing-check/src/main/java/com/br/marketing/check/job/halo/HaloCallbackJob.java
@@ -6,6 +6,7 @@ import com.br.marketing.service.Impl.halo.IHaloCallbackService;
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -31,7 +32,10 @@ public class HaloCallbackJob extends AbstractSimpleElasticJob {
 
         JSONObject param = JSON.parseObject(context.getJobParameter());
         String batchNumber = param.getString("batchNumber");
-        String whereSql = Optional.ofNullable(param.getString("whereSql")).orElse("");
+        String whereSql = Optional.ofNullable(param.getString("whereSql")).orElse(" and status = 0 ");
+        if(StringUtils.isNotBlank(batchNumber) && StringUtils.isBlank(param.getString("whereSql"))) {
+            whereSql = " and status = 2 ";
+        }
         haloCallbackService.pushDataCallback(batchNumber, whereSql);
         long end = System.currentTimeMillis();
         log.warn("哈啰硅基人数据回传调度结束, 耗时:{}", end - start);
--- marketing-cs/src/main/java/com/br/marketing/service/Impl/halo/HaloCallbackServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/Impl/halo/HaloCallbackServiceImpl.java
@@ -89,9 +89,8 @@ public class HaloCallbackServiceImpl implements IHaloCallbackService {
                 log.warn("任务中断触发，停止硅基人回调读取");
                 break;
             }
-            String retrySql = retry ? " and status = 2 " : " and status = 0 ";
             String scoreSql = "select id, cell, section, cus_num custNum from b_marketing_score_" + batchNumber +
-                    " where 1 = 1 " + whereSql + retrySql + " and id > " + lastId + " order by id asc limit " + pageSize;
+                    " where 1 = 1 " + whereSql + " and id > " + lastId + " order by id asc limit " + pageSize;
             List<Map<String, Object>> results = reportStatisticsScoreMapper.queryDataMapNumbI_(scoreSql);
 
             if (CollectionUtils.isEmpty(results)) {
