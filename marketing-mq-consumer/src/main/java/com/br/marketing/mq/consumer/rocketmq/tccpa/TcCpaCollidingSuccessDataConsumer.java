f97707c335cecea318e1ff2add0a9b5a23af7142
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-09-09 16:19:28 +0800

    同程CPA撞库自动化一期fix: mq-consumer压测-消息对象转换

--- marketing-mq-consumer/src/main/java/com/br/marketing/mq/consumer/rocketmq/tccpa/TcCpaCollidingSuccessDataConsumer.java
+++ marketing-mq-consumer/src/main/java/com/br/marketing/mq/consumer/rocketmq/tccpa/TcCpaCollidingSuccessDataConsumer.java
@@ -56,7 +56,13 @@ public class TcCpaCollidingSuccessDataConsumer extends BaseMqMessageListener imp
 //            long start = System.currentTimeMillis();
 //            Integer priority = marketingCommonConfig.getTcyrCpaCollidingDealShardConfig().getInteger("priority");
 //            String bodyString = new String(messageExt.getBody(), StandardCharsets.UTF_8);
-            log.warn("TITLE:{}MQ消费,msgId:{}",TITLE,messageExt.getMsgId());
+            //log.warn("TITLE:{}MQ消费,msgId:{}",TITLE,messageExt.getMsgId());
+            long start = System.currentTimeMillis();
+            String bodyString = new String(messageExt.getBody(), StandardCharsets.UTF_8);
+            TcyrCpaSuccessMqDTO tcCpaSuccessMqDTO = JSON.parseObject(bodyString,
+                        new TypeReference<TcyrCpaSuccessMqDTO>() {}.getType());
+            log.warn("TITLE:{}MQ消费耗时:{},msgId:{}, requestId:{}, dataId:{}",
+                    TITLE,System.currentTimeMillis()-start,messageExt.getMsgId(),tcCpaSuccessMqDTO.getRequestId(),tcCpaSuccessMqDTO.getDataId());
 //            if (priority > 1) {
 //                TcyrCpaSuccessMqDTO tcCpaSuccessMqDTO = JSON.parseObject(bodyString,
 //                        new TypeReference<TcyrCpaSuccessMqDTO>() {}.getType());
