5d947eff2c5d8fb9fa5385ac859ecdbeb011552c
guangxiu.li <guangxiu.li@brgroup.com>
2025-10-11 20:20:41 +0800

    解决groovy

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/DataBridgeApplication.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/DataBridgeApplication.java
@@ -10,13 +10,14 @@ import io.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(exclude = {MultipartAutoConfiguration.class, SpringBootConfiguration.class}, scanBasePackages = {"com.br.marketing"})
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class, SpringBootConfiguration.class, GroovyTemplateAutoConfiguration.class}, scanBasePackages = {"com.br.marketing"})
 @EnableAspectJAutoProxy
 @MapperScan("com.br.marketing.mapper")
 @ImportResource(locations = {"classpath:scheduler.xml"})
