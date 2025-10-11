c4822f19bbc5fb2e528055a30d3c0ab701fc8856
guangxiu.li <guangxiu.li@brgroup.com>
2025-10-11 14:38:24 +0800

    brokerLayerFutureStub.sendMsg(sendrequest)包导入更改

--- marketing-cs/src/main/java/com/br/marketing/rpcclient/rpcclientImpl/BrokerGrpcClient.java
+++ marketing-cs/src/main/java/com/br/marketing/rpcclient/rpcclientImpl/BrokerGrpcClient.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import shaded.com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
