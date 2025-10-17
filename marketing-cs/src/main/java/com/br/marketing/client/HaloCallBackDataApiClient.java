c8ffd0af585e3cac85d909320e69c4c7246447d0
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-10-17 19:25:54 +0800

    哈啰-三方营销数据回传: 修改哈啰三方接口调用为http代理

--- marketing-cs/src/main/java/com/br/marketing/client/HaloCallBackDataApiClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/HaloCallBackDataApiClient.java
@@ -1,28 +1,19 @@
 package com.br.marketing.client;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.br.cloud.counter.BrCounter;
-import com.br.cloud.web.MethodType;
-import com.br.cloud.web.PrometheusTimeMethod;
 import com.br.common.log.AlertLog;
-import com.br.marketing.client.net.ApiCallerUtil;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
-import com.br.marketing.common.utils.net.ThirdApiResultTransfer;
-import com.br.marketing.mapper.datasource.log.InterfaceLogMapper;
-import com.br.marketing.monitor.PrometheusMonitorUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.HashMap;
 
 /**发送邮件客户端
  * @author 10400
@@ -34,36 +25,28 @@ public class HaloCallBackDataApiClient {
     @Value("${api.halo.openUrl:00}")
     private String haloCallBackUrl;
 
-    @Autowired
-    RestTemplate restTemplate;
-
     @Resource
-    InterfaceLogMapper interfaceLogMapper;
-
-    @Qualifier("interfaceLogDbpool")
-    @Autowired
-    ThreadPoolExecutor interfaceLogDbpool;
-
+    private HttpProxyClient httpProxyClient;
 
     public Result dealMarketingCallBack(String apiCode,JSONObject requestJson) {
-       Result result = new Result();
+        Result result = new Result();
         try {
-            ThirdApiResultTransfer transfer = new ApiCallerUtil(restTemplate,interfaceLogMapper,interfaceLogDbpool)
-                    .setUrl(haloCallBackUrl)
-                    .setContentType(MediaType.APPLICATION_JSON).setEncode(true)
-                    .setRequestParam(requestJson).postTransferStr();
-            JSONObject jsonObject = JSON.parseObject(transfer.getResult());
+            Header[] headers = new Header[] {
+                    new BasicHeader("Content-Type", "application/json"),
+            };
+            HashMap<String, String> resultMap = httpProxyClient.sendByCodeWithLogWithHeader(requestJson,haloCallBackUrl,true,
+                    MediaType.APPLICATION_JSON_UTF8_VALUE,"",true,false,headers);
             log.warn("dealMarketingCallBack,apiCode:{},requestParam:{},result:{}",apiCode,
-                    requestJson.toJSONString(),JSONObject.toJSONString(transfer));
-            if (transfer.getHttpCode() != 200) {
-                result.setCode(ResultCode.INTERNAL_SERVER_ERROR.getValue());
-                result.setMessage(JSONObject.toJSONString(transfer));
-                return result;
+                    requestJson.toJSONString(),JSONObject.toJSONString(resultMap));
+            String resultContentStr = resultMap.get("content");
+            if (!resultMap.get("httpcode").equals("200")) {
+                return result.setCode(ResultCode.FAIL.getValue()).setMessage(resultContentStr);
             }
-            if ("10000".equals(jsonObject.getString("code"))) {
-                result.setCode(ResultCode.SUCCESS.getValue());
-            } else {
-                result.setCode(ResultCode.FAIL.getValue()).setMessage(transfer.getResult());
+            JSONObject resultData = JSONObject.parseObject(resultContentStr);
+            boolean isSuccess = "10000".equals(resultData.getString("code"));
+            result.setCode(isSuccess ? ResultCode.SUCCESS.getValue() : ResultCode.FAIL.getValue());
+            if (!isSuccess) {
+                result.setMessage(resultContentStr);
             }
         } catch (Exception ex) {
             log.warn(AlertLog.buildErrorMessage(AlarmSendCodeEnum.HALUO_CALLBACK_DATA_INTERFACEERROR.getCode(), ex.getMessage()), ex);
