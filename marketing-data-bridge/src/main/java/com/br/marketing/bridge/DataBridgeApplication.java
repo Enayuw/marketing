c6bd3403a585d61012864c6fe1c16a1203898f92
guangxiu.li <guangxiu.li@brgroup.com>
2025-11-03 10:09:03 +0800

    marketing-data-bridge配置更改

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/DataBridgeApplication.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/DataBridgeApplication.java
@@ -2,6 +2,7 @@ package com.br.marketing.bridge;
 
 import com.br.cloud.boot.EnablePrometheusEndpoint;
 import com.br.cloud.counter.EnableBrCounter;
+import com.br.cloud.datasource.EnableDataSourcePrometheus;
 import com.br.cloud.jvm.EnablePrometheusJvm;
 import com.br.cloud.web.EnablePrometheusTiming;
 import com.br.grpc.utils.BrGrpcUtils;
@@ -23,6 +24,7 @@ import org.springframework.context.annotation.ImportResource;
 @EnablePrometheusEndpoint
 @EnablePrometheusJvm
 @EnablePrometheusTiming
+@EnableDataSourcePrometheus
 @EnableBrCounter(namespace = "marketing_data_bridge")
 @Slf4j
 public class DataBridgeApplication {
