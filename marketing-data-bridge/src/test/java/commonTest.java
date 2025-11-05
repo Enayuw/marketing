d9d6a01c9f96768cb1f8e4a4d1dadb1c6f4321ec
dongshuo.he <dongshuo.he@brgroup.com>
2025-11-05 21:58:03 +0800

    同程失败数据处理 日期转化处理

--- marketing-data-bridge/src/test/java/commonTest.java
+++ marketing-data-bridge/src/test/java/commonTest.java
@@ -1,4 +1,5 @@
 import com.br.marketing.bridge.DataBridgeApplication;
+import com.br.marketing.common.utils.DateHelper;
 import com.br.marketing.mapper.SyncConfigMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -12,7 +13,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -33,6 +36,18 @@ public class commonTest implements ApplicationContextAware {
     @Test
     public void test(){
         log.info("123456, 上山打老虎");
+    }
+
+    @Test
+    public void test0011() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("2026-01-27 23:59:59");
+        list.add("2099-12-31");
+        list.add("25-11-05");
+        for (String s : list) {
+            Date date = DateHelper.stringToDate(s);
+            System.out.println(date);
+        }
     }
 
 }
