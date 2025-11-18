b12d49ee036400c134c1ed8d41c758b196ed970e
guangxiu.li <guangxiu.li@brgroup.com>
2025-11-18 11:12:58 +0800

    奇富非实时数据查询

---
+++ marketing-check/src/main/java/com/br/marketing/check/service/qifu/QiFuQueryCallService.java
@@ -0,0 +1,12 @@
+package com.br.marketing.check.service.qifu;
+
+/**
+ * 奇富查询外呼信息Service
+ */
+public interface QiFuQueryCallService {
+    /**
+     * 查询外呼信息
+     */
+    void queryCallMessage();
+}
+
