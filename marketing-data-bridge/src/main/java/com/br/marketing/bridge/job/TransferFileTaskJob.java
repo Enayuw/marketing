83cc30f2d40897b7d17005b9825caa545110e723
zhen.li1 <zhen.li1@brgroup.com>
2025-11-18 14:28:36 +0800

    fix:提取修复

--- marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
+++ marketing-data-bridge/src/main/java/com/br/marketing/bridge/job/TransferFileTaskJob.java
@@ -305,8 +305,7 @@ public class TransferFileTaskJob extends AbstractSimpleElasticJob {
                                 String localPath = datum.getFilePath();
                                 sftpUploadHandlerService.insertSftpUploadTask(datum.getApiCode(),localPath,datum.getFileName(),
                                         DataTypeEnum.TRANSFER.getValue(),
-                                        "update b_transfer_file_task set status = 4 where api_code ='"+datum.getApiCode()
-                                                +"' and file_name = '"+datum.getFileName()+"'");
+                                        "update b_transfer_file_task set status = 4 where id ="+datum.getId());
                                     //第一次执行，查询为空，不会进行删除，直接返回
                                     //第二次执行，删除b_sync_log的记录
                                     /*String fileChildDir = "";
