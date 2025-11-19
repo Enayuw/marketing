80ec5aab7900f4c662a7a790274c33b078d68348
xiong.luo <xiong.luo@brgroup.com>
2025-11-19 20:47:04 +0800

    同程CPA跑分文件定制化数据包页面

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCustomizeController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCustomizeController.java
@@ -1,39 +1,14 @@
 package com.br.marketing.innerapi.controller;
 
-import com.br.marketing.aspect.AuthDataControllerPermission;
-import com.br.marketing.aspect.LogAnnotation;
-import com.br.marketing.aspect.LogRecordAnnotation;
 import com.br.marketing.common.commondto.ApiResult;
-import com.br.marketing.common.commondto.Result;
-import com.br.marketing.commonentity.PageResultReturn;
-import com.br.marketing.context.ThreadContextInfo;
-import com.br.marketing.dto.*;
 import com.br.marketing.dto.tccpa.TcCpDataCleanTaskDTO;
 import com.br.marketing.dto.tccpa.TcCpDataPackageGenDTO;
-import com.br.marketing.entity.TcyrCpaCollidingDataCleanTask;
-import com.br.marketing.enums.InterfaceOperationsEnum;
-import com.br.marketing.innerapi.service.RuleCenterCollidingService;
-import com.br.marketing.mysqlInterceptor.AddDataAuthBusiness;
-import com.br.marketing.service.Impl.RuleCenterServiceImpl;
-import com.br.marketing.service.PushRuleService;
-import com.br.marketing.service.ReportScoreRuleService;
-import com.br.marketing.service.datagroup.rulecenter.RuleCenterLabelService;
-import com.br.marketing.service.halo.HaloRuleCenterCallbackService;
 import com.br.marketing.service.tccpa.TcCpaDataPackageService;
-import com.br.marketing.vo.*;
-import com.br.marketing.vo.xiecheng.PushViewVO;
-import com.br.marketing.vo.xiecheng.XiechengCollidingDataVO;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
 /**
@@ -41,10 +16,10 @@ import java.util.Set;
  */
 @RestController
 @RequestMapping("/tcCpa")
-@Tag(value = "TcCpaCustomizeController")
+@Api(value = "TcCpaCustomizeController")
 public class TcCpaCustomizeController {
 
-    private static final Integer CODE_1 = Integer.valueOf(1);
+    private static final Integer CODE_1 = 1;
 
     @Resource
     private TcCpaDataPackageService tcCpaDataPackageService;
@@ -54,7 +29,7 @@ public class TcCpaCustomizeController {
      * @param dto
      * @return
      */
-    @Operation(value = "同程CPA跑分文件数据包删除", notes = "同程CPA跑分文件数据包删除", httpMethod = "POST")
+    @ApiOperation(value = "同程CPA跑分文件数据包删除", notes = "同程CPA跑分文件数据包删除", httpMethod = "POST")
     @PostMapping("/delete")
     public ApiResult delete(@RequestBody TcCpDataPackageGenDTO dto) {
         return new ApiResult().fromResult(tcCpaDataPackageService.delete(dto), CODE_1);
@@ -66,7 +41,7 @@ public class TcCpaCustomizeController {
      * @param status 状态 0:禁用 1:启用
      * @return
      */
-    @Operation(value = "同程CPA跑分文件数据包启用禁用", notes = "同程CPA跑分文件数据包启用禁用", httpMethod = "POST")
+    @ApiOperation(value = "同程CPA跑分文件数据包启用禁用", notes = "同程CPA跑分文件数据包启用禁用", httpMethod = "POST")
     @GetMapping("/enable")
     public ApiResult delete(@RequestParam("packageName") String packageName, @RequestParam("status") Integer status) {
         return new ApiResult().fromResult(tcCpaDataPackageService.enable(packageName, status), CODE_1);
@@ -77,7 +52,7 @@ public class TcCpaCustomizeController {
      * @param dto
      * @return
      */
-    @Operation(value = "同程CPA跑分文件清洗分层任务生成", notes = "同程CPA跑分文件清洗分层任务生成", httpMethod = "POST")
+    @ApiOperation(value = "同程CPA跑分文件清洗分层任务生成", notes = "同程CPA跑分文件清洗分层任务生成", httpMethod = "POST")
     @PostMapping("/genCleanTask")
     public ApiResult genCleanTask(@RequestBody TcCpDataCleanTaskDTO dto) {
         return new ApiResult().fromResult(tcCpaDataPackageService.genCleanTask(dto), CODE_1);
