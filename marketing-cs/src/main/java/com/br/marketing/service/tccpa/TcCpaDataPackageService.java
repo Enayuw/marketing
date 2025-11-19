b124fd842f897a1f95c2ec2f113e0dfd29ac8ddf
xiong.luo <xiong.luo@brgroup.com>
2025-11-19 17:26:54 +0800

    同程CPA跑分文件定制化数据包页面

--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/TcCpaDataPackageService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/TcCpaDataPackageService.java
@@ -1,7 +1,9 @@
 package com.br.marketing.service.tccpa;
 
 import com.br.marketing.common.commondto.Result;
+import com.br.marketing.dto.tccpa.TcCpDataCleanTaskDTO;
 import com.br.marketing.dto.tccpa.TcCpDataPackageGenDTO;
+import com.br.marketing.entity.TcyrCpaCollidingDataCleanTask;
 
 public interface TcCpaDataPackageService {
 
@@ -11,4 +13,26 @@ public interface TcCpaDataPackageService {
      * @return
      */
     Result tcDataPackageGen(TcCpDataPackageGenDTO dto);
+
+    /**
+     * 同程数据包删除
+     * @param dto
+     * @return
+     */
+    Result delete(TcCpDataPackageGenDTO dto);
+
+    /**
+     * 同程数据包 启用禁用
+     * @param packageName 数据包名称
+     * @param status 状态：1启用 0禁用
+     * @return
+     */
+    Result enable(String packageName, Integer status);
+
+    /**
+     * 同程数据包清洗任务新增
+     * @param task
+     * @return
+     */
+    Result genCleanTask(TcCpDataCleanTaskDTO task);
 }
--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaDataPackageServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaDataPackageServiceImpl.java
@@ -5,14 +5,21 @@ import com.alibaba.fastjson.JSONObject;
 import com.br.marketing.common.commondto.Result;
 import com.br.marketing.common.commondto.ResultCode;
 import com.br.marketing.common.utils.Constants;
+import com.br.marketing.dto.tccpa.TcCpDataCleanTaskDTO;
 import com.br.marketing.dto.tccpa.TcCpDataPackageGenDTO;
+import com.br.marketing.entity.TcyrCpaCollidingDataCleanTask;
+import com.br.marketing.entity.TcyrCpaCollidingDataCleanTaskExample;
 import com.br.marketing.entity.TcyrCpaCollidingDataPackage;
 import com.br.marketing.entity.TcyrCpaCollidingDataPackageExample;
+import com.br.marketing.enums.clean.DataCleanStatusEnum;
+import com.br.marketing.mapper.TcyrCpaCollidingDataCleanTaskMapper;
 import com.br.marketing.mapper.TcyrCpaCollidingDataPackageMapper;
 import com.br.marketing.service.tccpa.TcCpaDataPackageService;
 import com.br.marketing.util.EsConditionTransferSqlUtil;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.Resource;
 
 @Service
@@ -21,6 +28,9 @@ public class TcCpaDataPackageServiceImpl implements TcCpaDataPackageService {
 
     @Resource
     TcyrCpaCollidingDataPackageMapper tcyrCpaCollidingDataPackageMapper;
+
+    @Resource
+    private TcyrCpaCollidingDataCleanTaskMapper tcyrCpaCollidingDataCleanTaskMapper;
 
     @Override
     public Result tcDataPackageGen(TcCpDataPackageGenDTO dto) {
@@ -40,6 +50,48 @@ public class TcCpaDataPackageServiceImpl implements TcCpaDataPackageService {
         dataPackage.setBatchNumbers(String.join(",", dto.getBatchNumberList()));
         dataPackage.setConditions(EsConditionTransferSqlUtil.jsonTransferSql(conditionJson, ""));
         tcyrCpaCollidingDataPackageMapper.insertSelective(dataPackage);
+        return new Result().setCode(ResultCode.SUCCESS.getValue());
+    }
+
+    @Override
+    public Result delete(TcCpDataPackageGenDTO dto) {
+        TcyrCpaCollidingDataCleanTaskExample taskExample = new TcyrCpaCollidingDataCleanTaskExample();
+        taskExample.createCriteria().andCleanStatusIn(Lists.newArrayList(DataCleanStatusEnum.READY.getCode(),
+                DataCleanStatusEnum.RUNNING.getCode()));
+        if (tcyrCpaCollidingDataCleanTaskMapper.countByExample(taskExample) > 0) {
+            return new Result().setCode(ResultCode.FAIL.getValue()).setMessage("存在清洗中或待清洗的任务，禁止删除数据包");
+        }
+        TcyrCpaCollidingDataPackageExample dataPackageExample = new TcyrCpaCollidingDataPackageExample();
+        dataPackageExample.createCriteria().andPackageNameEqualTo(dto.getPackageName());
+        TcyrCpaCollidingDataPackage dataPackage = new TcyrCpaCollidingDataPackage();
+        dataPackage.setIsDel(Constants.STATUS_DELETE);
+        tcyrCpaCollidingDataPackageMapper.updateByExampleSelective(dataPackage, dataPackageExample);
+        return new Result().setCode(ResultCode.SUCCESS.getValue());
+    }
+
+    @Override
+    public Result enable(String packageName, Integer status) {
+        TcyrCpaCollidingDataPackageExample dataPackageExample = new TcyrCpaCollidingDataPackageExample();
+        dataPackageExample.createCriteria().andPackageNameEqualTo(packageName);
+
+        TcyrCpaCollidingDataPackage dataPackage = new TcyrCpaCollidingDataPackage();
+        dataPackage.setEnabled(status);
+        tcyrCpaCollidingDataPackageMapper.updateByExampleSelective(dataPackage, dataPackageExample);
+        return new Result().setCode(ResultCode.SUCCESS.getValue());
+    }
+
+    @Override
+    public Result genCleanTask(TcCpDataCleanTaskDTO dto) {
+        TcyrCpaCollidingDataCleanTaskExample taskExample = new TcyrCpaCollidingDataCleanTaskExample();
+        taskExample.createCriteria().andCleanStatusIn(Lists.newArrayList(DataCleanStatusEnum.READY.getCode(),
+                DataCleanStatusEnum.RUNNING.getCode()));
+        if (tcyrCpaCollidingDataCleanTaskMapper.countByExample(taskExample) > 0) {
+            return new Result().setCode(ResultCode.FAIL.getValue()).setMessage("存在清洗中或待清洗的任务，禁止新增清洗任务");
+        }
+        TcyrCpaCollidingDataCleanTask cleanTask = new TcyrCpaCollidingDataCleanTask();
+        cleanTask.setPackageIds(String.join(",", dto.getPackageList()));
+        cleanTask.setCleanStatus(DataCleanStatusEnum.READY.getCode());
+        tcyrCpaCollidingDataCleanTaskMapper.insertSelective(cleanTask);
         return new Result().setCode(ResultCode.SUCCESS.getValue());
     }
 }
