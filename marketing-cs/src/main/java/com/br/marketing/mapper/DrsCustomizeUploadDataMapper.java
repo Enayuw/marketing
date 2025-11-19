7937ef20c9e0ee373e95bcc93b493a3092d9bf07
hang.zhou <hang.zhou@bggroup.com>
2025-11-19 10:03:03 +0800

    fix

--- marketing-cs/src/main/java/com/br/marketing/mapper/DrsCustomizeUploadDataMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/DrsCustomizeUploadDataMapper.java
@@ -34,6 +34,17 @@ public interface DrsCustomizeUploadDataMapper {
     List<DrsCustomizeUploadData> selectByApiCodeAndDate(@Param("tCid") String tCid, @Param("apiCode") String apiCode, @Param("createDate") String createDate);
 
     /**
+     * 查询数据的最大最小id
+     *
+     * @param dateType  日期类型：history表示<=，today表示=
+     * @param dateValue 日期值
+     */
+    Map<String, Long> getDataIdRange(@Param("tCid") String tCid
+            , @Param("apiCodes") List<String> apiCodes
+            , @Param("dateType") String dateType
+            , @Param("dateValue") String dateValue);
+
+    /**
      * 根据id范围查询数据（用于分批处理）
      *
      * @param pageSize  分页大小
