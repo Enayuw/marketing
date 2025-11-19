eb935a280d81a79b8d20771c3e9139981118a363
xiong.luo <xiong.luo@brgroup.com>
2025-11-19 21:18:37 +0800

    同程CPA跑分文件定制化数据包页面 jdk17部分注解修改

--- marketing-cs/src/main/java/com/br/marketing/dto/tccpa/TcCpDataPackageGenDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/tccpa/TcCpDataPackageGenDTO.java
@@ -1,7 +1,7 @@
 package com.br.marketing.dto.tccpa;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -10,16 +10,16 @@ import java.util.List;
 @Data
 public class TcCpDataPackageGenDTO {
 
-    @ApiModelProperty(value = "跑分文件批次号")
+    @Schema(description = "跑分文件批次号")
     @NotEmpty(message = "跑分文件批次号不能为空")
     private List<String> batchNumberList;
 
-    @ApiModelProperty(value = "查询规则")
+    @Schema(description = "查询规则")
     @NotNull(message = "查询规则不能为空")
     @JsonProperty("mRuleCondition")
     private String mRuleCondition;
 
-    @ApiModelProperty(value = "数据包名称")
+    @Schema(description = "数据包名称")
     @NotNull(message = "数据包名称不能为空")
     private String packageName;
 
--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCustomizeController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCustomizeController.java
@@ -4,8 +4,8 @@ import com.br.marketing.common.commondto.ApiResult;
 import com.br.marketing.dto.tccpa.TcCpDataCleanTaskDTO;
 import com.br.marketing.dto.tccpa.TcCpDataPackageGenDTO;
 import com.br.marketing.service.tccpa.TcCpaDataPackageService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,7 +16,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/tcCpa")
-@Api(value = "TcCpaCustomizeController")
+@Tag(value = "TcCpaCustomizeController")
 public class TcCpaCustomizeController {
 
     private static final Integer CODE_1 = 1;
@@ -29,7 +29,7 @@ public class TcCpaCustomizeController {
      * @param dto
      * @return
      */
-    @ApiOperation(value = "同程CPA跑分文件数据包删除", notes = "同程CPA跑分文件数据包删除", httpMethod = "POST")
+    @Operation(value = "同程CPA跑分文件数据包删除", notes = "同程CPA跑分文件数据包删除", httpMethod = "POST")
     @PostMapping("/delete")
     public ApiResult delete(@RequestBody TcCpDataPackageGenDTO dto) {
         return new ApiResult().fromResult(tcCpaDataPackageService.delete(dto), CODE_1);
@@ -41,7 +41,7 @@ public class TcCpaCustomizeController {
      * @param status 状态 0:禁用 1:启用
      * @return
      */
-    @ApiOperation(value = "同程CPA跑分文件数据包启用禁用", notes = "同程CPA跑分文件数据包启用禁用", httpMethod = "POST")
+    @Operation(value = "同程CPA跑分文件数据包启用禁用", notes = "同程CPA跑分文件数据包启用禁用", httpMethod = "POST")
     @GetMapping("/enable")
     public ApiResult delete(@RequestParam("packageName") String packageName, @RequestParam("status") Integer status) {
         return new ApiResult().fromResult(tcCpaDataPackageService.enable(packageName, status), CODE_1);
@@ -52,7 +52,7 @@ public class TcCpaCustomizeController {
      * @param dto
      * @return
      */
-    @ApiOperation(value = "同程CPA跑分文件清洗分层任务生成", notes = "同程CPA跑分文件清洗分层任务生成", httpMethod = "POST")
+    @Operation(value = "同程CPA跑分文件清洗分层任务生成", notes = "同程CPA跑分文件清洗分层任务生成", httpMethod = "POST")
     @PostMapping("/genCleanTask")
     public ApiResult genCleanTask(@RequestBody TcCpDataCleanTaskDTO dto) {
         return new ApiResult().fromResult(tcCpaDataPackageService.genCleanTask(dto), CODE_1);
