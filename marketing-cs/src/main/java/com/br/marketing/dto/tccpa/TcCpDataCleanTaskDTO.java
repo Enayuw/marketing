79244378cb17aea40ae45ea931e2b2da81234f45
xiong.luo <xiong.luo@brgroup.com>
2025-11-20 10:28:18 +0800

    同程CPA跑分文件定制化数据包页面

--- marketing-cs/src/main/java/com/br/marketing/dto/tccpa/TcCpDataCleanTaskDTO.java
+++ marketing-cs/src/main/java/com/br/marketing/dto/tccpa/TcCpDataCleanTaskDTO.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
