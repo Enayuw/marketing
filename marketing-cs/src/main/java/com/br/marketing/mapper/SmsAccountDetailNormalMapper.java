668e95f0509f8dd7ff6b5a6a68b12983f8df41ad
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-11-28 19:13:28 +0800

    短信三方字段同步:bridge LineSmsCostToDbJob->detail mapper相关修改

--- marketing-cs/src/main/java/com/br/marketing/mapper/SmsAccountDetailNormalMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/SmsAccountDetailNormalMapper.java
@@ -1,5 +1,8 @@
 package com.br.marketing.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface SmsAccountDetailNormalMapper extends SmsAccountDetailNormalMapperBase{
 
+    Long selectCount(@Param("vendorId") Long vendorId, @Param("channelId") Long channelId);
 }
--- marketing-cs/src/main/java/com/br/marketing/service/LineSmsAccountNormalService.java
+++ marketing-cs/src/main/java/com/br/marketing/service/LineSmsAccountNormalService.java
@@ -5,6 +5,7 @@ import com.br.marketing.common.commondto.Result;
 import com.br.marketing.commonentity.PageResultReturn;
 import com.br.marketing.dto.LineBaseShowInfoDto;
 import com.br.marketing.dto.account.LineAccountDto;
+import com.br.marketing.dto.account.SmsAccountDto;
 import com.br.marketing.vo.LineAccountDetailVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -30,4 +31,6 @@ public interface LineSmsAccountNormalService {
     Result allowLineAccount(Long groupId);
 
     Result deleteLineAccount(Long groupId);
+
+    Result addSmsAccount(SmsAccountDto smsAccountDto);
 }
