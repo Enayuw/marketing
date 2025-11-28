3933da7b82bb95f305f1bfee114078d5cb5d0a1e
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-11-28 18:53:42 +0800

    短信三方字段同步:bridge SmsBaseInfoSyncJob相关mapper类

--- marketing-cs/src/main/java/com/br/marketing/mapper/SmsVendorInfoNormalMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/SmsVendorInfoNormalMapper.java
@@ -1,5 +1,11 @@
 package com.br.marketing.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface SmsVendorInfoNormalMapper  extends SmsVendorInfoNormalMapperBase{
 
+    Long selectCount(@Param("vendorId") Long vendorId, @Param("vendorName") String vendorName);
+
+    void updateInfoById(@Param("vendorPrimaryId") Long vendorPrimaryId, @Param("vendorName") String vendorName,
+                        @Param("opeStatus") Integer opeStatus);
 }
