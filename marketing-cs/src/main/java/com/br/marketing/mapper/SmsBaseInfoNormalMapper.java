1023c0b9a1e2ace2eeaf930b15c72c07f6d878b0
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2025-11-28 18:53:42 +0800

    短信三方字段同步:bridge SmsBaseInfoSyncJob相关mapper类

--- marketing-cs/src/main/java/com/br/marketing/mapper/SmsBaseInfoNormalMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/SmsBaseInfoNormalMapper.java
@@ -1,5 +1,17 @@
 package com.br.marketing.mapper;
 
+import com.br.marketing.dto.SmsBaseFullInfoDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface SmsBaseInfoNormalMapper extends SmsBaseInfoNormalMapperBase{
 
+    List<SmsBaseFullInfoDTO> selectSmsBaeUseInfoList();
+
+    void updateOnlyDbOpStatus(@Param("onlyInDbIdList") List<Long> onlyInDbIdList,
+                              @Param("opeStatus") Integer opStatus);
+
+    void updateBaseInfoById(@Param("id") Long id, @Param("channelName") String channelName,
+                            @Param("vendorId") Long vendorId, @Param("opeStatus") Integer opeStatus);
 }
