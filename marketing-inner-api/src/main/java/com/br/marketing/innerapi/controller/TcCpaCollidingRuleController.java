98e4cb56676c891b10d4d1d248feb42c5ce36283
dongshuo.he <dongshuo.he@brgroup.com>
2025-12-03 13:06:48 +0800

    同程自动化二期 撞库任务新增页面 jdk17修改controller的注解

--- marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCollidingRuleController.java
+++ marketing-inner-api/src/main/java/com/br/marketing/innerapi/controller/TcCpaCollidingRuleController.java
@@ -6,6 +6,8 @@ import com.br.marketing.dto.tc.TcCpaMagnitudeDistDTO;
 import com.br.marketing.service.tccpa.TcCpaCollidingRuleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,7 +18,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/tcCpa/customize/collidingRule")
-@Api(value = "TcCpaCollidingRuleController")
+@Tag(name = "同程CPA撞库规则定制页面", description = "同程CPA撞库规则定制页面")
 public class TcCpaCollidingRuleController {
 
     private static final Integer CODE_1 = 1;
@@ -24,13 +26,13 @@ public class TcCpaCollidingRuleController {
     @Resource
     private TcCpaCollidingRuleService tcCpaCollidingRuleService;
 
-    @ApiOperation(value = "同程撞库规则基础信息", notes = "同程撞库规则基础信息", httpMethod = "GET")
+    @Operation(summary = "同程撞库规则基础信息", description = "同程撞库规则基础信息")
     @GetMapping("/info")
     public ApiResult info() {
         return new ApiResult<TcCpaCollidingRuleInfoDTO>().fromResult(tcCpaCollidingRuleService.info(), CODE_1);
     }
 
-    @ApiOperation(value = "同程撞库规则基础信息", notes = "同程撞库规则基础信息", httpMethod = "POST")
+    @Operation(summary = "同程撞库规则基础信息", description = "同程撞库规则基础信息")
     @PostMapping("/magnitudeDist")
     public ApiResult magnitudeDist(@RequestBody List<String> releaseTimes) {
         return new ApiResult<List<TcCpaMagnitudeDistDTO>>().fromResult(tcCpaCollidingRuleService.magnitudeDist(releaseTimes), CODE_1);
