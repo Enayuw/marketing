0498c7ab201bcf23d3cde9059b4c22d62ea4844b
xiong.luo <xiong.luo@brgroup.com>
2025-12-11 09:32:13 +0800

    携程自动化第二期 1211

--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaDeleteRuleServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaDeleteRuleServiceImpl.java
@@ -185,7 +185,7 @@ public class TcCpaDeleteRuleServiceImpl implements TcCpaDataDeleteRuleService {
     @Override
     public Result enable(Long id, Integer enabled) {
         TcyrCpaCollidingTaskExample taskExample = new TcyrCpaCollidingTaskExample();
-        taskExample.createCriteria().andDeleteRuleIdsLike(String.valueOf(id));
+        taskExample.createCriteria().andDeleteRuleIdsLike("%" + id + "%");
         if(tcyrCpaCollidingTaskMapper.countByExample(taskExample) > 0 && Objects.equals(enabled, Constants.ENABLED_FORB)) {
             return new Result().setCode(ResultCode.FAIL.getValue()).setMessage("存在已使用该规则的撞库任务，不能禁用剔除规则");
         }
@@ -204,7 +204,7 @@ public class TcCpaDeleteRuleServiceImpl implements TcCpaDataDeleteRuleService {
         example.createCriteria().andIdEqualTo(id);
 
         TcyrCpaCollidingTaskExample taskExample = new TcyrCpaCollidingTaskExample();
-        taskExample.createCriteria().andDeleteRuleIdsLike(String.valueOf(id));
+        taskExample.createCriteria().andDeleteRuleIdsLike("%" + id + "%");
         if(tcyrCpaCollidingTaskMapper.countByExample(taskExample) > 0) {
             return new Result().setCode(ResultCode.FAIL.getValue()).setMessage("存在已使用该规则的撞库任务，不能删除剔除规则");
         }
