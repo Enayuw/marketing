bf7cc6f574715c7cc0bf1cdf95526a3c06eaf6af
xiong.luo <xiong.luo@brgroup.com>
2025-12-11 09:37:54 +0800

    携程自动化第二期 1211 JDK8去掉钉钉告警

--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCollidingDataMagnitudeServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCollidingDataMagnitudeServiceImpl.java
@@ -1,12 +1,8 @@
 package com.br.marketing.service.tccpa.impl;
 
-import com.alibaba.fastjson.JSONObject;
 import com.br.common.log.AlertLog;
-import com.br.marketing.client.MiddleHeavenAviatorScriptApiClient;
 import com.br.marketing.common.enums.AlarmSendCodeEnum;
 import com.br.marketing.common.utils.Constants;
-import com.br.marketing.dto.CostPriceExRecordDto;
-import com.br.marketing.dto.DdLinsSmsCostAlarmDto;
 import com.br.marketing.dto.tccpa.TcCpaDeleteRuleExecuteInfoDTO;
 import com.br.marketing.entity.*;
 import com.br.marketing.enums.TcCpaCollectStatusEnum;
@@ -17,8 +13,6 @@ import com.br.marketing.mapper.TcyrCpaCollidingTaskMapper;
 import com.br.marketing.mapper.TcyrCpaDeleteRuleMapper;
 import com.br.marketing.service.tccpa.TcCpaCollidingDataMagnitudeService;
 import com.br.marketing.service.tccpa.TcCpaCommonService;
-import com.br.marketing.speedconfig.MarketingCommonConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
@@ -28,7 +22,6 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -38,12 +31,6 @@ public class TcCpaCollidingDataMagnitudeServiceImpl implements TcCpaCollidingDat
 
     @Resource
     private ObjectMapper objectMapper;
-
-    @Resource
-    private MarketingCommonConfig marketingCommonConfig;
-
-    @Resource
-    private MiddleHeavenAviatorScriptApiClient aviatorScriptApiClient;
 
     @Resource
     private TcyrCpaCollectTaskMapper tcyrCpaCollectTaskMapper;
@@ -106,32 +93,6 @@ public class TcCpaCollidingDataMagnitudeServiceImpl implements TcCpaCollidingDat
                 log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.TONGCHENG_CPA_SERVICEERROR.getCode(),
                         "撞库任务更新量级失败，taskId：" + collidingTask.getId(), TITLE), e);
             }
-        }
-    }
-
-    private void dealAlarm(DdLinsSmsCostAlarmDto smsCostAlarmDto) {
-        JSONObject requstObj = new JSONObject();
-        JSONObject paramObj = new JSONObject();
-        paramObj.put("title", smsCostAlarmDto.getCardTitle());
-        paramObj.put("totalCount", smsCostAlarmDto.getTotalCount());
-        paramObj.put("existCount", smsCostAlarmDto.getExistCount());
-        paramObj.put("successCount", smsCostAlarmDto.getSuccessCost());
-        paramObj.put("errorCount", smsCostAlarmDto.getFailCount());
-        try {
-            String errorListJson = objectMapper.writeValueAsString(smsCostAlarmDto.getCostPriceExRecordDtoList()
-                    .stream().map(CostPriceExRecordDto::getDdReason).collect(Collectors.toList())
-            );
-            paramObj.put("errorList", errorListJson);
-            requstObj.put("param", paramObj);
-            requstObj.put("scriptCode", marketingCommonConfig.getLinsSmsCostToDbConfig().getString("scriptCode"));
-            //调用钉钉报警接口
-            String aviatorScriptUrl = marketingCommonConfig.getLinsSmsCostToDbConfig().getString("aviatorScriptUrl");
-            boolean isProxy = marketingCommonConfig.getLinsSmsCostToDbConfig().getBoolean("isProxy");
-            aviatorScriptApiClient.dealAviatorScriptRequest(aviatorScriptUrl, requstObj, isProxy);
-        } catch (JsonProcessingException e) {
-            // 异常了钉钉报警 不推送钉钉通知
-            log.warn(AlertLog.buildWarnMessage(AlarmSendCodeEnum.MARKETING_AVIATORSCRIPT_LINESMS_ERROR.getCode(),
-                    e.getMessage(), TITLE), e);
         }
     }
 }
