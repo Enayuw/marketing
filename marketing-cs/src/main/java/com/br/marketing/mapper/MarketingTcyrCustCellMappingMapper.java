8ad8295a08804700b6459c65f6ef25e26ecd63b7
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2026-01-08 15:18:49 +0800

    同程-cell匹配优化:MarketingTcyrCustCellMappingMapper优化，去除无用的mapper查询

--- marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTcyrCustCellMappingMapper.java
+++ marketing-cs/src/main/java/com/br/marketing/mapper/MarketingTcyrCustCellMappingMapper.java
@@ -7,17 +7,7 @@ import java.util.Map;
 
 public interface MarketingTcyrCustCellMappingMapper extends MarketingTcyrCustCellMappingMapperBase{
 
-    String selectNumUserKeyCellBytikv_(@Param("userKey")String userKey);
+    String selectCellBySyncId(@Param("apiCode")String apiCde,@Param("id") Long id);
 
-    String selectStrUserKeyCellBytikv_(@Param("userKey")String userKey);
-
-    List<Map<String, Object>> selectCellInfotikv_(@Param("apiCode")String apiCode,@Param("idList") List<Long> idList);
-
-    void saveNewCustCellInfo(@Param("userKeyId")Long userKeyId, @Param("cell")String cell);
-
-    void batchSaveCustCell(@Param("insertSql") StringBuilder insertSql);
-
-    void saveStrCustCellInfo(@Param("userKey") String userKey, @Param("cell") String cell);
-
-    String selectCellBySyncId(@Param("apiCode")String apiCode,@Param("id") Long id);
+    List<Map<String, Object>> selectCellInfotikv_(@Param("apiCode") String apiCode,@Param("idList") List<Long> idList);
 }
--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCustCellMappingServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCustCellMappingServiceImpl.java
@@ -8,10 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
