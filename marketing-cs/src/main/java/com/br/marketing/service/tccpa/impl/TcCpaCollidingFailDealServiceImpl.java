ae396933db323b9fb412cf8a45dd97e7c188b468
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2026-01-19 16:17:19 +0800

    同程-cell匹配优化:TcCpaCollidingFailDealShardJob-job开关名称优化，避免歧义

--- marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCollidingFailDealServiceImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/service/tccpa/impl/TcCpaCollidingFailDealServiceImpl.java
@@ -117,8 +117,7 @@ public class TcCpaCollidingFailDealServiceImpl implements TcCpaCollidingFailDeal
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
                 if (lines.size() == marketingCommonConfig.getTcyrCpaCollidingFailDealConfig().getInteger("lineReadSize")) {
-                    if (marketingCommonConfig.getTcyrCpaCollidingFailDealConfig().containsKey("jobSwitch")
-                            && !marketingCommonConfig.getTcyrCpaCollidingFailDealConfig().getBoolean("jobSwitch")) {
+                    if (marketingCommonConfig.getTcyrCpaCollidingFailDealConfig().getBoolean("jobStopSwitch")) {
                         lines.clear();
                         break;
                     }
