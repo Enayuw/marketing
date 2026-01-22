bba5d0e2f5694fbab31a711b978fb34a786a1311
zhiyong.zhang <zhiyong.zhang@brgroup.com>
2026-01-22 16:54:27 +0800

    泰康-0121:泰康大健康线索传输新增字段 - 预发联调通过，去除测试日志

--- marketing-cs/src/main/java/com/br/marketing/client/taikang/TaikangClient.java
+++ marketing-cs/src/main/java/com/br/marketing/client/taikang/TaikangClient.java
@@ -37,13 +37,11 @@ public class TaikangClient {
     MarketingCommonConfig marketingCommonConfig;
 
     public String process(TaikangMarketingEvent taikangMarketingEvent) {
-        log.warn("taikangMarketingEvent:{}", JSON.toJSONString(taikangMarketingEvent));
         Map<String, String> taikangConfig = marketingCommonConfig.getTaikangConfig();
         try {
             enrichEventWithConfig(taikangMarketingEvent, taikangConfig);
             ChannelRequest channelRequest = buildChannelRequest(taikangMarketingEvent, taikangConfig);
             Header[] headers = buildHeaders(taikangConfig);
-            log.warn("taikangMarketingEvent:{}", JSON.toJSONString(taikangMarketingEvent));
             HashMap<String, String> result = httpProxyClient.sendByCodePoolTaikang(
                     channelRequest,
                     taikangConfig.get("url"),
