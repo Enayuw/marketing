6246c82bcccdc0758afd380708985f52dad34f7d
dongshuo.he <dongshuo.he@brgroup.com>
2026-02-12 14:55:17 +0800

    信用飞 代码修改

--- marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
+++ marketing-cs/src/main/java/com/br/marketing/speedconfig/MarketingCommonConfig.java
@@ -3644,9 +3644,4 @@ public class MarketingCommonConfig {
      * 信用飞apiCode
      */
     private String xyfApiCode;
-
-    /**
-     * 同程易融cpa撞库成功文件表头
-     */
-    private List<String> xyfSyncExtendFields;
  }
