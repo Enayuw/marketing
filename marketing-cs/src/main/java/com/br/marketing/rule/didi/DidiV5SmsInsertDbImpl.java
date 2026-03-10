a6c5c85d56ba3404834befafa975d6a41bd3d43e
xiong.luo <xiong.luo@brgroup.com>
2026-03-10 15:16:50 +0800

    滴滴V5撞库开始时间修改

--- marketing-cs/src/main/java/com/br/marketing/rule/didi/DidiV5SmsInsertDbImpl.java
+++ marketing-cs/src/main/java/com/br/marketing/rule/didi/DidiV5SmsInsertDbImpl.java
@@ -50,6 +50,15 @@ public class DidiV5SmsInsertDbImpl implements AssembleData<DidiCallBackDataDTO> 
         didiCallRecord.setExtend(JSON.toJSONString(cbo));
         JSONObject jsonObject = JSON.parseObject(cbo.getReserveField1());
         didiCallRecord.setScas(jsonObject.getString("scas"));
+        String custNum = didiCallRecord.getCustNum();
+        String apiCode = didiCallRecord.getApiCode();
+        Map<String, SyncUserValidityPeriodsBO> validityPeriodsBOMap = transferDataValidityPeriodService
+                .getValidityPeriodsByCustNum(Collections.singleton(custNum), apiCode, null);
+        SyncUserValidityPeriodsBO bo = validityPeriodsBOMap.get(custNum);
+        if (bo != null) {
+            List<MarketingSyncUser> syncUsers = bo.getSyncUsers();
+            didiCallRecord.setCell(syncUsers.get(0).getCell());
+        }
         return StringUtils.isBlank(didiCallRecord.getCell()) ? null : didiCallRecord;
     }
 
