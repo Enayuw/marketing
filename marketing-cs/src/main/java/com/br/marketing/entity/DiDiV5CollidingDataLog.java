01c3dc1f4fcef0cedf4792aba2178cea44454202
xiong.luo <xiong.luo@brgroup.com>
2026-03-24 18:09:57 +0800

    滴滴V5增加优惠券字段

--- marketing-cs/src/main/java/com/br/marketing/entity/DiDiV5CollidingDataLog.java
+++ marketing-cs/src/main/java/com/br/marketing/entity/DiDiV5CollidingDataLog.java
@@ -1,7 +1,8 @@
 package com.br.marketing.entity;
 
-import java.util.Date;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class DiDiV5CollidingDataLog {
@@ -89,4 +90,9 @@ public class DiDiV5CollidingDataLog {
      * 修改时间
      */
     private Date updateTime;
+
+    /**
+     * 券类型
+     */
+    private String couponType;
 }
